7cc63d5d5ca2df2ef8d8a9c10db933a9d350c15c
Sashko Stubailo <sashko@stubailo.com>
2016-03-21 19:41:08 -0700

    Remove extra files

--- test/index.js
+++
@@ -1,6 +0,0 @@
-// This file cannot be written with ECMAScript 2015 because it has to load
-// the Babel require hook to enable ECMAScript 2015 features!
-require('typescript-require');
-
-// The tests, however, can and should be written with ECMAScript 2015.
-require('./tests');

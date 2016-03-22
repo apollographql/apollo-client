cc575520a3c6dec78ddaf4f794e69a4b5fee99bb
Sashko Stubailo <sashko@stubailo.com>
2016-03-22 14:30:26 -0700

    Make an entry point

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import {
+  NetworkInterface,
+  createNetworkInterface,
+} from './networkInterface';
+
+import {
+  Store,
+} from 'redux';
+
+import {
+  createApolloStore,
+} from './store';
+
+import {
+  QueryManager,
+  WatchedQueryHandle,
+} from './QueryManager';
+
+export class ApolloClient {
+  public networkInterface: NetworkInterface;
+  public apolloStore: Store;
+  public queryManager: QueryManager;
+
+  constructor({
+    networkInterface,
+    apolloStore,
+  }: {
+    networkInterface?: NetworkInterface,
+    apolloStore?: Store,
+  }) {
+    this.networkInterface = networkInterface ? networkInterface :
+      createNetworkInterface('/graphql');
+
+    this.apolloStore = apolloStore ? apolloStore : createApolloStore();
+
+    this.queryManager = new QueryManager({
+      networkInterface,
+      store: this.apolloStore,
+    });
+  }
+
+  public watchQuery({
+    query,
+  }: {
+    query: string,
+  }): WatchedQueryHandle {
+    return this.queryManager.watchQuery({ query });
+  }
+}

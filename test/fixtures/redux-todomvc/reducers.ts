075bbead1a53ebfe3cc89874fd150ab37929ec2b
James Baxley <james.baxley@newspring.cc>
2016-03-31 11:29:20 -0400

    add fixture for generic redux store

---
+++ test/fixtures/redux-todomvc/reducers.ts
@@ -0,0 +1,72 @@
+import { combineReducers } from 'redux';
+import { assign } from 'lodash';
+
+import {
+  ADD_TODO,
+  DELETE_TODO,
+  EDIT_TODO,
+  COMPLETE_TODO,
+  COMPLETE_ALL,
+  CLEAR_COMPLETED
+} from './types';
+
+const initialState = [
+  {
+    text: 'Use Redux',
+    completed: false,
+    id: 0,
+  },
+];
+
+function todos(state = initialState, action): any {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        {
+          id: state.reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1,
+          completed: false,
+          text: action.text
+        },
+        ...state
+      ]
+
+    case DELETE_TODO:
+      return state.filter(todo =>
+        todo.id !== action.id
+      )
+
+    case EDIT_TODO:
+      return state.map(todo =>
+        todo.id === action.id ?
+          assign({}, todo, { text: action.text }) :
+          todo
+      )
+
+    case COMPLETE_TODO:
+      return state.map(todo =>
+        todo.id === action.id ?
+          assign({}, todo, { completed: !todo.completed }) :
+          todo
+      )
+
+    case COMPLETE_ALL:
+      const areAllMarked = state.every(todo => todo.completed)
+      return state.map(todo => assign({}, todo, {
+        completed: !areAllMarked
+      }))
+
+    case CLEAR_COMPLETED:
+      return state.filter(todo => todo.completed === false)
+
+    default:
+      return state
+  }
+}
+
+const rootReducer = combineReducers({
+  todos
+});
+
+export {
+  rootReducer
+}
---
+++ test/fixtures/redux-todomvc/types.ts
@@ -0,0 +1,12 @@
+// action types
+export const ADD_TODO = 'ADD_TODO';
+export const DELETE_TODO = 'DELETE_TODO';
+export const EDIT_TODO = 'EDIT_TODO';
+export const COMPLETE_TODO = 'COMPLETE_TODO';
+export const COMPLETE_ALL = 'COMPLETE_ALL';
+export const CLEAR_COMPLETED = 'CLEAR_COMPLETED';
+
+// todo types
+export const SHOW_ALL = 'show_all'
+export const SHOW_COMPLETED = 'show_completed'
+export const SHOW_ACTIVE = 'show_active'

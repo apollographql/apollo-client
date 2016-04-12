86b595b4c887ebe0363f27d7cb1a1afbee08ee3a
Martijn Walraven <martijn@martijnwalraven.com>
2016-04-12 16:46:07 +0200

    Convert mockNetworkInterface code to class

--- test/mocks/mockNetworkInterface.ts
+++ test/mocks/mockNetworkInterface.ts
@@ -9,31 +9,34 @@ import {
   print
 } from 'graphql';
 
+// Pass in multiple mocked responses, so that you can test flows that end up
+// making multiple queries to the server
+export default function mockNetworkInterface(
+  ...mockedResponses: MockedResponse[]
+): NetworkInterface { return new MockNetworkInterface(...mockedResponses) as any }
+
 export interface MockedResponse {
   request: Request
   result: GraphQLResult
   delay?: number
 }
 
-// Pass in an array of requests and responses, so that you can test flows that end up making
-// multiple queries to the server
-export default function mockNetworkInterface(
-  ...mockedResponses: MockedResponse[]
-) {
-  const requestToResultMap: any = {};
-  const requestToDelayMap: any = {};
+class MockNetworkInterface {
+  private requestToResultMap: any = {};
+  private requestToDelayMap: any = {};
 
-  // Populate set of mocked requests
-  mockedResponses.forEach(({ request, result, delay }) => {
-    requestToResultMap[requestToKey(request)] = result as GraphQLResult;
-    requestToDelayMap[requestToKey(request)] = delay;
-  });
+  constructor(...mockedResponses: MockedResponse[]) {
+    // Populate set of mocked requests
+    mockedResponses.forEach(({ request, result, delay }) => {
+      this.requestToResultMap[requestToKey(request)] = result as GraphQLResult;
+      this.requestToDelayMap[requestToKey(request)] = delay;
+    });
+  }
 
-  // A mock for the query method
-  const queryMock = (request: Request) => {
+  query(request: Request) {
     return new Promise((resolve, reject) => {
-      const resultData = requestToResultMap[requestToKey(request)];
-      const delay = requestToDelayMap[requestToKey(request)];
+      const resultData = this.requestToResultMap[requestToKey(request)];
+      const delay = this.requestToDelayMap[requestToKey(request)];
 
       if (! resultData) {
         throw new Error(`Passed request that wasn't mocked: ${requestToKey(request)}`);
@@ -43,11 +46,7 @@ export default function mockNetworkInterface(
         resolve(resultData);
       }, delay ? delay : 0);
     });
-  };
-
-  return {
-    query: queryMock,
-  } as NetworkInterface;
+  }
 }
 
 function requestToKey(request: Request): string {

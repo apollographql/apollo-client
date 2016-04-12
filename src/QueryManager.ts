927b89c5a101cc9286cc38ac33fb74a85d85e839
Martijn Walraven <martijn@martijnwalraven.com>
2016-04-12 18:12:28 +0200

    Add QueryObserver with callback for error responses

--- src/QueryManager.ts
+++ src/QueryManager.ts
@@ -44,7 +44,7 @@ export class QueryManager {
   private store: ApolloStore;
   private reduxRootKey: string;
 
-  private resultCallbacks: { [queryId: number]: QueryResultCallback[] };
+  private observers: { [queryId: number]: QueryObserver[] };
 
   private idCounter = 0;
 
@@ -63,7 +63,7 @@ export class QueryManager {
     this.store = store;
     this.reduxRootKey = reduxRootKey;
 
-    this.resultCallbacks = {};
+    this.observers = {};
 
     this.store.subscribe(() => {
       this.broadcastNewStore(this.store.getState()[this.reduxRootKey]);
@@ -123,7 +123,7 @@ export class QueryManager {
     this.idCounter++;
 
 
-    this.resultCallbacks[queryId] = [];
+    this.observers[queryId] = [];
 
     const queryString = query;
 
@@ -199,7 +199,7 @@ export class QueryManager {
             queryId,
           });
         }).catch((error: Error) => {
-           // XXX handle errors
+          this.broadcastQueryError(queryId, error);
         });
     }
 
@@ -259,10 +259,20 @@ export class QueryManager {
       stop: () => {
         this.stopQuery(queryId);
       },
+      subscribe: (observer: QueryObserver) => {
+        if (isStopped()) { throw new Error('Query was stopped. Please create a new one.'); }
+
+        this.registerObserver(queryId, observer);
+      },
       onResult: (callback) => {
         if (isStopped()) { throw new Error('Query was stopped. Please create a new one.'); }
 
-        this.registerResultCallback(queryId, callback);
+        const observer = {
+          onResult: callback,
+          onError: () => { return; },
+        };
+
+        this.registerObserver(queryId, observer);
       },
     };
   }
@@ -273,17 +283,23 @@ export class QueryManager {
       queryId,
     });
 
-    delete this.resultCallbacks[queryId];
+    delete this.observers[queryId];
   }
 
   private broadcastQueryChange(queryId: string, result: GraphQLResult) {
-    this.resultCallbacks[queryId].forEach((callback) => {
-      callback(result);
+    this.observers[queryId].forEach((observer) => {
+      observer.onResult(result);
     });
   }
 
-  private registerResultCallback(queryId: string, callback: QueryResultCallback): void {
-    this.resultCallbacks[queryId].push(callback);
+  private broadcastQueryError(queryId: string, error: Error) {
+    this.observers[queryId].forEach((observer) => {
+      observer.onError(error);
+    });
+  }
+
+  private registerObserver(queryId: string, observer: QueryObserver): void {
+    this.observers[queryId].push(observer);
   }
 }
 
@@ -291,10 +307,17 @@ export interface WatchedQueryHandle {
   id: string;
   isStopped: () => boolean;
   stop();
+  subscribe(observer: QueryObserver);
   onResult(callback: QueryResultCallback);
 }
 
 export type QueryResultCallback = (result: GraphQLResult) => void;
+
+export interface QueryObserver {
+  onResult: (result: GraphQLResult) => void;
+  onError: (error: Error) => void;
+  onStop?: () => void;
+}
 
 export interface WatchQueryOptions {
   query: string;

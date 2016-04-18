6f9be9da6aba34929c7b26e4763af3da3cdbc1ec
Martijn Walraven <martijn@martijnwalraven.com>
2016-04-18 20:14:38 +0200

    Remove rxjs dependency and add simplified Observable polyfill

--- src/util/Observable.ts
+++ src/util/Observable.ts
@@ -1,1 +1,24 @@
-export { Observable } from 'rxjs/Observable';
+export type SubscriberFunction<T> = (observer: Observer<T>) => Function;
+
+export class Observable<T> {
+  constructor(private subscriberFunction: SubscriberFunction<T>) {
+  }
+
+  subscribe(observer: Observer<T>) {
+    const tearDownFunction = this.subscriberFunction(observer);
+
+    return {
+      unsubscribe: tearDownFunction,
+    };
+  }
+}
+
+export interface Observer<T> {
+  next?: (value: T) => void;
+  error?: (error: Error) => void;
+  complete?: () => void;
+}
+
+export interface Subscription {
+  unsubscribe: () => void
+}

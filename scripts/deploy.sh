09abbb50d47e9f2012b4eb511242942d5b1f9803
James Baxley <james.baxley@newspring.cc>
2016-05-07 23:29:06 -0400

    reroute npm publish to deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -e
-
+# Because of a long-running npm issue (https://github.com/npm/npm/issues/3059)
+# prepublish runs after `npm install` and `npm pack`.
+# In order to only run prepublish before `npm publish`, we have to check argv.
+if node -e "process.exit(($npm_config_argv).original[0].indexOf('pu') === 0)"; then
+  exit 0;
+fi
 
 # When we publish to npm, the published files are available in the root
 # directory, which allows for a clean include or require of sub-modules.

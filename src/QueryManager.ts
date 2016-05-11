9fc275ed18e1d74dabb14fadf1e627b657981bb9
Sashko Stubailo <sashko@stubailo.com>
2016-05-10 23:20:56 -0700

    Print unhandled network errors to console

--- src/QueryManager.ts
+++ src/QueryManager.ts
@@ -188,6 +188,8 @@ export class QueryManager {
             // XXX we might not want to re-broadcast the same error over and over if it didn't change
             if (observer.error) {
               observer.error(queryStoreValue.networkError);
+            } else {
+              console.error('Unhandled network error', queryStoreValue.networkError);
             }
           } else {
             const resultFromStore = readSelectionSetFromStore({

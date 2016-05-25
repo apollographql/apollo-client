a7ec696b352ffbf1895871029f8e2f73be25fd8e
Dhaivat Pandya <dhaivat@meteor.com>
2016-05-25 12:07:06 -0700

    added a checkDocument method to getFromAST; should not choke on named fragments anymore

--- ambient.d.ts
+++ ambient.d.ts
@@ -75,7 +75,17 @@ declare module 'lodash.mapvalues' {
 
 declare module 'lodash.clonedeep' {
   import main = require('~lodash/index');
-  export = main.cloneDeep
+  export = main.cloneDeep;
+}
+
+declare module 'lodash.countby' {
+  import main = require('~lodash/index');
+  export = main.countBy;
+}
+
+declare module 'lodash.identity' {
+  import main = require('~lodash/index');
+  export = main.identity;
 }
 
 /*
--- src/queries/getFromAST.ts
+++ src/queries/getFromAST.ts
@@ -4,6 +4,9 @@ import {
   FragmentDefinition,
 } from 'graphql';
 
+import countBy = require('lodash.countby');
+import identity = require('lodash.identity');
+
 export function getMutationDefinition(doc: Document): OperationDefinition {
   if (doc.kind !== 'Document') {
     throw new Error(`Expecting a parsed GraphQL document. Perhaps you need to wrap the query \
@@ -23,15 +26,26 @@ string in a "gql" tag? http://docs.apollostack.com/apollo-client/core.html#gql`)
   return mutationDef;
 }
 
-export function getQueryDefinition(doc: Document): OperationDefinition {
+// Checks the document for errors and throws an exception if there is an error.
+export function checkDocument(doc: Document) {
   if (doc.kind !== 'Document') {
     throw new Error(`Expecting a parsed GraphQL document. Perhaps you need to wrap the query \
 string in a "gql" tag? http://docs.apollostack.com/apollo-client/core.html#gql`);
   }
 
-  if (doc.definitions.length > 1) {
-    throw new Error('Query must have exactly one operation definition.');
+  const definitionTypes = doc.definitions.map((definition) => {
+    return definition.kind;
+  });
+  const typeCounts = countBy(definitionTypes, identity);
+
+  // can't have more than one operation definition per query
+  if (typeCounts['OperationDefinition'] > 1) {
+    throw new Error('Queries must have exactly one operation definition.');
   }
+}
+
+export function getQueryDefinition(doc: Document): OperationDefinition {
+  checkDocument(doc);
 
   const queryDef = doc.definitions[0] as OperationDefinition;
 

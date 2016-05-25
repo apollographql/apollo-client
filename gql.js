adc7e295952cc2bf1dbc84b36786784944523fb6
abhiaiyer91 <abhiaiyer91@gmail.com>
2016-05-24 21:41:17 -0700

    back at it again with the comments

--- gql.js
+++ gql.js
@@ -1,1 +1,4 @@
+/* We are placing this file in the root to enable npm-link development
+ * Currently, gql resides in a submodule and is not able to be imported when linked
+ */
 module.exports = require('./lib/src/gql');

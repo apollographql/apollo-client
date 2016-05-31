d48454f6c8ce646bccd84f2325bd8e8cfb4fccb1
Dhaivat Pandya <dhaivat@meteor.com>
2016-05-31 14:35:12 -0700

    in progress commit for query batching

---
+++ test/batching.ts
@@ -0,0 +1,11 @@
+import { QueryScheduler } from '../batching';
+
+describe('QueryScheduler', () => {
+  it('should construct', () => {
+    const scheduler = QueryScheduler({
+      options: {
+        forceFetch: true,
+      },
+
+  });
+});

a12115c4b5057e53f300634fcd66c0be2f7769a0
Dhaivat Pandya <dhaivat@meteor.com>
2016-06-06 17:07:05 -0700

    added all the stuff needed for named fragments, queries, inline fragments to work within query merging

--- src/queries/queryMerging.ts
+++ src/queries/queryMerging.ts
@@ -4,33 +4,104 @@
 import {
   OperationDefinition,
   Field,
+  FragmentDefinition,
+  FragmentSpread,
+  InlineFragment,
+  Document,
+  SelectionSet,
 } from 'graphql';
 
 // Takes a query to add to a root query and aliases the child query's top-level
 // field names.
 export function addQueryToRoot(rootQuery: OperationDefinition,
                                childQuery: OperationDefinition,
-                               childQueryIndex: Number): OperationDefinition {
+                               childQueryIndex: number)
+: OperationDefinition {
   const childAliasName = getQueryAliasName(childQuery, childQueryIndex);
-  const aliasedChildQuery = applyAliasName(childQuery, childAliasName);
+  const aliasedChildQuery = applyAliasNameToQuery(childQuery, childAliasName, 0);
   rootQuery.selectionSet.selections =
     rootQuery.selectionSet.selections.concat(aliasedChildQuery.selectionSet.selections);
   return rootQuery;
 }
 
-// Applies the alias name to the top level fields of a query.
-export function applyAliasName(childQuery: OperationDefinition,
-                               aliasName: string): OperationDefinition {
-  let selections = childQuery.selectionSet.selections;
-  childQuery.selectionSet.selections = selections.map((selection, selectionIndex) => {
-    if (selection.kind === 'Field') {
-      return aliasField(selection as Field, `${aliasName}__fieldIndex_${selectionIndex}`);
+// Recursively steps through the query tree and renames the query fragment spreads to
+// their aliased names.
+export function renameFragmentSpreads(selSet: SelectionSet, aliasName: string): SelectionSet {
+  if (selSet && selSet.selections) {
+    selSet.selections = selSet.selections.map((selection) => {
+      if (selection.kind === 'FragmentSpread') {
+        const fragmentSpread = selection as FragmentSpread;
+        fragmentSpread.name.value = getFragmentAliasName(fragmentSpread, aliasName);
+        return fragmentSpread;
+      } else {
+        const withSelSet = selection as (InlineFragment | Field);
+        withSelSet.selectionSet = renameFragmentSpreads(withSelSet.selectionSet, aliasName);
+        return selection;
+      }
+    });
+  }
+  return selSet;
+}
+
+export function applyAliasNameToDocument(document: Document,
+                                         aliasName: string)
+: Document {
+
+  // replace the definitions within the document with the aliased versions
+  // of those definitions.
+  document.definitions = document.definitions.map((definition) => {
+    if (definition.kind === 'OperationDefinition' || definition.kind === 'FragmentDefinition') {
+      const qDef = definition as (OperationDefinition | FragmentDefinition);
+      qDef.selectionSet = this.renameFragmentSpreads(qDef.selectionSet, aliasName);
+      return qDef;
     } else {
-      return selection;
+      return definition;
     }
   });
 
+  let currStartIndex = 0;
+  document.definitions = document.definitions.map((definition) => {
+    if (definition.kind === 'OperationDefinition' &&
+        (definition as OperationDefinition).operation === 'query') {
+      const queryDef = definition as OperationDefinition;
+      const retDef = applyAliasNameToQuery(queryDef, aliasName, currStartIndex);
+      currStartIndex += queryDef.selectionSet.selections.length;
+      return retDef;
+    } else if (definition.kind === 'FragmentDefinition') {
+      const fragmentDef = definition as FragmentDefinition;
+      const retDef = applyAliasNameToFragment(fragmentDef, aliasName, currStartIndex);
+      currStartIndex += fragmentDef.selectionSet.selections.length;
+      return retDef;
+    } else {
+      throw new Error('Cannot apply alias names to documents that contain mutations.');
+    }
+  });
+  return document;
+}
+
+export function applyAliasNameToFragment(fragment: FragmentDefinition,
+                                         aliasName: string,
+                                         startIndex: number)
+: FragmentDefinition {
+  fragment.name.value = getFragmentAliasName(fragment, aliasName);
+  fragment.selectionSet.selections =
+    applyAliasNameToSelections(fragment.selectionSet.selections, aliasName, startIndex);
+  return fragment;
+}
+
+// Applies the alias name to the top level fields of a query.
+export function applyAliasNameToQuery(childQuery: OperationDefinition,
+                                      aliasName: string,
+                                      startIndex: number)
+: OperationDefinition {
+  childQuery.selectionSet.selections =
+    applyAliasNameToSelections(childQuery.selectionSet.selections, aliasName, startIndex);
   return childQuery;
+}
+
+export function getFragmentAliasName(fragment: FragmentDefinition | FragmentSpread,
+                                     queryAliasName: string): string {
+  return `${queryAliasName}__${fragment.name.value}`;
 }
 
 // Returns an alias name for the query using the query's index
@@ -38,7 +109,7 @@ export function applyAliasName(childQuery: OperationDefinition,
 // query's name is "listOfAuthors" and has index "3", the name will
 // be "__listOfAuthors__queryIndex_3".
 export function getQueryAliasName(childQuery: OperationDefinition,
-                                  childQueryIndex: Number) {
+                                  childQueryIndex: number) {
   let childQueryName = '';
   if (childQuery.name) {
     childQueryName = childQuery.name.value;
@@ -46,7 +117,6 @@ export function getQueryAliasName(childQuery: OperationDefinition,
 
   return `__${childQueryName}__queryIndex_${childQueryIndex}`;
 }
-
 
 export function aliasField(field: Field, alias: string): Field {
   field.alias = {
@@ -71,4 +141,17 @@ export function addPrefixToVariables(prefix: string,
     newVariables[prefix + variableName] = variables[variableName];
   });
   return newVariables;
+}
+
+function applyAliasNameToSelections(selections: (Field | FragmentSpread | InlineFragment)[],
+                                    aliasName: string, startIndex: number)
+: (Field | FragmentSpread | InlineFragment)[] {
+  return selections.map((selection, selectionIndex) => {
+    if (selection.kind === 'Field') {
+      return aliasField(selection as Field,
+                        `${aliasName}__fieldIndex_${selectionIndex + startIndex}`);
+    } else {
+      return selection;
+    }
+  });
 }

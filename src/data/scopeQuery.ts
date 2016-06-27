e24a4d545ca995410f6ccf82f22481eb884cab94
Sashko Stubailo <sashko@stubailo.com>
2016-06-27 16:20:57 -0700

    Finish scoping

--- src/data/scopeQuery.ts
+++ src/data/scopeQuery.ts
@@ -10,6 +10,7 @@ import {
 import {
   isField,
   isInlineFragment,
+  resultKeyNameFromField,
 } from './storeUtils';
 
 import cloneDeep = require('lodash.clonedeep');
@@ -31,14 +32,18 @@ export function scopeSelectionSetToResultPath({
     // Arrays are not represented in GraphQL AST
     .filter((pathSegment) => !isNumber(pathSegment))
     .forEach((pathSegment) => {
-      currSelSet = followOnePathSegment(currSelSet, pathSegment as string);
+      currSelSet = followOnePathSegment(currSelSet, pathSegment as string, fragmentMap);
     });
 
   return currSelSet;
 }
 
-function followOnePathSegment(currSelSet: SelectionSet, pathSegment: string): SelectionSet {
-  const matchingFields: Field[] = getMatchingFields(currSelSet, pathSegment);
+function followOnePathSegment(
+  currSelSet: SelectionSet,
+  pathSegment: string,
+  fragmentMap: FragmentMap
+): SelectionSet {
+  const matchingFields: Field[] = getMatchingFields(currSelSet, pathSegment, fragmentMap);
 
   if (matchingFields.length < 1) {
     throw new Error(`No matching field found in query for path segment: ${pathSegment}`);
@@ -52,14 +57,25 @@ function followOnePathSegment(currSelSet: SelectionSet, pathSegment: string): Se
   return matchingFields[0].selectionSet;
 }
 
-function getMatchingFields(currSelSet: SelectionSet, pathSegment: string): Field[] {
-  const matching = [];
+function getMatchingFields(
+  currSelSet: SelectionSet,
+  pathSegment: string,
+  fragmentMap: FragmentMap
+): Field[] {
+  let matching = [];
 
   currSelSet.selections.forEach((selection) => {
     if (isField(selection)) {
-     if (selection.name.value === pathSegment) {
-       matching.push(selection);
-     }
+      if (resultKeyNameFromField(selection) === pathSegment) {
+        matching.push(selection);
+      }
+    } else if (isInlineFragment(selection)) {
+      matching = matching.concat(
+        getMatchingFields(selection.selectionSet, pathSegment, fragmentMap));
+    } else { // is named fragment
+      const fragment = fragmentMap[selection.name.value];
+      matching = matching.concat(
+        getMatchingFields(fragment.selectionSet, pathSegment, fragmentMap));
     }
   });
 

45d45f23ead9fb353bb390d9aec0a47d21944254
Sashko Stubailo <sashko@stubailo.com>
2016-06-28 09:14:37 -0700

    Put back registerGqlTag

--- gql.js
+++ gql.js
@@ -1,2 +1,10 @@
 // This is here for backcompat, even though the right way is to use the other package
 module.exports = require('graphql-tag');
+
+module.exports.registerGqlTag = function () {
+  if (typeof window !== 'undefined') {
+    window['gql'] = gql;
+  } else if (typeof global !== 'undefined') {
+    global['gql'] = gql;
+  }
+}

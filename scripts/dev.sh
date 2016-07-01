eb9438767d428962daf7d90c773d2fd721df4a00
Slava Kim <imslavko@gmail.com>
2016-07-01 12:42:38 -0700

    pass test grep arguments through the dev.sh script

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,1 +1,6 @@
-./node_modules/.bin/concurrently -r -k "npm run watch" "./scripts/test_and_lint.sh"
+test_and_lint_command="./scripts/test_and_lint.sh "
+test_and_lint_command+="$@"
+
+echo $test_and_lint_command
+
+./node_modules/.bin/concurrently -r -k "npm run watch" "$test_and_lint_command"

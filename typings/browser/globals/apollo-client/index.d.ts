17d7c34a11283a2d96a839937447d04a98369f11
Dhaivat Pandya <dhaivat@meteor.com>
2016-07-21 13:29:56 -0700

    typings needed to update for some reason

--- typings/browser/globals/apollo-client/index.d.ts
+++ typings/browser/globals/apollo-client/index.d.ts
@@ -85,6 +85,11 @@ declare module 'lodash.identity' {
   export = main.identity;
 }
 
+declare module 'lodash.find' {
+  import main = require('~lodash/index');
+  export = main.find;
+}
+
 /*
 
   GRAPHQL

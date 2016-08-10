95a4f498ed457aca7af96e9dbfbf53e282a69cc5
Ian MacLeod <ian@nevir.net>
2016-08-10 15:33:07 -0700

    [Bug?] ObservableQuery#refetch no longer rejects on error
    Current behavior: `refetch()` returns `data: undefined` and does not reject if the underlying request fails, or has errors.
    
    It would be ideal if it were to reject, IMO.  For example, using `refetch` via `react-apollo` on a pull to refresh, and knowing when to display something to the user indicating error.

--- test/QueryManager.ts
+++ test/QueryManager.ts
@@ -624,6 +624,47 @@ describe('QueryManager', () => {
     });
   });
 
+  it('exposes errors on a refetch as a rejection', (done) => {
+    const request = {
+      query: gql`
+      {
+        people_one(id: 1) {
+          name
+        }
+      }`,
+    };
+    const firstResult = {
+      data: {
+        people_one: {
+          name: 'Luke Skywalker',
+        },
+      },
+    };
+    const secondResult = {
+      errors: [
+        {
+          name: 'PeopleError',
+          message: 'This is not the person you are looking for.',
+        },
+      ],
+    };
+
+    const queryManager = mockRefetch({ request, firstResult, secondResult });
+
+    const handle = queryManager.watchQuery(request);
+    handle.subscribe({});
+
+    handle.refetch().catch((error) => {
+      assert.deepEqual(error.graphQLErrors, [
+        {
+          name: 'PeopleError',
+          message: 'This is not the person you are looking for.',
+        },
+      ]);
+      done();
+    });
+  });
+
   it('allows you to refetch queries with new variables', (done) => {
     const query = gql`
       {

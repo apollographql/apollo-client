42cbd18718c6b2ba4b11931f6bc392a6c900a314
Sashko Stubailo <sashko@stubailo.com>
2016-10-15 19:11:08 -0700

    Move all of the observable query stuff out of the closure

--- src/core/ObservableQuery.ts
+++ src/core/ObservableQuery.ts
@@ -32,26 +32,6 @@ export interface UpdateQueryOptions {
 }
 
 export class ObservableQuery extends Observable<ApolloQueryResult> {
-  public refetch: (variables?: any) => Promise<ApolloQueryResult>;
-  /**
-   * Reset this query to take a new set of options.
-   */
-  public setOptions: (options: ModifiableWatchQueryOptions) => Promise<ApolloQueryResult>;
-  /**
-   * Update the variables of this observable query, and fetch the new results
-   * if they've changed. If you want to force new results, use `refetch`.
-   *
-   * Note: if the variables have not changed, the promise will return the old
-   * results immediately, and the `next` callback will *not* fire.
-   *
-   * @param variables: The new set of variables. If there are missing variables,
-   * the previous values of those variables will be used.
-   */
-  public setVariables: (variables: any) => Promise<ApolloQueryResult>;
-  public fetchMore: (options: FetchMoreQueryOptions & FetchMoreOptions) => Promise<any>;
-  public updateQuery: (mapFn: (previousQueryResult: any, options: UpdateQueryOptions) => any) => void;
-  public stopPolling: () => void;
-  public startPolling: (p: number) => void;
   public options: WatchQueryOptions;
   public queryId: string;
   /**
@@ -59,6 +39,9 @@ export class ObservableQuery extends Observable<ApolloQueryResult> {
    * The current value of the variables for this query. Can change.
    */
   public variables: { [key: string]: any };
+
+  private isPollingQuery: boolean;
+  private shouldSubscribe: boolean;
   private scheduler: QueryScheduler;
   private queryManager: QueryManager;
 
@@ -76,170 +59,18 @@ export class ObservableQuery extends Observable<ApolloQueryResult> {
     const isPollingQuery = !!options.pollInterval;
 
     const subscriberFunction = (observer: Observer<ApolloQueryResult>) => {
-      const retQuerySubscription = {
-        unsubscribe: () => {
-          if (isPollingQuery) {
-            scheduler.stopPollingQuery(queryId);
-          }
-          queryManager.stopQuery(queryId);
-        },
-      };
-
-      if (shouldSubscribe) {
-        queryManager.addObservableQuery(queryId, this);
-        queryManager.addQuerySubscription(queryId, retQuerySubscription);
-      }
-
-      if (isPollingQuery) {
-        if (this.options.noFetch) {
-          throw new Error('noFetch option should not use query polling.');
-        }
-
-        this.scheduler.startPollingQuery(
-          this.options,
-          queryId
-        );
-      }
-      queryManager.startQuery(
-        queryId,
-        this.options,
-        queryManager.queryListenerForObserver(queryId, this.options, observer)
-      );
-
-      return retQuerySubscription;
+      return this.onSubscribe(observer);
     };
+
     super(subscriberFunction);
+
+    this.isPollingQuery = isPollingQuery;
     this.options = options;
     this.variables = this.options.variables || {};
     this.scheduler = scheduler;
     this.queryManager = queryManager;
     this.queryId = queryId;
-
-    this.refetch = (variables?: any) => {
-      this.variables = assign({}, this.variables, variables);
-
-      if (this.options.noFetch) {
-        throw new Error('noFetch option should not use query refetch.');
-      }
-      // Update the existing options with new variables
-      assign(this.options, {
-        variables: this.variables,
-      });
-      // Override forceFetch for this call only
-      const combinedOptions = assign({}, this.options, {
-        forceFetch: true,
-      });
-      return this.queryManager.fetchQuery(this.queryId, combinedOptions)
-      .then(result => this.queryManager.transformResult(result));
-    };
-
-    this.setOptions = (opts: ModifiableWatchQueryOptions) => {
-      this.options = assign({}, this.options, opts) as WatchQueryOptions;
-      if (opts.pollInterval) {
-        this.startPolling(opts.pollInterval);
-      } else if (opts.pollInterval === 0) {
-        this.stopPolling();
-      }
-
-      return this.setVariables(opts.variables);
-    };
-
-    // There's a subtle difference between setVariables and refetch:
-    //   - setVariables will take results from the store unless the query
-    //   is marked forceFetch (and definitely if the variables haven't changed)
-    //   - refetch will always go to the server
-    this.setVariables = (variables: any) => {
-      const newVariables = assign({}, this.variables, variables);
-
-      if (isEqual(newVariables, this.variables)) {
-        return this.result();
-      } else {
-        this.variables = newVariables;
-        // Use the same options as before, but with new variables
-        return this.queryManager.fetchQuery(this.queryId, assign(this.options, {
-          variables: this.variables,
-        }) as WatchQueryOptions)
-        .then(result => this.queryManager.transformResult(result));
-      }
-    };
-
-    this.fetchMore = (fetchMoreOptions: WatchQueryOptions & FetchMoreOptions) => {
-      return Promise.resolve()
-        .then(() => {
-          const qid = this.queryManager.generateQueryId();
-          let combinedOptions: any = null;
-
-          if (fetchMoreOptions.query) {
-            // fetch a new query
-            combinedOptions = fetchMoreOptions;
-          } else {
-            // fetch the same query with a possibly new variables
-            const variables = assign({}, this.variables, fetchMoreOptions.variables);
-
-            combinedOptions = assign({}, this.options, fetchMoreOptions, {
-              variables,
-            });
-          }
-
-          combinedOptions = assign({}, combinedOptions, {
-            forceFetch: true,
-          }) as WatchQueryOptions;
-          return this.queryManager.fetchQuery(qid, combinedOptions);
-        })
-        .then((fetchMoreResult) => {
-          const reducer = fetchMoreOptions.updateQuery;
-          const mapFn = (previousResult: any, { variables }: {variables: any }) => {
-
-            // TODO REFACTOR: reached max recursion depth (figuratively). Continue renaming to variables further down when we have time.
-            const queryVariables = variables;
-            return reducer(
-              previousResult, {
-                fetchMoreResult,
-                queryVariables,
-              });
-          };
-          this.updateQuery(mapFn);
-          return fetchMoreResult;
-        });
-    };
-
-    this.updateQuery = (mapFn) => {
-      const {
-        previousResult,
-        variables,
-        document,
-      } = this.queryManager.getQueryWithPreviousResult(this.queryId);
-
-      const newResult = tryFunctionOrLogError(
-        () => mapFn(previousResult, { variables }));
-
-      if (newResult) {
-        this.queryManager.store.dispatch({
-          type: 'APOLLO_UPDATE_QUERY_RESULT',
-          newResult,
-          variables,
-          document,
-        });
-      }
-    };
-
-    this.stopPolling = () => {
-      if (isPollingQuery) {
-        this.scheduler.stopPollingQuery(this.queryId);
-      }
-    };
-
-    this.startPolling = (pollInterval) => {
-      if (this.options.noFetch) {
-        throw new Error('noFetch option should not use query polling.');
-      }
-
-      if (isPollingQuery) {
-        this.scheduler.stopPollingQuery(this.queryId);
-      }
-      this.options.pollInterval = pollInterval;
-      this.scheduler.startPollingQuery(this.options, this.queryId, false);
-    };
+    this.shouldSubscribe = shouldSubscribe;
   }
 
   public result(): Promise<ApolloQueryResult> {
@@ -273,5 +104,179 @@ export class ObservableQuery extends Observable<ApolloQueryResult> {
       || (partial && !this.options.noFetch);
 
     return { data, loading };
+  }
+
+  public refetch(variables?: any): Promise<ApolloQueryResult> {
+    this.variables = assign({}, this.variables, variables);
+
+    if (this.options.noFetch) {
+      throw new Error('noFetch option should not use query refetch.');
+    }
+
+    // Update the existing options with new variables
+    assign(this.options, {
+      variables: this.variables,
+    });
+
+    // Override forceFetch for this call only
+    const combinedOptions = assign({}, this.options, {
+      forceFetch: true,
+    });
+
+    return this.queryManager.fetchQuery(this.queryId, combinedOptions)
+      .then(result => this.queryManager.transformResult(result));
+  }
+
+  public fetchMore(
+    fetchMoreOptions: FetchMoreQueryOptions & FetchMoreOptions
+  ): Promise<ApolloQueryResult> {
+    return Promise.resolve()
+      .then(() => {
+        const qid = this.queryManager.generateQueryId();
+        let combinedOptions: any = null;
+
+        if (fetchMoreOptions.query) {
+          // fetch a new query
+          combinedOptions = fetchMoreOptions;
+        } else {
+          // fetch the same query with a possibly new variables
+          const variables = assign({}, this.variables, fetchMoreOptions.variables);
+
+          combinedOptions = assign({}, this.options, fetchMoreOptions, {
+            variables,
+          });
+        }
+
+        combinedOptions = assign({}, combinedOptions, {
+          forceFetch: true,
+        }) as WatchQueryOptions;
+        return this.queryManager.fetchQuery(qid, combinedOptions);
+      })
+      .then((fetchMoreResult) => {
+        const reducer = fetchMoreOptions.updateQuery;
+        const mapFn = (previousResult: any, { variables }: {variables: any }) => {
+
+          // TODO REFACTOR: reached max recursion depth (figuratively). Continue renaming to variables further down when we have time.
+          const queryVariables = variables;
+          return reducer(
+            previousResult, {
+              fetchMoreResult,
+              queryVariables,
+            });
+        };
+        this.updateQuery(mapFn);
+        return fetchMoreResult;
+      });
+  }
+
+  public setOptions(opts: ModifiableWatchQueryOptions): Promise<ApolloQueryResult> {
+    this.options = assign({}, this.options, opts) as WatchQueryOptions;
+      if (opts.pollInterval) {
+        this.startPolling(opts.pollInterval);
+      } else if (opts.pollInterval === 0) {
+        this.stopPolling();
+      }
+
+      return this.setVariables(opts.variables);
+  }
+
+  /**
+   * Update the variables of this observable query, and fetch the new results
+   * if they've changed. If you want to force new results, use `refetch`.
+   *
+   * Note: if the variables have not changed, the promise will return the old
+   * results immediately, and the `next` callback will *not* fire.
+   *
+   * @param variables: The new set of variables. If there are missing variables,
+   * the previous values of those variables will be used.
+   */
+  public setVariables(variables: any): Promise<ApolloQueryResult> {
+    const newVariables = assign({}, this.variables, variables);
+
+    if (isEqual(newVariables, this.variables)) {
+      return this.result();
+    } else {
+      this.variables = newVariables;
+      // Use the same options as before, but with new variables
+      return this.queryManager.fetchQuery(this.queryId, assign(this.options, {
+        variables: this.variables,
+      }) as WatchQueryOptions)
+        .then(result => this.queryManager.transformResult(result));
+    }
+  }
+
+  public updateQuery(
+    mapFn: (previousQueryResult: any, options: UpdateQueryOptions) => any
+  ): void {
+    const {
+      previousResult,
+      variables,
+      document,
+    } = this.queryManager.getQueryWithPreviousResult(this.queryId);
+
+    const newResult = tryFunctionOrLogError(
+      () => mapFn(previousResult, { variables }));
+
+    if (newResult) {
+      this.queryManager.store.dispatch({
+        type: 'APOLLO_UPDATE_QUERY_RESULT',
+        newResult,
+        variables,
+        document,
+      });
+    }
+  }
+
+  public stopPolling() {
+    if (this.isPollingQuery) {
+      this.scheduler.stopPollingQuery(this.queryId);
+    }
+  }
+
+  public startPolling(pollInterval: number) {
+    if (this.options.noFetch) {
+      throw new Error('noFetch option should not use query polling.');
+    }
+
+    if (this.isPollingQuery) {
+      this.scheduler.stopPollingQuery(this.queryId);
+    }
+    this.options.pollInterval = pollInterval;
+    this.scheduler.startPollingQuery(this.options, this.queryId, false);
+  }
+
+  private onSubscribe(observer: Observer<ApolloQueryResult>) {
+    const retQuerySubscription = {
+      unsubscribe: () => {
+        if (this.isPollingQuery) {
+          this.scheduler.stopPollingQuery(this.queryId);
+        }
+        this.queryManager.stopQuery(this.queryId);
+      },
+    };
+
+    if (this.shouldSubscribe) {
+      this.queryManager.addObservableQuery(this.queryId, this);
+      this.queryManager.addQuerySubscription(this.queryId, retQuerySubscription);
+    }
+
+    if (this.isPollingQuery) {
+      if (this.options.noFetch) {
+        throw new Error('noFetch option should not use query polling.');
+      }
+
+      this.scheduler.startPollingQuery(
+        this.options,
+        this.queryId
+      );
+    }
+
+    this.queryManager.startQuery(
+      this.queryId,
+      this.options,
+      this.queryManager.queryListenerForObserver(this.queryId, this.options, observer)
+    );
+
+    return retQuerySubscription;
   }
 }

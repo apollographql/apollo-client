6ad6da8d782d5877932e0edde0531c4705454ab0
Kamil Kisiela <kamil.kisiela@gmail.com>
2017-01-04 09:29:50 +0100

    Add lodash to globals in rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
+function globals(mod) {
+  if (mod.indexOf('lodash/') === 0) return '_';
+}
+
 export default {
   entry: 'lib/src/index.js',
   dest: 'lib/apollo.umd.js',
   format: 'umd',
   sourceMap: true,
   moduleName: 'apollo',
-  globals: {
-    'lodash': '_'
-  }
+  globals
 };

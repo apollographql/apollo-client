4881bec26dcdc07d111e54a2ab4228b06a7c7b70
Jonas Helfer <jonas@helfer.email>
2017-01-10 01:26:39 +0800

    copy deep-freeze instead of importing

--- src/util/maybeDeepFreeze.ts
+++ src/util/maybeDeepFreeze.ts
@@ -1,4 +1,19 @@
-import * as deepFreeze from 'deep-freeze';
+
+// taken straight from https://github.com/substack/deep-freeze to avoid import hassles with rollup
+function deepFreeze (o: any) {
+  Object.freeze(o);
+
+  Object.getOwnPropertyNames(o).forEach(function (prop) {
+    if (o.hasOwnProperty(prop)
+    && o[prop] !== null
+    && (typeof o[prop] === 'object' || typeof o[prop] === 'function')
+    && !Object.isFrozen(o[prop])) {
+      deepFreeze(o[prop]);
+    }
+  });
+
+  return o;
+};
 
 export default function maybeDeepFreeze(obj: any) {
   if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {

972026ceeb1fc43601af22098f126aeff4036397
Dhaivat Pandya <dhaivat@meteor.com>
2017-01-10 10:35:28 -0800

    made benchmark.js easier to use with some util functions and added more benchmarks

--- benchmark/trial.ts
+++ benchmark/trial.ts
@@ -15,9 +15,6 @@ import {
 const Benchmark = require('benchmark');
 const bsuite = new Benchmark.Suite();
 
-let globalClient: ApolloClient = null;
-let globalObservableQuery: ObservableQuery<Object> = null;
-
 const simpleQuery = gql`
   query {
     author {
@@ -38,93 +35,127 @@ const simpleReqResp = {
   result: simpleResult,
 };
 
-bsuite
-  .add('construct instance', () => {
-    new ApolloClient({});
-  })
-  .add('fetch a query result from mocked server', {
-    defer: true,
-    setup: () => {
-      globalClient = new ApolloClient({
-        networkInterface: mockNetworkInterface({
-          request: { query: simpleQuery },
-          result: simpleResult,
-        }),
-        addTypename: false,
-      });
-    },
-    fn: (deferred: any) => {
-      globalClient.query({ query: simpleQuery }).then((result) => {
-        deferred.resolve();
-      });
-    },
-  })
-  .add('read simple query result from cache', {
-    defer: true,
-    setup: (deferred: any) => {
-      globalClient = new ApolloClient({
-        networkInterface: mockNetworkInterface(simpleReqResp),
-        addTypename: false,
-      });
+// This provides several utilities that make it a bit easier to 
+// interact with benchmark.js.
+//
+// Specifically, it provides `group` and `benchmark`, examples of which
+// can be seen below.The functions allow you to manage scope more easily
+// and handle async code more easily than benchmark.js typically allows.
+// 
+// `group` is meant to provide a way to execute code that sets up the scope variables for your
+// benchmark. It is only run once before the benchmark, not on every call of the code to
+// be benchmarked.
+type DoneFunction = () => void;
+type CycleFunction = (doneFn: DoneFunction) => void;
+type BenchmarkFunction = (description: string, cycleFn: CycleFunction) => void;
+type GroupFunction = (done: DoneFunction) => void;
+let benchmark: BenchmarkFunction = null;
 
-      // insert the result into the cache
-      globalClient.query({ query: simpleQuery }).then((result) => {
-        deferred.resolve();
+const groupPromises: Promise<void>[] = [];
+const group = (groupFn: GroupFunction) => {
+  const oldBenchmark = benchmark;
+  const scope = {
+    benchmark: (description: string, benchmarkFn: (done: () => void) => void) => {
+      console.log('Adding benchmark: %s', description);
+      bsuite.add(description, {
+        defer: true,
+        fn: (deferred: any) => {
+          const done = () => {
+            deferred.resolve();
+          };
+          
+          benchmarkFn(done);
+        },
       });
     },
-    fn: (deferred: any) => {
-      globalClient.query({
+  };
+
+  groupPromises.push(new Promise<void>((resolve, reject) => {
+    const groupDone = () => {
+      resolve();
+    };
+    
+    benchmark = scope.benchmark;
+    groupFn(groupDone);
+    benchmark = oldBenchmark;
+  }));
+};
+
+const getClientInstance = () => {
+  return new ApolloClient({
+    networkInterface: mockNetworkInterface({
+      request: { query: simpleQuery },
+      result: simpleResult,
+    }),
+    addTypename: false,
+  });
+};
+
+group((end) => {
+  benchmark('constructing an instance', (done) => {
+    new ApolloClient({});
+    done();
+  });
+  end();
+});
+
+group((end) => {
+  const client = getClientInstance();
+  benchmark('fetching a query result from mocked server', (done) => {
+    client.query({ query: simpleQuery }).then((result) => {
+      done();
+    });
+  });
+  end();
+});
+
+group((end) => {
+  const client = getClientInstance();
+  const myBenchmark = benchmark;
+  client.query({ query: simpleQuery }).then(() => {
+    myBenchmark('read + write simple query result in cache', (done) => {
+      // read from the cache
+      client.query({
         query: simpleQuery,
         noFetch: true,
       }).then((result) => {
-        deferred.resolve();
-      })
-    },
-  })
-  .add('write data and receive update from the cache in dev', {
-    // Should benchmark writing a query result to
-    // the cache that affects another query in "development"
-    // mode, i.e. no deep freezing.
-    defer: true,
-    fn: (deferred: any) => {
-      const client = new ApolloClient({
-        networkInterface: mockNetworkInterface(simpleReqResp),
-        addTypename: false,
+        done();
       });
-      
-      const observable = client.watchQuery({
-        query: simpleQuery,
-        noFetch: true,
-      });
-      observable.subscribe({
-        next(res: ApolloQueryResult<Object>) {
-          if(Object.keys(res.data).length > 0) {
-            deferred.resolve();
-          }
-        },
-        
-        error(err: Error) {
-          console.log('Error happened: ', err);
+    });
+    end();
+  });
+});
+
+group((end) => {
+
+  benchmark('write data and receive update from the cache', (done) => {
+    const client = getClientInstance();
+    const observable = client.watchQuery({
+      query: simpleQuery,
+      noFetch: true,
+    });
+    observable.subscribe({
+      next(res: ApolloQueryResult<Object>) {
+        if (Object.keys(res.data).length > 0) {
+          done();
         }
-      });
-      client.query({ query: simpleQuery }); 
-    }
-  })/*
-  .add('write data and receive update from the cache in prod', {
-    // Should benchmark writing a query result to the cache
-    // that affects another query in "production" mode, i.e.
-    // with deep freezing.
-  })
-  .add('store state writes', {
-    // Should benchmark a single update to store state that (currently)
-    // results in a clone of the store state.
-  }) */
-  .on('cycle', function(event: any) {
-    console.log('Mean time in ms: ', event.target.stats.mean * 1000);
-    console.log(String(event.target));
-  })
-  .on('error', function(event: any) {
-    console.log('Error in event. Event object: ');
-    console.log(event);
-  })
-  .run({'async': true});
+      },
+      error(err: Error) {
+        console.warn('Error occurred in observable.');
+      }
+    });
+    client.query({ query: simpleQuery });
+  });
+  
+  end();
+});
+
+Promise.all(groupPromises).then(() => {
+  console.log('Running benchmarks.');
+  bsuite
+    .on('cycle', function(event: any) {
+      console.log('Mean time in ms: ', event.target.stats.mean * 1000);
+      console.log(String(event.target));
+    })
+    .run({'async': false});
+});

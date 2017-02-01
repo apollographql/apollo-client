a6fd09b53ec53e5ac46a8c4efc27cfaab6a062d3
Caleb Meredith <calebmeredith8@gmail.com>
2017-02-01 13:49:18 -0500

    Update QueryManager.ts

--- src/core/QueryManager.ts
+++ src/core/QueryManager.ts
@@ -115,7 +115,7 @@ import { WatchQueryOptions } from './watchQueryOptions';
 import { ObservableQuery } from './ObservableQuery';
 
 export class QueryManager {
-  public pollingTimers: {[queryId: string]: any}; // intended for NodeJS.Timer | any
+  public pollingTimers: {[queryId: string]: any};
   public scheduler: QueryScheduler;
   public store: ApolloStore;
 

15a07685e0200cbf78d810faf7746ed6d3efc33f
Jonas Helfer <jonas@helfer.email>
2017-03-07 18:27:08 -0800

    implement cache-and-network fetchPolicy

--- src/core/watchQueryOptions.ts
+++ src/core/watchQueryOptions.ts
@@ -16,15 +16,15 @@ import {
   PureQueryOptions,
 } from './types';
 
-  /**
-   * fetchPolicy determines where the client may return a result from. The options are:
-   * - cache-first (default): return result from cache. Only fetch from network if cached result is not available.
-   * - cache-and-network: returns result from cache first (if it exists), then return network result once it's available
-   * - network-first: return result from network, but if network request fails, use result from cache, if available.
-   * - cache-only: return result from cache if avaiable, fail otherwise.
-   * - network-only: return result from network, fail if network call doesn't succeed.
-   */
-export type FetchPolicy = 'cache-first' | 'cache-and-network' | 'network-first' | 'cache-only' | 'network-only';
+/**
+ * fetchPolicy determines where the client may return a result from. The options are:
+ * - cache-first (default): return result from cache. Only fetch from network if cached result is not available.
+ * - cache-and-network: returns result from cache first (if it exists), then return network result once it's available
+ * - cache-only: return result from cache if avaiable, fail otherwise.
+ * - network-only: return result from network, fail if network call doesn't succeed.
+ */
+
+export type FetchPolicy = 'cache-first' | 'cache-and-network' | 'network-only' | 'cache-only';
 
 /**
  * We can change these options to an ObservableQuery

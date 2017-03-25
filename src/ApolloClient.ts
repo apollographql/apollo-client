83ba9b247961ce9894df8cff69277cfc36467cbb
Jonas Helfer <jonas@helfer.email>
2017-03-25 10:31:28 -0700

    Move the devtools install suggestion

--- src/ApolloClient.ts
+++ src/ApolloClient.ts
@@ -85,27 +85,6 @@ import {
 } from './version';
 
 
-
-/**
- * Suggest installing the devtools for developers who are using Apollo Client
- */
-if (process.env.NODE_ENV !== 'production') {
-  if ( typeof window !== 'undefined' && window.document && window.top === window.self) {
-
-    // First check if devtools is not installed
-    if (typeof (window as any).__APOLLO_DEVTOOLS_GLOBAL_HOOK__ === 'undefined') {
-      // Only for Chrome
-      if (navigator.userAgent.indexOf('Chrome') > -1) {
-        // tslint:disable-next-line
-        console.debug('Download the Apollo DevTools ' +
-        'for a better development experience: ' +
-        'https://chrome.google.com/webstore/detail/apollo-client-developer-t/jdkknkkbebbapilgoeccciglkfbmbnfm');
-      }
-    }
-  }
-}
-
-
 /**
  * This type defines a "selector" function that receives state from the Redux store
  * and returns the part of it that is managed by ApolloClient
@@ -258,6 +237,25 @@ export default class ApolloClient implements DataProxy {
 
     if (typeof connectToDevTools === 'undefined' ? defaultConnectToDevTools : connectToDevTools) {
       (window as any).__APOLLO_CLIENT__ = this;
+    }
+
+    /**
+     * Suggest installing the devtools for developers who don't have them
+     */
+    if (!isProduction()) {
+      if ( typeof window !== 'undefined' && window.document && window.top === window.self) {
+
+        // First check if devtools is not installed
+        if (typeof (window as any).__APOLLO_DEVTOOLS_GLOBAL_HOOK__ === 'undefined') {
+          // Only for Chrome
+          if (navigator.userAgent.indexOf('Chrome') > -1) {
+            // tslint:disable-next-line
+            console.debug('Download the Apollo DevTools ' +
+            'for a better development experience: ' +
+            'https://chrome.google.com/webstore/detail/apollo-client-developer-t/jdkknkkbebbapilgoeccciglkfbmbnfm');
+          }
+        }
+      }
     }
 
     this.version = version;

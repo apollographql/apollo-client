5834489c5deb5eab2baf94f017145bcb99563223
James Baxley <james@meteor.com>
2017-08-25 13:41:17 -0400

    remove internal observable imlementation

--- packages/apollo-client/src/util/Observable.ts
+++ packages/apollo-client/src/util/Observable.ts
@@ -1,49 +1,11 @@
 // This simplified polyfill attempts to follow the ECMAScript Observable proposal.
 // See https://github.com/zenparsing/es-observable
-
+import { Observable as LinkObservable } from 'apollo-link-core';
 import $$observable from 'symbol-observable';
 
-export type CleanupFunction = () => void;
-export type SubscriberFunction<T> = (
-  observer: Observer<T>,
-) => Subscription | CleanupFunction;
-
-function isSubscription(
-  subscription: Function | Subscription,
-): subscription is Subscription {
-  return (<Subscription>subscription).unsubscribe !== undefined;
-}
-
-export class Observable<T> {
-  private subscriberFunction: SubscriberFunction<T>;
-
-  constructor(subscriberFunction: SubscriberFunction<T>) {
-    this.subscriberFunction = subscriberFunction;
-  }
-
+// rxjs interopt
+export class Observable<T> extends LinkObservable<T> {
   public [$$observable]() {
     return this;
   }
-
-  public subscribe(observer: Observer<T>): Subscription {
-    let subscriptionOrCleanupFunction = this.subscriberFunction(observer);
-
-    if (isSubscription(subscriptionOrCleanupFunction)) {
-      return subscriptionOrCleanupFunction;
-    } else {
-      return {
-        unsubscribe: subscriptionOrCleanupFunction,
-      };
-    }
-  }
-}
-
-export interface Observer<T> {
-  next?: (value: T) => void;
-  error?: (error: Error) => void;
-  complete?: () => void;
-}
-
-export interface Subscription {
-  unsubscribe: CleanupFunction;
 }

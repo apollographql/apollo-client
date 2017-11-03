36d563c69160b66e7e30b1a78dfc0163c7a7d032
Kamil Kisiela <kamil.kisiela@gmail.com>
2017-11-03 11:02:32 +0100

    Support ESModules and AMD bundles in all packages
    Make every package tree-shakable.
    Standarize bundle naming across all packages.

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+export const globals = {
+  // Apollo
+  'apollo-client': 'apollo.core',
+  'apollo-cache': 'apollo.cache.core',
+  'apollo-link': 'apolloLinkCore',
+  'apollo-link-dedup': 'dedupLink',
+  'apollo-utilities': 'apollo.utilities'
+};
+
+export default name => ({
+  input: 'lib/src/index.js',
+  output: {
+    file: 'lib/bundle.umd.js',
+    format: 'umd'
+  },
+  name,
+  exports: 'named',
+  sourcemap: true,
+  external: Object.keys(globals),
+  onwarn,
+  globals,
+});
+
+function onwarn(message) {
+  const suppressed = [
+    'UNRESOLVED_IMPORT',
+    'THIS_IS_UNDEFINED'
+  ];
+
+  if (!suppressed.find(code => message.code === code)) {
+    return console.warn(message.message);
+  }
+}

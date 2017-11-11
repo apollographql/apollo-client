662ae34b339a2d9d1d0f1feb402918b1b80dcc0e
Jake Dawkins <dawkinsjh@gmail.com>
2017-11-11 18:06:58 -0500

    feat(graphql-anywhere): added tests

--- packages/graphql-anywhere/src/__tests__/matcher.ts
+++ packages/graphql-anywhere/src/__tests__/matcher.ts
@@ -56,4 +56,59 @@ describe('fragment matcher', () => {
       },
     });
   });
+
+  it('can return a promise', async () => {
+    const resolver = fieldName => fieldName;
+
+    const query = gql`
+      {
+        a {
+          b
+          ...trueFrag
+          ...falseFrag
+          ... on True {
+            e
+          }
+          ... on False {
+            f
+          }
+        }
+      }
+
+      fragment trueFrag on True {
+        c
+      }
+
+      fragment falseFrag on False {
+        d
+      }
+    `;
+
+    const fragmentMatcher: FragmentMatcher = (_, typeCondition) =>
+      Promise.resolve(typeCondition === 'True');
+
+    const resultWithMatcher = await graphql(resolver, query, '', null, null, {
+      fragmentMatcher,
+    });
+
+    expect(resultWithMatcher).toEqual({
+      a: {
+        b: 'b',
+        c: 'c',
+        e: 'e',
+      },
+    });
+
+    const resultNoMatcher = graphql(resolver, query, '', null, null);
+
+    expect(resultNoMatcher).toEqual({
+      a: {
+        b: 'b',
+        c: 'c',
+        d: 'd',
+        e: 'e',
+        f: 'f',
+      },
+    });
+  });
 });

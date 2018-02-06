93f21674f2cdd66fe3ee321bc1e238ff05826165
Peggy Rayzis <peggyrayzis@gmail.com>
2018-02-06 16:23:22 -0500

    Added new functionality to preset client

--- packages/apollo-client-preset/src/index.ts
+++ packages/apollo-client-preset/src/index.ts
@@ -1,20 +1,74 @@
+/* necessary for backward compat */
 export * from 'apollo-client';
 export * from 'apollo-link';
-import { HttpLink } from 'apollo-link-http';
 export * from 'apollo-cache-inmemory';
+
+import { Operation, ApolloLink } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { ErrorLink } from 'apollo-link-error';
+import { withClientState, ClientStateConfig } from 'apollo-link-state';
+import { setContext, ContextSetter } from 'apollo-link-context';
+import { onError } from 'apollo-link-error';
 import { InMemoryCache, NormalizedCache } from 'apollo-cache-inmemory';
 
 import gql from 'graphql-tag';
-import ApolloClient from 'apollo-client';
+import ApolloClient, { ApolloClientOptions } from 'apollo-client';
 
 export { gql, InMemoryCache, HttpLink };
+
+export interface PresetConfig {
+  request?: (operation: Operation) => any;
+  uri?: string;
+  fetchOptions?: HttpLink.Options;
+  clientState?: ClientStateConfig;
+  errorHandler?: ErrorLink.ErrorHandler;
+  context?: ContextSetter;
+}
 
 export default class DefaultClient<
   TCache = NormalizedCache
 > extends ApolloClient<TCache> {
-  constructor(config: any = {}) {
-    if (!config.cache) config.cache = new InMemoryCache();
-    if (!config.link) config.link = new HttpLink({ uri: '/graphql' });
-    super(config);
+  constructor(config: PresetConfig) {
+    const cache = new InMemoryCache();
+
+    const forwardLink = new ApolloLink((operation, forward) =>
+      forward(operation),
+    );
+
+    const stateLink = config.clientState
+      ? withClientState({ ...config.clientState, cache })
+      : forwardLink;
+
+    const contextLink = config.context
+      ? setContext(config.context)
+      : forwardLink;
+
+    const errorLink = config.errorHandler
+      ? onError(config.errorHandler)
+      : forwardLink;
+
+    const requestHandler = new ApolloLink((operation, forward) => {
+      if (config.request) {
+        config.request(operation);
+      }
+      return forward(operation);
+    });
+
+    const httpLink = new HttpLink({
+      uri: config.uri || '/graphql',
+      fetchOptions: config.fetchOptions || {},
+      credentials: 'same-origin',
+    });
+
+    super({
+      cache,
+      link: ApolloLink.from([
+        errorLink,
+        requestHandler,
+        contextLink,
+        stateLink,
+        httpLink,
+      ]),
+    } as ApolloClientOptions<TCache>);
   }
 }

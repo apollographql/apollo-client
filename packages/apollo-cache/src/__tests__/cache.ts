6fc5a621f79581388adf627322200245a58a19d5
James Burgess <jamesmillerburgess@gmail.com>
2018-02-26 11:16:30 +0100

    Expand apollo-cache code coverage

--- packages/apollo-cache/src/__tests__/cache.ts
+++ packages/apollo-cache/src/__tests__/cache.ts
@@ -1,13 +1,114 @@
+import gql from 'graphql-tag';
+
 import { ApolloCache as Cache } from '../cache';
 
 class TestCache extends Cache {}
 
 describe('abstract cache', () => {
-  it('implements readQuery which by default runs the read method', () => {
-    const test = new TestCache();
-    test.read = jest.fn();
+  describe('readQuery', () => {
+    it('runs the read method', () => {
+      const test = new TestCache();
+      test.read = jest.fn();
 
-    test.readQuery({});
-    expect(test.read).toBeCalled();
+      test.readQuery({});
+      expect(test.read).toBeCalled();
+    });
+
+    it('defaults optimistic to false', () => {
+      const test = new TestCache();
+      test.read = ({ optimistic }) => optimistic;
+
+      expect(test.readQuery({})).toBe(false);
+      expect(test.readQuery({}, true)).toBe(true);
+    });
+  });
+
+  describe('readFragment', () => {
+    it('runs the read method', () => {
+      const test = new TestCache();
+      test.read = jest.fn();
+      const fragment = {
+        id: 'frag',
+        fragment: gql`
+          fragment a on b {
+            name
+          }
+        `,
+      };
+
+      test.readFragment(fragment);
+      expect(test.read).toBeCalled();
+    });
+
+    it('defaults optimistic to false', () => {
+      const test = new TestCache();
+      test.read = ({ optimistic }) => optimistic;
+      const fragment = {
+        id: 'frag',
+        fragment: gql`
+          fragment a on b {
+            name
+          }
+        `,
+      };
+
+      expect(test.readFragment(fragment)).toBe(false);
+      expect(test.readFragment(fragment, true)).toBe(true);
+    });
+  });
+
+  describe('writeQuery', () => {
+    it('runs the write method', () => {
+      const test = new TestCache();
+      test.write = jest.fn();
+
+      test.writeQuery({});
+      expect(test.write).toBeCalled();
+    });
+  });
+
+  describe('writeFragment', () => {
+    it('runs the write method', () => {
+      const test = new TestCache();
+      test.write = jest.fn();
+      const fragment = {
+        id: 'frag',
+        fragment: gql`
+          fragment a on b {
+            name
+          }
+        `,
+      };
+
+      test.writeFragment(fragment);
+      expect(test.write).toBeCalled();
+    });
+  });
+
+  describe('writeData', () => {
+    it('either writes a fragment or a query', () => {
+      const test = new TestCache();
+      test.read = jest.fn();
+      test.writeFragment = jest.fn();
+      test.writeQuery = jest.fn();
+
+      test.writeData({});
+      expect(test.writeQuery).toBeCalled();
+
+      test.writeData({ id: 1 });
+      expect(test.read).toBeCalled();
+      expect(test.writeFragment).toBeCalled();
+    });
+
+    it('suppresses read errors', () => {
+      const test = new TestCache();
+      test.read = () => {
+        throw new Error();
+      };
+      test.writeFragment = jest.fn();
+
+      expect(() => test.writeData({ id: 1 })).not.toThrow();
+      expect(test.writeFragment).toBeCalled();
+    });
   });
 });

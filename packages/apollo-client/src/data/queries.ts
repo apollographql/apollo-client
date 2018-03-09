b7b2588386db2b48a33a456a8f98bf898f10c5f0
Evans Hauser <evanshauser@gmail.com>
2018-03-08 21:51:20 -0800

    remove the networkStatus paused

--- packages/apollo-client/src/data/queries.ts
+++ packages/apollo-client/src/data/queries.ts
@@ -146,17 +146,6 @@ export class QueryStore {
     }
   }
 
-  public markQueryPaused(queryId: string, fetchMoreForQueryId?: string) {
-    this.store[queryId].networkStatus = NetworkStatus.paused;
-
-    // If we have a `fetchMoreForQueryId` then we need to update the network
-    // status for that query. See the branch for query initialization for more
-    // explanation about this process.
-    if (typeof fetchMoreForQueryId === 'string') {
-      this.store[fetchMoreForQueryId].networkStatus = NetworkStatus.paused;
-    }
-  }
-
   public markQueryResultClient(queryId: string, complete: boolean) {
     if (!this.store[queryId]) return;
 
@@ -182,7 +171,7 @@ export class QueryStore {
           // XXX set loading to true so listeners don't trigger unless they want results with partial data
           res[key] = {
             ...this.store[key],
-            networkStatus: NetworkStatus.paused,
+            networkStatus: NetworkStatus.loading,
           };
 
           return res;

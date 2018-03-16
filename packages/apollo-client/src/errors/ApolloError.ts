9f3150a66e8c4cf04e37edeeae3ce956d6cad71e
Hugo Alliaume <kocal@live.fr>
2018-03-16 19:36:04 +0100

    ApolloError is now properly recognized as instance of ApolloError

--- packages/apollo-client/src/errors/ApolloError.ts
+++ packages/apollo-client/src/errors/ApolloError.ts
@@ -1,8 +1,7 @@
 import { GraphQLError } from 'graphql';
 
-// XXX some duck typing here because for some reason new ApolloError is not instanceof ApolloError
 export function isApolloError(err: Error): err is ApolloError {
-  return err.hasOwnProperty('graphQLErrors');
+  return err instanceof ApolloError;
 }
 
 // Sets the error message on this error according to the
@@ -65,5 +64,7 @@ export class ApolloError extends Error {
     }
 
     this.extraInfo = extraInfo;
+
+    Object.setPrototypeOf(this, ApolloError.prototype);
   }
 }

63d2b87e6fdb13b44564ec7a43b97d366fd0cf30
Hugh Willson <hugh@octonary.com>
2018-05-14 06:38:25 -0400

    Explain why `Object.setPrototypeOf` isn't being used

--- packages/apollo-client/src/errors/ApolloError.ts
+++ packages/apollo-client/src/errors/ApolloError.ts
@@ -65,6 +65,8 @@ export class ApolloError extends Error {
 
     this.extraInfo = extraInfo;
 
+    // We're not using `Object.setPrototypeOf` here as it isn't fully
+    // supported on Android (see issue #3236).
     (this as any).__proto__ = ApolloError.prototype;
   }
 }

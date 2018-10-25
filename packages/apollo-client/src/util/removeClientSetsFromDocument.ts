04d7024c8a7a160cc98400cda1c4b9e074c646f6
Hugh Willson <hugh@octonary.com>
2018-10-25 14:53:53 -0400

    Stop tracking doc references
    This helps avoid the related memory leak issue identified
    in https://github.com/apollographql/apollo-client/pull/3444.

--- packages/apollo-client/src/util/removeClientSetsFromDocument.ts
+++ packages/apollo-client/src/util/removeClientSetsFromDocument.ts
@@ -1,13 +1,9 @@
 import { DocumentNode, DirectiveNode } from 'graphql';
 import { checkDocument, removeDirectivesFromDocument } from 'apollo-utilities';
 
-const removed = new Map();
 export function removeClientSetsFromDocument(
   query: DocumentNode,
 ): DocumentNode | null {
-  const cached = removed.get(query);
-  if (cached) return cached;
-
   checkDocument(query);
 
   const docClone = removeDirectivesFromDocument(
@@ -20,6 +16,5 @@ export function removeClientSetsFromDocument(
     query,
   );
 
-  removed.set(query, docClone);
   return docClone;
 }

865ff71e1301c35302f2e23db2e5f8dfdaffba94
Kevin Ross <kevin.ross@alienfast.com>
2018-12-25 00:33:54 -0600

    reduce config - tests pass
    Test Suites: 96 passed, 96 total
    Tests:       14 skipped, 1888 passed, 1902 total
    Snapshots:   108 passed, 108 total
    Time:        91s
    Ran all test suites in 7 projects.

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,6 @@
 module.exports = {
   rootDir: '.',
-  // rootDir must be included as a project otherwise it finds no tests in packages below rootDir.
-  // http://jestjs.io/docs/en/configuration.html#projects-array-string-projectconfig
-  projects: ['<rootDir>', '<rootDir>/packages/*'],
-  globals: {
-    'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.json',
-    },
-  },
-  transform: {
-    '^.+\\.(j|t)sx?$': 'ts-jest',
-    '^(?!.*\\.json$)': '<rootDir>/config/jsonTransform.ts',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  projects: ['<rootDir>/packages/*'],
 
   moduleNameMapper: {
     '(apollo-boost|apollo-cache|apollo-cache-inmemory|apollo-client|apollo-utilities|graphql-anywhere)(.*)':

b86545585cf3c91cc9493c51d6501088a32924a6
Kevin Ross <kevin.ross@alienfast.com>
2018-12-25 16:49:56 -0600

    jest should ignore incremental bulid artifacts

--- config/jest.config.settings.js
+++ config/jest.config.settings.js
@@ -3,6 +3,7 @@ module.exports = {
     '.(ts|tsx)': 'ts-jest',
   },
   testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/lib/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   testURL: 'http://localhost',
 };

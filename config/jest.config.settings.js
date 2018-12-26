27ec6cede2d125e7807396c45aa98df792b29c6f
Kevin Ross <kevin.ross@alienfast.com>
2018-12-25 18:45:10 -0600

    move shared tsconfig.settings to the config folder, work on reducing test run duplication

--- config/jest.config.settings.js
+++ config/jest.config.settings.js
@@ -2,8 +2,39 @@ module.exports = {
   transform: {
     '.(ts|tsx)': 'ts-jest',
   },
-  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
-  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/lib/'],
+
+  // globals: {
+  //   'ts-jest': {
+  //     tsConfig: {
+  //       allowJs: true,
+  //     },
+  //   },
+  // },
+
+  // testMatch: ['**/__tests__/**/*.ts?(x)'],
+  // testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx)$',
+  // testPathIgnorePatterns: [
+  //   '/node_modules/',
+  //   '/lib/',
+  //   '<rootDir>/lib/',
+  //   '<rootDir>/../*/lib/',
+  //   '<rootDir>/../.*/lib/',
+  //   '<rootDir>/packages/*/lib/',
+  //   '<rootDir>/packages/.*/lib/',
+  // ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   testURL: 'http://localhost',
+
+  // start with defaults and spec all
+  // testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+  // testPathIgnorePatterns: ['/node_modules/'],
+  // testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.jsx?$',
+  testMatch: ['<rootDir>/src/**/__tests__/*.ts'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/lib/',
+    '<rootDir>/lib/',
+    // '<rootDir>/../*/lib/',
+  ],
+  // testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.jsx?$',
 };

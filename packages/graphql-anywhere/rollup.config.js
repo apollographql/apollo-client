dbae5b77b55eb127285350b469c179922e6e207a
Kevin Ross <kevin.ross@alienfast.com>
2018-12-25 19:21:38 -0600

    make sure the async umd is included in rollup, looked for by name by apollo-link-state

--- packages/graphql-anywhere/rollup.config.js
+++ packages/graphql-anywhere/rollup.config.js
@@ -2,4 +2,13 @@ import buildUmdConfig from '../../config/buildUmdConfig';
 import buildEsmConfig from '../../config/buildEsmConfig';
 import pkg from './package.json';
 
-export default [buildUmdConfig('graphqlAnywhere'), buildEsmConfig(pkg)];
+export default [
+  buildUmdConfig('graphqlAnywhere'),
+  buildUmdConfig('graphqlAnywhereAsync', {
+    input: 'lib/graphql-async.js',
+    output: {
+      file: 'lib/async.js',
+    },
+  }),
+  buildEsmConfig(pkg),
+];

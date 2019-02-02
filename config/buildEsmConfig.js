4ae47c452eb919d2cbe353a0094a58eb1ac443e2
Ben Newman <ben@apollographql.com>
2019-02-01 19:03:55 -0500

    Inline tslib helpers in ESM bundle, again.

--- config/buildEsmConfig.js
+++ config/buildEsmConfig.js
@@ -6,15 +6,12 @@ import path from 'path';
 // treat as externals not relative and not absolute paths
 const external = id => !id.startsWith('.') && !id.startsWith('/');
 
-const extensions = ['.ts', '.tsx'];
-const input = './src/index.ts';
-
 export default pkg => {
   const projectDir = path.join(__filename, '..');
   console.info(`Building project esm ${projectDir}`);
   const tsconfig = `${projectDir}/tsconfig.json`;
   return {
-    input,
+    input: './src/index.ts',
     external,
     output: {
       file: pkg.module,
@@ -22,7 +19,14 @@ export default pkg => {
       sourcemap: true,
     },
     plugins: [
-      nodeResolve({ extensions }),
+      nodeResolve({
+        extensions: ['.ts', '.tsx'],
+        // Inline anything imported from the tslib package, e.g. __extends
+        // and __assign. This depends on the "importHelpers":true option in
+        // tsconfig.base.json.
+        module: true,
+        only: ['tslib'],
+      }),
       typescriptPlugin({ typescript, tsconfig }),
     ],
   };

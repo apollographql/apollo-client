2863135416a3683461e8a21c02ef020f3e373ba3
Ben Newman <ben@apollographql.com>
2019-02-26 17:27:55 -0500

    Simplify building the graphql-anywhere/lib/async entry point.
    Should fix https://github.com/apollographql/apollo-link-rest/issues/194.

--- packages/graphql-anywhere/rollup.config.js
+++ packages/graphql-anywhere/rollup.config.js
@@ -2,9 +2,4 @@ import { rollup } from '../../config/rollup.config';
 
 export default [
   ...rollup({ name: 'graphql-anywhere' }),
-  ...rollup({
-    name: 'graphql-anywhere-async',
-    input: 'src/graphql-async.ts',
-    outputPrefix: 'async',
-  }),
 ];

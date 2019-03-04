bd156411fd52fbc555014b47c2d25ef8aff1ec9e
Trevor Blades <tdblades@gmail.com>
2019-03-04 15:44:06 -0800

    Move from hexo to gatsby

---
+++ docs/gatsby-config.js
@@ -0,0 +1,60 @@
+module.exports = {
+  __experimentalThemes: [
+    {
+      resolve: 'gatsby-theme-apollo-docs',
+      options: {
+        root: __dirname,
+        subtitle: 'Apollo Client',
+        description: 'A guide to using the Apollo GraphQL Client with React',
+        contentDir: 'source',
+        basePath: '/docs/react',
+        githubRepo: 'apollographql/apollo-client',
+        sidebarCategories: {
+          null: [
+            'index',
+            'why-apollo',
+            'integrations',
+            'react-apollo-migration'
+          ],
+          Essentials: [
+            'essentials/get-started',
+            'essentials/queries',
+            'essentials/mutations',
+            'essentials/local-state'
+          ],
+          Features: [
+            'features/error-handling',
+            'features/pagination',
+            'features/optimistic-ui',
+            'features/server-side-rendering',
+            'features/developer-tooling',
+            'features/defer-support'
+          ],
+          Advanced: [
+            'advanced/boost-migration',
+            'advanced/subscriptions',
+            'advanced/network-layer',
+            'advanced/caching',
+            'advanced/fragments'
+          ],
+          Recipes: [
+            'recipes/authentication',
+            'recipes/testing',
+            'recipes/static-typing',
+            'recipes/performance',
+            'recipes/react-native',
+            'recipes/babel',
+            'recipes/webpack',
+            'recipes/meteor',
+            'recipes/recompose',
+            'recipes/2.0-migration'
+          ],
+          API: [
+            'api/apollo-client',
+            'api/react-apollo'
+          ]
+        }
+      }
+    }
+  ]
+};

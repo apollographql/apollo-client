6ba5bffbb59341c5447c64b103d343d1125519bb
Ben Newman <ben@apollographql.com>
2019-05-21 12:18:31 -0400

    Update comment about non-network fetchPolicy values.
    https://github.com/apollographql/apollo-client/pull/4840#discussion_r286098654

--- packages/apollo-client/src/core/ObservableQuery.ts
+++ packages/apollo-client/src/core/ObservableQuery.ts
@@ -296,8 +296,9 @@ export class ObservableQuery<
       ));
     }
 
-    // Override fetchPolicy for this call only
-    // only network-only and no-cache are safe to use
+    // Unless the provided fetchPolicy always consults the network
+    // (no-cache, network-only, or cache-and-network), override it with
+    // network-only to force the refetch for this fetchQuery call.
     if (fetchPolicy !== 'no-cache' &&
         fetchPolicy !== 'cache-and-network') {
       fetchPolicy = 'network-only';

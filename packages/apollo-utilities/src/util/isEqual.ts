26849a804a1be65071818285c9c31de8a8e4fb13
Ben Newman <ben@apollographql.com>
2019-06-06 10:34:46 -0400

    Reimplement isEqual without pulling in massive lodash.isequal. (#4924)
    * Reimplement isEqual without pulling in massive lodash.isequal.
    
    Follow-up to #4915. I have kept the tests from that PR, just not the
    lodash-based implementation of isEqual.
    
    The lodash.isequal package has a ton of extra features we don't need, and
    weighs in at a whopping 10KB minified (3.8KB after gzip). Very little of
    that machinery is really necessary to fix #4824.
    
    I extracted this functionality into a separate npm package called
    @wry/equality, so that we have the flexibility to depend on the same logic
    in other packages without importing apollo-utilities.
    
    * Treat @wry/equality as an external package when bundling apollo-utilities.
    
    * Update @wry/equality to latest version (0.1.2).
    
    * Mention PR #4924 in CHANGELOG.md.

--- packages/apollo-utilities/src/util/isEqual.ts
+++ packages/apollo-utilities/src/util/isEqual.ts
@@ -1,8 +1,1 @@
-import isEqualLodash from 'lodash.isequal';
-
-/**
- * Performs a deep equality check on two JavaScript values.
- */
-export function isEqual(a: any, b: any): boolean {
-  return isEqualLodash(a, b);
-}
+export { equal as isEqual } from '@wry/equality';

78aaa5939dd1eea1e5357ae9b438483bd3b0491c
Ben Newman <ben@apollographql.com>
2019-08-07 13:29:08 -0400

    Disallow creating generated Reference instances.

--- packages/apollo-cache-inmemory/src/references.ts
+++ packages/apollo-cache-inmemory/src/references.ts
@@ -2,19 +2,15 @@ import { IdValue } from 'apollo-utilities';
 
 export class Reference implements Readonly<IdValue> {
   public readonly type = 'id';
+  public readonly generated = false;
   constructor(
     public readonly id: string,
     public readonly typename: string | undefined,
-    public readonly generated = false,
   ) {}
 }
 
-export function makeReference(
-  id: string,
-  typename?: string,
-  generated?: boolean,
-): Reference {
-  return new Reference(id, typename, generated);
+export function makeReference(id: string, typename?: string): Reference {
+  return new Reference(id, typename);
 }
 
 export function isReference(obj: any): obj is Reference {

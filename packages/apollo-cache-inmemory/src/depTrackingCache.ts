93e5605a8e726c8e3bc8d656a7febc33b839bbaf
Ben Newman <ben@apollographql.com>
2019-08-13 21:11:17 -0400

    Move cache layering logic into depTrackingCache.ts.

--- packages/apollo-cache-inmemory/src/depTrackingCache.ts
+++ packages/apollo-cache-inmemory/src/depTrackingCache.ts
@@ -3,42 +3,47 @@ import { wrap, OptimisticWrapperFunction } from 'optimism';
 
 const hasOwn = Object.prototype.hasOwnProperty;
 
-export class DepTrackingCache implements NormalizedCache {
-  // Wrapper function produced by the optimism library, used to depend on
-  // dataId strings, for easy invalidation of specific IDs.
-  private depend: OptimisticWrapperFunction<[string], StoreObject | undefined>;
+type DependType = OptimisticWrapperFunction<[string], StoreObject> | null;
 
-  constructor(private data: NormalizedCacheObject = Object.create(null)) {
-    this.depend = wrap((dataId: string) => this.data[dataId], {
+export abstract class DepTrackingCache implements NormalizedCache {
+  protected data: NormalizedCacheObject = Object.create(null);
+  public readonly depend: DependType = null;
+
+  protected makeDepend(): DependType {
+    return wrap((dataId: string) => this.data[dataId], {
       disposable: true,
-      makeCacheKey(dataId: string) {
+      makeCacheKey(dataId) {
         return dataId;
       },
     });
   }
+
+  public abstract addLayer(
+    id: string,
+    replay: (layer: DepTrackingCache) => any,
+  ): DepTrackingCache;
+
+  public abstract removeLayer(id: string): DepTrackingCache;
 
   public toObject(): NormalizedCacheObject {
     return this.data;
   }
 
   public get(dataId: string): StoreObject {
-    this.depend(dataId);
+    if (this.depend) this.depend(dataId);
     return this.data[dataId]!;
   }
 
-  public set(dataId: string, value?: StoreObject) {
-    const oldValue = this.data[dataId];
-    if (value !== oldValue) {
+  public set(dataId: string, value: StoreObject): void {
+    if (!hasOwn.call(this.data, dataId) || value !== this.data[dataId]) {
       this.data[dataId] = value;
-      this.depend.dirty(dataId);
+      if (this.depend) this.depend.dirty(dataId);
     }
   }
 
   public delete(dataId: string): void {
-    if (hasOwn.call(this.data, dataId)) {
-      delete this.data[dataId];
-      this.depend.dirty(dataId);
-    }
+    this.data[dataId] = void 0;
+    if (this.depend) this.depend.dirty(dataId);
   }
 
   public clear(): void {
@@ -46,25 +51,111 @@ export class DepTrackingCache implements NormalizedCache {
   }
 
   public replace(newData: NormalizedCacheObject | null): void {
+    Object.keys(this.data).forEach(dataId => {
+      if (!(newData && hasOwn.call(newData, dataId))) {
+        this.delete(dataId);
+      }
+    });
     if (newData) {
       Object.keys(newData).forEach(dataId => {
         this.set(dataId, newData[dataId]);
       });
-      Object.keys(this.data).forEach(dataId => {
-        if (!hasOwn.call(newData, dataId)) {
-          this.delete(dataId);
-        }
-      });
-    } else {
-      Object.keys(this.data).forEach(dataId => {
-        this.delete(dataId);
-      });
     }
+  }
+}
+
+export namespace DepTrackingCache {
+  // Refer to this class as DepTrackingCache.Root outside this namespace.
+  export class Root extends DepTrackingCache {
+    // Although each Root instance gets its own unique this.depend
+    // function, any Layer instances created by calling addLayer need to
+    // share a single distinct dependency function. Since this shared
+    // function must outlast the Layer instances themselves, it needs to
+    // be created and owned by the Root instance.
+    private sharedLayerDepend: DependType = null;
+
+    constructor({
+      resultCaching = true,
+      seed,
+    }: {
+      resultCaching?: boolean;
+      seed?: NormalizedCacheObject;
+    }) {
+      super();
+      if (resultCaching) {
+        (this as any).depend = this.makeDepend();
+        this.sharedLayerDepend = this.makeDepend();
+      }
+      if (seed) this.replace(seed);
+    }
+
+    public addLayer(
+      id: string,
+      replay: (layer: DepTrackingCache) => any,
+    ): DepTrackingCache {
+      return new Layer(id, this, replay, this.sharedLayerDepend);
+    }
+
+    public removeLayer(): Root {
+      return this;
+    }
+  }
+}
+
+// Not exported, since all Layer instances are created by the addLayer method
+// of the DepTrackingCache.Root class.
+class Layer extends DepTrackingCache {
+  constructor(
+    private id: string,
+    private parent: DepTrackingCache,
+    private replay: (layer: DepTrackingCache) => any,
+    public readonly depend: DependType,
+  ) {
+    super();
+    replay(this);
+  }
+
+  public addLayer(
+    id: string,
+    replay: (layer: DepTrackingCache) => any,
+  ): DepTrackingCache {
+    return new Layer(id, this, replay, this.depend);
+  }
+
+  public removeLayer(id: string): DepTrackingCache {
+    const parent = this.parent.removeLayer(id);
+    if (id === this.id) {
+      // Dirty every ID we're removing.
+      // TODO Some of these IDs could escape dirtying if value unchanged.
+      if (this.depend) {
+        Object.keys(this.data).forEach(dataId => this.depend.dirty(dataId));
+      }
+      return parent;
+    }
+    if (parent === this.parent) return this;
+    return parent.addLayer(this.id, this.replay);
+  }
+
+  public toObject(): NormalizedCacheObject {
+    return {
+      ...this.parent.toObject(),
+      ...this.data,
+    };
+  }
+
+  public get(dataId: string): StoreObject {
+    if (hasOwn.call(this.data, dataId)) {
+      return super.get(dataId);
+    }
+    if (this.depend && this.depend !== this.parent.depend) {
+      this.depend(dataId);
+    }
+    return this.parent.get(dataId);
   }
 }
 
 export function defaultNormalizedCacheFactory(
   seed?: NormalizedCacheObject,
 ): NormalizedCache {
-  return new DepTrackingCache(seed);
+  return new DepTrackingCache.Root({ resultCaching: true, seed });
 }

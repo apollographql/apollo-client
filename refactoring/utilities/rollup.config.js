21ded05e19f599f07de30ec911b1f9ef8477cc9f
hwillson <hugh@octonary.com>
2019-09-09 07:38:15 -0400

    Remove temporary refactoring directory and contents
    Everyhing we need (for now) has been moved elsewhere.

--- refactoring/utilities/rollup.config.js
+++
@@ -1,9 +0,0 @@
-import { rollup } from '../../config/rollup.config';
-
-export default rollup({
-  name: 'apollo-utilities',
-  extraGlobals: {
-    'fast-json-stable-stringify': 'stringify',
-    '@wry/equality': 'wryEquality',
-  },
-});

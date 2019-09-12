29b2af81a360b600a80a1b1d1586566f7d43d5d6
Ben Newman <ben@apollographql.com>
2019-09-12 16:27:18 -0400

    Remove unnecessary describe(...) block from testUtils.ts.

--- src/util/testUtils.ts
+++ src/util/testUtils.ts
@@ -3,18 +3,3 @@ export function cloneWithoutTypename<D>(data: D): D {
     return key === '__typename' ? void 0 : value;
   });
 }
-
-describe('cloneWithoutTypename', () => {
-  it('needs at least one test to be defined', () => {
-    expect(
-      cloneWithoutTypename({
-        a: 1,
-        __typename: 'SomeType',
-        b: true,
-      }),
-    ).toEqual({
-      a: 1,
-      b: true,
-    });
-  });
-});

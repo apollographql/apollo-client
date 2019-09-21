ceedde95e4a93334c8c38709169b38c3a47cebfb
hwillson <hugh@octonary.com>
2019-09-21 12:45:46 -0400

    Rollup bundling changes
    These changes wire up a new Rollup based bundling approach. We
    no longer build and provide an ESM bundle. By providing an ESM
    bundle, and pointing to it via the `package.json` `module`
    property, we were inadvertently reducing the effectiveness of
    build tool tree shaking / dead code elimination capabilities.
    For example, since webpack was getting the ESM bundle from the
    `module` property, it wasn't able to get to the fully extracted
    ESM source in `./lib`, to use it for DCE.
    
    With the changes in this commit, the `package.json` continues
    to point `main` to the CJS bundle (with all Apollo Client code
    in it), and now points `module` to the non-bundled ESM entry
    point of `./lib/index.js`. This means we're leaving the bundling
    of ESM code up to Apollo Client consumers and their build tooling,
    most of which will be able to leverage the expanded ESM source
    to build more efficient bundles on their side.
    
    We'll re-visit this strategy as work on AC 3 progresses, but so
    far the results have been promising.

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,22 +1,10 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
-import typescriptPlugin from 'rollup-plugin-typescript2';
-import typescript from 'typescript';
-import path from 'path';
-import fs from 'fs';
-import { transformSync } from '@babel/core';
-import cjsModulesTransform from '@babel/plugin-transform-modules-commonjs';
 import invariantPlugin from 'rollup-plugin-invariant';
 import { terser as minify } from 'rollup-plugin-terser';
 
-function onwarn(message) {
-  const suppressed = ['UNRESOLVED_IMPORT', 'THIS_IS_UNDEFINED'];
+import packageJson from '../package.json';
 
-  if (!suppressed.find(code => message.code === code)) {
-    return console.warn(message.message);
-  }
-}
-
-const defaultGlobals = {
+const globals = {
   'apollo-link': 'apolloLink.core',
   'tslib': 'tslib',
   'ts-invariant': 'invariant',
@@ -30,117 +18,75 @@ const defaultGlobals = {
   react: 'React'
 };
 
-function rollup({
-  input = './src/index.ts',
-  outputPrefix = 'apollo-client',
-  extraGlobals = {},
-} = {}) {
-  const projectDir = path.join(__dirname, '..');
-  console.info(`Building project esm ${projectDir}`);
-  const tsconfig = `${projectDir}/tsconfig.json`;
+function external(id) {
+  return Object.prototype.hasOwnProperty.call(globals, id);
+}
 
-  const globals = {
-    ...defaultGlobals,
-    ...extraGlobals,
+function prepareESM() {
+  return {
+    input: packageJson.module,
+    external,
+    preserveModules: true,
+    output: {
+      dir: './lib',
+      format: 'esm',
+      sourcemap: true,
+    },
+    plugins: [
+      nodeResolve(),
+      invariantPlugin({
+        // Instead of completely stripping InvariantError messages in
+        // production, this option assigns a numeric code to the
+        // production version of each error (unique to the call/throw
+        // location), which makes it much easier to trace production
+        // errors back to the unminified code where they were thrown,
+        // where the full error string can be found. See #4519.
+        errorCodes: true,
+      }),
+    ]
   };
+}
 
-  function external(id) {
-    return Object.prototype.hasOwnProperty.call(globals, id);
-  }
-
-  function outputFile(format) {
-    return `./lib/${outputPrefix}.${format}.js`;
-  }
-
-  function fromSource(format) {
-    return {
-      input,
-      external,
-      output: {
-        file: outputFile(format),
-        format,
-        sourcemap: true,
-      },
-      plugins: [
-        nodeResolve({
-          extensions: ['.ts', '.tsx'],
-        }),
-        typescriptPlugin({ typescript, tsconfig }),
-        invariantPlugin({
-          // Instead of completely stripping InvariantError messages in
-          // production, this option assigns a numeric code to the
-          // production version of each error (unique to the call/throw
-          // location), which makes it much easier to trace production
-          // errors back to the unminified code where they were thrown,
-          // where the full error string can be found. See #4519.
-          errorCodes: true,
-        }),
-      ],
-      onwarn,
-    };
-  }
-
-  function fromESM(toFormat) {
-    return {
-      input: outputFile('esm'),
-      output: {
-        file: outputFile(toFormat),
-        format: 'esm',
-        sourcemap: false,
-      },
-      plugins: [{
-        transform(source, id) {
-          const output = transformSync(source, {
-            inputSourceMap: JSON.parse(fs.readFileSync(id + '.map')),
-            sourceMaps: true,
-            plugins: [
-              [cjsModulesTransform, {
-                loose: true,
-                allowTopLevelThis: true,
-              }],
-            ],
-          });
-
-          // There doesn't seem to be any way to get Rollup to emit a source map
-          // that goes all the way back to the source file (rather than just to
-          // the bundle.esm.js intermediate file), so we pass sourcemap:false in
-          // the output options above, and manually write the CJS source
-          // maps here.
-          fs.writeFileSync(
-            outputFile(toFormat) + '.map',
-            JSON.stringify(output.map),
-          );
-
-          return {
-            code: output.code,
-          };
-        }
-      }],
+function prepareCJS() {
+  return {
+    input: packageJson.module,
+    external,
+    output: {
+      file: packageJson.main,
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'named'
     }
   }
+}
 
-  return [
-    fromSource('esm'),
-    fromESM('cjs'),
-    {
-      input: outputFile('cjs'),
-      output: {
-        file: outputFile('cjs.min'),
-        format: 'esm',
-      },
-      plugins: [
-        minify({
-          mangle: {
-            toplevel: true,
-          },
-          compress: {
-            global_defs: {
-              '@process.env.NODE_ENV': JSON.stringify('production'),
-            },
-          },
-        }),
-      ],
+function prepareCJSMinified() {
+  return {
+    input: packageJson.main,
+    output: {
+      file: packageJson.main.replace('.js', '.min.js'),
+      format: 'cjs',
     },
+    plugins: [
+      minify({
+        mangle: {
+          toplevel: true,
+        },
+        compress: {
+          global_defs: {
+            '@process.env.NODE_ENV': JSON.stringify('production'),
+          },
+        },
+      }),
+    ],
+  };
+}
+
+function rollup() {
+  return [
+    prepareESM(),
+    prepareCJS(),
+    prepareCJSMinified()
   ];
 }
 

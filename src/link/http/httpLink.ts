6d574df31a0e9dc3339abbdd91fc040b1eb37311
hwillson <hugh@octonary.com>
2019-10-04 11:22:21 -0400

    Expose `HttpLink` options to make unit testing easier

--- src/link/http/httpLink.ts
+++ src/link/http/httpLink.ts
@@ -4,7 +4,9 @@ import { createHttpLink } from './createHttpLink';
 
 export class HttpLink extends ApolloLink {
   public requester: RequestHandler;
-  constructor(opts?: HttpOptions) {
-    super(createHttpLink(opts).request);
+  public options: HttpOptions;
+  constructor(options?: HttpOptions) {
+    super(createHttpLink(options).request);
+    this.options = options;
   }
 }

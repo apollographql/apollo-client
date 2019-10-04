fc5c0688ad99074132b550a96e315e19af97ad5f
Ben Newman <ben@apollographql.com>
2019-10-04 13:29:29 -0400

    Use constructor member declaration in HttpLink.

--- src/link/http/HttpLink.ts
+++ src/link/http/HttpLink.ts
@@ -4,9 +4,7 @@ import { createHttpLink } from './createHttpLink';
 
 export class HttpLink extends ApolloLink {
   public requester: RequestHandler;
-  public options: HttpOptions;
-  constructor(options?: HttpOptions) {
+  constructor(public options: HttpOptions = {}) {
     super(createHttpLink(options).request);
-    this.options = options;
   }
 }

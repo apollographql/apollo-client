74dbbf7d24dd5618c1b58ac3add6db4fc58998d5
Ben Newman <ben@apollographql.com>
2019-10-04 13:05:15 -0400

    Simplify parseAndCheckHttpResponse.

--- src/link/http/parseAndCheckHttpResponse.ts
+++ src/link/http/parseAndCheckHttpResponse.ts
@@ -1,56 +1,57 @@
 import { Operation } from '../core';
 import { throwServerError } from '../utils/throwServerError';
 
+const { hasOwnProperty } = Object.prototype;
+
 export type ServerParseError = Error & {
   response: Response;
   statusCode: number;
   bodyText: string;
 };
 
-export const parseAndCheckHttpResponse =
-  (operations: Operation | Operation[]) => (response: Response) => {
-    return (
-      response
-        .text()
-        .then(bodyText => {
-          try {
-            return JSON.parse(bodyText);
-          } catch (err) {
-            const parseError = err as ServerParseError;
-            parseError.name = 'ServerParseError';
-            parseError.response = response;
-            parseError.statusCode = response.status;
-            parseError.bodyText = bodyText;
-            return Promise.reject(parseError);
-          }
-        })
-        .then((result: any) => {
-          if (response.status >= 300) {
-            //Network error
-            throwServerError(
-              response,
-              result,
-              `Response not successful: Received status code ${response.status}`,
-            );
-          }
+export function parseAndCheckHttpResponse(
+  operations: Operation | Operation[],
+) {
+  return (response: Response) => response
+    .text()
+    .then(bodyText => {
+      try {
+        return JSON.parse(bodyText);
+      } catch (err) {
+        const parseError = err as ServerParseError;
+        parseError.name = 'ServerParseError';
+        parseError.response = response;
+        parseError.statusCode = response.status;
+        parseError.bodyText = bodyText;
+        throw parseError;
+      }
+    })
+    .then((result: any) => {
+      if (response.status >= 300) {
+        // Network error
+        throwServerError(
+          response,
+          result,
+          `Response not successful: Received status code ${response.status}`,
+        );
+      }
 
-          if (
-            !Array.isArray(result) &&
-            !result.hasOwnProperty('data') &&
-            !result.hasOwnProperty('errors')
-          ) {
-            //Data error
-            throwServerError(
-              response,
-              result,
-              `Server response was missing for query '${
-                Array.isArray(operations)
-                  ? operations.map(op => op.operationName)
-                  : operations.operationName
-              }'.`,
-            );
-          }
-          return result;
-        })
-    );
-  };
+      if (
+        !Array.isArray(result) &&
+        !hasOwnProperty.call(result, 'data') &&
+        !hasOwnProperty.call(result, 'errors')
+      ) {
+        // Data error
+        throwServerError(
+          response,
+          result,
+          `Server response was missing for query '${
+            Array.isArray(operations)
+              ? operations.map(op => op.operationName)
+              : operations.operationName
+          }'.`,
+        );
+      }
+      return result;
+    });
+}

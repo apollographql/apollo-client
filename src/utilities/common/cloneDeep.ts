1965e8815002d6b51e68ed6ba42256d368086cb1
Ben Newman <ben@apollographql.com>
2019-10-10 12:35:39 -0400

    Avoid creating unused Map for primitive cloneDeep inputs.

--- src/utilities/common/cloneDeep.ts
+++ src/utilities/common/cloneDeep.ts
@@ -4,12 +4,13 @@ const { toString } = Object.prototype;
  * Deeply clones a value to create a new instance.
  */
 export function cloneDeep<T>(value: T): T {
-  return cloneDeepHelper(value, new Map());
+  return cloneDeepHelper(value);
 }
 
-function cloneDeepHelper<T>(val: T, seen: Map<any, any>): T {
+function cloneDeepHelper<T>(val: T, seen?: Map<any, any>): T {
   switch (toString.call(val)) {
   case "[object Array]": {
+    seen = seen || new Map;
     if (seen.has(val)) return seen.get(val);
     const copy: T & any[] = (val as any).slice(0);
     seen.set(val, copy);
@@ -20,6 +21,7 @@ function cloneDeepHelper<T>(val: T, seen: Map<any, any>): T {
   }
 
   case "[object Object]": {
+    seen = seen || new Map;
     if (seen.has(val)) return seen.get(val);
     // High fidelity polyfills of Object.create and Object.getPrototypeOf are
     // possible in all JS environments, so we will assume they exist/work.

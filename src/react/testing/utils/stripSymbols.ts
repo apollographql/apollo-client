08699222b3ade834fbe50e140a5a518b118e8e85
hwillson <hugh@octonary.com>
2019-10-11 07:57:25 -0400

    Remove duplicate `stripSymbols` utility

--- src/react/testing/utils/stripSymbols.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Apollo-client adds Symbols to the data in the store. In order to make
- * assertions in our tests easier we strip these Symbols from the data.
- */
-export function stripSymbols<T>(data: T): T {
-  return JSON.parse(JSON.stringify(data));
-}

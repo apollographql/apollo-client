01376a3ad09e1a806d7517cad44502b4355456af
Ben Newman <ben@apollographql.com>
2019-10-22 14:12:58 -0400

    Stop reading from cache in ObservableQuery#getCurrentResult.
    Since getCurrentResult must return its result synchronously, reading from
    the cache in this method prevents us from making cache reads asynchronous
    in the future.
    
    This may be a breaking change for code that attempts to read cache data
    from an ObservableQuery immediately after calling watchQuery, but that's a
    behavior that should never have been enforced by our test suite in the
    first place.

--- src/core/ObservableQuery.ts
+++ src/core/ObservableQuery.ts
@@ -137,46 +137,44 @@ export class ObservableQuery<
     });
   }
 
-  /**
-   * Return the result of the query from the local cache as well as some fetching status
-   * `loading` and `networkStatus` allow to know if a request is in flight
-   * `partial` lets you know if the result from the local cache is complete or partial
-   * @return {data: Object, error: ApolloError, loading: boolean, networkStatus: number, partial: boolean}
-   */
   public getCurrentResult(): ApolloCurrentQueryResult<TData> {
-    if (this.isTornDown) {
-      const { lastResult } = this;
-      return {
-        data: !this.lastError && lastResult && lastResult.data || void 0,
-        error: this.lastError,
-        loading: false,
-        networkStatus: NetworkStatus.error,
-      };
-    }
-
-    const { data, partial } = this.queryManager.getCurrentQueryResult(this);
-    const queryStoreValue = this.queryManager.queryStore.get(this.queryId);
-    let result: ApolloQueryResult<TData>;
-
+    const { lastResult, lastError } = this;
     const { fetchPolicy } = this.options;
-
     const isNetworkFetchPolicy =
       fetchPolicy === 'network-only' ||
       fetchPolicy === 'no-cache';
 
+    const networkStatus =
+      lastError ? NetworkStatus.error :
+      lastResult ? lastResult.networkStatus :
+      isNetworkFetchPolicy ? NetworkStatus.loading :
+      NetworkStatus.ready;
+
+    const result: ApolloCurrentQueryResult<TData> = {
+      data: !lastError && lastResult && lastResult.data || void 0,
+      error: this.lastError,
+      loading: isNetworkRequestInFlight(networkStatus),
+      networkStatus,
+      partial: false,
+    };
+
+    if (this.isTornDown) {
+      return result;
+    }
+
+    const queryStoreValue = this.queryManager.queryStore.get(this.queryId);
     if (queryStoreValue) {
       const { networkStatus } = queryStoreValue;
 
       if (hasError(queryStoreValue, this.options.errorPolicy)) {
-        return {
+        return Object.assign(result, {
           data: void 0,
-          loading: false,
           networkStatus,
           error: new ApolloError({
             graphQLErrors: queryStoreValue.graphQLErrors,
             networkError: queryStoreValue.networkError,
           }),
-        };
+        });
       }
 
       // Variables might have been added dynamically at query time, when
@@ -192,38 +190,19 @@ export class ObservableQuery<
         this.variables = this.options.variables;
       }
 
-      result = {
-        data,
+      Object.assign(result, {
         loading: isNetworkRequestInFlight(networkStatus),
         networkStatus,
-      } as ApolloQueryResult<TData>;
+      });
 
       if (queryStoreValue.graphQLErrors && this.options.errorPolicy === 'all') {
         result.errors = queryStoreValue.graphQLErrors;
       }
-
-    } else {
-      // We need to be careful about the loading state we show to the user, to try
-      // and be vaguely in line with what the user would have seen from .subscribe()
-      // but to still provide useful information synchronously when the query
-      // will not end up hitting the server.
-      // See more: https://github.com/apollostack/apollo-client/issues/707
-      // Basically: is there a query in flight right now (modolo the next tick)?
-      const loading = isNetworkFetchPolicy ||
-        (partial && fetchPolicy !== 'cache-only');
-
-      result = {
-        data,
-        loading,
-        networkStatus: loading ? NetworkStatus.loading : NetworkStatus.ready,
-      } as ApolloQueryResult<TData>;
     }
 
-    if (!partial) {
-      this.updateLastResult({ ...result, stale: false });
-    }
+    this.updateLastResult({ ...result, stale: false });
 
-    return { ...result, partial };
+    return result;
   }
 
   // Compares newResult to the snapshot we took of this.lastResult when it was
--- src/core/__tests__/ObservableQuery.ts
+++ src/core/__tests__/ObservableQuery.ts
@@ -306,11 +306,8 @@ describe('ObservableQuery', () => {
         } else if (handleCount === 3) {
           expect(stripSymbols(result.data)).toEqual(data2);
           // go back to first set of variables
-          await observable.setOptions({ variables });
-          const current = observable.getCurrentResult();
+          const current = await observable.setOptions({ variables });
           expect(stripSymbols(current.data)).toEqual(data);
-          const secondCurrent = observable.getCurrentResult();
-          expect(current.data).toEqual(secondCurrent.data);
           resolve();
         }
       });
@@ -1461,7 +1458,7 @@ describe('ObservableQuery', () => {
         loading: true,
         data: undefined,
         networkStatus: 1,
-        partial: true,
+        partial: false,
       });
 
       setTimeout(
@@ -1470,37 +1467,11 @@ describe('ObservableQuery', () => {
             loading: true,
             data: undefined,
             networkStatus: 1,
-            partial: true,
+            partial: false,
           });
         }),
         0,
       );
-    });
-
-    itAsync('returns results from the store immediately', (resolve, reject) => {
-      const queryManager = mockQueryManager(reject, {
-        request: { query, variables },
-        result: { data: dataOne },
-      });
-
-      return queryManager.query({ query, variables }).then((result: any) => {
-        expect(stripSymbols(result)).toEqual({
-          data: dataOne,
-          loading: false,
-          networkStatus: 7,
-          stale: false,
-        });
-        const observable = queryManager.watchQuery({
-          query,
-          variables,
-        });
-        expect(stripSymbols(observable.getCurrentResult())).toEqual({
-          data: dataOne,
-          loading: true,
-          networkStatus: NetworkStatus.loading,
-          partial: false,
-        });
-      }).then(resolve, reject);
     });
 
     itAsync('returns errors from the store immediately', (resolve, reject) => {
@@ -1592,7 +1563,7 @@ describe('ObservableQuery', () => {
       }).then(resolve, reject);
     });
 
-    itAsync('returns partial data from the store immediately', (resolve, reject) => {
+    itAsync('returns partial data from the store', (resolve, reject) => {
       const superQuery = gql`
         query superQuery($id: ID!) {
           people_one(id: $id) {
@@ -1629,10 +1600,10 @@ describe('ObservableQuery', () => {
         });
 
         expect(observable.getCurrentResult()).toEqual({
-          data: dataOne,
+          data: void 0,
           loading: true,
           networkStatus: 1,
-          partial: true,
+          partial: false,
         });
 
         // we can use this to trigger the query
@@ -1647,7 +1618,7 @@ describe('ObservableQuery', () => {
 
           if (handleCount === 1) {
             expect(subResult).toEqual({
-              data: dataOne,
+              data: void 0,
               loading: true,
               networkStatus: 1,
               stale: false,
@@ -1655,11 +1626,20 @@ describe('ObservableQuery', () => {
 
           } else if (handleCount === 2) {
             expect(subResult).toEqual({
+              data: dataOne,
+              loading: false,
+              networkStatus: 7,
+              stale: false,
+            });
+
+          } else if (handleCount === 3) {
+            expect(subResult).toEqual({
               data: superDataOne,
               loading: false,
               networkStatus: 7,
               stale: false,
             });
+
             resolve();
           }
         });
@@ -1698,6 +1678,7 @@ describe('ObservableQuery', () => {
             loading,
             networkStatus,
           } = observable.getCurrentResult();
+
           expect(subResult).toEqual({
             data,
             loading,
@@ -1705,13 +1686,22 @@ describe('ObservableQuery', () => {
             stale: false,
           });
 
-          if (handleCount === 2) {
+          if (handleCount === 1) {
+            expect(stripSymbols(subResult)).toEqual({
+              data: void 0,
+              loading: true,
+              networkStatus: NetworkStatus.loading,
+              stale: false,
+            });
+
+          } else if (handleCount === 2) {
             expect(stripSymbols(subResult)).toEqual({
               data: dataTwo,
               loading: false,
-              networkStatus: 7,
+              networkStatus: NetworkStatus.ready,
               stale: false,
             });
+
             resolve();
           }
         });
@@ -1737,6 +1727,7 @@ describe('ObservableQuery', () => {
           variables,
           fetchPolicy: 'no-cache',
         });
+
         expect(stripSymbols(observable.getCurrentResult())).toEqual({
           data: undefined,
           loading: true,
@@ -1750,14 +1741,16 @@ describe('ObservableQuery', () => {
             loading,
             networkStatus,
           } = observable.getCurrentResult();
-          expect(subResult).toEqual({
-            data,
-            loading,
-            networkStatus,
-            stale: false,
-          });
 
-          if (handleCount === 2) {
+          if (handleCount === 1) {
+            expect(subResult).toEqual({
+              data,
+              loading,
+              networkStatus,
+              partial: false,
+              stale: false,
+            });
+          } else if (handleCount === 2) {
             expect(stripSymbols(subResult)).toEqual({
               data: dataTwo,
               loading: false,

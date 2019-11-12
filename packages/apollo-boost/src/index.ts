709b9c86a2d6a68e128eb807908eb39847b4ac98
Benjamin Coullie <ben.coullie@vendhq.com>
2019-11-13 11:54:51 +1300

    add assumeImmutableResults typing to apollo boost PresetConfig interface

--- packages/apollo-boost/src/index.ts
+++ packages/apollo-boost/src/index.ts
@@ -42,6 +42,7 @@ export interface PresetConfig {
   resolvers?: Resolvers | Resolvers[];
   typeDefs?: string | string[] | DocumentNode | DocumentNode[];
   fragmentMatcher?: LocalStateFragmentMatcher;
+  assumeImmutableResults?: boolean;
 }
 
 // Yes, these are the exact same as the `PresetConfig` interface. We're

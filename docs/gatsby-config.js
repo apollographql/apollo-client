bea10f471dc1cca896af06665f3cc003715a60f7
hwillson <hugh@octonary.com>
2020-01-09 09:42:57 -0500

    Disable all doc sets except for 2.6
    Hmm - broken links in other doc sets are preventing use from
    re-enabling the docs version switcher. We'll disable everything
    except for 2.6 for now, to fix prod, and fix the rest separately.

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -13,11 +13,16 @@ module.exports = {
         githubRepo: 'apollographql/apollo-client',
         defaultVersion: 2.6,
         versions: {
+          '2.6': 'version-2.6',
+        },
+        /*
+        versions: {
           '3.0 beta': 'release-3.0',
           '2.6': 'version-2.6',
           '2.5': 'version-2.5',
           '2.4': 'version-2.4',
         },
+        */
         checkLinksOptions: {
           exceptions: [
             '/api/core/',

3b207cc2c674c26b104b4169c6380706f2239178
hwillson <hugh@octonary.com>
2020-01-27 16:31:04 -0500

    Add IE11 mention/comment

--- src/react/context/ApolloContext.ts
+++ src/react/context/ApolloContext.ts
@@ -13,6 +13,8 @@ export interface ApolloContextValue {
 // Since the created context is React specific, we've decided to attach it to
 // the `React` object for sharing.
 
+// If Symbol's aren't available, we'll use a fallback string as the context
+// property (we're looking at you, IE11).
 const contextSymbol = typeof Symbol === 'function' ?
   Symbol.for('__APOLLO_CONTEXT__') :
   '__APOLLO_CONTEXT__';

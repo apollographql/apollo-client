1f0e5babdecb51a7f626f767e5583ee52e05940e
Ben Newman <ben@benjamn.com>
2020-04-30 12:03:01 -0400

    Allow asyncMap to take an optional catch callback.

--- src/utilities/observables/observables.ts
+++ src/utilities/observables/observables.ts
@@ -31,36 +31,46 @@ export function multiplex<T>(inner: Observable<T>): Observable<T> {
 // optionally return a Promise (or be async).
 export function asyncMap<V, R>(
   observable: Observable<V>,
-  mapFn: (value: V) => R | Promise<R>,
+  mapFn: (value: V) => R | PromiseLike<R>,
+  catchFn?: (error: any) => R | PromiseLike<R>,
 ): Observable<R> {
   return new Observable<R>(observer => {
     const { next, error, complete } = observer;
-    let activeNextCount = 0;
+    let activeCallbackCount = 0;
     let completed = false;
 
+    function makeCallback(
+      examiner: typeof mapFn | typeof catchFn,
+      delegate: typeof next | typeof error,
+    ): (arg: any) => void {
+      if (examiner) {
+        return arg => {
+          ++activeCallbackCount;
+          new Promise(resolve => resolve(examiner(arg))).then(
+            result => {
+              --activeCallbackCount;
+              next && next.call(observer, result);
+              if (completed) {
+                handler.complete!();
+              }
+            },
+            e => {
+              --activeCallbackCount;
+              error && error.call(observer, e);
+            },
+          );
+        };
+      } else {
+        return arg => delegate && delegate.call(observer, arg);
+      }
+    }
+
     const handler: Observer<V> = {
-      next(value) {
-        ++activeNextCount;
-        new Promise(resolve => {
-          resolve(mapFn(value));
-        }).then(
-          result => {
-            --activeNextCount;
-            next && next.call(observer, result);
-            completed && handler.complete!();
-          },
-          e => {
-            --activeNextCount;
-            error && error.call(observer, e);
-          },
-        );
-      },
-      error(e) {
-        error && error.call(observer, e);
-      },
+      next: makeCallback(mapFn, next),
+      error: makeCallback(catchFn, error),
       complete() {
         completed = true;
-        if (!activeNextCount) {
+        if (!activeCallbackCount) {
           complete && complete.call(observer);
         }
       },

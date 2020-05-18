fdcd519cddc6fd44b57e8254e5eb13b5f257c2ee
Hugh Willson <hugh@octonary.com>
2020-05-18 18:31:51 -0400

    Replace unfetch/node-fetch recommendation with cross-fetch (#6280)
    The `node-fetch` types aren't fully compatible with AC3, leading to
    TS compiler errors in some cases (for the full backstory, see
    https://github.com/apollographql/apollo-link/issues/513).
    `cross-fetch` addresses these type issues (while still using
    `node-fetch` under the hood), and also supports older browsers
    as well. This means we can recommend the use of `cross-fetch`
    instead of both `unfetch` and `node-fetch`. This PR updates our
    docs and code checks accordingly.

--- src/link/http/checkFetcher.ts
+++ src/link/http/checkFetcher.ts
@@ -2,19 +2,17 @@ import { InvariantError } from 'ts-invariant';
 
 export const checkFetcher = (fetcher: WindowOrWorkerGlobalScope['fetch'] | undefined) => {
   if (!fetcher && typeof fetch === 'undefined') {
-    let library: string = 'unfetch';
-    if (typeof window === 'undefined') library = 'node-fetch';
-    throw new InvariantError(
-      '"fetch" has not been found globally and no fetcher has been ' +
-      'configured. To fix this, install a fetch package ' +
-      `(like https://www.npmjs.com/package/${library}), instantiate the ` +
-      'fetcher, and pass it into your `HttpLink` constructor. For example:' +
-      '\n\n' +
-      `import fetch from '${library}';\n` +
-      "import { ApolloClient, HttpLink } from '@apollo/client';\n" +
-      'const client = new ApolloClient({\n' +
-      "  link: new HttpLink({ uri: '/graphq', fetch })\n" +
-      '});'
-    );
+    throw new InvariantError(`
+"fetch" has not been found globally and no fetcher has been \
+configured. To fix this, install a fetch package (like \
+https://www.npmjs.com/package/cross-fetch), instantiate the \
+fetcher, and pass it into your HttpLink constructor. For example:
+
+import fetch from 'cross-fetch';
+import { ApolloClient, HttpLink } from '@apollo/client';
+const client = new ApolloClient({
+  link: new HttpLink({ uri: '/graphql', fetch })
+});
+    `);
   }
 };

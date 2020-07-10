cc9b92948c69ccf569fe0b9ec2dadb234cd2cc0c
hwillson <hugh@octonary.com>
2020-07-10 15:12:13 -0400

    Import the `apollo-link-retry` source

---
+++ src/link/retry/__tests__/retryFunction.ts
@@ -0,0 +1,39 @@
+import { Operation } from '../../core/types';
+
+import { buildRetryFunction } from '../retryFunction';
+
+describe('buildRetryFunction', () => {
+  const operation = { operationName: 'foo' } as Operation;
+
+  it('stops after hitting maxTries', () => {
+    const retryFunction = buildRetryFunction({ max: 3 });
+
+    expect(retryFunction(2, operation, {})).toEqual(true);
+    expect(retryFunction(3, operation, {})).toEqual(false);
+    expect(retryFunction(4, operation, {})).toEqual(false);
+  });
+
+  it('skips retries if there was no error, by default', () => {
+    const retryFunction = buildRetryFunction();
+
+    expect(retryFunction(1, operation, undefined)).toEqual(false);
+    expect(retryFunction(1, operation, {})).toEqual(true);
+  });
+
+  it('supports custom predicates, but only if max is not exceeded', () => {
+    const stub = jest.fn(() => true);
+    const retryFunction = buildRetryFunction({ max: 3, retryIf: stub });
+
+    expect(retryFunction(2, operation, null)).toEqual(true);
+    expect(retryFunction(3, operation, null)).toEqual(false);
+  });
+
+  it('passes the error and operation through to custom predicates', () => {
+    const stub = jest.fn(() => true);
+    const retryFunction = buildRetryFunction({ max: 3, retryIf: stub });
+
+    const error = { message: 'bewm' };
+    retryFunction(1, operation, error);
+    expect(stub).toHaveBeenCalledWith(error, operation);
+  });
+});

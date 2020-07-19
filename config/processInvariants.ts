c98921246f6b3847781a47925b4fb061c7c19123
Ben Newman <ben@apollographql.com>
2020-07-19 14:50:54 -0400

    Standalone script for processing invariants.
    Adapted from https://github.com/apollographql/invariant-packages/tree/master/packages/rollup-plugin-invariant,
    this script wraps invariant(...), new InvariantError(...), and
    invariant.{warn,error} calls with process.env.NODE_ENV logic, so that long
    error messages can be stripped from production builds.
    
    I decided to abandon the Rollup plugin for this task because I'm planning
    to change how Rollup determines if imported modules are "external," and
    Rollup (understandably) stops traversing the dependency graph when it hits
    an external import, which means we can no longer rely on Rollup to
    traverse all modules that need invariants to be transformed.
    
    Also, this transformation step was never especially intertwined with the
    dependency traversal and rewriting logic that Rollup provides, so I think
    it makes things simpler to keep them separate.
    
    If Rollup provided hooks for performing arbitrary build steps, we might
    have been able to do this work in some initial step, but I couldn't find
    anything in the Rollup documentation that fit the bill (and that's fine: I
    wouldn't expect Rollup to attempt to be a generic build pipeline).
    
    Now, the CJS bundles for each nested entry point (e.g. @apollo/client/core
    or @apollo/client/react/ssr) can be computed from code that has been
    processed by the invariant transform, but has not yet been tampered with
    by Rollup, so we can trust that the exact same module identifiers from the
    source code are still intact.

---
+++ config/processInvariants.ts
@@ -0,0 +1,120 @@
+import { readFileSync, writeFileSync } from "fs";
+import { resolve, relative } from "path";
+import glob = require("glob");
+
+const distDir = resolve(__dirname, "..", "dist");
+
+glob(`${distDir}/**/*.js`, (error, files) => {
+  if (error) throw error;
+
+  files.sort().forEach(file => {
+    const relPath = relative(distDir, file);
+
+    // Outside the distDir, somehow.
+    if (relPath.startsWith("../")) return;
+
+    // Avoid re-transforming CommonJS bundle files.
+    if (relPath.endsWith(".cjs.js")) return;
+
+    const source = readFileSync(file, "utf8");
+    const output = transform(source, relPath);
+    if (source !== output) {
+      console.log("transformed invariants in " + relPath);
+      writeFileSync(file, output, "utf8");
+    }
+  });
+});
+
+import * as recast from "recast";
+import * as parser from "recast/parsers/babel";
+const b = recast.types.builders;
+let nextErrorCode = 1;
+
+function transform(code: string, id: string) {
+  // If the code doesn't seem to contain anything invariant-related, we
+  // can skip parsing and transforming it.
+  if (!/invariant/i.test(code)) {
+    return code;
+  }
+
+  const ast = recast.parse(code, { parser });
+
+  recast.visit(ast, {
+    visitCallExpression(path) {
+      this.traverse(path);
+      const node = path.node;
+
+      if (isCallWithLength(node, "invariant", 1)) {
+        const newArgs = node.arguments.slice(0, 1);
+        newArgs.push(b.numericLiteral(nextErrorCode++));
+
+        return b.conditionalExpression(
+          makeNodeEnvTest(),
+          b.callExpression.from({
+            ...node,
+            arguments: newArgs,
+          }),
+          node,
+        );
+      }
+
+      if (node.callee.type === "MemberExpression" &&
+          isIdWithName(node.callee.object, "invariant") &&
+          isIdWithName(node.callee.property, "warn", "error")) {
+        return b.logicalExpression("||", makeNodeEnvTest(), node);
+      }
+    },
+
+    visitNewExpression(path) {
+      this.traverse(path);
+      const node = path.node;
+      if (isCallWithLength(node, "InvariantError", 0)) {
+        const newArgs = [
+          b.numericLiteral(nextErrorCode++),
+        ];
+
+        return b.conditionalExpression(
+          makeNodeEnvTest(),
+          b.newExpression.from({
+            ...node,
+            arguments: newArgs,
+          }),
+          node,
+        );
+      }
+    }
+  });
+
+  return recast.print(ast).code;
+}
+
+type CallExpression = recast.types.namedTypes.CallExpression;
+type NewExpression = recast.types.namedTypes.NewExpression;
+
+function isIdWithName(node: any, ...names: string[]) {
+  return recast.types.namedTypes.Identifier.check(node) &&
+    names.some(name => name === node.name);
+}
+
+function isCallWithLength(
+  node: CallExpression | NewExpression,
+  name: string,
+  length: number,
+) {
+  return isIdWithName(node.callee, name) &&
+    node.arguments.length > length;
+}
+
+function makeNodeEnvTest() {
+  return b.binaryExpression(
+    "===",
+    b.memberExpression(
+      b.memberExpression(
+        b.identifier("process"),
+        b.identifier("env")
+      ),
+      b.identifier("NODE_ENV"),
+    ),
+    b.stringLiteral("production"),
+  );
+}

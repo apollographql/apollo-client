44b5415aa1eee8db20145827744a54a591d9e0e7
Ben Newman <ben@apollographql.com>
2020-07-20 11:39:18 -0400

    Make @apollo/client/errors an entry point.

--- config/entryPoints.js
+++ config/entryPoints.js
@@ -2,6 +2,7 @@ module.exports = [
   { dirs: [], bundleName: "main" },
   { dirs: ['cache'] },
   { dirs: ['core'] },
+  { dirs: ['errors'] },
   { dirs: ['link', 'batch'] },
   { dirs: ['link', 'batch-http'] },
   { dirs: ['link', 'context'] },

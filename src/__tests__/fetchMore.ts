716fa9de963d0fbe9e3859edf6e7e78e0cefbb49
Ben Newman <ben@apollographql.com>
2020-07-27 18:29:28 -0400

    Slight tweak to merge function typing in fetchMore tests.

--- src/__tests__/fetchMore.ts
+++ src/__tests__/fetchMore.ts
@@ -2,7 +2,7 @@ import { assign, cloneDeep } from 'lodash';
 import gql from 'graphql-tag';
 
 import { itAsync, mockSingleLink, subscribeAndCount } from '../testing';
-import { InMemoryCache, InMemoryCacheConfig } from '../cache';
+import { InMemoryCache, InMemoryCacheConfig, FieldMergeFunction } from '../cache';
 import { ApolloClient, NetworkStatus, ObservableQuery } from '../core';
 import { offsetLimitPagination, concatPagination } from '../utilities';
 
@@ -412,7 +412,8 @@ describe('fetchMore on an observable query', () => {
     const { merge } = groceriesFieldPolicy;
     groceriesFieldPolicy.merge = function (existing, incoming, options) {
       mergeArgsHistory.push(options.args);
-      return (merge as any).call(this, existing, incoming, options);
+      return (merge as FieldMergeFunction<any>).call(
+        this, existing, incoming, options);
     };
 
     const cache = new InMemoryCache({

458960673148b70051315a4c0e73f9f22d749e2d
Ben Newman <ben@apollographql.com>
2020-07-28 11:31:05 -0400

    Allow importing @apollo/client/testing when global.it undefined.
    https://github.com/apollographql/apollo-client/pull/6576#issuecomment-664510739

--- src/utilities/testing/itAsync.ts
+++ src/utilities/testing/itAsync.ts
@@ -1,5 +1,7 @@
+const itIsDefined = typeof it === "object";
+
 function wrap<TResult>(
-  original: (...args: any[]) => TResult,
+  original: ((...args: any[]) => TResult) | false,
 ) {
   return (
     message: string,
@@ -8,20 +10,20 @@ function wrap<TResult>(
       reject: (reason?: any) => void,
     ) => any,
     timeout?: number,
-  ) => original(message, function () {
+  ) => original && original(message, function () {
     return new Promise(
       (resolve, reject) => callback.call(this, resolve, reject),
     );
   }, timeout);
 }
 
-const wrappedIt = wrap(it);
+const wrappedIt = wrap(itIsDefined && it);
 export function itAsync(...args: Parameters<typeof wrappedIt>) {
   return wrappedIt.apply(this, args);
 }
 
 export namespace itAsync {
-  export const only = wrap(it.only);
-  export const skip = wrap(it.skip);
-  export const todo = wrap(it.todo);
+  export const only = wrap(itIsDefined && it.only);
+  export const skip = wrap(itIsDefined && it.skip);
+  export const todo = wrap(itIsDefined && it.todo);
 }

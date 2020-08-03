2bef27897cd8ec1ad5cd395c6e88a1566dd2f2fc
Jenn Creighton <jenncreighton@jenns-mbp.home>
2020-08-03 14:55:24 -0400

    Check for import size before potential removal

--- codemods/ac2-to-ac3/imports.js
+++ codemods/ac2-to-ac3/imports.js
@@ -40,8 +40,10 @@ export default function transformer(file, api) {
   ].forEach(link => renameImport(`apollo-link-${link}`, `@apollo/client/link/${link}`));
 
   const apolloClientImport = getImport('@apollo/client');
-  if (!apolloClientImport.get('specifiers', 'length').value) {
-    apolloClientImport.remove();
+  if (apolloClientImport.size()) {
+    if (!apolloClientImport.get('specifiers', 'length').value) {
+      apolloClientImport.remove();
+    }
   }
 
   return source.toSource();

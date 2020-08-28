ab5a4f063c211554d81f46b742b9a8937f44445a
Ben Newman <ben@apollographql.com>
2020-08-28 14:51:43 -0400

    Better type inference for {Query,Subscription,Mutation}.propTypes.
    https://github.com/apollographql/apollo-client/pull/6774#issuecomment-680904942

--- src/react/components/Subscription.tsx
+++ src/react/components/Subscription.tsx
@@ -11,13 +11,15 @@ export function Subscription<TData = any, TVariables = OperationVariables>(
   return props.children && result ? props.children(result) : null;
 }
 
-export namespace Subscription {
-  export const propTypes = {
-    subscription: PropTypes.object.isRequired,
-    variables: PropTypes.object,
-    children: PropTypes.func,
-    onSubscriptionData: PropTypes.func,
-    onSubscriptionComplete: PropTypes.func,
-    shouldResubscribe: PropTypes.oneOfType([PropTypes.func, PropTypes.bool])
-  };
+export interface Subscription<TData, TVariables> {
+  propTypes: PropTypes.InferProps<SubscriptionComponentOptions<TData, TVariables>>;
 }
+
+Subscription.propTypes = {
+  subscription: PropTypes.object.isRequired,
+  variables: PropTypes.object,
+  children: PropTypes.func,
+  onSubscriptionData: PropTypes.func,
+  onSubscriptionComplete: PropTypes.func,
+  shouldResubscribe: PropTypes.oneOfType([PropTypes.func, PropTypes.bool])
+};

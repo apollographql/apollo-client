8e92be3e8b791187d8557fccd30ffc82feac5cd4
Ben Newman <ben@apollographql.com>
2020-10-10 13:21:24 -0400

    Default args.offset to 0 in offsetLimitPagination. (#7141)
    https://github.com/apollographql/apollo-client/issues/7135#issuecomment-706415063

--- src/utilities/policies/pagination.ts
+++ src/utilities/policies/pagination.ts
@@ -31,10 +31,18 @@ export function offsetLimitPagination<T = Reference>(
     keyArgs,
     merge(existing, incoming, { args }) {
       const merged = existing ? existing.slice(0) : [];
-      const start = args ? args.offset : merged.length;
-      const end = start + incoming.length;
-      for (let i = start; i < end; ++i) {
-        merged[i] = incoming[i - start];
+      if (args) {
+        // Assume an offset of 0 if args.offset omitted.
+        const { offset = 0 } = args;
+        for (let i = 0; i < incoming.length; ++i) {
+          merged[offset + i] = incoming[i];
+        }
+      } else {
+        // It's unusual (probably a mistake) for a paginated field not
+        // to receive any arguments, so you might prefer to throw an
+        // exception here, instead of recovering by appending incoming
+        // onto the existing array.
+        merged.push.apply(merged, incoming);
       }
       return merged;
     },

2b8ee34c220c8f216fdaffaa540f52a07e9d9d9b
javier-garcia-meteologica <61506396+javier-garcia-meteologica@users.noreply.github.com>
2020-10-14 23:55:53 +0200

    Fix cleanup observer blocking unsubscribe (#7165)
    Fixes #6985.

--- src/utilities/observables/Concast.ts
+++ src/utilities/observables/Concast.ts
@@ -95,26 +95,34 @@ export class Concast<T> extends Observable<T> {
     this.handlers.complete!();
   }
 
+  private deliverLastMessage(observer: Observer<T>) {
+    if (this.latest) {
+      const nextOrError = this.latest[0];
+      const method = observer[nextOrError];
+      if (method) {
+        method.call(observer, this.latest[1]);
+      }
+      // If the subscription is already closed, and the last message was
+      // a 'next' message, simulate delivery of the final 'complete'
+      // message again.
+      if (this.sub === null &&
+          nextOrError === "next" &&
+          observer.complete) {
+        observer.complete();
+      }
+    }
+  }
+
+  // Note: cleanup observers do not count towards this total.
+  private addCount = 0;
+
   public addObserver(observer: Observer<T>) {
     if (!this.observers.has(observer)) {
       // Immediately deliver the most recent message, so we can always
       // be sure all observers have the latest information.
-      if (this.latest) {
-        const nextOrError = this.latest[0];
-        const method = observer[nextOrError];
-        if (method) {
-          method.call(observer, this.latest[1]);
-        }
-        // If the subscription is already closed, and the last message was
-        // a 'next' message, simulate delivery of the final 'complete'
-        // message again.
-        if (this.sub === null &&
-            nextOrError === "next" &&
-            observer.complete) {
-          observer.complete();
-        }
-      }
+      this.deliverLastMessage(observer);
       this.observers.add(observer);
+      ++this.addCount;
     }
   }
 
@@ -201,7 +209,13 @@ export class Concast<T> extends Observable<T> {
     const once = () => {
       if (!called) {
         called = true;
-        this.observers.delete(observer);
+        // If there have been no other (non-cleanup) observers added, pass
+        // true for the quietly argument, so the removal of the cleanup
+        // observer does not call this.sub.unsubscribe. If a cleanup
+        // observer is added and removed before any other observers
+        // subscribe, we do not want to prevent other observers from
+        // subscribing later.
+        this.removeObserver(observer, !this.addCount);
         callback();
       }
     }
@@ -210,7 +224,12 @@ export class Concast<T> extends Observable<T> {
       error: once,
       complete: once,
     };
+    const count = this.addCount;
     this.addObserver(observer);
+    // Normally addObserver increments this.addCount, but we can "hide"
+    // cleanup observers by restoring this.addCount to its previous value
+    // after adding any cleanup observer.
+    this.addCount = count;
   }
 
   // A public way to abort observation and broadcast.

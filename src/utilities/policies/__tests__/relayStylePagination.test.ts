27c7096316023cdc127c59a2a3b2a908b2ff3bd0
Beau Collins <beau@collins.pub>
2020-10-23 12:14:38 -0700

    test for cursors with empty edges

---
+++ src/utilities/policies/__tests__/relayStylePagination.test.ts
@@ -0,0 +1,50 @@
+import { FieldFunctionOptions, InMemoryCache, isReference } from '../../../core';
+import { relayStylePagination } from '../pagination';
+
+describe('relayStylePagination', () => {
+  const policy = relayStylePagination();
+
+  describe('merge', () => {
+
+    const merge = policy.merge;
+    // The merge function should exist, make TS aware
+    if (merge == null || typeof merge ===  'boolean') {
+      throw new Error('Expecting merge function');
+    }
+
+    const options: FieldFunctionOptions = {
+      args: null,
+      fieldName: 'fake',
+      storeFieldName: 'fake',
+      field: null,
+      isReference: isReference,
+      toReference: () => undefined,
+      storage: {},
+      cache: new InMemoryCache(),
+      readField: () => undefined,
+      canRead: () => false,
+      mergeObjects: (existing, _incoming) => existing,
+    }
+    it('should maintain endCursor and startCursor with empty edges', () => {
+      const incoming: Parameters<typeof merge>[1] = {
+        pageInfo: {
+          hasPreviousPage: false,
+          hasNextPage: true,
+          startCursor: 'abc',
+          endCursor: 'xyz',
+        }
+      }
+      const result = merge(undefined, incoming, options);
+      expect(result).toEqual({
+        edges: [],
+        pageInfo: {
+          hasPreviousPage: false,
+          hasNextPage: true,
+          startCursor: 'abc',
+          endCursor: 'xyz'
+        }
+      })
+    })
+
+  })
+});

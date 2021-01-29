7d76c7d23ad3abb2acb17bf4511b56e9b1a5d5a3
Ben Newman <ben@apollographql.com>
2021-01-29 17:31:15 -0500

    Cheapen process.env.NODE_ENV lookups in CommonJS bundles (#7627)

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -70,6 +70,27 @@ function prepareBundle({
       sourcemap: true,
       exports: 'named',
       externalLiveBindings: false,
+      // In Node.js, where these CommonJS bundles are most commonly used,
+      // the expression process.env.NODE_ENV can be very expensive to
+      // evaluate, because process.env is a wrapper for the actual OS
+      // environment, and lookups are not cached. We need to preserve the
+      // syntax of process.env.NODE_ENV expressions for dead code
+      // elimination to work properly, but we can apply our own caching by
+      // shadowing the global process variable with a stripped-down object
+      // that saves a snapshot of process.env.NODE_ENV when the bundle is
+      // first evaluated. If we ever need other process properties, we can
+      // add more stubs here.
+      intro: '!(function (process) {',
+      outro: [
+        '}).call(this, {',
+        '  env: {',
+        '    NODE_ENV: typeof process === "object"',
+        '      && process.env',
+        '      && process.env.NODE_ENV',
+        '      || "development"',
+        '  }',
+        '});',
+      ].join('\n'),
     },
     plugins: [
       extensions ? nodeResolve({ extensions }) : nodeResolve(),

f38e02ebf1ca4cc415a05f12074cfdefd70e49c5
Ben Newman <ben@apollographql.com>
2021-02-01 19:26:46 -0500

    Support subclassing Observable with non-class constructor functions.
    Now that the zen-observable-ts package has the ability to export
    Observable as a native class (#7615), we need to be careful when extending
    Observable using classes (like ObservableQuery and Concast) that have been
    compiled to ES5 constructor functions (rather than native classes),
    because the generated _super.call(this, subscriber) code throws when
    _super is a native class constructor (#7635).
    
    Rather than attempting to change the way the TypeScript compiler
    transforms super(subscriber) calls, this commit wraps Observable.call and
    Observable.apply to work as expected, by using Reflect.construct to invoke
    the superclass constructor correctly, when the Reflect API is available:
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Reflect/construct
    
    Another option would be to ship native class syntax with @apollo/client,
    by changing the "target" in tsconfig.json from "es5" to "es2015" or later,
    so that consumers of @apollo/client would be forced to compile native
    class syntax however they see fit. That would be a more disruptive change,
    in part because it would prevent subclassing Apollo Client-defined classes
    using anything other than native class syntax and/or the Reflect.construct
    API, which is the very same problem this commit is trying to fix for the
    Observable class.

--- src/utilities/observables/Observable.ts
+++ src/utilities/observables/Observable.ts
@@ -8,10 +8,37 @@ import {
 // proposal (https://github.com/zenparsing/es-observable)
 import 'symbol-observable';
 
+export type Subscriber<T> = ZenObservable.Subscriber<T>;
 export type {
   Observer,
   ObservableSubscription,
 };
+
+Observable.call = function<T>(
+  this: typeof Observable,
+  obs: Observable<T>,
+  sub: ZenObservable.Subscriber<T>,
+): Observable<T> {
+  return construct(this, obs, sub);
+};
+
+Observable.apply = function<T>(
+  this: typeof Observable,
+  obs: Observable<T>,
+  args: [ZenObservable.Subscriber<T>],
+): Observable<T> {
+  return construct(this, obs, args[0]);
+}
+
+function construct<T>(
+  Super: typeof Observable,
+  self: Observable<T>,
+  subscriber: ZenObservable.Subscriber<T>,
+): Observable<T> {
+  return typeof Reflect === 'object'
+    ? Reflect.construct(Super, [subscriber], self.constructor)
+    : Function.prototype.call.call(Super, self, subscriber);
+}
 
 // Use global module augmentation to add RxJS interop functionality. By
 // using this approach (instead of subclassing `Observable` and adding an

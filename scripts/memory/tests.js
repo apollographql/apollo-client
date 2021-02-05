ad20f88fb0704b4d0debf5d59137ea9d5645af9a
Ben Newman <ben@apollographql.com>
2021-02-05 18:53:50 -0500

    Convert standalone GC test to use Mocha.

---
+++ scripts/memory/tests.js
@@ -0,0 +1,54 @@
+const assert = require("assert");
+const {
+  ApolloClient,
+  InMemoryCache,
+  gql,
+  makeVar,
+} = require("@apollo/client");
+
+describe("garbage collection", () => {
+  it("should collect client.cache after client.stop() called", done => {
+    let timeout;
+    (function pollGC() {
+      gc(); // enabled by --expose-gc
+      timeout = setTimeout(pollGC, 100);
+    })();
+
+    const registry = new FinalizationRegistry(key => {
+      clearTimeout(timeout);
+      assert.strictEqual(key, "client.cache");
+      done();
+    });
+
+    const localVar = makeVar(123);
+
+    (function (client) {
+      registry.register(client.cache, "client.cache");
+
+      client.watchQuery({
+        query: gql`query { local }`,
+      }).subscribe({
+        next(result) {
+          assert.deepStrictEqual(result.data, {
+            local: 123,
+          });
+
+          client.stop();
+        },
+      });
+
+    })(new ApolloClient({
+      cache: new InMemoryCache({
+        typePolicies: {
+          Query: {
+            fields: {
+              local() {
+                return localVar();
+              },
+            },
+          },
+        },
+      }),
+    }));
+  });
+});

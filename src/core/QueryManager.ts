49b45e311987cb73b7be038c43acd9a761c6ac46
Ben Newman <ben@apollographql.com>
2021-03-15 12:50:01 -0400

    Revert "Merge pull request #7761 from apollographql/7611-preserve-no-cache-fetchpolicy"
    This reverts commit 319993eac4b5ef5fad82994c76e3e7fff6a30dcb, reversing
    changes made to 6120401181612e9245873cb4735d9fe0098e70a0.
    
    Because #7611 could be a visible change for some applications, we've
    decided to relocate these changes onto the release-3.4 branch, rather than
    main, so we revert the PR on main, merge main into release-3.4, then
    un-revert the PR on the release-3.4 branch.

--- src/core/QueryManager.ts
+++ src/core/QueryManager.ts
@@ -914,6 +914,7 @@ export class QueryManager<TStore> {
     const query = this.transform(options.query).document;
     const variables = this.getVariables(query, options.variables) as TVars;
     const queryInfo = this.getQuery(queryId);
+    const oldNetworkStatus = queryInfo.networkStatus;
 
     let {
       fetchPolicy = "cache-first" as WatchQueryFetchPolicy,
@@ -922,6 +923,26 @@ export class QueryManager<TStore> {
       notifyOnNetworkStatusChange = false,
       context = {},
     } = options;
+
+    const mightUseNetwork =
+      fetchPolicy === "cache-first" ||
+      fetchPolicy === "cache-and-network" ||
+      fetchPolicy === "network-only" ||
+      fetchPolicy === "no-cache";
+
+    if (mightUseNetwork &&
+        notifyOnNetworkStatusChange &&
+        typeof oldNetworkStatus === "number" &&
+        oldNetworkStatus !== networkStatus &&
+        isNetworkRequestInFlight(networkStatus)) {
+      // In order to force delivery of an incomplete cache result with
+      // loading:true, we tweak the fetchPolicy to include the cache, and
+      // pretend that returnPartialData was enabled.
+      if (fetchPolicy !== "cache-first") {
+        fetchPolicy = "cache-and-network";
+      }
+      returnPartialData = true;
+    }
 
     const normalized = Object.assign({}, options, {
       query,
@@ -1017,10 +1038,7 @@ export class QueryManager<TStore> {
       errorPolicy,
       returnPartialData,
       context,
-      notifyOnNetworkStatusChange,
     } = options;
-
-    const oldNetworkStatus = queryInfo.networkStatus;
 
     queryInfo.init({
       document: query,
@@ -1074,13 +1092,6 @@ export class QueryManager<TStore> {
         errorPolicy,
       });
 
-    const shouldNotifyOnNetworkStatusChange = () => (
-      notifyOnNetworkStatusChange &&
-      typeof oldNetworkStatus === "number" &&
-      oldNetworkStatus !== networkStatus &&
-      isNetworkRequestInFlight(networkStatus)
-    );
-
     switch (fetchPolicy) {
     default: case "cache-first": {
       const diff = readCache();
@@ -1098,13 +1109,6 @@ export class QueryManager<TStore> {
         ];
       }
 
-      if (shouldNotifyOnNetworkStatusChange()) {
-        return [
-          resultsFromCache(diff),
-          resultsFromLink(true),
-        ];
-      }
-
       return [
         resultsFromLink(true),
       ];
@@ -1113,7 +1117,7 @@ export class QueryManager<TStore> {
     case "cache-and-network": {
       const diff = readCache();
 
-      if (diff.complete || returnPartialData || shouldNotifyOnNetworkStatusChange()) {
+      if (diff.complete || returnPartialData) {
         return [
           resultsFromCache(diff),
           resultsFromLink(true),
@@ -1131,22 +1135,9 @@ export class QueryManager<TStore> {
       ];
 
     case "network-only":
-      if (shouldNotifyOnNetworkStatusChange()) {
-        const diff = readCache();
-
-        return [
-          resultsFromCache(diff),
-          resultsFromLink(true),
-        ];
-      }
-
       return [resultsFromLink(true)];
 
     case "no-cache":
-      if (shouldNotifyOnNetworkStatusChange()) {
-          return [resultsFromCache(queryInfo.getDiff()), resultsFromLink(false)];
-      }
-
       return [resultsFromLink(false)];
 
     case "standby":

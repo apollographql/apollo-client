219004058593adc83cd8fe3e3c03a3c957090954
Ben Newman <ben@apollographql.com>
2021-04-02 14:10:41 -0400

    Failing test of RenderPromises QueryInfo garbage collection.
    This is my best guess at a reproduction of issue #7942, given the heap
    snapshot traces provided by @AlexMost.

--- scripts/memory/tests.js
+++ scripts/memory/tests.js
@@ -145,4 +145,96 @@ describe("garbage collection", () => {
       });
     })();
   });
+
+  itAsync("getMarkupFromTree and RenderPromises", (resolve, reject) => {
+    const {
+      createElement,
+    } = require("react");
+    assert.strictEqual(typeof createElement, "function");
+
+    const {
+      useQuery,
+      useApolloClient,
+      getApolloContext,
+    } = require("@apollo/client/react");
+    assert.strictEqual(typeof useQuery, "function");
+    assert.strictEqual(typeof useApolloClient, "function");
+    assert.strictEqual(typeof getApolloContext, "function");
+
+    const {
+      getDataFromTree,
+      RenderPromises,
+    } = require("@apollo/client/react/ssr");
+    assert.strictEqual(typeof getDataFromTree, "function");
+
+    const expectedKeys = new Set(["cache", "queryInfo1"]);
+    const renderPromisesSet = new Set;
+    const registry = makeRegistry(key => {
+      // By retaining the RenderPromises object in a Set in the scope of
+      // this callback function, we artificially ensure the RenderPromises
+      // won't be garbage collected before this function runs, so we can
+      // verify that renderPromises.clear() was called by getDataFromTree.
+      assert.strictEqual(renderPromisesSet.size, 1);
+
+      if (expectedKeys.delete(key) && !expectedKeys.size) {
+        resolve();
+      }
+    }, reject);
+
+    const query = gql`query { __typename }`;
+
+    function Component() {
+      const client = useApolloClient();
+      assert.strictEqual(client instanceof ApolloClient, true);
+
+      const { loading, data } = useQuery(query, {
+        fetchPolicy: "cache-only",
+      });
+
+      if (loading || !data) {
+        return "loading...";
+      }
+
+      registry.register(client.cache, "cache");
+      // Register any/all watched ObservableQuery objects with the registry.
+      client.queryManager.queries.forEach((queryInfo, queryId) => {
+        registry.register(queryInfo, "queryInfo" + queryId);
+      });
+
+      const ApolloContext = getApolloContext();
+      return createElement(
+        ApolloContext.Consumer,
+        null,
+        context => {
+          assert.ok(
+            context.renderPromises instanceof RenderPromises,
+            context.renderPromises,
+          );
+          // This keeps the RenderPromises object alive artificially so we can
+          // verify that it is properly cleared.
+          renderPromisesSet.add(context.renderPromises);
+          return createElement("code", null, JSON.stringify(data));
+        },
+      );
+    }
+
+    const tree = createElement(Component);
+
+    (function () {
+      const client = new ApolloClient({
+        cache: new InMemoryCache,
+        ssrMode: true,
+      });
+
+      getDataFromTree(tree, {
+        client,
+      }).then(html => {
+        assert.strictEqual(
+          html,
+          '<code>{&quot;__typename&quot;:&quot;Query&quot;}</code>',
+        );
+        client.stop();
+      }).catch(reject);
+    })();
+  });
 });

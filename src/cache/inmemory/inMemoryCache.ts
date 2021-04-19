4b66ff286669240b5257bc2c3638e1ac241cb1dc
Ben Newman <ben@apollographql.com>
2021-04-19 11:45:17 -0400

    Use deep equality check before calling watch.callback.
    Similar to #7997, but further upstream.

--- src/cache/inmemory/inMemoryCache.ts
+++ src/cache/inmemory/inMemoryCache.ts
@@ -3,6 +3,7 @@ import './fixPolyfills';
 
 import { DocumentNode } from 'graphql';
 import { OptimisticWrapperFunction, wrap } from 'optimism';
+import { equal } from '@wry/equality';
 
 import { ApolloCache, BatchOptions } from '../core/cache';
 import { Cache } from '../core/types/Cache';
@@ -498,7 +499,7 @@ export class InMemoryCache extends ApolloCache<NormalizedCacheObject> {
       }
     }
 
-    if (!lastDiff || lastDiff.result !== diff.result) {
+    if (!lastDiff || !equal(lastDiff.result, diff.result)) {
       c.callback(c.lastDiff = diff, lastDiff);
     }
   }

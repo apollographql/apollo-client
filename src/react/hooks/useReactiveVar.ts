9f078f043fa86e96262bc02174ef2141455c87ac
Ben Newman <ben@apollographql.com>
2021-05-04 17:19:20 -0400

    Recheck rv() against previous value in useIsomorphicEffect.
    While rereading the code @jcreighton added in #7652, I remembered it's
    safe to call rv() in a useEffect callback (without useReactiveVar),
    which allows us to compare the latest variable value to the previous
    value before deciding whether to call rv.onNextChange(setValue) in the
    effect callback function. If the values already disagree, we can skip
    listening and just call setValue right away.
    
    In React <StrictMode>, the two initial rv() values generated prior to
    the useIsomorphicEffect callback should always be the same, unless the
    reactive variable somehow gets updated in between those renders, which I
    don't think is possible, since React invokes those renders sychronously,
    one after the other:
    https://github.com/facebook/react/blob/0e100ed00fb52cfd107db1d1081ef18fe4b9167f/packages/react-reconciler/src/ReactUpdateQueue.new.js#L391-L399
    
    The key difference between this approach and calling rv.onNextChange
    synchronously in render is that (in this new approach) we can ignore the
    extra value without doing anything to clean up its resources, whereas
    with rv.onNextChange we were also responsible for calling the extra
    cancel function produced by the extra rv.onNextChange call.

--- src/react/hooks/useReactiveVar.ts
+++ src/react/hooks/useReactiveVar.ts
@@ -1,7 +1,6 @@
 import { useEffect, useLayoutEffect, useState } from 'react';
 import { ReactiveVar } from '../../core';
 
-
 const isBrowser = typeof window !== 'undefined' &&
   typeof window.document !== 'undefined' &&
   typeof window.document.createElement !== 'undefined';
@@ -10,21 +9,24 @@ const useIsomorphicEffect = isBrowser ? useLayoutEffect : useEffect;
 
 export function useReactiveVar<T>(rv: ReactiveVar<T>): T {
   const value = rv();
+
   // We don't actually care what useState thinks the value of the variable
   // is, so we take only the update function from the returned array.
   const [, setValue] = useState(value);
+
   // We subscribe to variable updates on initial mount and when the value has
   // changed. This avoids a subtle bug in React.StrictMode where multiple listeners
   // are added, leading to inconsistent updates.
-  useIsomorphicEffect(() => rv.onNextChange(setValue), [value]);
-  // Once the component is unmounted, ignore future updates. Note that the
-  // above useEffect function returns a mute function without calling it,
-  // allowing it to be called when the component unmounts. This is
-  // equivalent to the following, but shorter:
-  // useEffect(() => {
-  //   const mute = rv.onNextChange(setValue);
-  //   return () => mute();
-  // }, [value])
+  useIsomorphicEffect(() => {
+    const probablySameValue = rv();
+    if (value !== probablySameValue) {
+      // If the value of rv has already changed, we don't need to listen for the
+      // next change, because we can report this change immediately.
+      setValue(probablySameValue);
+    } else {
+      return rv.onNextChange(setValue);
+    }
+  }, [value]);
 
   // We check the variable's value in this useEffect and schedule an update if
   // the value has changed. This check occurs once, on the initial render, to avoid

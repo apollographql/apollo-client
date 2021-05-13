4fbeade3dd0e1cc6cb31ccfc3661a08d9b66bdee
Brian Kim <briankimpossible@gmail.com>
2021-05-13 13:03:02 -0400

    inline parseAndCheckHttpResponse nonsense

--- src/link/http/HttpLink.ts
+++ src/link/http/HttpLink.ts
@@ -1,8 +1,7 @@
-import { ApolloLink, RequestHandler } from '../core';
+import { ApolloLink, Operation, RequestHandler } from '../core';
 import { visit, DefinitionNode, VariableDefinitionNode } from 'graphql';
-import { Observable } from '../../utilities';
+import { Observable, Observer } from '../../utilities';
 import { serializeFetchParameter } from './serializeFetchParameter';
-import { parseAndCheckHttpResponse } from './parseAndCheckHttpResponse';
 import { checkFetcher } from './checkFetcher';
 import { selectURI } from './selectURI';
 import {
@@ -12,10 +11,183 @@ import {
 } from './selectHttpOptionsAndBody';
 import { createSignalIfSupported } from './createSignalIfSupported';
 import { rewriteURIForGET } from './rewriteURIForGET';
-import { fromError } from '../utils';
+import { fromError, throwServerError } from '../utils';
 import { maybe } from '../../utilities';
 
 const backupFetch = maybe(() => fetch);
+const { hasOwnProperty } = Object.prototype;
+
+function parseMultipartBoundary(contentType: string) {
+  const boundary = contentType.split('boundary=')[1];
+  if (boundary) {
+    return boundary.replace(/^('|")/, '').replace(/('|")$/, '');
+  }
+
+  return '-';
+}
+
+function parseHeaders(headerText: string): Headers {
+  const headersInit: Record<string, string> = {};
+  headerText.split('\n').forEach(line => {
+    const i = line.indexOf(':');
+    if (i > -1) {
+      const name = line.slice(0, i).trim();
+      const value = line.slice(i + 1).trim();
+      headersInit[name] = value;
+    }
+  });
+
+  return new Headers(headersInit);
+}
+
+export type ServerParseError = Error & {
+  response: Response;
+  statusCode: number;
+  bodyText: string;
+};
+
+// TODO: better return type
+function parseJSONBody(response: Response, bodyText: string): any {
+  try {
+    return JSON.parse(bodyText);
+  } catch (err) {
+    const parseError = err as ServerParseError;
+    parseError.name = 'ServerParseError';
+    parseError.response = response;
+    parseError.statusCode = response.status;
+    parseError.bodyText = bodyText;
+    throw parseError;
+  }
+}
+
+function handleError(err: any, observer: Observer<any>) {
+  if (err.name === 'AbortError') return;
+  // if it is a network error, BUT there is graphql result info fire
+  // the next observer before calling error this gives apollo-client
+  // (and react-apollo) the `graphqlErrors` and `networErrors` to
+  // pass to UI this should only happen if we *also* have data as
+  // part of the response key per the spec
+  if (err.result && err.result.errors && err.result.data) {
+    // if we don't call next, the UI can only show networkError
+    // because AC didn't get any graphqlErrors this is graphql
+    // execution result info (i.e errors and possibly data) this is
+    // because there is no formal spec how errors should translate to
+    // http status codes. So an auth error (401) could have both data
+    // from a public field, errors from a private field, and a status
+    // of 401
+    // {
+    //  user { // this will have errors
+    //    firstName
+    //  }
+    //  products { // this is public so will have data
+    //    cost
+    //  }
+    // }
+    //
+    // the result of above *could* look like this:
+    // {
+    //   data: { products: [{ cost: "$10" }] },
+    //   errors: [{
+    //      message: 'your session has timed out',
+    //      path: []
+    //   }]
+    // }
+    // status code of above would be a 401
+    // in the UI you want to show data where you can, errors as data where you can
+    // and use correct http status codes
+    observer.next?.(err.result);
+  }
+
+  observer.error?.(err);
+}
+
+// TODO: naming
+function readJSONBody(
+  response: Response,
+  operation: Operation,
+  observer: Observer<any>,
+) {
+  response.text()
+    .then(bodyText => parseJSONBody(response, bodyText))
+    .then((result: any) => {
+      if (response.status >= 300) {
+        // Network error
+        throwServerError(
+          response,
+          result,
+          `Response not successful: Received status code ${response.status}`,
+        );
+      }
+
+      if (
+        !Array.isArray(result) &&
+        !hasOwnProperty.call(result, 'data') &&
+        !hasOwnProperty.call(result, 'errors')
+      ) {
+        // Data error
+        throwServerError(
+          response,
+          result,
+          `Server response was missing for query '${operation.operationName}'.`,
+        );
+      }
+
+      observer.next?.(result);
+      observer.complete?.();
+    })
+    .catch(err => handleError(err, observer));
+}
+
+function readMultipartBody(
+  response: Response,
+  boundary: string,
+  observer: Observer<any>,
+) {
+  // TODO: does this have to polyfilled? If there is a body, then there is likely a decoder.
+  const decoder = new TextDecoder('utf8');
+  let buffer = '';
+  const messageBoundary = '--' + boundary;
+  // TODO: End message boundary
+  const reader = (response.body as ReadableStream<Uint8Array>).getReader();
+  (function read() {
+    reader.read()
+      .then(iteration => {
+        if (iteration.done) {
+          return;
+        }
+
+        const chunk = decoder.decode(iteration.value);
+        buffer += chunk;
+
+        let bi = buffer.indexOf(messageBoundary);
+        while (bi > -1) {
+          let message: string;
+          [message, buffer] = [
+            buffer.slice(0, bi),
+            buffer.slice(bi + messageBoundary.length),
+          ];
+
+          if (message.trim()) {
+            const i = message.indexOf("\r\n\r\n");
+            const headers = parseHeaders(message.slice(0, i));
+            const contentType = headers.get('content-type');
+            if (contentType !== null && contentType.indexOf('application/json') === -1) {
+              throw new Error('Unsupported patch content type');
+            }
+
+            const body = message.slice(i);
+            const result = parseJSONBody(response, body);
+            observer.next?.(result);
+          }
+
+          bi = buffer.indexOf(messageBoundary);
+        }
+
+        read();
+      })
+      .catch(err => handleError(err, observer));
+  })();
+}
 
 export class HttpLink extends ApolloLink {
   public requester: RequestHandler;
@@ -148,55 +320,26 @@ export class HttpLink extends ApolloLink {
         currentFetch!(chosenURI, options)
           .then(response => {
             operation.setContext({ response });
-            return response;
-          })
-          .then(parseAndCheckHttpResponse(operation))
-          .then(result => {
-            // we have data and can send it to back up the link chain
-            observer.next(result);
-            observer.complete();
-            return result;
-          })
-          .catch(err => {
-            // fetch was cancelled so it's already been cleaned up in the unsubscribe
-            if (err.name === 'AbortError') return;
-            // if it is a network error, BUT there is graphql result info fire
-            // the next observer before calling error this gives apollo-client
-            // (and react-apollo) the `graphqlErrors` and `networErrors` to
-            // pass to UI this should only happen if we *also* have data as
-            // part of the response key per the spec
-            if (err.result && err.result.errors && err.result.data) {
-              // if we don't call next, the UI can only show networkError
-              // because AC didn't get any graphqlErrors this is graphql
-              // execution result info (i.e errors and possibly data) this is
-              // because there is no formal spec how errors should translate to
-              // http status codes. So an auth error (401) could have both data
-              // from a public field, errors from a private field, and a status
-              // of 401
-              // {
-              //  user { // this will have errors
-              //    firstName
-              //  }
-              //  products { // this is public so will have data
-              //    cost
-              //  }
-              // }
-              //
-              // the result of above *could* look like this:
-              // {
-              //   data: { products: [{ cost: "$10" }] },
-              //   errors: [{
-              //      message: 'your session has timed out',
-              //      path: []
-              //   }]
-              // }
-              // status code of above would be a 401
-              // in the UI you want to show data where you can, errors as data where you can
-              // and use correct http status codes
-              observer.next(err.result);
+            const contentType = response.headers?.get('content-type');
+            if (contentType !== null && /^multipart\/mixed/.test(contentType)) {
+              const boundary = parseMultipartBoundary(contentType!);
+              // TODO: use ducktyping rather than instanceof
+              if (response.body instanceof ReadableStream) {
+                readMultipartBody(response, boundary, observer);
+              // TODO: detect and handle node streams
+              } else if (response.body) {
+                throw new Error('TODO');
+              }
+
+              throw new Error(
+                'Streaming bodies not supported by provided fetch implementation',
+              );
+            } else {
+              readJSONBody(response, operation, observer);
             }
-            observer.error(err);
-          });
+          })
+          // TODO: handle fetch errors closer to the fetcher stuff
+          .catch(err => handleError(err, observer));
 
         return () => {
           if (controller) controller.abort();

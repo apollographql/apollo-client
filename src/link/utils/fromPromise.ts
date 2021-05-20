6ab0046b195917b46a1878106ee0799eefdf1cd2
Brian Kim <briankimpossible@gmail.com>
2021-05-20 03:05:55 -0400

    get rid of a superfluous catch

--- src/link/utils/fromPromise.ts
+++ src/link/utils/fromPromise.ts
@@ -2,11 +2,12 @@ import { Observable } from '../../utilities';
 
 export function fromPromise<T>(promise: Promise<T>): Observable<T> {
   return new Observable<T>(observer => {
-    promise
-      .then((value: T) => {
+    promise.then(
+      (value: T) => {
         observer.next(value);
         observer.complete();
-      })
-      .catch(observer.error.bind(observer));
+      },
+      (err) => observer.error(err),
+    );
   });
 }

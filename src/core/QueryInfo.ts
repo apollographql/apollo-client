50742b2b5e0a50f5ee6dba078ec1c42692703de1
Brian Kim <briankimpossible@gmail.com>
2021-05-24 13:04:40 -0400

    explicitly specify return type of QueryInfo.markReady()

--- src/core/QueryInfo.ts
+++ src/core/QueryInfo.ts
@@ -444,7 +444,7 @@ export class QueryInfo {
     }
   }
 
-  public markReady() {
+  public markReady(): NetworkStatus {
     this.networkError = null;
     return this.networkStatus = NetworkStatus.ready;
   }

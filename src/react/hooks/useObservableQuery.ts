e36de812a92358ec18d9adb2e7fb13f4cfd6cf5c
Ben Newman <ben@apollographql.com>
2021-06-10 11:19:46 -0400

    Return use{Loading,NetworkStatus,Error,Data} hooks from useObservableQuery.
    This design lets the component selectively depend on ObservableQuery
    metadata that it cares about, rather than always rerendering the
    component whenever any item of that metadata has been updated.
    
    For example, the useLoading hook uses useReactiveVar to read from
    result.loadingVar, which is kept in sync with the result.loading
    properties of any results received by the ObservableQuery. Likewise for
    useNetworkStatus, useError, and useData, though useData is especially
    discouraged because it defeats the main purpose of useObservableQuery,
    which is to avoid rerendering the component tree every time anything
    triggers a change in result.data.
    
    The ObservableQuery itself is now returned as the result.observable
    property from useObservableQuery, which gives us room to add additional
    sibling properties in the future.

--- src/react/hooks/useObservableQuery.ts
+++ src/react/hooks/useObservableQuery.ts
@@ -1,65 +1,137 @@
 import { useRef, useEffect } from "react";
-import { invariant } from "ts-invariant";
 import equal from "@wry/equality";
 
 import {
-  ApolloQueryResult,
-  mergeOptions,
-  ObservableQuery,
   WatchQueryOptions,
+  ObservableQuery,
+  NetworkStatus,
+  mergeOptions,
+  makeVar,
+  ReactiveVar,
+  ApolloError,
 } from "../../core";
-import { cloneDeep, Observer } from "../../utilities";
-import { useApolloClient } from "./useApolloClient";
 
-function defaultErrorHandler(error: any) {
-  invariant.error("in useObservableQuery default error handler:", error);
-}
+import { useApolloClient } from "./useApolloClient";
+import { useReactiveVar } from "./useReactiveVar";
 
 export function useObservableQuery<TData, TVars>(
   queryOrOptions:
     | ObservableQuery<TData, TVars>
     | WatchQueryOptions<TVars, TData>,
-  observer?: Observer<ApolloQueryResult<TData>>,
-): ObservableQuery<TData, TVars> {
-  const client = useApolloClient();
-
-  const oqOptionsRef = useRef<{
-    oq: ObservableQuery<TData, TVars>;
-    options: WatchQueryOptions<TVars, TData>;
-  }>();
-
-  const { oq, options } = oqOptionsRef.current ||
-    (oqOptionsRef.current = queryOrOptions instanceof ObservableQuery ? {
-      oq: queryOrOptions,
-      options: queryOrOptions.options,
-    } : {
-      oq: client.watchQuery(queryOrOptions),
-      // Store the initial options we passed to useObservableQuery, so we can
-      // compare future queryOrOptions objects against those options, to decide
-      // whether we need to call oq.setOptions in useEffect, below.
-      options: cloneDeep(queryOrOptions),
-    });
+): UseObservableQueryResult<TData, TVars> {
+  const state = useInternalState(queryOrOptions);
+  const { observable } = state;
 
   useEffect(() => {
     if (
-      !(queryOrOptions instanceof ObservableQuery) &&
-      !equal(options, queryOrOptions)
+      !isObservableQuery(queryOrOptions) &&
+      !equal(state.options, queryOrOptions)
     ) {
-      // TODO Take the differences between options and queryOrOptions and merge
-      // them into oq.options, somehow.
-      oq.setOptions(mergeOptions(
-        oq.options,
-        oqOptionsRef.current!.options = queryOrOptions,
+      observable.setOptions(mergeOptions(
+        observable.options,
+        state.options = queryOrOptions,
       ));
     }
 
-    const sub = oq.subscribe({
-      error: defaultErrorHandler,
-      ...observer,
+    const sub = observable.subscribe({
+      next(result) {
+        state.loadingVar(result.loading);
+        state.networkStatusVar(result.networkStatus);
+        state.errorVar(result.error);
+        state.dataVar(result.data);
+      },
+      error(error) {
+        state.loadingVar(false);
+        state.networkStatusVar(NetworkStatus.error);
+        state.errorVar(error);
+        // Intentionally not clearing state.dataVar, since it may still be
+        // useful even though there's been an error.
+        // state.dataVar(void 0);
+      },
     });
 
     return () => sub.unsubscribe();
-  }, [oq]);
+  }, [state]);
 
-  return oq;
+  return state;
+}
+
+export interface UseObservableQueryResult<TData, TVars> {
+  observable: ObservableQuery<TData, TVars>;
+  // By returning hook functions that the component can choose to call (or not),
+  // useObservableQuery is technically a "higher-order hook," in the same way a
+  // function that returns other functions is a higher-order function.
+  useLoading(): boolean;
+  useNetworkStatus(): NetworkStatus;
+  useError(): ApolloError | undefined;
+  useData(): TData | undefined;
+}
+
+interface InternalState<TData, TVars>
+extends UseObservableQueryResult<TData, TVars> {
+  options: WatchQueryOptions<TVars, TData>;
+  loadingVar: ReactiveVar<boolean>;
+  networkStatusVar: ReactiveVar<NetworkStatus>;
+  errorVar: ReactiveVar<ApolloError | undefined>;
+  dataVar: ReactiveVar<TData | undefined>;
+}
+
+function useInternalState<TData, TVars>(
+  queryOrOptions:
+    | ObservableQuery<TData, TVars>
+    | WatchQueryOptions<TVars, TData>,
+): InternalState<TData, TVars> {
+  const client = useApolloClient();
+  const ref = useRef<InternalState<TData, TVars>>();
+  return ref.current || (
+    ref.current = isObservableQuery(queryOrOptions)
+      ? internalStateFromObservableQuery(queryOrOptions)
+      : internalStateFromOptions(client, queryOrOptions)
+  );
+}
+
+function isObservableQuery(value: any): value is ObservableQuery<any, any> {
+  return value instanceof ObservableQuery;
+}
+
+function makeSharedVars<TData, TVars>(
+  observable: ObservableQuery<TData, TVars>,
+) {
+  const result = observable.getCurrentResult();
+  const loadingVar = makeVar(result.loading);
+  const networkStatusVar = makeVar(result.networkStatus);
+  const errorVar = makeVar(result.error);
+  const dataVar = makeVar(result.data);
+
+  return {
+    loadingVar,
+    networkStatusVar,
+    errorVar,
+    dataVar,
+
+    useLoading: () => useReactiveVar(loadingVar),
+    useNetworkStatus: () => useReactiveVar(networkStatusVar),
+    useError: () => useReactiveVar(errorVar),
+    useData: () => useReactiveVar(dataVar),
+  };
+}
+
+function internalStateFromObservableQuery<TData, TVars>(
+  observable: ObservableQuery<TData, TVars>,
+): InternalState<TData, TVars> {
+  return Object.assign(makeSharedVars(observable), {
+    observable: observable,
+    options: observable.options,
+  });
+}
+
+function internalStateFromOptions<TData, TVars>(
+  client: ReturnType<typeof useApolloClient>,
+  options: WatchQueryOptions<TVars, TData>,
+): InternalState<TData, TVars> {
+  const observable = client.watchQuery(options);
+  return Object.assign(makeSharedVars(observable), {
+    observable,
+    options,
+  });
 }

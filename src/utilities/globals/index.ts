fcb2c7d34f0da8c5a41da162ee0560800cf4eb66
Ben Newman <ben@apollographql.com>
2021-06-15 10:29:14 -0400

    Temporarily polyfill global.process while importing graphql package.
    As explained in https://github.com/apollographql/apollo-client/pull/8347#issue-661254832
    
    We can revert this commit when no supported version of the `graphql`
    package still uses process.env.NODE_ENV.

--- src/utilities/globals/index.ts
+++ src/utilities/globals/index.ts
@@ -1,1 +1,12 @@
-export { default as __DEV__ } from "./__DEV__";
+// Just in case the graphql package switches from process.env.NODE_ENV to
+// __DEV__, make sure __DEV__ is polyfilled before importing graphql.
+import { default as __DEV__ } from "./__DEV__";
+export { __DEV__ }
+
+// Import graphql/jsutils/instanceOf safely, working around its unchecked usage
+// of process.env.NODE_ENV and https://github.com/graphql/graphql-js/pull/2894.
+import { applyFixes } from "./graphql";
+
+// Synchronously undo the global process.env.NODE_ENV polyfill that we created
+// temporarily while importing the offending graphql/jsutils/instanceOf module.
+applyFixes();

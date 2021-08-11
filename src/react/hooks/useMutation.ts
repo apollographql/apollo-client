7365598122a9ba65879f85d9515af19eb79708b8
Brian Kim <briankimpossible@gmail.com>
2021-08-11 15:34:43 -0400

    refactor useMutation to not use MutationData

--- src/react/hooks/useMutation.ts
+++ src/react/hooks/useMutation.ts
@@ -1,19 +1,23 @@
-import { useContext, useEffect, useRef, useState } from 'react';
+import {
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import { DocumentNode } from 'graphql';
 import { TypedDocumentNode } from '@graphql-typed-document-node/core';
 import { invariant } from 'ts-invariant';
 
 import {
-  MutationDataOptions,
   MutationFunctionOptions,
   MutationHookOptions,
   MutationResult,
-  MutationTuple
+  MutationTuple,
 } from '../types/types';
 
 import {
   ApolloCache,
-  ApolloClient,
   DefaultContext,
   mergeOptions,
   OperationVariables,
@@ -24,137 +28,6 @@ import { DocumentType, verifyDocumentType } from '../parser';
 import { ApolloError } from '../../errors';
 import { FetchResult } from '../../link/core';
 
-type MutationResultWithoutClient<TData = any> = Omit<MutationResult<TData>, 'client'>;
-
-class MutationData<
-  TData = any,
-  TVariables = OperationVariables,
-  TContext = DefaultContext,
-  TCache extends ApolloCache<any> = ApolloCache<any>,
-> {
-  private mostRecentMutationId: number;
-  private result: MutationResultWithoutClient<TData>;
-  private previousResult?: MutationResultWithoutClient<TData>;
-  private setResult: (result: MutationResultWithoutClient<TData>) => any;
-  private isMounted: boolean;
-  public client: ApolloClient<unknown>;
-  public options: MutationDataOptions<TData, TVariables, TContext, TCache>;
-  constructor({
-    options,
-    client,
-    result,
-    setResult
-  }: {
-    client: ApolloClient<unknown>,
-    options: MutationDataOptions<TData, TVariables, TContext, TCache>;
-    result: MutationResultWithoutClient<TData>;
-    setResult: (result: MutationResultWithoutClient<TData>) => any;
-  }) {
-    this.result = result;
-    this.setResult = setResult;
-    this.mostRecentMutationId = 0;
-    this.isMounted = false;
-    this.client = client;
-    this.options = options;
-  }
-
-  public execute(result: MutationResultWithoutClient<TData>): MutationTuple<TData, TVariables, TContext, TCache> {
-    this.isMounted = true;
-    return [
-      this.runMutation,
-      { ...result, client: this.client }
-    ] as MutationTuple<TData, TVariables, TContext, TCache>;
-  }
-
-  public afterExecute() {
-    this.isMounted = true;
-    return () => {
-      this.isMounted = false;
-    };
-  }
-
-  private runMutation = (
-    mutationFunctionOptions: MutationFunctionOptions<
-      TData,
-      TVariables,
-      TContext,
-      TCache
-    > = {},
-  ) => {
-    if (!this.result.loading && !this.options.ignoreResults) {
-      this.updateResult({
-        loading: true,
-        error: undefined,
-        data: undefined,
-        called: true
-      });
-    }
-
-    const mutationId = ++this.mostRecentMutationId;
-    const options = mergeOptions(
-      this.options,
-      mutationFunctionOptions as any,
-    );
-
-    return this.client.mutate(options)
-      .then((response: FetchResult<TData>) => {
-        const { onCompleted, ignoreResults } = this.options;
-        const { data, errors } = response;
-        const error =
-          errors && errors.length > 0
-            ? new ApolloError({ graphQLErrors: errors })
-            : undefined;
-
-        if (this.mostRecentMutationId === mutationId && !ignoreResults) {
-          this.updateResult({
-            called: true,
-            loading: false,
-            data,
-            error
-          });
-        }
-
-        if (onCompleted) {
-          onCompleted(data!);
-        }
-
-        return response;
-      })
-      .catch((error: ApolloError) => {
-        if (this.mostRecentMutationId === mutationId) {
-          this.updateResult({
-            loading: false,
-            error,
-            data: undefined,
-            called: true
-          });
-        }
-
-        const { onError } = this.options;
-        if (onError) {
-          onError(error);
-          return {
-            data: undefined,
-            errors: error,
-          };
-        }
-
-        throw error;
-      });
-  };
-
-  private updateResult(result: MutationResultWithoutClient<TData>): MutationResultWithoutClient<TData> | undefined {
-    if (
-      this.isMounted &&
-      (!this.previousResult || !equal(this.previousResult, result))
-    ) {
-      this.setResult(result);
-      this.previousResult = result;
-      return result;
-    }
-  }
-}
-
 export function useMutation<
   TData = any,
   TVariables = OperationVariables,
@@ -162,10 +35,10 @@ export function useMutation<
   TCache extends ApolloCache<any> = ApolloCache<any>,
 >(
   mutation: DocumentNode | TypedDocumentNode<TData, TVariables>,
-  hookOptions?: MutationHookOptions<TData, TVariables, TContext>
+  options?: MutationHookOptions<TData, TVariables, TContext>,
 ): MutationTuple<TData, TVariables, TContext, TCache> {
   const context = useContext(getApolloContext());
-  const client = hookOptions?.client || context.client;
+  const client = options?.client || context.client;
   invariant(
     !!client,
     'Could not find "client" in the context or passed in as an option. ' +
@@ -173,28 +46,101 @@ export function useMutation<
     'ApolloClient instance in via options.',
   );
   verifyDocumentType(mutation, DocumentType.Mutation);
+  const [result, setResult] = useState<MutationResult>({
+    called: false,
+    loading: false,
+    client,
+  });
 
-  const [result, setResult] = useState({ called: false, loading: false });
-  const options = { ...hookOptions, mutation };
+  const ref = useRef({
+    result,
+    mutationId: 0,
+    isMounted: true,
+  });
 
-  const mutationDataRef = useRef<MutationData<TData, TVariables, TContext>>();
-  function getMutationDataRef() {
-    if (!mutationDataRef.current) {
-      mutationDataRef.current = new MutationData<TData, TVariables, TContext>({
-        client: client!,
-        options,
-        result,
-        setResult
+  const execute = useCallback((
+    executeOptions: MutationFunctionOptions<
+      TData,
+      TVariables,
+      TContext,
+      TCache
+    > = {},
+  ) => {
+    if (!ref.current.result.loading && !options?.ignoreResults) {
+      setResult(ref.current.result = {
+        loading: true,
+        error: void 0,
+        data: void 0,
+        called: true,
+        client,
       });
     }
 
-    return mutationDataRef.current;
-  }
+    const mutationId = ++ref.current.mutationId;
+    const clientOptions = mergeOptions(
+      { ...options, mutation },
+      executeOptions as any,
+    );
+    return client.mutate(clientOptions)
+      .then((response: FetchResult<TData>) => {
+        const { data, errors } = response;
+        const error =
+          errors && errors.length > 0
+            ? new ApolloError({ graphQLErrors: errors })
+            : void 0;
 
-  const mutationData = getMutationDataRef();
-  mutationData.options = options;
-  mutationData.client = client;
-  useEffect(() => mutationData.afterExecute());
+        if (mutationId === ref.current.mutationId && !options?.ignoreResults) {
+          const result = {
+            called: true,
+            loading: false,
+            data,
+            error,
+            client,
+          };
 
-  return mutationData.execute(result);
+          if (
+            ref.current.isMounted &&
+            !equal(ref.current.result, result)
+          ) {
+            ref.current.result = result;
+            setResult(result);
+          }
+        }
+
+        options?.onCompleted?.(data!);
+        return response;
+      }).catch((error) => {
+        if (mutationId === ref.current.mutationId) {
+          const result = {
+            loading: false,
+            error,
+            data: void 0,
+            called: true,
+            client,
+          };
+
+          if (
+            ref.current.isMounted &&
+            !equal(ref.current.result, result)
+          ) {
+            ref.current.result = result;
+            setResult(result);
+          }
+        }
+
+        if (options?.onError) {
+          options.onError(error);
+          // TODO(brian): why are we returning this here???
+          return { data: void 0, errors: error };
+        }
+
+        throw error;
+      });
+  }, [client, options, mutation]);
+
+  useEffect(() => () => {
+    ref.current.isMounted = false;
+  }, []);
+
+  return [execute, result];
 }

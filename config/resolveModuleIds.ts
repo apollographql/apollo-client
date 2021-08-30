91ae6ee04ca6ecc28ce1f207d5fe9241d019c2d1
Ben Newman <ben@apollographql.com>
2021-08-30 11:09:20 -0400

    Use Transformer class for config/resolveModuleIds.ts.

--- config/resolveModuleIds.ts
+++ config/resolveModuleIds.ts
@@ -6,7 +6,9 @@ import { distDir, eachFile, reparse, reprint } from './helpers';
 eachFile(distDir, (file, relPath) => new Promise((resolve, reject) => {
   fs.readFile(file, "utf8", (error, source) => {
     if (error) return reject(error);
-    const output = transform(source, file);
+
+    const output = new Transformer().transform(source, file);
+
     if (source === output) {
       resolve(file);
     } else {
@@ -21,85 +23,88 @@ import * as recast from "recast";
 const n = recast.types.namedTypes;
 type Node = recast.types.namedTypes.Node;
 
-function transform(code: string, file: string) {
-  const ast = reparse(code);
+class Transformer {
+  transform(code: string, file: string) {
+    const ast = reparse(code);
+    const transformer = this;
 
-  recast.visit(ast, {
-    visitImportDeclaration(path) {
-      this.traverse(path);
-      normalizeSourceString(file, path.node.source);
-    },
+    recast.visit(ast, {
+      visitImportDeclaration(path) {
+        this.traverse(path);
+        transformer.normalizeSourceString(file, path.node.source);
+      },
 
-    visitImportExpression(path) {
-      this.traverse(path);
-      normalizeSourceString(file, path.node.source);
-    },
+      visitImportExpression(path) {
+        this.traverse(path);
+        transformer.normalizeSourceString(file, path.node.source);
+      },
 
-    visitExportAllDeclaration(path) {
-      this.traverse(path);
-      normalizeSourceString(file, path.node.source);
-    },
+      visitExportAllDeclaration(path) {
+        this.traverse(path);
+        transformer.normalizeSourceString(file, path.node.source);
+      },
 
-    visitExportNamedDeclaration(path) {
-      this.traverse(path);
-      normalizeSourceString(file, path.node.source);
-    },
-  });
+      visitExportNamedDeclaration(path) {
+        this.traverse(path);
+        transformer.normalizeSourceString(file, path.node.source);
+      },
+    });
 
-  return reprint(ast);
-}
+    return reprint(ast);
+  }
 
-function isRelative(id: string) {
-  return id.startsWith("./") || id.startsWith("../");
-}
+  isRelative(id: string) {
+    return id.startsWith("./") || id.startsWith("../");
+  }
 
-function normalizeSourceString(file: string, source?: Node | null) {
-  if (source && n.StringLiteral.check(source)) {
-    try {
-      source.value = isRelative(source.value)
-        ? normalizeId(source.value, file)
-        : normalizeNonRelativeId(source.value, file);
-    } catch (error) {
-      console.error(`Failed to resolve ${source.value} in ${file} with error ${error}`);
-      process.exit(1);
+  normalizeSourceString(file: string, source?: Node | null) {
+    if (source && n.StringLiteral.check(source)) {
+      try {
+        source.value = this.isRelative(source.value)
+          ? this.normalizeId(source.value, file)
+          : this.normalizeNonRelativeId(source.value, file);
+      } catch (error) {
+        console.error(`Failed to resolve ${source.value} in ${file} with error ${error}`);
+        process.exit(1);
+      }
     }
   }
-}
 
-function normalizeNonRelativeId(id: string, file: string) {
-  const normal = normalizeId(id, file);
-  const normalParts = normal.split("/");
-  const sourceParts = id.split("/");
-  const nodeModulesIndex = normalParts.lastIndexOf("node_modules");
-  if (
-    nodeModulesIndex >= 0 &&
-    normalParts[nodeModulesIndex + 1] === sourceParts[0]
-  ) {
-    const bareModuleIdentifier =
-      normalParts.slice(nodeModulesIndex + 1).join("/");
-    if (normal === normalizeId(bareModuleIdentifier, file)) {
-      return bareModuleIdentifier;
+  normalizeNonRelativeId(id: string, file: string) {
+    const normal = this.normalizeId(id, file);
+    const normalParts = normal.split("/");
+    const sourceParts = id.split("/");
+    const nodeModulesIndex = normalParts.lastIndexOf("node_modules");
+    if (
+      nodeModulesIndex >= 0 &&
+      normalParts[nodeModulesIndex + 1] === sourceParts[0]
+    ) {
+      const bareModuleIdentifier =
+        normalParts.slice(nodeModulesIndex + 1).join("/");
+      if (normal === this.normalizeId(bareModuleIdentifier, file)) {
+        return bareModuleIdentifier;
+      }
+      console.error(`Leaving ${id} import in ${file} unchanged because ${
+        bareModuleIdentifier
+      } does not resolve to the same module`);
     }
-    console.error(`Leaving ${id} import in ${file} unchanged because ${
-      bareModuleIdentifier
-    } does not resolve to the same module`);
+    return id;
   }
-  return id;
-}
 
-function normalizeId(id: string, file: string) {
-  const basedir = path.dirname(file);
-  const absPath = resolve.sync(id, {
-    basedir,
-    extensions: [".mjs", ".js"],
-    packageFilter(pkg) {
-      return pkg.module ? {
-        ...pkg,
-        main: pkg.module,
-      } : pkg;
-    },
-  });
-  const relPath = path.relative(basedir, absPath);
-  const relId = relPath.split(path.sep).join('/');
-  return isRelative(relId) ? relId : "./" + relId;
+  normalizeId(id: string, file: string) {
+    const basedir = path.dirname(file);
+    const absPath = resolve.sync(id, {
+      basedir,
+      extensions: [".mjs", ".js"],
+      packageFilter(pkg) {
+        return pkg.module ? {
+          ...pkg,
+          main: pkg.module,
+        } : pkg;
+      },
+    });
+    const relPath = path.relative(basedir, absPath);
+    const relId = relPath.split(path.sep).join('/');
+    return this.isRelative(relId) ? relId : "./" + relId;
+  }
 }

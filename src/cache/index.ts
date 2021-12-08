3af6173b87798c07f3c3021d186f63e5f01e62a3
Ben Newman <ben@apollographql.com>
2021-12-08 11:49:00 -0500

    Make useFragment(...).missing a MissingTree rather than an error array.

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -4,6 +4,7 @@ export { Transaction, ApolloCache } from './core/cache';
 export { Cache } from './core/types/Cache';
 export { DataProxy } from './core/types/DataProxy';
 export {
+  MissingTree,
   MissingFieldError,
   ReadFieldOptions
 } from './core/types/common';

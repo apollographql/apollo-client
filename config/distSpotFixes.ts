ac6aa4c06699c77fae381d34d3017fa2ddbc3b1d
Ben Newman <ben@apollographql.com>
2022-02-15 10:08:23 -0500

    Improve comment wording.

--- config/distSpotFixes.ts
+++ config/distSpotFixes.ts
@@ -12,10 +12,10 @@ function sanitizeDEV() {
   // The global.d.ts types are useful within the @apollo/client codebase to
   // provide a type for the global __DEV__ constant, but actually shipping that
   // declaration as a globally-declared type runs too much risk of conflict with
-  // other __DEV__ declarations attempting to do achieve the same thing, most
+  // other __DEV__ declarations attempting to achieve the same thing, most
   // notably the one in @types/react-native/index.d.ts. We preserve the default
-  // export so that index.d.ts doesn't need to change, but otherwise we remove
-  // all traces of __DEV__ from global.d.ts.
+  // export so that index.d.ts can remain unchanged, but otherwise we remove all
+  // traces of __DEV__ from global.d.ts.
   if (/__DEV__/.test(globalDTs)) {
     fs.writeFileSync(globalDTsPath, [
       "declare const _default: typeof globalThis;",

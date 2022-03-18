a155948234633e5c511548367e0279830161e374
Trevor Blades <tdblades@gmail.com>
2022-03-18 16:30:06 -0700

    Keep docs dir name

--- docs/deploy-preview.sh
+++ docs/deploy-preview.sh
@@ -2,18 +2,12 @@
 
 cd ../
 
-mv docs content
+git clone https://github.com/apollographql/docs --branch tb/local-dev --single-branch monodocs
 
-git clone https://github.com/apollographql/docs --branch tb/local-dev --single-branch
-
-cd docs
+cd monodocs
 
 npm i
 
-cp -r ../content local
-
-ls
-
-ls local
+cp -r ../docs local
 
 DOCS_LOCAL=true npm run build

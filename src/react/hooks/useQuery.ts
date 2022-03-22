6540b233af17794f8aac248c3045b11084d37cbf
Ben Newman <ben@apollographql.com>
2022-03-22 10:59:36 -0400

    Call this.observable.setOptions when options change in useOptions.

--- src/react/hooks/useQuery.ts
+++ src/react/hooks/useQuery.ts
@@ -215,9 +215,35 @@ class InternalState<TData, TVariables> {
     // Update this.watchQueryOptions, but only when they have changed, which
     // allows us to depend on the referential stability of
     // this.watchQueryOptions elsewhere.
-    if (!equal(watchQueryOptions, this.watchQueryOptions)) {
+    const currentWatchQueryOptions = this.watchQueryOptions;
+    const currentResult = this.result;
+    let needToSetResult = false;
+    if (!equal(watchQueryOptions, currentWatchQueryOptions)) {
       this.watchQueryOptions = watchQueryOptions;
+      if (currentWatchQueryOptions && this.observable) {
+        // Though it might be tempting to postpone this line to the useEffect
+        // block, we need getCurrentResult to return an appropriate loading
+        // result synchronously (later within the same call to useQuery). Since
+        // we already have this.observable here (not true for the very first
+        // call to useQuery), we are not initiating any new subscriptions,
+        // though it does feel less than ideal to be (potentially) kicking off a
+        // network request (for example, if the variables have changed).
+        this.observable.setOptions(watchQueryOptions).catch(() => {});
+        needToSetResult = true;
+      }
     }
+    useEffect(() => {
+      // If we called this.observable.reobserve above, and this.result hasn't
+      // changed since then, report the latest current result to this.setResult.
+      if (needToSetResult && this.result === currentResult) {
+        const latestResult = this.observable.getCurrentResult();
+        if (!equal(latestResult, currentResult)) {
+          // This usually forces a rerender, which is why it must be done in
+          // useEffect.
+          this.setResult(latestResult);
+        }
+      }
+    }, [needToSetResult, currentResult]);
 
     this.ssrDisabled = !!(
       options.ssr === false ||
@@ -459,27 +485,6 @@ class InternalState<TData, TVariables> {
         obsQuery.setOptions(this.watchQueryOptions).catch(() => {});
       }
     }
-
-    const prevOptionsRef = useRef({
-      watchQueryOptions: this.watchQueryOptions,
-    });
-
-    // An effect to keep obsQuery.options up to date in case
-    // state.watchQueryOptions changes.
-    useEffect(() => {
-      if (this.renderPromises) {
-        // Do nothing during server rendering.
-      } else if (
-        // The useOptions method only updates this.watchQueryOptions if new new
-        // watchQueryOptions are not deep-equal to the previous options, so we
-        // only need a reference check (!==) here.
-        this.watchQueryOptions !== prevOptionsRef.current.watchQueryOptions
-      ) {
-        obsQuery.setOptions(this.watchQueryOptions).catch(() => {});
-        prevOptionsRef.current.watchQueryOptions = this.watchQueryOptions;
-        this.setResult(obsQuery.getCurrentResult());
-      }
-    }, [obsQuery, this.watchQueryOptions]);
 
     return obsQuery;
   }

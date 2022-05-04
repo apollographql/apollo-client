c26f9993445a479772f5973953ef40611153d3da
Ben Newman <ben@apollographql.com>
2022-05-04 13:21:12 -0400

    Export canUseDOM from utilities/common/canUse.ts.

--- src/utilities/common/canUse.ts
+++ src/utilities/common/canUse.ts
@@ -1,10 +1,14 @@
-export const canUseWeakMap = typeof WeakMap === 'function' && !(
-  typeof navigator === 'object' &&
-  navigator.product === 'ReactNative'
-);
+import { maybe } from "../globals";
+
+export const canUseWeakMap =
+  typeof WeakMap === 'function' &&
+  maybe(() => navigator.product) !== 'ReactNative';
 
 export const canUseWeakSet = typeof WeakSet === 'function';
 
 export const canUseSymbol =
   typeof Symbol === 'function' &&
   typeof Symbol.for === 'function';
+
+export const canUseDOM =
+  typeof maybe(() => window.document.createElement) === "function";

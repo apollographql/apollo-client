4edd24c0907ab3986220554cd6293b7dae49c5ea
Ben Newman <ben@apollographql.com>
2022-05-09 12:37:25 -0400

    Disable canUseDOM only when window.document comes from JSDOM.
    I believe this is a more precise change than my previous commit, which
    used isNode to determine canUseDOM. In most server-side rendering
    setups, there will be no window.document.createElement, so canUseDOM
    will already be false for that reason. It's only in Node.js environments
    using jsdom that window.document.createElement might seem to be defined,
    so that's the only case we really need to check.

--- src/utilities/common/canUse.ts
+++ src/utilities/common/canUse.ts
@@ -10,8 +10,20 @@ export const canUseSymbol =
   typeof Symbol === 'function' &&
   typeof Symbol.for === 'function';
 
-const isNode = !!maybe(() => `v${process.versions.node}` === process.version);
+const usingJSDOM: boolean =
+  // Following advice found in this comment from @domenic (maintainer of jsdom):
+  // https://github.com/jsdom/jsdom/issues/1537#issuecomment-229405327. Since we
+  // control the version of Jest used when running Apollo Client tests, and that
+  // version includes jsdom/x.y.z at the end of the user agent string, I believe
+  // this case is all we need to test for.
+  maybe(() => navigator.userAgent.indexOf("jsdom") >= 0) || false;
 
 export const canUseDOM =
-  !isNode &&
+  // Our tests should all continue to pass if we remove the !usingJSDOM
+  // condition, thereby allowing canUseDOM to be true when using jsdom.
+  // Unfortunately, if we allow that, then useSyncExternalStore generates a lot
+  // of warnings about useLayoutEffect doing nothing on the server. While these
+  // warnings are harmless, this !usingJSDOM condition seems to be the best way
+  // to prevent them (i.e. skipping useLayoutEffect when using jsdom).
+  !usingJSDOM &&
   typeof maybe(() => window.document.createElement) === "function";

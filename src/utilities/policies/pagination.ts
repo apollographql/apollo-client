7d6a5177b7f1c404adbcf021f0165f97733fc278
Ben Newman <ben@apollographql.com>
2022-05-16 18:35:22 -0400

    Stick with one return statement in offsetLimitPagination().merge.

--- src/utilities/policies/pagination.ts
+++ src/utilities/policies/pagination.ts
@@ -33,21 +33,22 @@ export function offsetLimitPagination<T = Reference>(
     merge(existing, incoming, { args }) {
       const merged = existing ? existing.slice(0) : [];
 
-      if (!incoming) return merged;
-
-      if (args) {
-        // Assume an offset of 0 if args.offset omitted.
-        const { offset = 0 } = args;
-        for (let i = 0; i < incoming.length; ++i) {
-          merged[offset + i] = incoming[i];
+      if (incoming) {
+        if (args) {
+          // Assume an offset of 0 if args.offset omitted.
+          const { offset = 0 } = args;
+          for (let i = 0; i < incoming.length; ++i) {
+            merged[offset + i] = incoming[i];
+          }
+        } else {
+          // It's unusual (probably a mistake) for a paginated field not
+          // to receive any arguments, so you might prefer to throw an
+          // exception here, instead of recovering by appending incoming
+          // onto the existing array.
+          merged.push.apply(merged, incoming);
         }
-      } else {
-        // It's unusual (probably a mistake) for a paginated field not
-        // to receive any arguments, so you might prefer to throw an
-        // exception here, instead of recovering by appending incoming
-        // onto the existing array.
-        merged.push.apply(merged, incoming);
       }
+
       return merged;
     },
   };

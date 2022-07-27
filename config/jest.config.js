8eded1ffd5093766d6c01775d0c03ae8f46a0809
alessia <alessia@apollographql.com>
2022-07-27 14:09:36 -0400

    test: update dependencies and jest config to run 17 tests

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,12 +1,15 @@
 const { compilerOptions } = require("../tsconfig.json");
 
-module.exports = {
-  rootDir: '../src',
-  transform: {
-    '.(ts|tsx)': 'ts-jest',
+const defaults = {
+  rootDir: "src",
+  preset: "ts-jest/presets/js-with-ts",
+  testEnvironment: "jsdom",
+  setupFiles: ["<rootDir>/config/jest/setup.ts"],
+  testEnvironmentOptions: {
+    url: "http://localhost",
   },
   globals: {
-    'ts-jest': {
+    "ts-jest": {
       diagnostics: true,
       tsconfig: {
         ...compilerOptions,
@@ -14,10 +17,28 @@ module.exports = {
       },
     },
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
-  testEnvironmentOptions: {
-    url: 'http://localhost',
+};
+
+// const tsStandardConfig = {
+//   ...defaults,
+//   displayName: "ReactDOM 18",
+// };
+
+const standardReact17Config = {
+  ...defaults,
+  displayName: "ReactDOM 17",
+  moduleNameMapper: {
+    "^react$": "react-17",
+    "^react-dom$": "react-dom-17",
+    "^react-dom/server$": "react-dom-17/server",
+    "^react-dom/test-utils$": "react-dom-17/test-utils",
+    "^@testing-library/react$": "@testing-library/react-12",
   },
-  setupFiles: ['<rootDir>/config/jest/setup.ts'],
-  testEnvironment: 'jsdom',
+};
+
+module.exports = {
+  projects: [
+    // tsStandardConfig,
+    standardReact17Config,
+  ],
 };

2555d6186194289807a4a9079e384d3ee68458f9
MrDoomBringer <emmanuelssr@gmail.com>
2022-08-08 09:49:26 -0400

    textEncoder global

--- src/react/ssr/index.ts
+++ src/react/ssr/index.ts
@@ -1,3 +1,7 @@
+
+import { TextEncoder } from 'util';
+global.TextEncoder = TextEncoder
+
 export { getMarkupFromTree, getDataFromTree } from './getDataFromTree';
 export { renderToStringWithData } from './renderToStringWithData';
 export { RenderPromises } from './RenderPromises';

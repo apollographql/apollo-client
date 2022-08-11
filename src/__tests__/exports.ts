d3aa3fc1c9bcaf7a906eaa1ce864b3727a0d5c42
MrDoomBringer <emmanuelssr@gmail.com>
2022-08-11 13:43:36 -0400

    More informative comment

--- src/__tests__/exports.ts
+++ src/__tests__/exports.ts
@@ -1,7 +1,7 @@
 /** @jest-environment node */
 // We run this in a node environment because:
-// A) JSDOM doesn't yet support the TextEncoder/TextDecoder elements, meaning certain imports will fail (See https://github.com/jsdom/jsdom/issues/2524) 
-// B) We're just testing imports/exports, so we don't need to slow down the test with a browser environment.
+// A) JSDOM doesn't yet support the TextEncoder/TextDecoder globals added in node 11, meaning certain imports (e.g. reactSSR) will fail (See https://github.com/jsdom/jsdom/issues/2524) 
+// B) We're just testing imports/exports, so no reason not to use Node for slightly better performance.
 
 import * as cache from "../cache";
 import * as client from "..";

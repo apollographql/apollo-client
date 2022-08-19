8f05fa79414736f6ea0ec456c9ca5a5325543fb0
MrDoomBringer <emmanuelssr@gmail.com>
2022-08-19 10:09:14 -0400

    Remove codemods, export core from react entrypoint

--- scripts/codemods/ac3-to-ac4/examples/import_a.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery, useMutation, ApolloClient } from '@apollo/client';
-
-/*
-Simple case where we move some react hooks from @apollo/client to a newly created @apollo/client/react import
-Should result in the following:
-
-import { ApolloClient } from '@apollo/client';
-import { useQuery, useMutation } from '@apollo/client/react';
-*/

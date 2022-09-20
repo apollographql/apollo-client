7843dc8d501303db745bce84545b39bb816559bd
alessia <alessia@apollographql.com>
2022-09-19 21:20:03 -0400

    chore(tests): import utilities/globals for __DEV__

--- src/react/hoc/__tests__/queries/recomposeWithState.ts
+++ src/react/hoc/__tests__/queries/recomposeWithState.ts
@@ -2,6 +2,7 @@
 // to avoid incurring an indirect dependency on ua-parser-js via fbjs.
 
 import React, { createFactory, Component } from "react";
+import '../../../../utilities/globals'; // For __DEV__
 
 const setStatic =
   (key: string, value: string) => (BaseComponent: React.ComponentClass) => {

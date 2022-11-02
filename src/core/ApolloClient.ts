ffe7ec9296e8a8d1100d0a4f8e34f2220a03d5b7
Chris Chen <lei110408@gmail.com>
2022-11-02 14:45:01 -0700

    add connectToDevTools check

--- src/core/ApolloClient.ts
+++ src/core/ApolloClient.ts
@@ -188,36 +188,36 @@ export class ApolloClient<TCacheShape> implements DataProxy {
     /**
      * Suggest installing the devtools for developers who don't have them
      */
-    if (!hasSuggestedDevtools && __DEV__) {
-      hasSuggestedDevtools = true;
-      if (
-        typeof window !== "undefined" &&
-        window.document &&
-        window.top === window.self &&
-        !(window as any).__APOLLO_DEVTOOLS_GLOBAL_HOOK__
-      ) {
-        const nav = window.navigator;
-        const ua = nav && nav.userAgent;
-        let url: string | undefined;
-        if (typeof ua === "string") {
-          if (ua.indexOf("Chrome/") > -1) {
-            url =
-              "https://chrome.google.com/webstore/detail/" +
-              "apollo-client-developer-t/jdkknkkbebbapilgoeccciglkfbmbnfm";
-          } else if (ua.indexOf("Firefox/") > -1) {
-            url =
-              "https://addons.mozilla.org/en-US/firefox/addon/apollo-developer-tools/";
-          }
-        }
-        if (url && !headers?.hideDevLog) {
-          invariant.log(
-            "Download the Apollo DevTools for a better development " +
-              "experience: " +
-              url
-          );
-        }
-      }
-    }
+   if (!hasSuggestedDevtools && __DEV__ && connectToDevTools) {
+     hasSuggestedDevtools = true;
+     if (
+       typeof window !== "undefined" &&
+       window.document &&
+       window.top === window.self &&
+       !(window as any).__APOLLO_DEVTOOLS_GLOBAL_HOOK__
+     ) {
+       const nav = window.navigator;
+       const ua = nav && nav.userAgent;
+       let url: string | undefined;
+       if (typeof ua === "string") {
+         if (ua.indexOf("Chrome/") > -1) {
+           url =
+             "https://chrome.google.com/webstore/detail/" +
+             "apollo-client-developer-t/jdkknkkbebbapilgoeccciglkfbmbnfm";
+         } else if (ua.indexOf("Firefox/") > -1) {
+           url =
+             "https://addons.mozilla.org/en-US/firefox/addon/apollo-developer-tools/";
+         }
+       }
+       if (url && !headers?.hideDevLog) {
+         invariant.log(
+           "Download the Apollo DevTools for a better development " +
+             "experience: " +
+             url
+         );
+       }
+     }
+   }
 
     this.version = version;
 

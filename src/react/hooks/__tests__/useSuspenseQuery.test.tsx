6cf8029cc1adf0d172f0ff395267797a82195a3e
Jerel Miller <jerelmiller@gmail.com>
2022-11-07 16:34:24 -0700

    Minor tweak to order of tests

--- src/react/hooks/__tests__/useSuspenseQuery.test.tsx
+++ src/react/hooks/__tests__/useSuspenseQuery.test.tsx
@@ -20,7 +20,55 @@ import {
 describe('useSuspenseQuery', () => {
   it('is importable and callable', () => {
     expect(typeof useSuspenseQuery).toBe('function');
-  })
+  });
+
+  it('validates the GraphQL query as a query', () => {
+    const consoleSpy = jest.spyOn(console, 'error').mockImplementation();
+
+    const query = gql`
+      mutation ShouldThrow {
+        createException
+      }
+    `;
+
+    const { result } = renderHook(() => useSuspenseQuery(query), {
+      wrapper: ({ children }) => <MockedProvider>{children}</MockedProvider>
+    });
+
+    expect(result.error).toEqual(
+      new InvariantError(
+        'Running a Query requires a graphql Query, but a Mutation was used instead.'
+      )
+    );
+
+    consoleSpy.mockRestore();
+  });
+
+  it('ensures a suspense cache is provided', () => {
+    const consoleSpy = jest.spyOn(console, 'error').mockImplementation();
+
+    const query = gql`
+      query { hello }
+    `;
+
+    const client = new ApolloClient({ cache: new InMemoryCache() });
+
+    const { result } = renderHook(() => useSuspenseQuery(query), {
+      wrapper: ({ children }) => (
+        <ApolloProvider client={client}>{children}</ApolloProvider>
+      )
+    });
+
+    expect(result.error).toEqual(
+      new InvariantError(
+        'Could not find a "suspenseCache" in the context. Wrap the root component ' +
+        'in an <ApolloProvider> and provide a suspenseCache.'
+      )
+    );
+
+    consoleSpy.mockRestore();
+  });
+
 
   it('suspends a query and return results', async () => {
     interface QueryData {
@@ -136,53 +184,79 @@ describe('useSuspenseQuery', () => {
     ]);
   });
 
-  it('validates the GraphQL query as a query', () => {
-    // supress console.error calls for this test since they are expected
-    const consoleSpy = jest.spyOn(console, 'error').mockImplementation();
+  it('returns the same results for the same variables', async () => {
+    interface QueryData {
+      character: {
+        id: string
+        name: string
+      };
+    };
 
-    const query = gql`
-      mutation ShouldThrow {
-        createException
+    interface QueryVariables {
+      id: string
+    }
+
+    const query: TypedDocumentNode<QueryData, QueryVariables> = gql`
+      query CharacterQuery($id: String!) {
+        character(id: $id) {
+          id
+          name
+        }
       }
     `;
 
-    const { result } = renderHook(() => useSuspenseQuery(query), {
-      wrapper: ({ children }) => <MockedProvider>{children}</MockedProvider>
-    });
+    const suspenseCache = new SuspenseCache();
 
-    expect(result.error).toEqual(
-      new InvariantError(
-        'Running a Query requires a graphql Query, but a Mutation was used instead.'
-      )
+    const mocks = [
+      {
+        request: { query, variables: { id: '1' } },
+        result: { data: { character: { id: '1', name: 'Spider-Man' } } }
+      }
+    ];
+
+    const results: UseSuspenseQueryResult<QueryData, QueryVariables>[] = [];
+    let renders = 0;
+
+    function Test({ id }: { id: string }) {
+      renders++;
+      const result = useSuspenseQuery(query, {
+        variables: { id }
+      });
+
+      results.push(result);
+
+      return <div>{result.data.character.name}</div>;
+    }
+
+    const { rerender } = render(
+      <MockedProvider mocks={mocks} suspenseCache={suspenseCache}>
+        <Suspense fallback="loading">
+          <Test id="1" />
+        </Suspense>
+      </MockedProvider>
     );
 
-    consoleSpy.mockRestore();
-  });
+    expect(await screen.findByText('Spider-Man')).toBeInTheDocument();
 
-  it('ensures a suspense cache is provided', () => {
-    // supress console.error calls for this test since they are expected
-    const consoleSpy = jest.spyOn(console, 'error').mockImplementation();
-
-    const query = gql`
-      query { hello }
-    `;
-
-    const client = new ApolloClient({ cache: new InMemoryCache() });
-
-    const { result } = renderHook(() => useSuspenseQuery(query), {
-      wrapper: ({ children }) => (
-        <ApolloProvider client={client}>{children}</ApolloProvider>
-      )
-    });
-
-    expect(result.error).toEqual(
-      new InvariantError(
-        'Could not find a "suspenseCache" in the context. Wrap the root component ' +
-        'in an <ApolloProvider> and provide a suspenseCache.'
-      )
+    rerender(
+      <MockedProvider mocks={mocks} suspenseCache={suspenseCache}>
+        <Suspense fallback="loading">
+          <Test id="1" />
+        </Suspense>
+      </MockedProvider>
     );
 
-    consoleSpy.mockRestore();
+    expect(renders).toBe(3);
+    expect(results).toEqual([
+      {
+        ...mocks[0].result,
+        variables: { id: '1' },
+      },
+      {
+        ...mocks[0].result,
+        variables: { id: '1' },
+      },
+    ]);
   });
 
   it('re-suspends the component when changing variables and suspensePolicy is set to "always"', async () => {
@@ -269,80 +343,6 @@ describe('useSuspenseQuery', () => {
     ]);
   });
 
-  it('returns the same results for the same variables', async () => {
-    interface QueryData {
-      character: {
-        id: string
-        name: string
-      };
-    };
-
-    interface QueryVariables {
-      id: string
-    }
-
-    const query: TypedDocumentNode<QueryData, QueryVariables> = gql`
-      query CharacterQuery($id: String!) {
-        character(id: $id) {
-          id
-          name
-        }
-      }
-    `;
-
-    const suspenseCache = new SuspenseCache();
-
-    const mocks = [
-      {
-        request: { query, variables: { id: '1' } },
-        result: { data: { character: { id: '1', name: 'Spider-Man' } } }
-      }
-    ];
-
-    const results: UseSuspenseQueryResult<QueryData, QueryVariables>[] = [];
-    let renders = 0;
-
-    function Test({ id }: { id: string }) {
-      renders++;
-      const result = useSuspenseQuery(query, {
-        variables: { id }
-      });
-
-      results.push(result);
-
-      return <div>{result.data.character.name}</div>;
-    }
-
-    const { rerender } = render(
-      <MockedProvider mocks={mocks} suspenseCache={suspenseCache}>
-        <Suspense fallback="loading">
-          <Test id="1" />
-        </Suspense>
-      </MockedProvider>
-    );
-
-    expect(await screen.findByText('Spider-Man')).toBeInTheDocument();
-
-    rerender(
-      <MockedProvider mocks={mocks} suspenseCache={suspenseCache}>
-        <Suspense fallback="loading">
-          <Test id="1" />
-        </Suspense>
-      </MockedProvider>
-    );
-
-    expect(renders).toBe(3);
-    expect(results).toEqual([
-      {
-        ...mocks[0].result,
-        variables: { id: '1' },
-      },
-      {
-        ...mocks[0].result,
-        variables: { id: '1' },
-      },
-    ]);
-  });
 
   it.skip('ensures a valid fetch policy is used', () => {});
   it.skip('result is referentially stable', () => {});

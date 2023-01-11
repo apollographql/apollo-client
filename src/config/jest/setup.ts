d2f26c540a67755edcf3ec4155235ff04f5c97e7
alessia <alessia@apollographql.com>
2023-01-11 10:27:38 -0500

    cleanup

--- src/config/jest/setup.ts
+++ src/config/jest/setup.ts
@@ -3,14 +3,4 @@ import gql from 'graphql-tag';
 // Turn off warnings for repeated fragment names
 gql.disableFragmentWarnings();
 
-if (!process.env.LISTENING_TO_UNHANDLED_REJECTION) {
-  process.on('unhandledRejection', (reason) => {
-    throw reason;
-  });
-  // Avoid memory leak by adding too many listeners
-  process.env.LISTENING_TO_UNHANDLED_REJECTION = "true";
-}
-
-beforeEach(() => {
-  jest.restoreAllMocks();
-});
+process.on('unhandledRejection', () => {});

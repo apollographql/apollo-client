8f538ff99e916e4c555f943efe64e8ffe4a34103
alessia <alessia@apollographql.com>
2023-01-25 12:27:19 -0500

    chore: remove unnecessary generic

--- src/utilities/common/incrementalResult.ts
+++ src/utilities/common/incrementalResult.ts
@@ -24,7 +24,7 @@ export function isExecutionPatchResult<T>(
 ): value is ExecutionPatchResult<T> {
   return (
     isExecutionPatchIncrementalResult(value) ||
-    isExecutionPatchInitialResult<T>(value)
+    isExecutionPatchInitialResult(value)
   );
 }
 

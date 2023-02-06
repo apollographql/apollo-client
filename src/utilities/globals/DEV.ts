28ba4e7aa44caa346e71647f361673e5a9b7ec35
Ben Newman <ben@apollographql.com>
2023-02-06 13:49:44 -0500

    Simplify utilities/globals/DEV.ts.
    https://github.com/apollographql/apollo-client/pull/10521#discussion_r1097541666

--- src/utilities/globals/DEV.ts
+++ src/utilities/globals/DEV.ts
@@ -1,13 +1,20 @@
 import global from "./global";
 import { maybe } from "./maybe";
 
-function getDEV() {
-  return "__DEV__" in global
+export default (
+  "__DEV__" in global
+    // We want it to be possible to set __DEV__ globally to control the result
+    // of this code, so it's important to check global.__DEV__ instead of
+    // assuming a naked reference like __DEV__ refers to global scope, since
+    // those references could be replaced with true or false by minifiers.
     ? Boolean(global.__DEV__)
-    // In a buildless browser environment, maybe(() => process.env.NODE_ENV)
-    // evaluates as undefined, so __DEV__ becomes true by default, but can be
-    // initialized to false instead by a script/module that runs earlier.
-    : maybe(() => process.env.NODE_ENV) !== "production";
-}
 
-export default getDEV();
+    // In a buildless browser environment, maybe(() => process.env.NODE_ENV)
+    // evaluates to undefined, so __DEV__ becomes true by default, but can be
+    // initialized to false instead by a script/module that runs earlier.
+    //
+    // If you use tooling to replace process.env.NODE_ENV with a string like
+    // "development", this code will become something like maybe(() =>
+    // "development") !== "production", which also works as expected.
+    : maybe(() => process.env.NODE_ENV) !== "production"
+);

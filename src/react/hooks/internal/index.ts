6b4036356830f74aaa22413726b41769740f9e69
Jerel Miller <jerelmiller@gmail.com>
2023-03-17 16:23:37 -0600

    Add __use to internal hooks export

--- src/react/hooks/internal/index.ts
+++ src/react/hooks/internal/index.ts
@@ -1,3 +1,4 @@
 // These hooks are used internally and are not exported publicly by the library
 export { useDeepMemo } from './useDeepMemo';
 export { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
+export { __use } from './__use';

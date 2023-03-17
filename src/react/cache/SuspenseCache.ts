1ae122788513c4edb07e280490a195b016e79e9e
Jerel Miller <jerelmiller@gmail.com>
2023-03-17 11:15:06 -0600

    Update suspense cache to only accept stateful promises

--- src/react/cache/SuspenseCache.ts
+++ src/react/cache/SuspenseCache.ts
@@ -6,14 +6,7 @@ import {
   OperationVariables,
 } from '../../core';
 import { canonicalStringify } from '../../cache';
-import { canUseWeakMap } from '../../utilities';
-
-type PromiseState<TValue> =
-  | { status: 'pending'; value?: never; reason?: never }
-  | { status: 'fulfilled'; value: TValue; reason?: never }
-  | { status: 'rejected'; value?: never; reason: unknown };
-
-type PromiseWithState<TValue> = Promise<TValue> & PromiseState<TValue>;
+import { PromiseWithState, canUseWeakMap } from '../../utilities';
 
 interface CacheEntry {
   query: DocumentNode;
@@ -30,50 +23,8 @@ function makeCacheKey(cacheKey: CacheKey) {
   return cacheKey;
 }
 
-function isStatefulPromise<TValue>(
-  promise: Promise<TValue>
-): promise is PromiseWithState<TValue> {
-  return 'status' in promise;
-}
-
-function addStateToPromise<TValue>(
-  promise: Promise<TValue>
-): PromiseWithState<TValue> {
-  if (isStatefulPromise(promise)) {
-    return promise;
-  }
-
-  const statefulPromise = promise as PromiseWithState<TValue>;
-
-  statefulPromise.status = 'pending';
-  statefulPromise.value = void 0;
-  statefulPromise.reason = void 0;
-
-  statefulPromise
-    .then((value) => {
-      statefulPromise.status = 'fulfilled';
-      statefulPromise.value = value;
-    })
-    .catch((reason) => {
-      statefulPromise.status = 'rejected';
-      statefulPromise.reason = reason;
-    });
-
-  return statefulPromise;
-}
-
 export class SuspenseCache {
   private queries = new Map<CacheKey, CacheEntry>();
-
-  private queriesByPromise = canUseWeakMap
-    ? new WeakMap<
-        PromiseWithState<ApolloQueryResult<unknown>>,
-        OperationVariables
-      >()
-    : new Map<
-        PromiseWithState<ApolloQueryResult<unknown>>,
-        OperationVariables
-      >();
 
   private cacheKeys = new Trie<CacheKey>(canUseWeakMap, makeCacheKey);
 
@@ -85,7 +36,7 @@ export class SuspenseCache {
   }: {
     query: DocumentNode;
     variables: OperationVariables | undefined;
-    promise: Promise<ApolloQueryResult<unknown>>;
+    promise: PromiseWithState<ApolloQueryResult<unknown>>;
     observable: ObservableQuery<unknown, OperationVariables>;
   }) {
     const cacheKey = this.getCacheKey(query, variables);
@@ -94,11 +45,10 @@ export class SuspenseCache {
       query,
       variables,
       observable,
-      promise: addStateToPromise(promise),
+      promise,
     };
 
     this.queries.set(cacheKey, entry);
-    this.queriesByPromise.set(entry.promise, entry);
 
     return entry;
   }
@@ -109,17 +59,12 @@ export class SuspenseCache {
     return this.queries.get(cacheKey);
   }
 
-  reverseLookup(promise: PromiseWithState<ApolloQueryResult<unknown>>) {
-    return this.queriesByPromise.get(promise);
-  }
-
   remove(query: DocumentNode, variables: OperationVariables | undefined) {
     const cacheKey = this.getCacheKey(query, variables);
     const entry = this.queries.get(cacheKey);
 
     if (entry && !entry.observable.hasObservers()) {
       this.queries.delete(cacheKey);
-      this.queriesByPromise.delete(entry.promise);
     }
   }
 

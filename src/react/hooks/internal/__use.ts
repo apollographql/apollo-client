eb94dec52bad9b1538bf64fe3bbbffe02ded3221
Jerel Miller <jerelmiller@gmail.com>
2023-03-17 16:16:51 -0600

    Create a custom __use hook to handle throwing promises

---
+++ src/react/hooks/internal/__use.ts
@@ -0,0 +1,23 @@
+import { wrapPromiseWithState } from '../../../utilities';
+
+// TODO: Replace the use of this with React's use once its available. For now,
+// this mimics its functionality (minus support for context) by adding
+// properties to the promise and reading them synchronously. This is named with
+// two underscores to allow this hook to evade typical rules of hooks (i.e. its
+// can be used conditionally)
+export function __use<TValue>(promise: Promise<TValue> | null) {
+  if (promise === null) {
+    return null;
+  }
+
+  const statefulPromise = wrapPromiseWithState(promise);
+
+  switch (statefulPromise.status) {
+    case 'pending':
+      throw statefulPromise;
+    case 'rejected':
+      throw statefulPromise.reason;
+    case 'fulfilled':
+      return statefulPromise.value;
+  }
+}

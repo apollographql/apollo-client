5c89498c0410794a9f48f4ea7acbd0367453b643
Jerel Miller <jerelmiller@gmail.com>
2023-03-18 00:36:39 -0600

    Export useLazyRef from hooks/internal

--- src/react/hooks/internal/index.ts
+++ src/react/hooks/internal/index.ts
@@ -1,4 +1,5 @@
 // These hooks are used internally and are not exported publicly by the library
 export { useDeepMemo } from './useDeepMemo';
+export { useLazyRef } from './useLazyRef';
 export { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
 export { __use } from './__use';

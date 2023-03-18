b237cad045e7b27d85008ca10d54b853bcf20e85
Jerel Miller <jerelmiller@gmail.com>
2023-03-17 18:29:05 -0600

    Allow useLayoutEffect in jest tests when importing useIsomorphicLayoutEffect

--- src/react/hooks/internal/useIsomorphicLayoutEffect.ts
+++ src/react/hooks/internal/useIsomorphicLayoutEffect.ts
@@ -1,6 +1,11 @@
 import { useLayoutEffect, useEffect } from 'react';
-import { canUseLayoutEffect } from '../../../utilities';
+import { canUseDOM } from '../../../utilities';
 
-export const useIsomorphicLayoutEffect = canUseLayoutEffect
+// use canUseDOM here instead of canUseLayoutEffect because we want to be able
+// to use useLayoutEffect in our jest tests. useLayoutEffect seems to work fine
+// in useSuspenseQuery tests, but to honor the original comment about the
+// warnings for useSyncExternalStore implementation, canUseLayoutEffect is left
+// alone.
+export const useIsomorphicLayoutEffect = canUseDOM
   ? useLayoutEffect
   : useEffect;

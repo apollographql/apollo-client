5440c839b77ca1295829536c6a99af97e18dd985
Jerel Miller <jerelmiller@gmail.com>
2023-03-22 00:16:22 -0600

    Remove dep in suspense cache for now

--- src/react/cache/SuspenseCache.ts
+++ src/react/cache/SuspenseCache.ts
@@ -1,13 +1,9 @@
 import { ApolloClient } from '../../core';
 import { SuspenseQueryCache } from './SuspenseQueryCache';
-import { dep, wrap } from 'optimism';
+import { wrap } from 'optimism';
 
 export class SuspenseCache {
-  public readonly clientDep = dep<ApolloClient<unknown>>();
-
   forClient = wrap((client: ApolloClient<unknown>) => {
-    this.clientDep(client);
-
     return new SuspenseQueryCache(client);
   });
 }

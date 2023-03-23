4d0a5d17f30c047bb042036a43c4a40e2ea59f19
Jerel Miller <jerelmiller@gmail.com>
2023-03-23 15:54:16 -0600

    Minor adjustment to comment

--- src/react/hooks/internal/__use.ts
+++ src/react/hooks/internal/__use.ts
@@ -1,10 +1,10 @@
 import { wrapPromiseWithState } from '../../../utilities/promises/decoration';
 
 // TODO: Replace the use of this with React's use once its available. For now,
-// this mimics its functionality (minus support for context) by adding
-// properties to the promise and reading them synchronously. This is named with
-// two underscores to allow this hook to evade typical rules of hooks (i.e. its
-// can be used conditionally)
+// this mimics its functionality for promises by adding
+// properties to the promise instance and reading them synchronously. This is
+// named with two underscores to allow this hook to evade typical rules of
+// hooks (i.e. it can be used conditionally)
 export function __use<TValue>(promise: Promise<TValue>) {
   const statefulPromise = wrapPromiseWithState(promise);
 

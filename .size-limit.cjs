1f0ce656e0d12d6b03ea497d07d78648aa9f2499
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2023-05-19 17:13:55 +0200

    udpate size limits

--- .size-limit.cjs
+++ .size-limit.cjs
@@ -1,7 +1,7 @@
 const checks = [
   {
     path: "dist/apollo-client.min.cjs",
-    limit: "36.96kb"
+    limit: "37074"
   },
   {
     path: "dist/main.cjs",
@@ -10,7 +10,7 @@ const checks = [
   {
     path: "dist/index.js",
     import: "{ ApolloClient, InMemoryCache, HttpLink }",
-    limit: "35.02kb"
+    limit: "32945"
   },
   ...[
     "ApolloProvider",

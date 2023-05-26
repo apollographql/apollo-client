e96bd6a6c5644ba3a2f34dca4d11ed787b802178
Jerel Miller <jerelmiller@gmail.com>
2023-05-26 12:11:54 -0600

    Minor tweak to conditional to load filter

--- docs/shared/DisplayClientError.js
+++ docs/shared/DisplayClientError.js
@@ -161,9 +161,9 @@ export default function DisplayClientError() {
 
   return (
     <div
-      style={
-        errorMessage === 'Loading...' ? { filter: 'blur(5px)' } : undefined
-      }
+      style={{
+        filter: errorMessage === 'Loading...' ? 'blur(5px)' : undefined,
+      }}
     >
       {error ? (
         <>

eb6d5f7702ef1a6359a8ddaa5a1a781c5f9bd109
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2023-05-26 16:57:55 +0200

    remove polyfilling code

--- src/utilities/globals/DEV.ts
+++ src/utilities/globals/DEV.ts
@@ -1,20 +1,6 @@
-import global from "./global";
-import { maybe } from "./maybe";
+function getDEV() {
+  // TODO: could probably also be removed, right now I'm just removing the polyfilling code
+  return Boolean(__DEV__);
+}
 
-export default (
-  "__DEV__" in global
-    // We want it to be possible to set __DEV__ globally to control the result
-    // of this code, so it's important to check global.__DEV__ instead of
-    // assuming a naked reference like __DEV__ refers to global scope, since
-    // those references could be replaced with true or false by minifiers.
-    ? Boolean(global.__DEV__)
-
-    // In a buildless browser environment, maybe(() => process.env.NODE_ENV)
-    // evaluates to undefined, so __DEV__ becomes true by default, but can be
-    // initialized to false instead by a script/module that runs earlier.
-    //
-    // If you use tooling to replace process.env.NODE_ENV with a string like
-    // "development", this code will become something like maybe(() =>
-    // "development") !== "production", which also works as expected.
-    : maybe(() => process.env.NODE_ENV) !== "production"
-);
+export default getDEV();

1fb8afba31fb0892c3b192d4fc5b87aea3d96132
Jerel Miller <jerelmiller@gmail.com>
2023-05-26 15:39:02 -0600

    Minor adjustment to code style by consolidating to single `forward` call

--- src/link/remove-typename/removeTypenameFromVariables.ts
+++ src/link/remove-typename/removeTypenameFromVariables.ts
@@ -22,13 +22,11 @@ export function removeTypenameFromVariables(
     const { except } = options;
     const { query, variables } = operation;
 
-    if (!variables) {
-      return forward(operation);
+    if (variables) {
+      operation.variables = except
+        ? maybeStripTypenameUsingConfig(query, variables, except)
+        : stripTypename(variables);
     }
-
-    operation.variables = except
-      ? maybeStripTypenameUsingConfig(query, variables, except)
-      : stripTypename(variables);
 
     return forward(operation);
   });

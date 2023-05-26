1298ad9f957380865363f15cfed2801c85ddc73d
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2023-05-26 10:46:42 +0200

    Update src/config/jest/setup.ts
    Co-authored-by: Jerel Miller <jerelmiller@gmail.com>

--- src/config/jest/setup.ts
+++ src/config/jest/setup.ts
@@ -1,6 +1,6 @@
 import gql from 'graphql-tag';
 import '@testing-library/jest-dom';
-import {loadErrorMessageHandler} from '../../dev/loadErrorMessageHandler';
+import { loadErrorMessageHandler } from '../../dev/loadErrorMessageHandler';
 // Turn off warnings for repeated fragment names
 gql.disableFragmentWarnings();
 

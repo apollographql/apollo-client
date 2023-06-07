fa619293ee36122b7d449267557e096395e2c0af
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2023-06-07 18:25:46 +0200

    use `React.version` as key for shared Contexts

--- src/react/context/ApolloContext.ts
+++ src/react/context/ApolloContext.ts
@@ -11,9 +11,10 @@ export interface ApolloContextValue {
   suspenseCache?: SuspenseCache;
 }
 
+type ReactVersion = string;
 declare global {
   interface Window {
-    [contextKey]: Map<typeof React, React.Context<ApolloContextValue>>;
+    [contextKey]: Map<ReactVersion, React.Context<ApolloContextValue>>;
   }
 }
 
@@ -44,11 +45,11 @@ export function getApolloContext(): React.Context<ApolloContextValue> {
     contextStorage = global[contextKey] = new Map();
   }
 
-  let context = contextStorage.get(React);
+  let context = contextStorage.get(React.version);
   if (!context) {
     context = React.createContext<ApolloContextValue>({});
     context.displayName = 'ApolloContext';
-    contextStorage.set(React, context);
+    contextStorage.set(React.version, context);
   }
   return context;
 }

c2620591aef24794cabb02fd5e1decdbbea98b49
alessia <alessia@apollographql.com>
2023-06-08 12:20:51 -0400

    fix: incorrect arg passed to filterOperationVariables

--- src/link/batch-http/batchHttpLink.ts
+++ src/link/batch-http/batchHttpLink.ts
@@ -134,7 +134,7 @@ export class BatchHttpLink extends ApolloLink {
         );
 
         if (result.body.variables && !includeUnusedVariables) {
-          result.body.variables = filterOperationVariables(result.body, operation);
+          result.body.variables = filterOperationVariables(result.body.variables, operation);
         }
 
         return result;

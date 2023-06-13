b2df09f8cdf64850b46da07b6f6d736c254596cf
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2023-06-13 10:38:15 +0200

    last fixups

--- src/utilities/globals/index.ts
+++ src/utilities/globals/index.ts
@@ -1,5 +1,9 @@
 import { invariant, newInvariantError, InvariantError } from "./invariantWrappers";
 
+/** 
+ * @deprecated this has not functionality anymore, 
+ * it is just exported for backwards compatbility 
+ */
 export function checkDEV(){}
 // Import graphql/jsutils/instanceOf safely, working around its unchecked usage
 // of process.env.NODE_ENV and https://github.com/graphql/graphql-js/pull/2894.
@@ -13,5 +17,10 @@ export { maybe } from "./maybe";
 export { default as global } from "./global";
 export { invariant, newInvariantError, InvariantError }
 
+/** 
+ * @deprecated we do not use this internally anymore, 
+ * it is just exported for backwards compatbility 
+ */
 // @ts-ignore
 export const DEV = __DEV__;
+export { DEV as __DEV__ };

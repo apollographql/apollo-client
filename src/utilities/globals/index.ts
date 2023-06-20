c5515fe7847767e0a751378db13c8316899b9e29
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2023-06-20 11:43:02 +0200

    Revert "experiment: remove `graphql` package esm fix"
    This reverts commit 930783b47e4ea1fdaef1e77d4343c262b13137bc.

--- src/utilities/globals/index.ts
+++ src/utilities/globals/index.ts
@@ -1,5 +1,13 @@
 import { invariant, newInvariantError, InvariantError } from "./invariantWrappers";
 
+// Import graphql/jsutils/instanceOf safely, working around its unchecked usage
+// of process.env.NODE_ENV and https://github.com/graphql/graphql-js/pull/2894.
+import { removeTemporaryGlobals } from "./fix-graphql";
+
+// Synchronously undo the global process.env.NODE_ENV polyfill that we created
+// temporarily while importing the offending graphql/jsutils/instanceOf module.
+removeTemporaryGlobals();
+
 export { maybe } from "./maybe";
 export { default as global } from "./global";
 export { invariant, newInvariantError, InvariantError }

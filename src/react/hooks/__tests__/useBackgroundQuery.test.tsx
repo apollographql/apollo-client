d94180da8674d4f3ad5636d3731eee3082e0af5c
alessia <alessia@apollographql.com>
2023-06-27 16:56:36 -0400

    tests: adds returnPartialData test case

--- src/react/hooks/__tests__/useBackgroundQuery.test.tsx
+++ src/react/hooks/__tests__/useBackgroundQuery.test.tsx
@@ -2991,10 +2991,7 @@ describe('useBackgroundQuery', () => {
       ]);
     });
 
-    // returnPartialData
     it('does not suspend when partial data is in the cache and using a "cache-first" fetch policy with returnPartialData', async () => {
-      // const user = userEvent.setup();
-
       interface Data {
         character: {
           id: string;
@@ -3025,6 +3022,19 @@ describe('useBackgroundQuery', () => {
         },
       ];
 
+      interface Renders {
+        errors: Error[];
+        errorCount: number;
+        suspenseCount: number;
+        count: number;
+      }
+      const renders: Renders = {
+        errors: [],
+        errorCount: 0,
+        suspenseCount: 0,
+        count: 0,
+      };
+
       const cache = new InMemoryCache();
 
       cache.writeQuery({
@@ -3050,6 +3060,7 @@ describe('useBackgroundQuery', () => {
       }
 
       function SuspenseFallback() {
+        renders.suspenseCount++;
         return <p>Loading</p>;
       }
 
@@ -3066,52 +3077,38 @@ describe('useBackgroundQuery', () => {
       }: {
         queryRef: QueryReference<DeepPartial<Data>>;
       }) {
-        const { data, networkStatus } = useReadQuery(queryRef);
+        const { data, networkStatus, error } = useReadQuery(queryRef);
+        renders.count++;
 
         return (
-          <div data-testid="todos">
-            {networkStatus} - {data.character?.id} - {data.character?.name}
-          </div>
+          <>
+            <div data-testid="character-id">{data.character?.id}</div>
+            <div data-testid="character-name">{data.character?.name}</div>
+            <div data-testid="network-status">{networkStatus}</div>
+            <div data-testid="error">{error?.message || 'undefined'}</div>
+          </>
         );
       }
 
       render(<App />);
 
-      // expect(screen.getByText('Loading')).toBeInTheDocument();
+      expect(renders.suspenseCount).toBe(0);
+      expect(screen.getByTestId('character-id')).toHaveTextContent('1');
+      expect(screen.getByTestId('character-name')).toHaveTextContent('');
+      expect(screen.getByTestId('network-status')).toHaveTextContent('1'); // loading
+      expect(screen.getByTestId('error')).toHaveTextContent('undefined');
 
-      // expect(await screen.findByTestId('todos')).toBeInTheDocument();
+      await waitFor(() => {
+        expect(screen.getByTestId('character-name')).toHaveTextContent(
+          'Doctor Strange'
+        );
+      });
+      expect(screen.getByTestId('character-id')).toHaveTextContent('1');
+      expect(screen.getByTestId('network-status')).toHaveTextContent('7'); // ready
+      expect(screen.getByTestId('error')).toHaveTextContent('undefined');
 
-      // const todos = screen.getByTestId('todos');
-      // const todo1 = screen.getByTestId('todo:1');
-      // const button = screen.getByText('Load more');
-
-      // expect(todo1).toBeInTheDocument();
-
-      // await act(() => user.click(button));
-
-      // // startTransition will avoid rendering the suspense fallback for already
-      // // revealed content if the state update inside the transition causes the
-      // // component to suspend.
-      // //
-      // // Here we should not see the suspense fallback while the component suspends
-      // // until the todo is finished loading. Seeing the suspense fallback is an
-      // // indication that we are suspending the component too late in the process.
-      // expect(screen.queryByText('Loading')).not.toBeInTheDocument();
-
-      // // We can ensure this works with isPending from useTransition in the process
-      // expect(todos).toHaveAttribute('aria-busy', 'true');
-
-      // // Ensure we are showing the stale UI until the new todo has loaded
-      // expect(todo1).toHaveTextContent('Clean room');
-
-      // // Eventually we should see the updated todos content once its done
-      // // suspending.
-      // await waitFor(() => {
-      //   expect(screen.getByTestId('todo:2')).toHaveTextContent(
-      //     'Take out trash (completed)'
-      //   );
-      //   expect(todo1).toHaveTextContent('Clean room');
-      // });
+      expect(renders.count).toBe(2);
+      expect(renders.suspenseCount).toBe(0);
     });
 
     it.todo(

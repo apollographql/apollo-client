1d2c13a11307b1a15169b9e24fe05cd582f3a9a8
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2023-06-28 15:58:20 +0200

    forgot one

--- integration-tests/cra4/tests/playwright/apollo-client.test.ts
+++ integration-tests/cra4/tests/playwright/apollo-client.test.ts
@@ -4,7 +4,9 @@ import { test } from 'shared/fixture';
 test('Basic Test', async ({ page, withHar }) => {
   await page.goto('http://localhost:3000');
 
-  await expect(page.getByText('loading')).toBeVisible();
-  await expect(page.getByText('loading')).not.toBeVisible();
+  // sometimes the page just loads too fast, so we cannot check the loading state
+  // without being flimsy
+  // await expect(page.getByText('loading')).toBeVisible();
+  // await expect(page.getByText('loading')).not.toBeVisible();
   await expect(page.getByText('Soft Warm Apollo Beanie')).toBeVisible();
 });

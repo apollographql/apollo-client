fdd4dce2b50420c073dba4124ce6977c96021a5f
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2023-06-30 13:52:54 +0200

    add build comparison action

--- config/compare-build-output-to.sh
+++ config/compare-build-output-to.sh
@@ -1,26 +1,82 @@
+#!/usr/bin/env bash
+set -euo pipefail
 upstream=$1
 comparison=/tmp/comparison_checkout
 root=$(git rev-parse --show-toplevel)
 
+temp=$(mktemp)
+trap 'rm -f "$temp"' EXIT
+
 
 patterndiff(){
-  cd dist
-  find -name "$1" -exec bash -c "echo {}; diff <(cat '$comparison/dist/'{} | tr \"'\" '\"') <(cat {} | tr \"'\" '\"' ) -w" \; > ../diff."$1".diff
+  cd dist || { echo "dist folder not found"; exit 1; }
+  count=0
+  while IFS= read -r -d '' file
+  do
+    if ! filediff="$(diff <(tr "'" '"' < "$comparison/dist/$file") <(tr "'" '"' < "$file") -w)"; then
+      (( count++ ))
+      echo "$file"
+      echo "$filediff"
+    fi
+  done >"$temp" < <(find . -name "$1" -print0)
+
+  output="$(cat <"$temp")"
+
+  cat <<EOF
+
+## differences in $1 files
+
+<details>
+  <summary>
+
+### $count files with differences
+
+  </summary>
+
+\`\`\`diff
+
+$output
+
+\`\`\`
+
+</details>
+EOF
+
   cd ..
 }
 
 [ -z "$upstream" ] && { echo "need upstream argument"; exit 1; }
 
-git worktree add --force --detach --checkout "$comparison" "$upstream"
+git worktree add --force --detach --checkout "$comparison" "$upstream" || cd "$comparison" && git checkout "$upstream" || exit 1
 
-cd "$comparison"
-yarn
-yarn build
-cd "$root"
-yarn build
+cd "$comparison" || { echo "checkout failed"; exit 1; }
+cp -r "$root/node_modules" .
+npm i >&2
+npm run build >&2
+cd "$root" || exit 1
+npm run build >&2
 
 patterndiff "*.js"
 patterndiff "*.cjs"
 patterndiff "*.d.ts"
 
-diff -r "$comparison/dist" "dist" -x "*.map" -x "*.native.*" -x "*.js" -x "*.cjs" -x "*.d.ts" -w > diff.rest.diff
+cat <<EOF
+
+
+## differences in other files
+
+<details>
+  <summary>
+
+### $(diff -qr "$comparison/dist" "dist" -x "*.map" -x "*.native.*" -x "*.js" -x "*.cjs" -x "*.d.ts" -w | wc -l) files with differences
+
+  </summary>
+
+\`\`\`diff
+
+$(diff -r "$comparison/dist" "dist" -x "*.map" -x "*.native.*" -x "*.js" -x "*.cjs" -x "*.d.ts" -w)
+
+\`\`\`
+
+</details>
+EOF

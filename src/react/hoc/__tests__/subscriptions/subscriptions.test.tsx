b1a72b5c5deb983513cd1e7679d455a9f8274653
alessia <alessia@apollographql.com>
2023-07-14 16:56:34 -0400

    fix: subscriptions tests since React 17/18 rerenders are now guaranteed to be the same

--- src/react/hoc/__tests__/subscriptions/subscriptions.test.tsx
+++ src/react/hoc/__tests__/subscriptions/subscriptions.test.tsx
@@ -11,8 +11,6 @@ import { itAsync, MockSubscriptionLink } from '../../../../testing';
 import { graphql } from '../../graphql';
 import { ChildProps } from '../../types';
 
-const IS_REACT_18 = React.version.startsWith("18");
-
 describe('subscriptions', () => {
   let error: typeof console.error;
 
@@ -300,32 +298,17 @@ describe('subscriptions', () => {
               expect(loading).toBeFalsy();
               if (count === 0)
                 expect(user).toEqual(results[0].result.data.user);
-              if (count === 1) {
-                if (IS_REACT_18) {
-                  expect(user).toEqual(results[1].result.data.user);
-                } else {
-                  expect(user).toEqual(results[0].result.data.user);
-                }
-              }
+              if (count === 1)
+                expect(user).toEqual(results[0].result.data.user);
               if (count === 2)
                 expect(user).toEqual(results[2].result.data.user);
               if (count === 3)
                 expect(user).toEqual(results[2].result.data.user);
-              if (count === 4) {
-                if (IS_REACT_18) {
-                  expect(user).toEqual(results[2].result.data.user);
-                } else {
-                  expect(user).toEqual(results3[2].result.data.user);
-                }
-              }
-              if (count === 5) {
-                if (IS_REACT_18) {
-                  expect(user).toEqual(results3[3].result.data.user);
-                } else {
-                  expect(user).toEqual(results3[2].result.data.user);
-                }
-                resolve();
-              }
+              if (count === 4)
+                expect(user).toEqual(results3[2].result.data.user);
+              if (count === 5)
+                expect(user).toEqual(results3[2].result.data.user);
+              resolve();
             } catch (e) {
               reject(e);
             }

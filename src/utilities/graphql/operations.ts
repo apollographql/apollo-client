4473e925ac5d6a53dc2b34867f034eda1b05aa00
Jerel Miller <jerelmiller@gmail.com>
2023-07-17 09:35:05 -0600

    Update operation helpers to use raw strings instead of OperationTypeNode (#11071)

--- src/utilities/graphql/operations.ts
+++ src/utilities/graphql/operations.ts
@@ -1,19 +1,21 @@
-import { OperationTypeNode } from 'graphql';
 import type { DocumentNode } from '../../core/index.js';
 import { getOperationDefinition } from './getFromAST.js';
 
-function isOperation(document: DocumentNode, operation: OperationTypeNode) {
+function isOperation(
+  document: DocumentNode,
+  operation: 'query' | 'mutation' | 'subscription'
+) {
   return getOperationDefinition(document)?.operation === operation;
 }
 
 export function isMutationOperation(document: DocumentNode) {
-  return isOperation(document, OperationTypeNode.MUTATION);
+  return isOperation(document, 'mutation');
 }
 
 export function isQueryOperation(document: DocumentNode) {
-  return isOperation(document, OperationTypeNode.QUERY);
+  return isOperation(document, 'query');
 }
 
 export function isSubscriptionOperation(document: DocumentNode) {
-  return isOperation(document, OperationTypeNode.SUBSCRIPTION);
+  return isOperation(document, 'subscription');
 }

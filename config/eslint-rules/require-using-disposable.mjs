65e31716c7f363158309e990946f73e410c73837
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2023-09-07 13:17:06 +0200

    eslint rule, broken modules

---
+++ config/eslint-rules/require-using-disposable.mjs
@@ -0,0 +1,55 @@
+// @ts-check
+import { ESLintUtils } from "@typescript-eslint/utils";
+import { unionTypeParts, isObjectType } from "ts-api-tools";
+
+export const rule = ESLintUtils.RuleCreator.withoutDocs({
+  create(context) {
+    return {
+      VariableDeclaration(node) {
+        for (const declarator of node.declarations) {
+          if (!declarator.init) continue;
+          const services = ESLintUtils.getParserServices(context);
+          const type = services.getTypeAtLocation(declarator.init);
+          for (const typePart of unionTypeParts(type)) {
+            if (!isObjectType(typePart)) {
+              continue;
+            }
+            if (
+              // bad check, but will do for now
+              // in the future, we should check for a `[Symbol.disposable]` property
+              // but I have no idea how to use that right now
+              typePart.symbol.escapedName === "Disposable" &&
+              node.kind != "using"
+            ) {
+              context.report({
+                messageId: "missingUsing",
+                node: declarator,
+              });
+            }
+            if (
+              // bad check
+              typePart.symbol.escapedName === "AsyncDisposable" &&
+              node.kind != "await using"
+            ) {
+              context.report({
+                messageId: "missingAwaitUsing",
+                node: declarator,
+              });
+            }
+          }
+        }
+      },
+    };
+  },
+  meta: {
+    messages: {
+      missingUsing:
+        "Disposables should be allocated with `using <disposable>`.",
+      missingAwaitUsing:
+        "AsyncDisposables should be allocated with `await using <disposable>`.",
+    },
+    type: "suggestion",
+    schema: [],
+  },
+  defaultOptions: [],
+});

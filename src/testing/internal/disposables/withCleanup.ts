812d3dcbdfdbb825eee47eb430c7ca0ec69b92c1
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2023-09-13 16:54:34 +0200

    Update src/testing/internal/disposables/withCleanup.ts

--- src/testing/internal/disposables/withCleanup.ts
+++ src/testing/internal/disposables/withCleanup.ts
@@ -7,6 +7,8 @@ export function withCleanup<T extends object>(
     ...item,
     [Symbol.dispose]() {
       cleanup(item);
+      // if `item` already has a cleanup function, we also need to call the original cleanup function
+      // (e.g. if something is wrapped in `withCleanup` twice)
       if (Symbol.dispose in item) {
         (item as Disposable)[Symbol.dispose]();
       }

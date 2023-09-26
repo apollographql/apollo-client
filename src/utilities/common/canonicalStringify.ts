5d57cdaaac29668c4bb9fc60101d307cb6e4e9bb
Ben Newman <ben@apollographql.com>
2023-09-26 13:35:57 -0400

    Improve canonicalStringify implementation using SortingTrie.

--- src/utilities/common/canonicalStringify.ts
+++ src/utilities/common/canonicalStringify.ts
@@ -5,7 +5,10 @@ export const canonicalStringify = Object.assign(
   },
   {
     reset() {
-      // TODO
+      // Blowing away the root-level trie map will reclaim all memory stored in
+      // the trie, without affecting the logical results of canonicalStringify,
+      // but potentially sacrificing performance until the trie is refilled.
+      sortingTrieRoot.next = Object.create(null);
     },
   }
 );
@@ -22,19 +25,54 @@ function stableObjectReplacer(key: string, value: any) {
     // We don't want to mess with objects that are not "plain" objects, which
     // means their prototype is either Object.prototype or null.
     if (proto === Object.prototype || proto === null) {
-      const sorted = Object.create(null);
-      // TODO This sorting step can be sped up in two ways:
-      // 1. If the keys are already sorted, we can skip the sorting step.
-      // 2. If we have sorted this sequence of keys before, we can look up the
-      //    previously sorted sequence in linear time rather than O(n log n)
-      //    time, which is the typical cost of sorting.
-      Object.keys(value)
-        .sort()
-        .forEach((key) => {
+      const keys = Object.keys(value);
+      const sortedKeys = sortKeys(keys);
+      if (sortedKeys !== keys) {
+        const sorted = Object.create(null);
+        // Reassigning the keys in sorted order will cause JSON.stringify to
+        // serialize them in sorted order.
+        sortedKeys.forEach((key) => {
           sorted[key] = value[key];
         });
-      return sorted;
+        return sorted;
+      }
     }
   }
   return value;
+}
+
+interface SortingTrie {
+  sorted?: readonly string[];
+  next: Record<string, SortingTrie>;
+}
+
+const sortingTrieRoot: SortingTrie = {
+  sorted: [],
+  next: Object.create(null),
+};
+
+// Sort the given keys using a lookup trie, and return the same (===) array in
+// case it was already sorted, so we can avoid always creating a new object in
+// the replacer function above.
+function sortKeys(keys: readonly string[]): readonly string[] {
+  let node = sortingTrieRoot;
+  let alreadySorted = true;
+  for (let k = 0, len = keys.length; k < len; ++k) {
+    const key = keys[k];
+    if (k > 0 && keys[k - 1] > key) {
+      alreadySorted = false;
+    }
+    const next = node.next;
+    node = next[key] || (next[key] = { next: Object.create(null) });
+  }
+  if (alreadySorted) {
+    // There may already be a node.sorted array that's equivalent to the
+    // already-sorted keys array, but if keys was already sorted, we always want
+    // to return that array, not node.sorted.
+    return node.sorted ? keys : (node.sorted = keys);
+  }
+  // The .slice(0) is necessary so that we do not modify the original keys array
+  // by calling keys.sort(), and also so that we always return a new (!==)
+  // sorted array when keys was not already sorted.
+  return node.sorted || (node.sorted = keys.slice(0).sort());
 }

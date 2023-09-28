6cddaaf6543f5c0b1fb04ba47480fb393ba10de7
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2023-09-28 18:39:35 +0200

    fix `nextFetchPolicy` behaviour with transformed documents (#11235)

--- src/core/QueryManager.ts
+++ src/core/QueryManager.ts
@@ -1220,9 +1220,9 @@ export class QueryManager<TStore> {
     // The initial networkStatus for this fetch, most often
     // NetworkStatus.loading, but also possibly fetchMore, poll, refetch,
     // or setVariables.
-    networkStatus = NetworkStatus.loading
+    networkStatus = NetworkStatus.loading,
+    query = options.query
   ): ConcastAndInfo<TData> {
-    const { query } = options;
     const variables = this.getVariables(query, options.variables) as TVars;
     const queryInfo = this.getQuery(queryId);
 

609af3fcc540cee712efcd99e684893ae13f24f8
phryneas <phryneas@users.noreply.github.com>
2023-11-09 12:44:38 +0000

    Clean up Prettier, Size-limit, and Api-Extractor

--- src/invariantErrorCodes.ts
+++ src/invariantErrorCodes.ts
@@ -2,7 +2,7 @@ export interface ErrorCodes {
   [key: number]: { file: string; condition?: string; message?: string };
 }
 
-export const errorCodes:     ErrorCodes = {};
+export const errorCodes: ErrorCodes = {};
 export const devDebug: ErrorCodes = {};
 export const devLog: ErrorCodes = {};
 export const devWarn: ErrorCodes = {};

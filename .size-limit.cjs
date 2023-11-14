d98a5e90d00a4507b0f87f46d6f7072419c3b017
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2023-11-14 13:14:36 +0100

    size-limit

--- .size-limit.cjs
+++ .size-limit.cjs
@@ -1,7 +1,7 @@
 const checks = [
   {
     path: "dist/apollo-client.min.cjs",
-    limit: "38142",
+    limit: "38144",
   },
   {
     path: "dist/main.cjs",
@@ -10,7 +10,7 @@ const checks = [
   {
     path: "dist/index.js",
     import: "{ ApolloClient, InMemoryCache, HttpLink }",
-    limit: "32167",
+    limit: "32172",
   },
   ...[
     "ApolloProvider",

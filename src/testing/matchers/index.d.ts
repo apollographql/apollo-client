188dd0ae17f406cf9d95a86550fdcee38881facd
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2023-12-04 10:35:53 +0100

    remove unneccessary type

--- src/testing/matchers/index.d.ts
+++ src/testing/matchers/index.d.ts
@@ -10,11 +10,6 @@ import {
   ProfiledHook,
 } from "../internal/index.js";
 
-declare class WeakRef<T extends WeakKey> {
-  constructor(target: T);
-  deref(): T | undefined;
-}
-
 interface ApolloCustomMatchers<R = void, T = {}> {
   /**
    * Used to determine if two GraphQL query documents are equal to each other by
@@ -25,30 +20,33 @@ interface ApolloCustomMatchers<R = void, T = {}> {
   /**
    * Used to determine if the Suspense cache has a cache entry.
    */
-  toHaveSuspenseCacheEntryUsing: T extends ApolloClient<any> ?
-    (
-      query: DocumentNode,
-      options?: {
-        variables?: OperationVariables;
-        queryKey?: string | number | any[];
-      }
-    ) => R
-  : { error: "matcher needs to be called on an ApolloClient instance" };
+  toHaveSuspenseCacheEntryUsing: T extends ApolloClient<any>
+    ? (
+        query: DocumentNode,
+        options?: {
+          variables?: OperationVariables;
+          queryKey?: string | number | any[];
+        }
+      ) => R
+    : { error: "matcher needs to be called on an ApolloClient instance" };
 
-  toRerender: T extends (
-    Profiler<any, any> | ProfiledComponent<any, any> | ProfiledHook<any, any>
-  ) ?
-    (options?: NextRenderOptions) => Promise<R>
-  : { error: "matcher needs to be called on a ProfiledComponent instance" };
+  toRerender: T extends
+    | Profiler<any, any>
+    | ProfiledComponent<any, any>
+    | ProfiledHook<any, any>
+    ? (options?: NextRenderOptions) => Promise<R>
+    : { error: "matcher needs to be called on a ProfiledComponent instance" };
 
-  toRenderExactlyTimes: T extends (
-    Profiler<any, any> | ProfiledComponent<any, any> | ProfiledHook<any, any>
-  ) ?
-    (count: number, options?: NextRenderOptions) => Promise<R>
-  : { error: "matcher needs to be called on a ProfiledComponent instance" };
+  toRenderExactlyTimes: T extends
+    | Profiler<any, any>
+    | ProfiledComponent<any, any>
+    | ProfiledHook<any, any>
+    ? (count: number, options?: NextRenderOptions) => Promise<R>
+    : { error: "matcher needs to be called on a ProfiledComponent instance" };
 
-  toBeGarbageCollected: T extends WeakRef<any> ? () => Promise<R>
-  : { error: "matcher needs to be called on a WeakRef instance" };
+  toBeGarbageCollected: T extends WeakRef<any>
+    ? () => Promise<R>
+    : { error: "matcher needs to be called on a WeakRef instance" };
 }
 
 declare global {

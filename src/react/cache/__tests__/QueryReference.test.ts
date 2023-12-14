613a8801904724b5e8196008bab37aa4850eee41
Jerel Miller <jerelmiller@gmail.com>
2023-12-13 18:03:00 -0700

    Add test to ensure query ref kicks off request immediately

--- src/react/cache/__tests__/QueryReference.test.ts
+++ src/react/cache/__tests__/QueryReference.test.ts
@@ -1,4 +1,9 @@
-import { ApolloClient, InMemoryCache } from "../../../core";
+import {
+  ApolloClient,
+  ApolloLink,
+  InMemoryCache,
+  Observable,
+} from "../../../core";
 import { MockLink, wait } from "../../../testing";
 import { spyOnConsole, setupSimpleCase } from "../../../testing/internal";
 import { InternalQueryReference } from "../QueryReference";
@@ -25,4 +30,23 @@ test("warns when calling `retain` on a disposed query ref", async () => {
   expect(console.warn).toHaveBeenCalledWith(
     expect.stringContaining("'retain' was called on a disposed queryRef")
   );
+});
+
+test("kicks off request immediately when created", async () => {
+  const { query } = setupSimpleCase();
+  let fetchCount = 0;
+
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new ApolloLink((operation) => {
+      fetchCount++;
+      return Observable.of({ data: { greeting: "Hello" } });
+    }),
+  });
+
+  const observable = client.watchQuery({ query });
+
+  expect(fetchCount).toBe(0);
+  new InternalQueryReference(observable, {});
+  expect(fetchCount).toBe(1);
 });

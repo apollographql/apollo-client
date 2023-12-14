61c80a3af47caab33958233a1df8a378c5fef50c
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2023-12-14 12:23:15 +0100

    remove `expect.objectContaining`

--- src/utilities/caching/__tests__/getMemoryInternals.ts
+++ src/utilities/caching/__tests__/getMemoryInternals.ts
@@ -36,59 +36,57 @@ it("returns information about cache usage (empty caches)", () => {
       .concat(removeTypenameFromVariables())
       .concat(ApolloLink.empty()),
   });
-  expect(client.getMemoryInternals?.()).toEqual(
-    expect.objectContaining({
-      limits: {
-        canonicalStringify: 1000,
-        documentTransform: 2000,
-        executeSelectionSet: 10000,
-        executeSubSelectedArray: 5000,
-        fragmentQueryDocuments: 1000,
-        fragmentRegistryFindFragmentSpreads: 4000,
-        fragmentRegistryLookup: 1000,
-        fragmentRegistryTransform: 2000,
-        getVariableDefinitions: 2000,
-        maybeBroadcastWatch: 5000,
-        parser: 1000,
-        persistedQueryHashes: 2000,
-        print: 2000,
-        queryManagerTransforms: 2000,
-      },
-      sizes: {
-        cache: {
-          addTypenameTransform: [0],
-          fragmentQueryDocuments: 0,
-          fragmentRegistry: {
-            findFragmentSpreads: 0,
-            lookup: 0,
-            transform: 0,
-          },
-          maybeBroadcastWatch: 0,
-          storeReader: {
-            executeSelectionSet: 0,
-            executeSubSelectedArray: 0,
-          },
+  expect(client.getMemoryInternals?.()).toEqual({
+    limits: {
+      canonicalStringify: 1000,
+      documentTransform: 2000,
+      executeSelectionSet: 10000,
+      executeSubSelectedArray: 5000,
+      fragmentQueryDocuments: 1000,
+      fragmentRegistryFindFragmentSpreads: 4000,
+      fragmentRegistryLookup: 1000,
+      fragmentRegistryTransform: 2000,
+      getVariableDefinitions: 2000,
+      maybeBroadcastWatch: 5000,
+      parser: 1000,
+      persistedQueryHashes: 2000,
+      print: 2000,
+      queryManagerTransforms: 2000,
+    },
+    sizes: {
+      cache: {
+        addTypenameTransform: [0],
+        fragmentQueryDocuments: 0,
+        fragmentRegistry: {
+          findFragmentSpreads: 0,
+          lookup: 0,
+          transform: 0,
         },
-        global: {
-          canonicalStringify: 0,
-          parser: 0,
-          print: 0,
-        },
-        links: [
-          {
-            persistedQueryHashes: 0,
-          },
-          {
-            getVariableDefinitions: 0,
-          },
-        ],
-        queryManager: {
-          Transforms: 0,
-          documentTransforms: [0, 0],
+        maybeBroadcastWatch: 0,
+        storeReader: {
+          executeSelectionSet: 0,
+          executeSubSelectedArray: 0,
         },
       },
-    })
-  );
+      global: {
+        canonicalStringify: 0,
+        parser: 0,
+        print: 0,
+      },
+      links: [
+        {
+          persistedQueryHashes: 0,
+        },
+        {
+          getVariableDefinitions: 0,
+        },
+      ],
+      queryManager: {
+        Transforms: 0,
+        documentTransforms: [0, 0],
+      },
+    },
+  });
 });
 
 it("returns information about cache usage (some query triggered)", () => {
@@ -117,57 +115,55 @@ it("returns information about cache usage (some query triggered)", () => {
       }
     `,
   });
-  expect(client.getMemoryInternals?.()).toEqual(
-    expect.objectContaining({
-      limits: {
-        canonicalStringify: 1000,
-        documentTransform: 2000,
-        executeSelectionSet: 10000,
-        executeSubSelectedArray: 5000,
-        fragmentQueryDocuments: 1000,
-        fragmentRegistryFindFragmentSpreads: 4000,
-        fragmentRegistryLookup: 1000,
-        fragmentRegistryTransform: 2000,
-        getVariableDefinitions: 2000,
-        maybeBroadcastWatch: 5000,
-        parser: 1000,
-        persistedQueryHashes: 2000,
-        print: 2000,
-        queryManagerTransforms: 2000,
-      },
-      sizes: {
-        cache: {
-          addTypenameTransform: [1],
-          fragmentQueryDocuments: 0,
-          fragmentRegistry: {
-            findFragmentSpreads: 1,
-            lookup: 0,
-            transform: 1,
-          },
-          maybeBroadcastWatch: 0,
-          storeReader: {
-            executeSelectionSet: 1,
-            executeSubSelectedArray: 0,
-          },
+  expect(client.getMemoryInternals?.()).toStrictEqual({
+    limits: {
+      canonicalStringify: 1000,
+      documentTransform: 2000,
+      executeSelectionSet: 10000,
+      executeSubSelectedArray: 5000,
+      fragmentQueryDocuments: 1000,
+      fragmentRegistryFindFragmentSpreads: 4000,
+      fragmentRegistryLookup: 1000,
+      fragmentRegistryTransform: 2000,
+      getVariableDefinitions: 2000,
+      maybeBroadcastWatch: 5000,
+      parser: 1000,
+      persistedQueryHashes: 2000,
+      print: 2000,
+      queryManagerTransforms: 2000,
+    },
+    sizes: {
+      cache: {
+        addTypenameTransform: [1],
+        fragmentQueryDocuments: 0,
+        fragmentRegistry: {
+          findFragmentSpreads: 1,
+          lookup: 0,
+          transform: 1,
         },
-        global: {
-          canonicalStringify: 0,
-          parser: 0,
-          print: 1,
-        },
-        links: [
-          {
-            persistedQueryHashes: 1,
-          },
-          {
-            getVariableDefinitions: 0,
-          },
-        ],
-        queryManager: {
-          Transforms: 1,
-          documentTransforms: [1, 1],
+        maybeBroadcastWatch: 0,
+        storeReader: {
+          executeSelectionSet: 1,
+          executeSubSelectedArray: 0,
         },
       },
-    })
-  );
+      global: {
+        canonicalStringify: 0,
+        parser: 0,
+        print: 1,
+      },
+      links: [
+        {
+          persistedQueryHashes: 1,
+        },
+        {
+          getVariableDefinitions: 0,
+        },
+      ],
+      queryManager: {
+        Transforms: 1,
+        documentTransforms: [1, 1],
+      },
+    },
+  });
 });

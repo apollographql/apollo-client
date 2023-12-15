142c0e35a2225d342ad854c56011af405cc56ef6
Jerel Miller <jerelmiller@gmail.com>
2023-12-15 15:44:30 -0700

    Remove test that checks warning for retain on disposed query ref

--- src/react/cache/__tests__/QueryReference.test.ts
+++ src/react/cache/__tests__/QueryReference.test.ts
@@ -4,33 +4,8 @@ import {
   InMemoryCache,
   Observable,
 } from "../../../core";
-import { MockLink, wait } from "../../../testing";
-import { spyOnConsole, setupSimpleCase } from "../../../testing/internal";
+import { setupSimpleCase } from "../../../testing/internal";
 import { InternalQueryReference } from "../QueryReference";
-
-test("warns when calling `retain` on a disposed query ref", async () => {
-  using _consoleSpy = spyOnConsole("warn");
-  const { query, mocks } = setupSimpleCase();
-  const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    link: new MockLink(mocks),
-  });
-  const observable = client.watchQuery({ query });
-
-  const queryRef = new InternalQueryReference(observable, {});
-  const dispose = queryRef.retain();
-
-  dispose();
-
-  await wait(0);
-
-  queryRef.retain();
-
-  expect(console.warn).toHaveBeenCalledTimes(1);
-  expect(console.warn).toHaveBeenCalledWith(
-    expect.stringContaining("'retain' was called on a disposed queryRef")
-  );
-});
 
 test("kicks off request immediately when created", async () => {
   const { query } = setupSimpleCase();

7a0c6a93c99342f2fa5f60d7981745400be95700
Jerel Miller <jerelmiller@gmail.com>
2023-12-18 20:05:27 -0700

    Update test that checks changing variables with partial data to Profiler

--- src/react/hooks/__tests__/useBackgroundQuery.test.tsx
+++ src/react/hooks/__tests__/useBackgroundQuery.test.tsx
@@ -3155,6 +3155,7 @@ it('does not suspend when partial data is in the cache and using a "cache-and-ne
 });
 
 it('suspends and does not use partial data when changing variables and using a "cache-and-network" fetch policy with returnPartialData', async () => {
+  const { query, mocks } = setupVariablesCase();
   const partialQuery = gql`
     query ($id: ID!) {
       character(id: $id) {
@@ -3167,46 +3168,79 @@ it('suspends and does not use partial data when changing variables and using a "
 
   cache.writeQuery({
     query: partialQuery,
-    data: { character: { id: "1" } },
+    data: { character: { __typename: "Character", id: "1" } },
     variables: { id: "1" },
   });
 
-  const { renders, mocks, rerender } = renderVariablesIntegrationTest({
-    variables: { id: "1" },
-    cache,
-    options: {
+  const Profiler = createDefaultProfiler<DeepPartial<VariablesCaseData>>();
+  const { SuspenseFallback, ReadQueryHook } =
+    createDefaultTrackedComponents(Profiler);
+
+  function App({ id }: { id: string }) {
+    useTrackRenders();
+    const [queryRef] = useBackgroundQuery(query, {
       fetchPolicy: "cache-and-network",
       returnPartialData: true,
-    },
+      variables: { id },
+    });
+
+    return (
+      <Suspense fallback={<SuspenseFallback />}>
+        <ReadQueryHook queryRef={queryRef} />
+      </Suspense>
+    );
+  }
+
+  const { rerender } = renderWithMocks(<App id="1" />, {
+    cache,
+    mocks,
+    wrapper: Profiler,
   });
 
-  expect(renders.suspenseCount).toBe(0);
+  {
+    const { snapshot, renderedComponents } = await Profiler.takeRender();
 
-  expect(await screen.findByText("1 - Spider-Man")).toBeInTheDocument();
-
-  rerender({ variables: { id: "2" } });
-
-  expect(await screen.findByText("2 - Black Widow")).toBeInTheDocument();
-
-  expect(renders.count).toBe(3);
-  expect(renders.suspenseCount).toBe(1);
-  expect(renders.frames).toMatchObject([
-    {
-      data: { character: { id: "1" } },
+    expect(renderedComponents).toStrictEqual([App, ReadQueryHook]);
+    expect(snapshot.result).toEqual({
+      data: { character: { __typename: "Character", id: "1" } },
+      error: undefined,
       networkStatus: NetworkStatus.loading,
+    });
+  }
+
+  {
+    const { snapshot, renderedComponents } = await Profiler.takeRender();
+
+    expect(renderedComponents).toStrictEqual([ReadQueryHook]);
+    expect(snapshot.result).toEqual({
+      data: {
+        character: { __typename: "Character", id: "1", name: "Spider-Man" },
+      },
       error: undefined,
-    },
-    {
-      ...mocks[0].result,
       networkStatus: NetworkStatus.ready,
+    });
+  }
+
+  rerender(<App id="2" />);
+
+  {
+    const { renderedComponents } = await Profiler.takeRender();
+
+    expect(renderedComponents).toStrictEqual([App, SuspenseFallback]);
+  }
+
+  {
+    const { snapshot, renderedComponents } = await Profiler.takeRender();
+
+    expect(renderedComponents).toStrictEqual([ReadQueryHook]);
+    expect(snapshot.result).toEqual({
+      data: {
+        character: { __typename: "Character", id: "2", name: "Black Widow" },
+      },
       error: undefined,
-    },
-    {
-      ...mocks[1].result,
       networkStatus: NetworkStatus.ready,
-      error: undefined,
-    },
-  ]);
+    });
+  }
 });
 
 it('does not suspend deferred queries with partial data in the cache and using a "cache-first" fetch policy with `returnPartialData`', async () => {

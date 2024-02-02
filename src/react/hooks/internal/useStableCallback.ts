895af15e225f025715608ab57113abbb8818581b
Jerel Miller <jerelmiller@gmail.com>
2024-02-02 13:04:45 -0700

    Add comment on useStableCallback

--- src/react/hooks/internal/useStableCallback.ts
+++ src/react/hooks/internal/useStableCallback.ts
@@ -1,5 +1,13 @@
 import React from "rehackt";
 
+/**
+ * A hook that provides a stable reference to a function without suffering from
+ * stale closures. Useful to get a stable function reference for callback
+ * function options in other hooks. This avoids the need for the user to wrap
+ * each callback in a `useCallback`.
+ *
+ * @param unstableCallback - A callback function
+ */
 export function useStableCallback<TArgs extends unknown[], TReturn>(
   unstableCallback: (...args: TArgs) => TReturn
 ) {

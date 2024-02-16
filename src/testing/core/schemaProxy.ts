d3d0ed9b2218601e2861c7a3f4a98c3d920a0a17
Alessia Bellisario <github@bellisar.io>
2024-02-16 10:25:31 -0500

    feat: introduce schema proxy, copy graphql-tools/utils fns and license into separate folder

---
+++ src/testing/core/schemaProxy.ts
@@ -0,0 +1,36 @@
+import { addResolversToSchema } from "@graphql-tools/schema";
+import type { GraphQLSchema } from "graphql";
+import type { Resolvers } from "../../core/types.js";
+
+const proxiedSchema = (
+  schemaWithMocks: GraphQLSchema,
+  resolvers: Resolvers
+) => {
+  let targetSchema = addResolversToSchema({
+    schema: schemaWithMocks,
+    resolvers,
+  });
+
+  const schema = new Proxy(targetSchema, {
+    get(target, p) {
+      if (p === "use") {
+        return (newResolvers: typeof resolvers) =>
+          (targetSchema = addResolversToSchema({
+            schema: schemaWithMocks,
+            resolvers: {
+              ...resolvers,
+              ...newResolvers,
+            },
+          }));
+      }
+      if (typeof targetSchema[p] === "function") {
+        return targetSchema[p].bind(targetSchema);
+      }
+      return Reflect.get(target, p);
+    },
+  });
+
+  return schema;
+};
+
+export { proxiedSchema };

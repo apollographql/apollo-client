ec3775dade723c85a4c03188e4435f1a0229f692
Jerel Miller <jerelmiller@gmail.com>
2024-03-06 22:15:56 -0700

    Add wrapper makeVar for reactive vars

--- src/cache/devtools/reactiveVars.ts
+++ src/cache/devtools/reactiveVars.ts
@@ -1,5 +1,38 @@
 import { makeVar as orig_makeVar } from "../index.js";
+import { sendToDevtools } from "../../devtools/index.js";
 
-export function makeVar<T>(value: T) {
+interface MakeVarOptions {
+  displayName?: string;
+}
+
+let idCounter = 0;
+
+export function makeVar<T>(
+  value: T,
+  options: MakeVarOptions = Object.create(null)
+) {
+  if (__DEV__) {
+    const id = ++idCounter;
+    const { displayName } = options;
+
+    sendToDevtools({
+      type: "reactiveVar.register",
+      payload: { id, displayName, initialValue: value },
+    });
+
+    const rv = orig_makeVar(value);
+
+    rv.onNextChange(function onNextChange(newValue) {
+      sendToDevtools({
+        type: "reactiveVar.updateValue",
+        payload: { id, value: newValue },
+      });
+
+      rv.onNextChange(onNextChange);
+    });
+
+    return rv;
+  }
+
   return orig_makeVar(value);
 }

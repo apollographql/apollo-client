289993b8fb9d3dc948e27dd675e0c4f9f5d46488
Jerel Miller <jerelmiller@gmail.com>
2024-03-06 22:15:49 -0700

    Rename message to payload

--- src/devtools/messages.ts
+++ src/devtools/messages.ts
@@ -2,7 +2,7 @@ import { getDevtoolsConnector } from "./connector.js";
 
 export interface Message<TMessage extends Record<string, unknown>> {
   type: string;
-  message: TMessage;
+  payload: TMessage;
 }
 
 export function sendToDevtools<TMessage extends Record<string, unknown>>(

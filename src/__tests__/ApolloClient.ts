ed96057a01f43d37ad28e1dc918b1ac1613873cf
Jerel Miller <jerelmiller@gmail.com>
2024-05-13 12:31:17 -0600

    Enable test in ApolloClient for watchFragment with nonreactive

--- src/__tests__/ApolloClient.ts
+++ src/__tests__/ApolloClient.ts
@@ -2365,7 +2365,7 @@ describe("ApolloClient", () => {
     });
     // The @nonreactive directive can only be used on fields or fragment
     // spreads in queries, and currently has no effect here
-    it.failing("does not support the @nonreactive directive", async () => {
+    it("supports the @nonreactive directive", async () => {
       const cache = new InMemoryCache();
       const client = new ApolloClient({
         cache,
@@ -2416,18 +2416,9 @@ describe("ApolloClient", () => {
         },
       });
 
-      {
-        const result = await stream.takeNext();
-
-        expect(result).toEqual({
-          data: {
-            __typename: "Item",
-            id: 5,
-            text: "Item #5",
-          },
-          complete: true,
-        });
-      }
+      await expect(stream.takeNext()).rejects.toThrow(
+        new Error("Timeout waiting for next event")
+      );
     });
   });
 

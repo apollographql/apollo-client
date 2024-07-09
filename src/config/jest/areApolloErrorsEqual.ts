b91d34b0b78807fe054db67d21bd58eff3b4a225
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-07-09 14:36:26 +0200

    correctly test for error equality on jest

---
+++ src/config/jest/areApolloErrorsEqual.ts
@@ -0,0 +1,30 @@
+import type { ApolloError } from "../../errors/index.js";
+import type { Tester } from "@jest/expect-utils";
+function isApolloError(e: any): e is ApolloError {
+  return e instanceof Error && e.name == "ApolloError";
+}
+
+export const areApolloErrorsEqual: Tester = function (a, b, customTesters) {
+  const isAApolloError = isApolloError(a);
+  const isBApolloError = isApolloError(b);
+
+  // the user is not explicitly testing for an ApolloError
+  // so we don't do a deeper check
+  if (!isBApolloError) return undefined;
+
+  if (isAApolloError && isBApolloError) {
+    return (
+      a.message === b.message &&
+      this.equals(a.graphQLErrors, b.graphQLErrors, customTesters) &&
+      this.equals(a.protocolErrors, b.protocolErrors, customTesters) &&
+      this.equals(a.clientErrors, b.clientErrors, customTesters) &&
+      this.equals(a.networkError, b.networkError, customTesters) &&
+      this.equals(a.cause, b.cause, customTesters) &&
+      this.equals(a.extraInfo, b.extraInfo, customTesters)
+    );
+  } else if (isAApolloError === isBApolloError) {
+    return undefined;
+  } else {
+    return false;
+  }
+};

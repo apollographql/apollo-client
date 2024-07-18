eb0b55031fffb89b8bd850544c013b681e7f5d0a
Jerel Miller <jerelmiller@gmail.com>
2024-07-18 15:13:59 -0600

    Fix build issue with assigning __DEV__ for withProdMode

--- src/testing/internal/disposables/withProdMode.ts
+++ src/testing/internal/disposables/withProdMode.ts
@@ -2,9 +2,9 @@ import { withCleanup } from "./withCleanup.js";
 
 export function withProdMode() {
   const prev = { prevDEV: __DEV__ };
-  (globalThis as any).__DEV__ = false;
+  Object.defineProperty(globalThis, "__DEV__", { value: false });
 
   return withCleanup(prev, ({ prevDEV }) => {
-    (globalThis as any).__DEV__ = prevDEV;
+    Object.defineProperty(globalThis, "__DEV__", { value: prevDEV });
   });
 }

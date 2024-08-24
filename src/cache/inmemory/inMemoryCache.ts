7a30aa3a7a29f579baaeeea65d46c338b3b7112b
Jerel Miller <jerelmiller@gmail.com>
2024-08-23 21:18:38 -0600

    Override lookupFragment in inMemoryCache that uses the fragment config to lookup a fragment by name.

--- src/cache/inmemory/inMemoryCache.ts
+++ src/cache/inmemory/inMemoryCache.ts
@@ -3,7 +3,11 @@ import { invariant } from "../../utilities/globals/index.js";
 // Make builtins like Map and Set safe to use with non-extensible objects.
 import "./fixPolyfills.js";
 
-import type { DocumentNode, InlineFragmentNode } from "graphql";
+import type {
+  DocumentNode,
+  FragmentDefinitionNode,
+  InlineFragmentNode,
+} from "graphql";
 import type { OptimisticWrapperFunction } from "optimism";
 import { wrap } from "optimism";
 import { equal } from "@wry/equality";
@@ -533,6 +537,10 @@ export class InMemoryCache extends ApolloCache<NormalizedCacheObject> {
     typename: string
   ): boolean {
     return this.policies.fragmentMatches(fragment, typename);
+  }
+
+  public lookupFragment(fragmentName: string): FragmentDefinitionNode | null {
+    return this.config.fragments?.lookup(fragmentName) || null;
   }
 
   protected broadcastWatches(options?: BroadcastOptions) {

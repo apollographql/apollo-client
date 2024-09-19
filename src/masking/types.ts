f52378e246a255f369d3e0b94b874f147d3e4c7b
Jerel Miller <jerelmiller@gmail.com>
2024-09-19 13:13:28 -0600

    Don't use TypedDocumentNode for FragmentType

--- src/masking/types.ts
+++ src/masking/types.ts
@@ -11,11 +11,9 @@ export type MaskedDocumentNode<
   TVariables = { [key: string]: any },
 > = TypedDocumentNode<Masked<TData>, TVariables>;
 
-export type FragmentType<TDocumentType extends TypedDocumentNode<any, any>> =
-  TDocumentType extends TypedDocumentNode<infer TType, any> ?
-    [TType] extends [{ " $fragmentName"?: infer TKey }] ?
-      TKey extends string ?
-        { " $fragmentRefs"?: { [key in TKey]: TType } }
-      : never
+export type FragmentType<TData> =
+  [TData] extends [{ " $fragmentName"?: infer TKey }] ?
+    TKey extends string ?
+      { " $fragmentRefs"?: { [key in TKey]: TData } }
     : never
   : never;

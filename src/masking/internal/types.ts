ac482900ebd838fd924240cdcdf85a8333e97ffc
Jerel Miller <jerelmiller@gmail.com>
2024-09-19 15:39:16 -0600

    Unwrap fragment refs instead of relying on $masked field

--- src/masking/internal/types.ts
+++ src/masking/internal/types.ts
@@ -1,9 +1,10 @@
-import type { Prettify } from "../../utilities/index.js";
+import type { Prettify, UnionToIntersection } from "../../utilities/index.js";
 import type { DataMasking } from "../types.js";
 
 /** @internal */
-export type Unmask<TData> =
-  TData extends { " $unmasked": infer TUnmaskedData } ? TUnmaskedData : TData;
+export type Unmask<TData> = RemoveMaskedMarkers<
+  UnwrapFragmentRefs<RemoveFragmentName<TData>>
+>;
 
 /** @internal */
 export type MaybeMasked<TData> =
@@ -11,6 +12,26 @@ export type MaybeMasked<TData> =
   : DataMasking extends { enabled: true } ? RemoveMaskedMarkers<TData>
   : Unmask<TData>;
 
-type RemoveMaskedMarkers<TData> = Prettify<
-  Omit<TData, "__masked" | " $unmasked">
->;
+type UnwrapFragmentRefs<TData> =
+  TData extends { " $fragmentRefs"?: infer FragmentRefs } ?
+    FragmentRefs extends Record<string, any> ?
+      Prettify<
+        UnwrapFragmentRefs<RemoveFragmentRefs<TData>> &
+          CombineFragmentRefs<FragmentRefs>
+      >
+    : never
+  : TData extends object ? { [K in keyof TData]: UnwrapFragmentRefs<TData[K]> }
+  : TData extends Array<infer TItem> ? UnwrapFragmentRefs<TItem>
+  : TData;
+
+type CombineFragmentRefs<FragmentRefMap extends Record<string, any>> =
+  UnionToIntersection<
+    {
+      [K in keyof FragmentRefMap]: RemoveFragmentName<FragmentRefMap[K]>;
+    }[keyof FragmentRefMap]
+  >;
+
+type RemoveMaskedMarkers<TData> = Prettify<Omit<TData, "__masked">>;
+
+type RemoveFragmentName<T> = Omit<T, " $fragmentName">;
+type RemoveFragmentRefs<T> = Omit<T, " $fragmentRefs">;

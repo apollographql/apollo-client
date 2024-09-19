a0e5ea2ea0d4b574fcac9612a7288f2e88b6fd66
Jerel Miller <jerelmiller@gmail.com>
2024-09-19 11:02:30 -0600

    Add a FragmentType helper

--- src/masking/index.ts
+++ src/masking/index.ts
@@ -1,2 +1,7 @@
-export type { DataMasking, Masked, MaskedDocumentNode } from "./types.js";
+export type {
+  DataMasking,
+  FragmentType,
+  Masked,
+  MaskedDocumentNode,
+} from "./types.js";
 export type { MaybeMasked, Unmask } from "./internal/types.js";
--- src/masking/types.ts
+++ src/masking/types.ts
@@ -10,3 +10,12 @@ export type MaskedDocumentNode<
   TData = { [key: string]: any },
   TVariables = { [key: string]: any },
 > = TypedDocumentNode<Masked<TData>, TVariables>;
+
+export type FragmentType<TDocumentType extends TypedDocumentNode<any, any>> =
+  TDocumentType extends TypedDocumentNode<infer TType, any> ?
+    [TType] extends [{ " $fragmentName"?: infer TKey }] ?
+      TKey extends string ?
+        { " $fragmentRefs"?: { [key in TKey]: TType } }
+      : never
+    : never
+  : never;

2abed4b39af20dec0d7b87bacca86e3700f6dfc6
Jerel Miller <jerelmiller@gmail.com>
2024-09-24 10:23:15 -0600

    Create a DeepPrettify helper

--- src/utilities/types/Prettify.ts
+++ src/utilities/types/Prettify.ts
@@ -1,3 +1,4 @@
-export type Prettify<T> = {
+export type Prettify<T> = { [K in keyof T]: T[K] } & {};
+export type DeepPrettify<T> = {
   [K in keyof T]: T[K] extends object ? Prettify<T[K]> : T[K];
 } & {};

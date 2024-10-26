582a850d8db65e49be2d503fd20f7c998c612c3a
Jerel Miller <jerelmiller@gmail.com>
2024-10-26 21:50:30 +0100

    Export observables from rxjs

--- src/utilities/observables/Observable.ts
+++ src/utilities/observables/Observable.ts
@@ -1,26 +1,5 @@
-import type {
-  Observer,
+export { Observable } from "rxjs";
+export type {
+  PartialObserver as Observer,
   Subscription as ObservableSubscription,
-  Subscriber,
-} from "zen-observable-ts";
-import { Observable } from "zen-observable-ts";
-
-// This simplified polyfill attempts to follow the ECMAScript Observable
-// proposal (https://github.com/zenparsing/es-observable)
-import "symbol-observable";
-
-export type { Observer, ObservableSubscription, Subscriber };
-
-// The zen-observable package defines Observable.prototype[Symbol.observable]
-// when Symbol is supported, but RxJS interop depends on also setting this fake
-// '@@observable' string as a polyfill for Symbol.observable.
-const { prototype } = Observable;
-const fakeObsSymbol = "@@observable" as keyof typeof prototype;
-if (!prototype[fakeObsSymbol]) {
-  // @ts-expect-error
-  prototype[fakeObsSymbol] = function () {
-    return this;
-  };
-}
-
-export { Observable };
+} from "rxjs";

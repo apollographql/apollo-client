fc7c7237f0429686d0f2c89209f5ed269400061c
Jerel Miller <jerelmiller@gmail.com>
2024-11-13 12:26:42 -0700

    Add additional example with extra spaces

--- scripts/codemods/data-masking/examples/queries.ts
+++ scripts/codemods/data-masking/examples/queries.ts
@@ -29,3 +29,84 @@ export const GetCurrentUser = gql`
 
   ${CurrentUserFieldsFragment}
 `;
+
+export const SKIP_TO_NEXT_MUTATION = gql`
+  mutation SkipToNextMutation {
+    skipToNext {
+      playbackState {
+        progressMs
+        item {
+          __typename
+          ... on Track {
+            id
+            name
+            album {
+              id
+              name
+              images {
+                url
+              }
+            }
+            artists {
+              id
+              name
+            }
+          }
+
+          ... on Episode {
+            id
+            name
+            show {
+              id
+              name
+              images {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const SKIP_TO_NEXT_MUTATION_WITH_FRAGMENT = gql`
+  mutation SkipToNextMutation {
+    skipToNext {
+      playbackState {
+        progressMs
+        item {
+          __typename
+          ... on Track {
+            id
+            name
+            album {
+              id
+              name
+              images {
+                url
+              }
+            }
+            artists {
+              id
+              name
+            }
+            ...TrackItem_track
+          }
+
+          ... on Episode {
+            id
+            name
+            show {
+              id
+              name
+              images {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;

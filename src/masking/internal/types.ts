ccaa6fcdffd6869910d644aae285ef2d50f9c9e1
Jerel Miller <jerelmiller@gmail.com>
2024-11-15 08:57:09 -0700

    Distribute over TData instead of using inner type

--- src/masking/internal/types.ts
+++ src/masking/internal/types.ts
@@ -1,23 +1,21 @@
 import type { Prettify, UnionToIntersection } from "../../utilities/index.js";
 
 export type UnwrapFragmentRefs<TData> =
-  // Leave TData alone if it is Record<string, any> and not a specific shape
-  string extends keyof NonNullable<TData> ? TData
-  : " $fragmentRefs" extends keyof NonNullable<TData> ?
-    null extends TData ?
-      ExtractFragmentRefs<TData> | null
-    : ExtractFragmentRefs<TData>
-  : TData extends object ? { [K in keyof TData]: UnwrapFragmentRefs<TData[K]> }
-  : TData;
-
-type ExtractFragmentRefs<TData> =
-  TData extends { " $fragmentRefs"?: infer FragmentRefs extends object } ?
-    Prettify<
-      {
-        [K in keyof TData as K extends " $fragmentRefs" ? never
-        : K]: UnwrapFragmentRefs<TData[K]>;
-      } & CombineFragmentRefs<FragmentRefs>
-    >
+  TData extends any ?
+    // Leave TData alone if it is Record<string, any> and not a specific shape
+    string extends keyof NonNullable<TData> ? TData
+    : " $fragmentRefs" extends keyof NonNullable<TData> ?
+      TData extends { " $fragmentRefs"?: infer FragmentRefs extends object } ?
+        Prettify<
+          {
+            [K in keyof TData as K extends " $fragmentRefs" ? never
+            : K]: UnwrapFragmentRefs<TData[K]>;
+          } & CombineFragmentRefs<FragmentRefs>
+        >
+      : never
+    : TData extends object ?
+      { [K in keyof TData]: UnwrapFragmentRefs<TData[K]> }
+    : TData
   : never;
 
 type CombineFragmentRefs<FragmentRefs extends Record<string, any>> =

96898dde7fef546e92d0b31cd03ee2ddd7b121cc
Jerel Miller <jerelmiller@gmail.com>
2024-11-26 16:42:29 -0700

    Add toComplete matcher

--- src/testing/matchers/index.ts
+++ src/testing/matchers/index.ts
@@ -3,10 +3,12 @@ import { toMatchDocument } from "./toMatchDocument.js";
 import { toHaveSuspenseCacheEntryUsing } from "./toHaveSuspenseCacheEntryUsing.js";
 import { toBeGarbageCollected } from "./toBeGarbageCollected.js";
 import { toBeDisposed } from "./toBeDisposed.js";
+import { toComplete } from "./toComplete.js";
 import { toEmitAnything } from "./toEmitAnything.js";
 import { toEmitNextValue } from "./toEmitNextValue.js";
 
 expect.extend({
+  toComplete,
   toEmitAnything,
   toEmitNextValue,
   toBeDisposed,

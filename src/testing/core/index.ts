81a39e323f45b23fb0db6aba65e5b84334e5961b
Jerel Miller <jerelmiller@gmail.com>
2024-12-13 13:03:46 -0700

    Remove subscribeAndCount from testing utilities

--- src/testing/core/index.ts
+++ src/testing/core/index.ts
@@ -9,7 +9,6 @@ export {
   mockObservableLink,
 } from "./mocking/mockSubscriptionLink.js";
 export { createMockClient } from "./mocking/mockClient.js";
-export { default as subscribeAndCount } from "./subscribeAndCount.js";
 export { itAsync } from "./itAsync.js";
 export { wait, tick } from "./wait.js";
 export * from "./withConsoleSpy.js";

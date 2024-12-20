972805d076ce97459064d27000c62b801ab5a9cb
Jerel Miller <jerelmiller@gmail.com>
2024-12-20 09:49:59 -0700

    Default TVariables to OperationVariables

--- src/react/hooks/useSuspenseFragment.ts
+++ src/react/hooks/useSuspenseFragment.ts
@@ -46,7 +46,7 @@ const NULL_PLACEHOLDER = [] as unknown as [
 
 export function useSuspenseFragment<
   TData,
-  TVariables extends OperationVariables,
+  TVariables extends OperationVariables = OperationVariables,
 >(
   options: UseSuspenseFragmentOptions<TData, TVariables> & {
     from: {};
@@ -55,7 +55,7 @@ export function useSuspenseFragment<
 
 export function useSuspenseFragment<
   TData,
-  TVariables extends OperationVariables,
+  TVariables extends OperationVariables = OperationVariables,
 >(
   options: UseSuspenseFragmentOptions<TData, TVariables> & {
     from: null;
@@ -64,7 +64,7 @@ export function useSuspenseFragment<
 
 export function useSuspenseFragment<
   TData,
-  TVariables extends OperationVariables,
+  TVariables extends OperationVariables = OperationVariables,
 >(
   options: UseSuspenseFragmentOptions<TData, TVariables> & {
     from: {} | null;
@@ -73,7 +73,7 @@ export function useSuspenseFragment<
 
 export function useSuspenseFragment<
   TData,
-  TVariables extends OperationVariables,
+  TVariables extends OperationVariables = OperationVariables,
 >(
   options: UseSuspenseFragmentOptions<TData, TVariables>
 ): UseSuspenseFragmentResult<TData>;

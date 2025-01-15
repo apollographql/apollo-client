49c7b76d99473525f512f4e5716368593b65d90a
Jerel Miller <jerelmiller@gmail.com>
2025-01-14 17:14:58 -0700

    Rewrite test with renderHookToSnapshotStream and toEqualQueryResult

--- src/react/hooks/__tests__/useQuery.test.tsx
+++ src/react/hooks/__tests__/useQuery.test.tsx
@@ -5462,41 +5462,80 @@ describe("useQuery Hook", () => {
         </MockedProvider>
       );
 
-      const { result } = renderHook(
-        () =>
-          useQuery(query, {
-            variables: { limit: 2 },
-            notifyOnNetworkStatusChange: true,
-          }),
-        { wrapper }
-      );
+      using _disabledAct = disableActEnvironment();
+      const { takeSnapshot, getCurrentSnapshot } =
+        await renderHookToSnapshotStream(
+          () =>
+            useQuery(query, {
+              variables: { limit: 2 },
+              notifyOnNetworkStatusChange: true,
+            }),
+          { wrapper }
+        );
 
-      expect(result.current.loading).toBe(true);
-      expect(result.current.networkStatus).toBe(NetworkStatus.loading);
-      expect(result.current.data).toBe(undefined);
+      {
+        const result = await takeSnapshot();
 
-      await waitFor(
-        () => {
-          expect(result.current.loading).toBe(false);
-        },
-        { interval: 1 }
-      );
-      expect(result.current.networkStatus).toBe(NetworkStatus.ready);
-      expect(result.current.data).toEqual({ letters: ab });
+        expect(result).toEqualQueryResult({
+          data: undefined,
+          called: true,
+          loading: true,
+          networkStatus: NetworkStatus.loading,
+          previousData: undefined,
+          variables: { limit: 2 },
+        });
+      }
 
-      act(() => void result.current.fetchMore({ variables: { limit: 2 } }));
-      expect(result.current.loading).toBe(true);
-      expect(result.current.networkStatus).toBe(NetworkStatus.fetchMore);
-      expect(result.current.data).toEqual({ letters: ab });
+      {
+        const result = await takeSnapshot();
 
-      await waitFor(
-        () => {
-          expect(result.current.loading).toBe(false);
-        },
-        { interval: 1 }
-      );
-      expect(result.current.networkStatus).toBe(NetworkStatus.ready);
-      expect(result.current.data).toEqual({ letters: ab.concat(cd) });
+        expect(result).toEqualQueryResult({
+          data: { letters: ab },
+          called: true,
+          loading: false,
+          networkStatus: NetworkStatus.ready,
+          previousData: undefined,
+          variables: { limit: 2 },
+        });
+      }
+
+      const fetchMoreResult = await getCurrentSnapshot().fetchMore({
+        variables: { limit: 2 },
+      });
+
+      expect(fetchMoreResult).toEqualApolloQueryResult({
+        data: { letters: cd },
+        loading: false,
+        networkStatus: NetworkStatus.ready,
+      });
+
+      {
+        const result = await takeSnapshot();
+
+        expect(result).toEqualQueryResult({
+          data: { letters: ab },
+          called: true,
+          loading: true,
+          networkStatus: NetworkStatus.fetchMore,
+          previousData: { letters: ab },
+          variables: { limit: 2 },
+        });
+      }
+
+      {
+        const result = await takeSnapshot();
+
+        expect(result).toEqualQueryResult({
+          data: { letters: ab.concat(cd) },
+          called: true,
+          loading: false,
+          networkStatus: NetworkStatus.ready,
+          previousData: { letters: ab },
+          variables: { limit: 2 },
+        });
+      }
+
+      await expect(takeSnapshot).not.toRerender();
     });
 
     // https://github.com/apollographql/apollo-client/issues/11965

fffaee042418ba99844c4eb8b30c46db3008d7e8
Jerel Miller <jerelmiller@gmail.com>
2025-01-22 12:10:50 -0700

    Remove MockedProvider from testing entrypoint

--- src/testing/index.ts
+++ src/testing/index.ts
@@ -1,4 +1,2 @@
 import "../utilities/globals/index.js";
-export type { MockedProviderProps } from "./react/MockedProvider.js";
-export { MockedProvider } from "./react/MockedProvider.js";
 export * from "./core/index.js";

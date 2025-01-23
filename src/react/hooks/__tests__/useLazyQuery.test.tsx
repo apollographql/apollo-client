6af1df1eee4e659781d56fa5058a82b1c34175cb
Jerel Miller <jerelmiller@gmail.com>
2025-01-22 18:03:06 -0700

    Update test to remove variables from options

--- src/react/hooks/__tests__/useLazyQuery.test.tsx
+++ src/react/hooks/__tests__/useLazyQuery.test.tsx
@@ -160,7 +160,7 @@ describe("useLazyQuery Hook", () => {
     }
   });
 
-  it("should use variables defined in hook options (if any), when running the lazy execution function", async () => {
+  it("should use variables passed to execute function, when running the lazy execution function", async () => {
     const query = gql`
       query ($id: number) {
         hello(id: $id)
@@ -177,17 +177,11 @@ describe("useLazyQuery Hook", () => {
 
     using _disabledAct = disableActEnvironment();
     const { takeSnapshot, getCurrentSnapshot } =
-      await renderHookToSnapshotStream(
-        () =>
-          useLazyQuery(query, {
-            variables: { id: 1 },
-          }),
-        {
-          wrapper: ({ children }) => (
-            <MockedProvider mocks={mocks}>{children}</MockedProvider>
-          ),
-        }
-      );
+      await renderHookToSnapshotStream(() => useLazyQuery(query), {
+        wrapper: ({ children }) => (
+          <MockedProvider mocks={mocks}>{children}</MockedProvider>
+        ),
+      });
 
     {
       const [, result] = await takeSnapshot();
@@ -198,12 +192,12 @@ describe("useLazyQuery Hook", () => {
         loading: false,
         networkStatus: NetworkStatus.ready,
         previousData: undefined,
-        variables: { id: 1 },
+        variables: undefined,
       });
     }
 
     const execute = getCurrentSnapshot()[0];
-    setTimeout(() => execute());
+    setTimeout(() => execute({ variables: { id: 1 } }));
 
     {
       const [, result] = await takeSnapshot();

b0c8aba6e4dbd3e31c0a46e19ca5364a127004e6
Michael Ferris <micfer@getmaintainx.com>
2025-01-26 17:20:07 -0500

    PR Review

--- .changeset/khaki-cars-develop.md
+++ .changeset/khaki-cars-develop.md
@@ -4,4 +4,4 @@
 
 Deprecate option `ignoreResults` in `useMutation`.
 Once this option is removed, existing code still using it might see increase in re-renders.
-Instead, please use `useApolloClient` to get your ApolloClient instance and call `client.mutate` directly.
+If you don't want to synchronize your component state with the mutation, please use `useApolloClient` to get your ApolloClient instance and call `client.mutate` directly.
--- src/react/types/types.ts
+++ src/react/types/types.ts
@@ -362,8 +362,7 @@ export interface BaseMutationOptions<
    * {@inheritDoc @apollo/client!MutationOptionsDocumentation#ignoreResults:member}
    *
    * @deprecated This property will be removed in the next major version of Apollo Client.
-   * Once this option is removed, existing code still using it might see increase in re-renders.
-   * Instead, please use `useApolloClient` to get your ApolloClient instance and call `client.mutate` directly.
+   * If you don't want to synchronize your component state with the mutation, please use `useApolloClient` to get your ApolloClient instance and call `client.mutate` directly.
    */
   ignoreResults?: boolean;
 }

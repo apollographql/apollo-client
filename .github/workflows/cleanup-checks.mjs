0abb79ff225bee06200b2676fc3ff9ccbdc21492
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-01-28 15:12:03 +0100

    log

--- .github/workflows/cleanup-checks.mjs
+++ .github/workflows/cleanup-checks.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 /** @param {import('@actions/github-script').AsyncFunctionArguments} AsyncFunctionArguments */
 export function setup({ context, github }) {
+  console.log({ context, github });
   return {
     async add_cleanup_label() {
       await github.rest.issues.addLabels({

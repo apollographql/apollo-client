06358929f62c1d28bba0805ae57bb6e1b623964d
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-02-07 17:35:42 +0100

    remove draft file, add links to issue

--- .changeset/heavy-pumas-boil.md
+++ .changeset/heavy-pumas-boil.md
@@ -3,3 +3,4 @@
 ---
 
 Deprecate the `onCompleted` and `onError` callbacks of `useQuery` and `useLazyQuery`.
+For more context, please see the [related issue](https://github.com/apollographql/apollo-client/issues/12352) on GitHub.
--- src/react/types/types.ts
+++ src/react/types/types.ts
@@ -71,14 +71,14 @@ export interface QueryFunctionOptions<
    * {@inheritDoc @apollo/client!QueryOptionsDocumentation#onCompleted:member}
    *
    * @deprecated This option will be removed in the next major version of Apollo Client.
-   * For more context, please see the [related issue]() on GitHub.
+   * For more context, please see the [related issue](https://github.com/apollographql/apollo-client/issues/12352) on GitHub.
    */
   onCompleted?: (data: MaybeMasked<TData>) => void;
   /**
    * {@inheritDoc @apollo/client!QueryOptionsDocumentation#onError:member}
    *
    * @deprecated This option will be removed in the next major version of Apollo Client.
-   * For more context, please see the [related issue]() on GitHub.
+   * For more context, please see the [related issue](https://github.com/apollographql/apollo-client/issues/12352) on GitHub.
    */
   onError?: (error: ApolloError) => void;
 
@@ -194,14 +194,14 @@ export interface LazyQueryHookOptions<
    * {@inheritDoc @apollo/client!QueryOptionsDocumentation#onCompleted:member}
    *
    * @deprecated This option will be removed in the next major version of Apollo Client.
-   * For more context, please see the [related issue]() on GitHub.
+   * For more context, please see the [related issue](https://github.com/apollographql/apollo-client/issues/12352) on GitHub.
    */
   onCompleted?: (data: MaybeMasked<TData>) => void;
   /**
    * {@inheritDoc @apollo/client!QueryOptionsDocumentation#onError:member}
    *
    * @deprecated This option will be removed in the next major version of Apollo Client.
-   * For more context, please see the [related issue]() on GitHub.
+   * For more context, please see the [related issue](https://github.com/apollographql/apollo-client/issues/12352) on GitHub.
    */
   onError?: (error: ApolloError) => void;
 

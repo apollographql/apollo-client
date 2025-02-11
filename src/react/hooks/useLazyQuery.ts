8afc78877444ecb71c104861c17123f20e9a93f3
Jerel Miller <jerelmiller@gmail.com>
2025-02-11 15:37:03 -0700

    Handle partial data changing between renders

--- src/react/hooks/useLazyQuery.ts
+++ src/react/hooks/useLazyQuery.ts
@@ -387,6 +387,7 @@ export function useLazyQuery<
       errorPolicy: stableOptions?.errorPolicy,
       context: stableOptions?.context,
       refetchWritePolicy: stableOptions?.refetchWritePolicy,
+      returnPartialData: stableOptions?.returnPartialData,
     });
   }, [observable, stableOptions]);
 
@@ -429,7 +430,12 @@ export function useLazyQuery<
         )
       ) {
         dirtyRef.current = false;
-        updateResult({ ...observable.getCurrentResult(), data: undefined });
+        const currentResult = observable.getCurrentResult();
+        updateResult({
+          ...currentResult,
+          data:
+            stableOptions?.returnPartialData ? currentResult.data : undefined,
+        });
         forceUpdateState();
       }
 

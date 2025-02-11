da856e96b6608bebaf8825016f02516e285604e5
Jerel Miller <jerelmiller@gmail.com>
2025-02-11 11:51:53 -0700

    Remove unneeded ?

--- src/react/hooks/useLazyQuery.ts
+++ src/react/hooks/useLazyQuery.ts
@@ -339,7 +339,7 @@ export function useLazyQuery<
       ...observableResult,
       client,
       previousData: previousDataRef.current,
-      variables: observable?.variables,
+      variables: observable.variables,
       observable,
       called: !!resultRef.current,
     }),

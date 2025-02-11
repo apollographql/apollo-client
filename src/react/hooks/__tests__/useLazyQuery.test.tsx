cc358b829d1921cb710a9e86c0f4df1ffc4650e4
Jerel Miller <jerelmiller@gmail.com>
2025-02-11 12:58:42 -0700

    Add todo comment

--- src/react/hooks/__tests__/useLazyQuery.test.tsx
+++ src/react/hooks/__tests__/useLazyQuery.test.tsx
@@ -1105,6 +1105,7 @@ describe("useLazyQuery Hook", () => {
       variables: { id: 2 },
     });
 
+    // TODO: Determine if we should have a render here without notifyOnNetworkStatusChange
     {
       const [, result] = await takeSnapshot();
 

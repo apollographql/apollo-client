d3f8f130718ef50531ca0079192c2672a513814a
Jerel Miller <jerelmiller@gmail.com>
2025-02-14 11:26:29 -0700

    Export `UseSuspenseFragmentOptions` (#12375)

--- src/react/hooks/index.ts
+++ src/react/hooks/index.ts
@@ -11,7 +11,10 @@ export type { UseSuspenseQueryResult } from "./useSuspenseQuery.js";
 export { useSuspenseQuery } from "./useSuspenseQuery.js";
 export type { UseBackgroundQueryResult } from "./useBackgroundQuery.js";
 export { useBackgroundQuery } from "./useBackgroundQuery.js";
-export type { UseSuspenseFragmentResult } from "./useSuspenseFragment.js";
+export type {
+  UseSuspenseFragmentResult,
+  UseSuspenseFragmentOptions,
+} from "./useSuspenseFragment.js";
 export { useSuspenseFragment } from "./useSuspenseFragment.js";
 export type {
   LoadQueryFunction,

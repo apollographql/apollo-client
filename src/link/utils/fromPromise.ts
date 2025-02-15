76723a9b537e30e50f565676503adbe8d1168b07
Jerel Miller <jerelmiller@gmail.com>
2025-02-15 10:08:27 -0700

    Use rxjs observable in fromPromise

--- src/link/utils/fromPromise.ts
+++ src/link/utils/fromPromise.ts
@@ -1,5 +1,6 @@
-import { Observable } from "../../utilities/index.js";
+import { Observable } from "rxjs";
 
+// TODO: Determine if we can use an rxjs util instead
 export function fromPromise<T>(promise: Promise<T>): Observable<T> {
   return new Observable<T>((observer) => {
     promise

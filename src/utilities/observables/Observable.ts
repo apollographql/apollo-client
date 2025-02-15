f9f28852706545da0f7fecbbb8a2631b1387fc99
Jerel Miller <jerelmiller@gmail.com>
2025-02-15 11:54:57 -0700

    Don't export zen observable Observable

--- src/utilities/observables/Observable.ts
+++ src/utilities/observables/Observable.ts
@@ -3,7 +3,6 @@ import type {
   Subscription as ObservableSubscription,
   Subscriber,
 } from "zen-observable-ts";
-import { Observable } from "zen-observable-ts";
 
 // This simplified polyfill attempts to follow the ECMAScript Observable
 // proposal (https://github.com/zenparsing/es-observable)
@@ -18,21 +17,4 @@ export type {
 
   /** @deprecated */
   Subscriber,
-};
-
-// The zen-observable package defines Observable.prototype[Symbol.observable]
-// when Symbol is supported, but RxJS interop depends on also setting this fake
-// '@@observable' string as a polyfill for Symbol.observable.
-const { prototype } = Observable;
-const fakeObsSymbol = "@@observable" as keyof typeof prototype;
-if (!prototype[fakeObsSymbol]) {
-  // @ts-expect-error
-  prototype[fakeObsSymbol] = function () {
-    return this;
-  };
-}
-
-export {
-  /** @deprecated */
-  Observable,
 };

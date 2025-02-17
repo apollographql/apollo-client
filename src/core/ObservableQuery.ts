597475042fa0cebb43b5e036dfbfa00597106023
Jerel Miller <jerelmiller@gmail.com>
2025-02-16 23:00:29 -0700

    Update hasObservers to return subject.observed

--- src/core/ObservableQuery.ts
+++ src/core/ObservableQuery.ts
@@ -1256,7 +1256,7 @@ Did you mean to call refetch(variables) instead of refetch({ variables })?`,
   }
 
   public hasObservers() {
-    return this.observers.size > 0;
+    return this.subject.observed;
   }
 
   private tearDownQuery() {

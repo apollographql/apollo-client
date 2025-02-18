2f1dd1328959461339f9ef5f678a333a116c0e53
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-02-18 12:45:59 +0100

    remove dead code

--- config/helpers.ts
+++ config/helpers.ts
@@ -13,45 +13,6 @@ import { relative } from "node:path";
 
 export const distDir = path.resolve(import.meta.dirname, "..", "dist");
 
-export function eachFile(
-  dir: string,
-  callback: (absPath: string, relPath: string) => any
-) {
-  const promises: Promise<any>[] = [];
-
-  return new Promise<void>((resolve, reject) => {
-    glob(`${dir.replace(/\\/g, "/")}/**/*.js`, (error, files) => {
-      if (error) return reject(error);
-
-      files.sort().forEach((file) => {
-        const relPath = path.relative(dir, file);
-
-        // Outside the distDir, somehow.
-        if (relPath.startsWith("../")) return;
-
-        // Avoid re-transforming CommonJS bundle files.
-        if (relPath.endsWith(".cjs")) return;
-        if (relPath.endsWith(".cjs.js")) return;
-        if (relPath.endsWith(".cjs.native.js")) return;
-
-        // Avoid re-transforming CommonJS bundle files.
-        if (relPath.endsWith(".min.js")) return;
-
-        // This file is not meant to be imported or processed.
-        if (relPath.endsWith("invariantErrorCodes.js")) return;
-
-        promises.push(
-          new Promise((resolve) => {
-            resolve(callback(file, relPath));
-          })
-        );
-      });
-
-      resolve();
-    });
-  }).then(() => Promise.all(promises));
-}
-
 export function reparse(source: string) {
   return recast.parse(source, { parser });
 }

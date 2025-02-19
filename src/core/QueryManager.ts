7e844ed180ef93a192546f2c7823f1e540d18890
Jerel Miller <jerelmiller@gmail.com>
2025-02-18 22:03:31 -0700

    Call error before cleanup when cancelling

--- src/core/QueryManager.ts
+++ src/core/QueryManager.ts
@@ -1365,9 +1365,8 @@ export class QueryManager<TStore> {
       subject.complete();
     };
     this.fetchCancelFns.set(queryId, (reason) => {
+      subject.error(reason);
       cleanupCancelFn();
-      // This delay ensures the concast variable has been initialized.
-      setTimeout(() => subject.error(reason));
     });
 
     const subject = new Subject<ApolloQueryResult<TData>>();

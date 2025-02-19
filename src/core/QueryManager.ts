ffc67a95f7c3c5632c2834a5242fa67d1b172757
Jerel Miller <jerelmiller@gmail.com>
2025-02-18 17:46:11 -0700

    Rename variable

--- src/core/QueryManager.ts
+++ src/core/QueryManager.ts
@@ -1332,7 +1332,7 @@ export class QueryManager<TStore> {
       // WatchQueryOptions object.
       normalized.variables = variables;
 
-      const sourcesWithInfo = this.fetchQueryByPolicy<TData, TVars>(
+      const observableWithInfo = this.fetchQueryByPolicy<TData, TVars>(
         queryInfo,
         normalized,
         networkStatus
@@ -1350,7 +1350,7 @@ export class QueryManager<TStore> {
         );
       }
 
-      return sourcesWithInfo;
+      return observableWithInfo;
     };
 
     // This cancel function needs to be set before the concast is created,

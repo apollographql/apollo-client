c6e4f20c1d66e683c12aa2b9da14ca6e1ab39245
Jerel Miller <jerelmiller@gmail.com>
2025-02-19 23:25:54 -0700

    Wrap in act

--- src/react/hooks/__tests__/useSuspenseQuery.test.tsx
+++ src/react/hooks/__tests__/useSuspenseQuery.test.tsx
@@ -4090,14 +4090,16 @@ describe("useSuspenseQuery", () => {
       });
     });
 
-    client.writeQuery({
-      query,
-      data: {
-        currentUser: {
-          id: "1",
-          name: "Cache User",
+    act(() => {
+      client.writeQuery({
+        query,
+        data: {
+          currentUser: {
+            id: "1",
+            name: "Cache User",
+          },
         },
-      },
+      });
     });
 
     await waitFor(() => {

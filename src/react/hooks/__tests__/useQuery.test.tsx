69d2c81883463ab69ef1966f0ad8fc3996531cd4
Jerel Miller <jerelmiller@gmail.com>
2025-02-19 17:27:18 -0700

    Update assertion

--- src/react/hooks/__tests__/useQuery.test.tsx
+++ src/react/hooks/__tests__/useQuery.test.tsx
@@ -6530,6 +6530,7 @@ describe("useQuery Hook", () => {
           error: new ApolloError({
             networkError: new Error("This is an error!"),
           }),
+          errors: [],
           called: true,
           loading: false,
           networkStatus: NetworkStatus.error,

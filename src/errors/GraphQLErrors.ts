b6d8b7fd7540cbbf8dc9c2d124af910734c53149
Jerel Miller <jerelmiller@gmail.com>
2025-03-03 21:21:55 -0700

    Create a GraphQLErrors class

---
+++ src/errors/GraphQLErrors.ts
@@ -0,0 +1,18 @@
+import type { GraphQLFormattedError } from "graphql";
+
+export class GraphQLErrors extends Error {
+  errors: Array<GraphQLFormattedError>;
+
+  constructor(errors: Array<GraphQLFormattedError>) {
+    super(formatMessage(errors));
+    this.errors = errors;
+    this.name = "GraphQLErrors";
+  }
+}
+
+function formatMessage(errors: Array<GraphQLFormattedError>) {
+  const messageList = errors.map((error) => `- ${error.message}`).join("\n");
+
+  return `The GraphQL server returned the following errors:
+${messageList}`;
+}

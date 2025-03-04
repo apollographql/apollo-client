abd2807f3ad899536557fade4debf9867b6dcede
Jerel Miller <jerelmiller@gmail.com>
2025-03-03 21:26:59 -0700

    Change error type to unknown in ApolloQueryResult

--- src/core/types.ts
+++ src/core/types.ts
@@ -148,7 +148,7 @@ export interface ApolloQueryResult<T> {
    * This will contain both a NetworkError field and any GraphQLErrors.
    * See https://www.apollographql.com/docs/react/data/error-handling/ for more information.
    */
-  error?: ApolloError;
+  error?: unknown;
   loading: boolean;
   networkStatus: NetworkStatus;
   /**

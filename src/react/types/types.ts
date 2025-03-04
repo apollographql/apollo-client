dcf9837efa2193aeccc2285ad66a91633557995a
Jerel Miller <jerelmiller@gmail.com>
2025-03-04 11:59:32 -0700

    Switch all error types in react types to Error

--- src/react/types/types.ts
+++ src/react/types/types.ts
@@ -20,7 +20,6 @@ import type {
   WatchQueryFetchPolicy,
   WatchQueryOptions,
 } from "@apollo/client/core";
-import type { ApolloError } from "@apollo/client/errors";
 import type { FetchResult } from "@apollo/client/link/core";
 import type { MaybeMasked, Unmasked } from "@apollo/client/masking";
 import type { OnlyRequiredProperties } from "@apollo/client/utilities";
@@ -272,7 +271,7 @@ export interface BaseMutationOptions<
     clientOptions?: BaseMutationOptions
   ) => void;
   /** {@inheritDoc @apollo/client!MutationOptionsDocumentation#onError:member} */
-  onError?: (error: ApolloError, clientOptions?: BaseMutationOptions) => void;
+  onError?: (error: Error, clientOptions?: BaseMutationOptions) => void;
 }
 
 export interface MutationFunctionOptions<
@@ -289,7 +288,7 @@ export interface MutationResult<TData = any> {
   /** {@inheritDoc @apollo/client!MutationResultDocumentation#data:member} */
   data?: MaybeMasked<TData> | null;
   /** {@inheritDoc @apollo/client!MutationResultDocumentation#error:member} */
-  error?: ApolloError;
+  error?: Error;
   /** {@inheritDoc @apollo/client!MutationResultDocumentation#loading:member} */
   loading: boolean;
   /** {@inheritDoc @apollo/client!MutationResultDocumentation#called:member} */
@@ -380,7 +379,7 @@ export interface BaseSubscriptionOptions<
   /** {@inheritDoc @apollo/client!SubscriptionOptionsDocumentation#onSubscriptionData:member} */
   onSubscriptionData?: (options: OnSubscriptionDataOptions<TData>) => any;
   /** {@inheritDoc @apollo/client!SubscriptionOptionsDocumentation#onError:member} */
-  onError?: (error: ApolloError) => void;
+  onError?: (error: Error) => void;
   /** {@inheritDoc @apollo/client!SubscriptionOptionsDocumentation#onSubscriptionComplete:member} */
   onSubscriptionComplete?: () => void;
   /**
@@ -396,7 +395,7 @@ export interface SubscriptionResult<TData = any, TVariables = any> {
   /** {@inheritDoc @apollo/client!SubscriptionResultDocumentation#data:member} */
   data?: MaybeMasked<TData>;
   /** {@inheritDoc @apollo/client!SubscriptionResultDocumentation#error:member} */
-  error?: ApolloError;
+  error?: Error;
   // This was added by the legacy useSubscription type, and is tested in unit
   // tests, but probably shouldn’t be added to the result.
   /**

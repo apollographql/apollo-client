21492fad35e7c782c906fc79b710ae8dd1feb9f8
Jerel Miller <jerelmiller@gmail.com>
2025-03-04 15:26:49 -0700

    Remove throwServerError helper in favor of new ServerError

--- src/link/utils/index.ts
+++ src/link/utils/index.ts
@@ -1,5 +1,3 @@
-export type { ServerError } from "./throwServerError.js";
-export { throwServerError } from "./throwServerError.js";
 export { validateOperation } from "./validateOperation.js";
 export { createOperation } from "./createOperation.js";
 export { transformOperation } from "./transformOperation.js";

e2874b57ebaa19c111cd538a01069c7a00169b6e
Jerel Miller <jerelmiller@gmail.com>
2025-03-03 17:09:35 -0700

    Remove unneeded handling for errors not error set

--- src/react/hooks/useQuery.ts
+++ src/react/hooks/useQuery.ts
@@ -558,14 +558,6 @@ function setResult<TData, TVariables extends OperationVariables>(
     resultData.previousData = previousResult.data;
   }
 
-  if (!nextResult.error && isNonEmptyArray(nextResult.errors)) {
-    // Until a set naming convention for networkError and graphQLErrors is
-    // decided upon, we map errors (graphQLErrors) to the error options.
-    // TODO: Is it possible for both result.error and result.errors to be
-    // defined here?
-    nextResult.error = new ApolloError({ graphQLErrors: nextResult.errors });
-  }
-
   resultData.current = toQueryResult(
     nextResult,
     resultData.previousData,

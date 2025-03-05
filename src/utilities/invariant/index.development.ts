bdab5c9a3314058499a49fdd49452d43a6619f0d
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-03-05 12:26:45 +0100

    disable eslint rule for wrapping reexport

--- src/utilities/invariant/index.development.ts
+++ src/utilities/invariant/index.development.ts
@@ -1,5 +1,6 @@
 import { loadDevMessages, loadErrorMessages } from "@apollo/client/dev";
 export * from "./index.js";
+// eslint-disable-next-line local-rules/import-from-export
 import { invariant as origInvariant } from "./index.js";
 export const invariant = () => {
   // side effects in an IIFE

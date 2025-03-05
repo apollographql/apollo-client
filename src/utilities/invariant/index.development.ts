1eabbf9373994f9099dc9fa15fbae8d69aede2e4
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-03-05 15:38:46 +0100

    actually call IIFE

--- src/utilities/invariant/index.development.ts
+++ src/utilities/invariant/index.development.ts
@@ -2,9 +2,9 @@ import { loadDevMessages, loadErrorMessages } from "@apollo/client/dev";
 export * from "./index.js";
 // eslint-disable-next-line local-rules/import-from-export
 import { invariant as origInvariant } from "./index.js";
-export const invariant = () => {
+export const invariant = (() => {
   // side effects in an IIFE
   loadDevMessages();
   loadErrorMessages();
   return origInvariant;
-};
+})();

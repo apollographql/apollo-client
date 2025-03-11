64bb6ec77ffbccd9b6f05dcfea05079123b92973
Jerel Miller <jerelmiller@gmail.com>
2025-03-11 11:19:04 -0600

    Stop checking errors in ObservableQuery

--- src/core/ObservableQuery.ts
+++ src/core/ObservableQuery.ts
@@ -328,10 +328,7 @@ export class ObservableQuery<
       // are cases where sometimes `error` is set, but not `errors` and
       // vice-versa. This will be updated in the next major version when
       // `errors` is deprecated in favor of `error`.
-      if (
-        result.networkStatus === NetworkStatus.ready &&
-        (result.error || result.errors)
-      ) {
+      if (result.networkStatus === NetworkStatus.ready && result.error) {
         result.networkStatus = NetworkStatus.error;
       }
 

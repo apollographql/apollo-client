c24ed4e70c8aac070d4a2f70150ad38f91f9a9f6
Jerel Miller <jerelmiller@gmail.com>
2025-03-12 12:26:32 -0600

    Add docblock to UnconventionalError

--- src/errors/UnconventionalError.ts
+++ src/errors/UnconventionalError.ts
@@ -1,3 +1,8 @@
+/**
+ * A wrapper error type that represents a non-standard error thrown from a
+ * response, such as a symbol or plain object. Read the `cause` property to
+ * determine the source of the error.
+ */
 export class UnconventionalError extends Error {
   constructor(errorType: unknown) {
     super("An error of unexpected shape occurred.", { cause: errorType });

77230f94b76b5fb1ce94a7d58725184251953ce9
Jerel Miller <jerelmiller@gmail.com>
2025-03-19 16:30:05 -0600

    Use setOptions in QueryReference and useQuery

--- src/react/hooks/useQuery.ts
+++ src/react/hooks/useQuery.ts
@@ -513,7 +513,7 @@ function useResubscribeIfNecessary<
     // subscriptions, though it does feel less than ideal that reobserve
     // (potentially) kicks off a network request (for example, when the
     // variables have changed), which is technically a side-effect.
-    observable.reobserve(
+    observable.setOptions(
       getObsQueryOptions(observable, client, watchQueryOptions)
     );
 
--- src/react/internal/cache/QueryReference.ts
+++ src/react/internal/cache/QueryReference.ts
@@ -354,7 +354,7 @@ export class InternalQueryReference<TData = unknown> {
       currentFetchPolicy === "standby" &&
       currentFetchPolicy !== watchQueryOptions.fetchPolicy
     ) {
-      this.initiateFetch(this.observable.reobserve(watchQueryOptions));
+      this.initiateFetch(this.observable.setOptions(watchQueryOptions));
     } else {
       this.observable.silentSetOptions(watchQueryOptions);
     }

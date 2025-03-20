7ffe51d1705d0a13da589a076805f96b47153c77
Jerel Miller <jerelmiller@gmail.com>
2025-03-19 19:46:28 -0600

    Export UseQueryResult from useQuery

--- src/react/hooks/index.ts
+++ src/react/hooks/index.ts
@@ -1,6 +1,7 @@
 export * from "./useApolloClient.js";
 export * from "./useLazyQuery.js";
 export * from "./useMutation.js";
+export type { UseQueryResult } from "./useQuery.js";
 export { useQuery } from "./useQuery.js";
 export * from "./useSubscription.js";
 export * from "./useReactiveVar.js";

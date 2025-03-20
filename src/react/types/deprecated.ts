cb3b829c09c281eecd80bd5805cbf6a3db5c31ed
Jerel Miller <jerelmiller@gmail.com>
2025-03-20 15:23:17 -0600

    Add deprecated useLazyQuery types

--- src/react/types/deprecated.ts
+++ src/react/types/deprecated.ts
@@ -1,5 +1,11 @@
 import type { OperationVariables } from "@apollo/client/core";
-import type { useQuery } from "@apollo/client/react/hooks";
+import type { useLazyQuery, useQuery } from "@apollo/client/react/hooks";
+
+/** @deprecated Use `useQuery.Options` instead */
+export type QueryHookOptions<
+  TData = unknown,
+  TVariables extends OperationVariables = OperationVariables,
+> = useQuery.Options<TData, TVariables>;
 
 /** @deprecated Use `useQuery.Result` instead */
 export type QueryResult<
@@ -7,8 +13,31 @@ export type QueryResult<
   TVariables extends OperationVariables = OperationVariables,
 > = useQuery.Result<TData, TVariables>;
 
-/** @deprecated Use `useQuery.options` instead */
-export type QueryHookOptions<
+/** @deprecated Use `useLazyQuery.Options` instead */
+export type LazyQueryHookOptions<
   TData = unknown,
   TVariables extends OperationVariables = OperationVariables,
-> = useQuery.Options<TData, TVariables>;
+> = useLazyQuery.Options<TData, TVariables>;
+
+/** @deprecated Use `useLazyQuery.Result` instead */
+export type LazyQueryResult<
+  TData = unknown,
+  TVariables extends OperationVariables = OperationVariables,
+> = useLazyQuery.Result<TData, TVariables>;
+
+/** @deprecated Use `useLazyQuery.ResultTuple` instead */
+export type LazyQueryResultTuple<
+  TData = unknown,
+  TVariables extends OperationVariables = OperationVariables,
+> = useLazyQuery.ResultTuple<TData, TVariables>;
+
+/** @deprecated Use `useLazyQuery.ExecOptions` instead */
+export type LazyQueryHookExecOptions<
+  TVariables extends OperationVariables = OperationVariables,
+> = useLazyQuery.ExecOptions<TVariables>;
+
+/** @deprecated Use `useLazyQuery.ExecOptions` instead */
+export type LazyQueryExecFunction<
+  TData = unknown,
+  TVariables extends OperationVariables = OperationVariables,
+> = useLazyQuery.ExecFunction<TData, TVariables>;

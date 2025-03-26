03c930e8dfc13c7fb0d88290412051b2ca252ca3
Jerel Miller <jerelmiller@gmail.com>
2025-03-26 15:29:44 -0600

    Add a hook for useObservableQueryValue

--- src/react/hooks/internal/index.ts
+++ src/react/hooks/internal/index.ts
@@ -1,5 +1,6 @@
 // These hooks are used internally and are not exported publicly by the library
 export { useDeepMemo } from "./useDeepMemo.js";
+export { useObservableQueryValue } from "./useObservableQueryValue.js";
 export { useRenderGuard } from "./useRenderGuard.js";
 export { __use } from "./__use.js";
 export { wrapHook } from "./wrapHook.js";

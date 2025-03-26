874b255024ee7cde9ac3cdae0de508f4e63facb8
Jerel Miller <jerelmiller@gmail.com>
2025-03-25 23:11:30 -0600

    Ensure default returnPartialData is false

--- src/core/ObservableQuery.ts
+++ src/core/ObservableQuery.ts
@@ -1157,7 +1157,7 @@ Did you mean to call refetch(variables) instead of refetch({ variables })?`,
   }
 
   private getInitialResult(): ApolloQueryResult<TData> {
-    const { variables, fetchPolicy, returnPartialData } = this.options;
+    const { variables, fetchPolicy, returnPartialData = false } = this.options;
     const defaultResult = {
       data: undefined,
       loading: true,

b15f6acd02e8fa96e04f6a15aff4753c99796b18
Jerel Miller <jerelmiller@gmail.com>
2025-04-01 16:21:58 -0600

    Add loading states to client tests

--- src/__tests__/client.ts
+++ src/__tests__/client.ts
@@ -729,6 +729,7 @@ describe("client", () => {
           hello
         }
       `,
+      notifyOnNetworkStatusChange: false,
     });
 
     const stream = new ObservableStream(observable);
@@ -3672,6 +3673,13 @@ describe("@connection", () => {
       void obs.refetch();
 
       await expect(stream).toEmitTypedValue({
+        data: { count: "initial" },
+        loading: true,
+        networkStatus: NetworkStatus.refetch,
+        partial: false,
+      });
+
+      await expect(stream).toEmitTypedValue({
         data: { count: 0 },
         loading: false,
         networkStatus: NetworkStatus.ready,
@@ -3695,6 +3703,13 @@ describe("@connection", () => {
       expect(nextFetchPolicyCallCount).toBe(3);
 
       client.cache.evict({ fieldName: "count" });
+
+      await expect(stream).toEmitTypedValue({
+        data: undefined,
+        loading: true,
+        networkStatus: NetworkStatus.loading,
+        partial: true,
+      });
 
       await expect(stream).toEmitTypedValue({
         data: { count: 1 },
@@ -3770,6 +3785,12 @@ describe("@connection", () => {
 
       expect(fetchPolicyRecord).toEqual(["cache-first", "network-only"]);
 
+      await expect(stream).toEmitTypedValue({
+        data: { linkCount: 1 },
+        loading: true,
+        networkStatus: NetworkStatus.refetch,
+        partial: false,
+      });
       await expect(stream).toEmitTypedValue({
         data: { linkCount: 2 },
         loading: false,
@@ -5282,6 +5303,15 @@ describe("custom document transforms", () => {
 
     expect(data).toEqual({
       products: [{ __typename: "Product", id: 2 }],
+    });
+
+    await expect(stream).toEmitTypedValue({
+      data: {
+        products: [{ __typename: "Product", id: 1, metrics: "1000/vpm" }],
+      },
+      loading: true,
+      networkStatus: NetworkStatus.fetchMore,
+      partial: false,
     });
 
     await expect(stream).toEmitTypedValue({

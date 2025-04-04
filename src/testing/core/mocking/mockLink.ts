fd8c3a36e348403000345f40dd2a290c544b53df
Jerel Miller <jerelmiller@gmail.com>
2025-04-04 13:11:45 -0600

    Export MockedRequest

--- src/testing/core/mocking/mockLink.ts
+++ src/testing/core/mocking/mockLink.ts
@@ -29,7 +29,7 @@ export type ResultFunction<T, V = Record<string, any>> = CovariantUnaryFunction<
 
 type VariableMatcher<V> = CovariantUnaryFunction<V, boolean>;
 
-interface MockedRequest<TVariables = Record<string, any>> {
+export interface MockedRequest<TVariables = Record<string, any>> {
   query: DocumentNode;
   variables?: TVariables | VariableMatcher<TVariables>;
 }

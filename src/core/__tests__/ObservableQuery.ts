643be164cbcd7b38aa71ad8bc86f4485c50044bb
Jerel Miller <jerelmiller@gmail.com>
2025-04-07 14:20:15 -0600

    Update another test with expected behavior

--- src/core/__tests__/ObservableQuery.ts
+++ src/core/__tests__/ObservableQuery.ts
@@ -3923,6 +3923,13 @@ describe("ObservableQuery", () => {
     const stream = new ObservableStream(observable);
 
     await expect(stream).toEmitTypedValue({
+      data: undefined,
+      loading: true,
+      networkStatus: NetworkStatus.loading,
+      partial: true,
+    });
+
+    await expect(stream).toEmitTypedValue({
       data: dataOne,
       loading: false,
       networkStatus: NetworkStatus.ready,

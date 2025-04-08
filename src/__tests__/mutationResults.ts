8bc09cce3e9957d37d1a78419ca0b6b28c70a47f
Jerel Miller <jerelmiller@gmail.com>
2025-04-07 22:27:11 -0600

    Add loading state to mutationResults test

--- src/__tests__/mutationResults.ts
+++ src/__tests__/mutationResults.ts
@@ -1289,6 +1289,13 @@ describe("mutation results", () => {
 
     const stream = new ObservableStream(watchedQuery);
 
+    await expect(stream).toEmitTypedValue({
+      data: undefined,
+      loading: true,
+      networkStatus: NetworkStatus.loading,
+      partial: true,
+    });
+
     await watchedQuery.refetch(variables2);
 
     await expect(stream).toEmitTypedValue({

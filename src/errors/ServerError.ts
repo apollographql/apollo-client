04cef1f16859ebd57e2db8baec10d8690e58acfb
Jerel Miller <jerelmiller@gmail.com>
2025-04-10 16:12:48 -0600

    Add static is to ServerError

--- src/errors/ServerError.ts
+++ src/errors/ServerError.ts
@@ -1,3 +1,5 @@
+import type { ErrorLike } from "@apollo/client";
+
 interface ServerErrorOptions {
   response: Response;
   result: Record<string, any> | string;
@@ -7,6 +9,17 @@ interface ServerErrorOptions {
  * Thrown when a non-200 response is returned from the server.
  */
 export class ServerError extends Error {
+  /** Determine if an error is a `ServerError` instance */
+  static is(error: ErrorLike): error is ServerError {
+    return (
+      error instanceof ServerError ||
+      // Fallback to check for the name property in case there are multiple
+      // versions of Apollo Client installed, or something else causes
+      // instanceof to return false.
+      error.name === "ServerError"
+    );
+  }
+
   /**
    * The server response.
    */

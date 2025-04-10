7d4fa046a27adf4ccafd42fd097faf80ced1d89d
Jerel Miller <jerelmiller@gmail.com>
2025-04-10 16:10:55 -0600

    Add static is to UnconventionalError

--- src/errors/UnconventionalError.ts
+++ src/errors/UnconventionalError.ts
@@ -1,9 +1,22 @@
+import type { ErrorLike } from "@apollo/client";
+
 /**
  * A wrapper error type that represents a non-standard error thrown from a
  * response, such as a symbol or plain object. Read the `cause` property to
  * determine the source of the error.
  */
 export class UnconventionalError extends Error {
+  /** Determine if an error is an `UnconventionalError` instance */
+  static is(error: ErrorLike): error is UnconventionalError {
+    return (
+      error instanceof UnconventionalError ||
+      // Fallback to check for the name property in case there are multiple
+      // versions of Apollo Client installed, or something else causes
+      // instanceof to return false.
+      error.name === "UnconventionalError"
+    );
+  }
+
   constructor(errorType: unknown) {
     super("An error of unexpected shape occurred.", { cause: errorType });
     this.name = "UnconventionalError";

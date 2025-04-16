8eb8296cef49ea09fb6aa7904ad843ec3c136f5e
Jerel Miller <jerelmiller@gmail.com>
2025-04-15 21:20:47 -0600

    Add tsdoc

--- src/errors/NetworkError.ts
+++ src/errors/NetworkError.ts
@@ -2,6 +2,11 @@ import type { ErrorLike } from "@apollo/client";
 
 import { brand, isBranded } from "./utils.js";
 
+/**
+ * A wrapper error type that wraps errors emitted from the link chain. Useful if
+ * you have custom error types in your application and you want to differentiate
+ * errors that come from the link chain.
+ */
 export class NetworkError extends Error {
   /** Determine if an error is a `NetworkError` instance */
   static is(error: unknown): error is NetworkError {

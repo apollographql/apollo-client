260a11a271af574bccfec628f7b48a72f64a46fd
Jerel Miller <jerelmiller@gmail.com>
2025-04-15 21:31:57 -0600

    Add equality tester for NetworkError

--- src/config/jest/setup.ts
+++ src/config/jest/setup.ts
@@ -21,6 +21,7 @@ import { areCombinedGraphQLErrorsEqual } from "./areCombinedGraphQLErrorsEqual.j
 import { areCombinedProtocolErrorsEqual } from "./areCombinedProtocolErrorsEqual.js";
 import { areGraphQLErrorsEqual } from "./areGraphQlErrorsEqual.js";
 import { areMissingFieldErrorsEqual } from "./areMissingFieldErrorsEqual.js";
+import { areNetworkErrorsEqual } from "./areNetworkErrorsEqual.js";
 import { areServerErrorsEqual } from "./areServerErrorsEqual.js";
 
 setLogVerbosity("log");
@@ -65,6 +66,7 @@ expect.addEqualityTesters([
   areCombinedProtocolErrorsEqual,
   areGraphQLErrorsEqual,
   areMissingFieldErrorsEqual,
+  areNetworkErrorsEqual,
 ]);
 
 // not available in JSDOM 🙄

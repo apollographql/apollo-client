2863fdaa26f5ebcedcdbd3810e4321e2ca2fab9b
Jerel Miller <jerelmiller@gmail.com>
2025-04-18 17:12:52 -0600

    Add createOperationWithDefaultContext

--- src/testing/internal/link.ts
+++ src/testing/internal/link.ts
@@ -1,5 +1,6 @@
 import type { ApolloContext, ApolloLink, GraphQLRequest } from "@apollo/client";
 import { ApolloClient, execute, InMemoryCache } from "@apollo/client";
+import { createOperation } from "@apollo/client/link/utils";
 
 function createDefaultApolloContext() {
   return { client: new ApolloClient({ cache: new InMemoryCache() }) };
@@ -11,4 +12,12 @@ export function executeWithDefaultContext(
   apolloContext: ApolloContext = createDefaultApolloContext()
 ) {
   return execute(link, operation, apolloContext);
+}
+
+export function createOperationWithDefaultContext(
+  context: any,
+  operation: GraphQLRequest,
+  apolloContext: ApolloContext = createDefaultApolloContext()
+) {
+  return createOperation(context, operation, apolloContext);
 }

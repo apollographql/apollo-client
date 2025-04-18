2d771922f5bbe09a712dbbac29c07645866ea3bc
Jerel Miller <jerelmiller@gmail.com>
2025-04-18 15:17:08 -0600

    Add some tests for client.query context

---
+++ src/core/__tests__/client.query/context.ts
@@ -0,0 +1,113 @@
+import { of } from "rxjs";
+
+import type { Operation } from "@apollo/client";
+import { ApolloClient, ApolloLink, gql, InMemoryCache } from "@apollo/client";
+
+test("can get apollo context from getApolloContext", async () => {
+  const query = gql`
+    query {
+      greeting
+    }
+  `;
+  const request = jest.fn((_: Operation) => {
+    return of({ data: { greeting: "Hello" } });
+  });
+
+  const cache = new InMemoryCache();
+
+  const client = new ApolloClient({
+    cache,
+    link: new ApolloLink(request),
+  });
+
+  await client.query({ query });
+  const [operation] = request.mock.calls[0];
+
+  expect(operation.getApolloContext()).toStrictEqualTyped({ cache });
+});
+
+test("allows custom context", async () => {
+  const query = gql`
+    query {
+      greeting
+    }
+  `;
+  const request = jest.fn((_: Operation) => {
+    return of({ data: { greeting: "Hello" } });
+  });
+
+  const cache = new InMemoryCache();
+  const client = new ApolloClient({
+    cache,
+    link: new ApolloLink(request),
+  });
+
+  await client.query({ query, context: { foo: true } });
+  const [operation] = request.mock.calls[0];
+
+  expect(operation.getContext()).toStrictEqualTyped({
+    foo: true,
+    clientAwareness: { name: undefined, version: undefined },
+  });
+  expect(operation.getApolloContext()).toStrictEqualTyped({ cache });
+});
+
+test("uses context from defaultOptions", async () => {
+  const query = gql`
+    query {
+      greeting
+    }
+  `;
+  const request = jest.fn((_: Operation) => {
+    return of({ data: { greeting: "Hello" } });
+  });
+
+  const cache = new InMemoryCache();
+
+  const client = new ApolloClient({
+    cache,
+    link: new ApolloLink(request),
+    defaultContext: {
+      foo: true,
+    },
+  });
+
+  await client.query({ query });
+  const [operation] = request.mock.calls[0];
+
+  expect(operation.getContext()).toStrictEqualTyped({
+    foo: true,
+    clientAwareness: { name: undefined, version: undefined },
+  });
+  expect(operation.getApolloContext()).toStrictEqualTyped({ cache });
+});
+
+test("can override global default", async () => {
+  const query = gql`
+    query {
+      greeting
+    }
+  `;
+  const request = jest.fn((_: Operation) => {
+    return of({ data: { greeting: "Hello" } });
+  });
+
+  const cache = new InMemoryCache();
+
+  const client = new ApolloClient({
+    cache,
+    link: new ApolloLink(request),
+    defaultContext: {
+      foo: true,
+    },
+  });
+
+  await client.query({ query, context: { foo: false } });
+  const [operation] = request.mock.calls[0];
+
+  expect(operation.getContext()).toStrictEqualTyped({
+    foo: false,
+    clientAwareness: { name: undefined, version: undefined },
+  });
+  expect(operation.getApolloContext()).toStrictEqualTyped({ cache });
+});

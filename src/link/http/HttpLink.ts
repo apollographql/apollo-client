711ae11e3eb8cb69b6ae1ab3e1cb6290b3b08fdc
Jerel Miller <jerelmiller@gmail.com>
2025-04-18 21:19:56 -0600

    Add http link context options

--- src/link/http/HttpLink.ts
+++ src/link/http/HttpLink.ts
@@ -1,7 +1,15 @@
 import { ApolloLink } from "@apollo/client/link/core";
 
 import { createHttpLink } from "./createHttpLink.js";
-import type { HttpOptions } from "./selectHttpOptionsAndBody.js";
+import type { HttpOptions, UriFunction } from "./selectHttpOptionsAndBody.js";
+
+export declare namespace HttpLink {
+  export interface ContextOptions {
+    uri?: string | UriFunction;
+    headers?: Record<string, string>;
+    credentials?: RequestCredentials;
+  }
+}
 
 export class HttpLink extends ApolloLink {
   constructor(public options: HttpOptions = {}) {

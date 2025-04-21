896ed739f60a94548591757f0d59fd6237a6ae65
Jerel Miller <jerelmiller@gmail.com>
2025-04-21 09:56:12 -0600

    Add resolvers type

--- src/link/local-resolvers/LocalResolversLink.ts
+++ src/link/local-resolvers/LocalResolversLink.ts
@@ -1,7 +1,32 @@
+import type { FieldNode } from "graphql";
+
 import { ApolloLink } from "@apollo/client/link/core";
+import type { FragmentMap } from "@apollo/client/utilities";
+
+export declare namespace LocalResolversLink {
+  export interface Options {
+    resolvers: Resolvers;
+  }
+
+  export interface Resolvers {
+    [typename: string]: {
+      [field: string]: Resolver;
+    };
+  }
+
+  export type Resolver = (
+    rootValue?: any,
+    args?: any,
+    context?: any,
+    info?: {
+      field: FieldNode;
+      fragmentMap: FragmentMap;
+    }
+  ) => any;
+}
 
 export class LocalResolversLink extends ApolloLink {
-  constructor() {
+  constructor(options: LocalResolversLink.Options) {
     super();
   }
 }

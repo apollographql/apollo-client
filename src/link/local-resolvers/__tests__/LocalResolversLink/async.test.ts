f70f93b34374880db1604cf3c2807e5ad295eb97
Jerel Miller <jerelmiller@gmail.com>
2025-04-21 18:29:29 -0600

    Split up test files

---
+++ src/link/local-resolvers/__tests__/LocalResolversLink/async.test.ts
@@ -0,0 +1,201 @@
+import { of } from "rxjs";
+
+import { ApolloLink } from "@apollo/client/link/core";
+import { LocalResolversLink } from "@apollo/client/link/local-resolvers";
+import {
+  executeWithDefaultContext as execute,
+  ObservableStream,
+} from "@apollo/client/testing/internal";
+
+import { gql } from "./testUtils.js";
+
+test("supports async @client resolvers", async () => {
+  const query = gql`
+    query Member {
+      isLoggedIn @client
+    }
+  `;
+
+  const link = new LocalResolversLink({
+    resolvers: {
+      Query: {
+        async isLoggedIn() {
+          return Promise.resolve(true);
+        },
+      },
+    },
+  });
+
+  const stream = new ObservableStream(execute(link, { query }));
+
+  await expect(stream).toEmitTypedValue({
+    data: { isLoggedIn: true },
+  });
+  await expect(stream).toComplete();
+});
+
+test("handles nested asynchronous @client resolvers", async () => {
+  const query = gql`
+    query DeveloperTicketComments($id: ID) {
+      developer(id: $id) @client {
+        id
+        handle
+        tickets @client {
+          id
+          comments @client {
+            id
+          }
+        }
+      }
+    }
+  `;
+
+  function randomDelay(range: number) {
+    return new Promise((resolve) =>
+      setTimeout(resolve, Math.round(Math.random() * range))
+    );
+  }
+
+  function uuid() {
+    return Math.random().toString(36).slice(2);
+  }
+
+  const developerId = uuid();
+
+  function times<T>(n: number, thunk: () => T): T[] {
+    const result: T[] = [];
+    for (let i = 0; i < n; ++i) {
+      result.push(thunk());
+    }
+    return result;
+  }
+
+  const ticketsPerDev = 5;
+  const commentsPerTicket = 5;
+
+  const link = new LocalResolversLink({
+    resolvers: {
+      Query: {
+        async developer(_, { id }) {
+          await randomDelay(50);
+          expect(id).toBe(developerId);
+          return {
+            __typename: "Developer",
+            id,
+            handle: "@benjamn",
+          };
+        },
+      },
+      Developer: {
+        async tickets(developer) {
+          await randomDelay(50);
+          expect(developer.__typename).toBe("Developer");
+
+          return Promise.all(
+            times(ticketsPerDev, () => ({
+              __typename: "Ticket",
+              id: uuid(),
+            }))
+          );
+        },
+      },
+      Ticket: {
+        async comments(ticket) {
+          await randomDelay(50);
+          expect(ticket.__typename).toBe("Ticket");
+
+          return Promise.all(
+            times(commentsPerTicket, () => ({
+              __typename: "Comment",
+              id: uuid(),
+            }))
+          );
+        },
+      },
+    },
+  });
+
+  const stream = new ObservableStream(
+    execute(link, { query, variables: { id: developerId } })
+  );
+
+  await expect(stream).toEmitTypedValue(
+    {
+      data: {
+        developer: {
+          __typename: "Developer",
+          id: developerId,
+          handle: "@benjamn",
+          tickets: times(ticketsPerDev, () => ({
+            __typename: "Ticket",
+            id: expect.any(String),
+            comments: times(commentsPerTicket, () => ({
+              __typename: "Comment",
+              id: expect.any(String),
+            })),
+          })),
+        },
+      },
+    },
+    { timeout: 1000 }
+  );
+
+  await expect(stream).toComplete();
+});
+
+test("supports async @client resolvers mixed with remotely resolved data", async () => {
+  const query = gql`
+    query Member {
+      member {
+        name
+        sessionCount @client
+        isLoggedIn @client
+      }
+    }
+  `;
+
+  const testMember = {
+    name: "John Smithsonian",
+    isLoggedIn: true,
+    sessionCount: 10,
+  };
+
+  const mockLink = new ApolloLink(() =>
+    of({
+      data: {
+        member: {
+          name: testMember.name,
+          __typename: "Member",
+        },
+      },
+    })
+  );
+
+  const localResolversLink = new LocalResolversLink({
+    resolvers: {
+      Member: {
+        async isLoggedIn() {
+          return Promise.resolve(testMember.isLoggedIn);
+        },
+        sessionCount() {
+          return testMember.sessionCount;
+        },
+      },
+    },
+  });
+
+  const link = ApolloLink.from([localResolversLink, mockLink]);
+  const stream = new ObservableStream(execute(link, { query }));
+
+  await expect(stream).toEmitTypedValue({
+    data: {
+      member: {
+        name: testMember.name,
+        isLoggedIn: testMember.isLoggedIn,
+        sessionCount: testMember.sessionCount,
+        __typename: "Member",
+      },
+    },
+  });
+  await expect(stream).toComplete();
+});
---
+++ src/link/local-resolvers/__tests__/LocalResolversLink/base.test.ts
@@ -0,0 +1,469 @@
+import { of } from "rxjs";
+
+import { ApolloLink } from "@apollo/client/link/core";
+import { LocalResolversLink } from "@apollo/client/link/local-resolvers";
+import { MockLink } from "@apollo/client/testing";
+import {
+  executeWithDefaultContext as execute,
+  ObservableStream,
+} from "@apollo/client/testing/internal";
+
+import { gql } from "./testUtils.js";
+
+test("runs resolvers for @client queries", async () => {
+  const query = gql`
+    query Test {
+      foo @client {
+        bar
+      }
+    }
+  `;
+
+  const link = new LocalResolversLink({
+    resolvers: {
+      Query: {
+        foo: () => ({ bar: true }),
+      },
+    },
+  });
+
+  const stream = new ObservableStream(execute(link, { query }));
+
+  await expect(stream).toEmitTypedValue({
+    data: { foo: { bar: true } },
+  });
+
+  await expect(stream).toComplete();
+});
+
+test("can add resolvers after the link is instantiated", async () => {
+  const query = gql`
+    query Test {
+      foo @client {
+        bar
+      }
+    }
+  `;
+
+  const link = new LocalResolversLink({ resolvers: {} });
+
+  link.addResolvers({
+    Query: {
+      foo: () => ({ bar: true }),
+    },
+  });
+
+  const stream = new ObservableStream(execute(link, { query }));
+
+  await expect(stream).toEmitTypedValue({
+    data: { foo: { bar: true } },
+  });
+
+  await expect(stream).toComplete();
+});
+
+test("handles queries with a mix of @client and server fields", async () => {
+  const query = gql`
+    query Mixed {
+      foo @client {
+        bar
+      }
+      bar {
+        baz
+      }
+    }
+  `;
+
+  const serverQuery = gql`
+    query Mixed {
+      bar {
+        baz
+      }
+    }
+  `;
+
+  const mockLink = new MockLink([
+    {
+      request: { query: serverQuery },
+      result: { data: { bar: { baz: true } } },
+    },
+  ]);
+
+  const localResolversLink = new LocalResolversLink({
+    resolvers: {
+      Query: {
+        foo: () => ({ bar: true }),
+      },
+    },
+  });
+
+  const link = ApolloLink.from([localResolversLink, mockLink]);
+  const stream = new ObservableStream(execute(link, { query }));
+
+  await expect(stream).toEmitTypedValue({
+    data: {
+      foo: { bar: true },
+      bar: { baz: true },
+    },
+  });
+
+  await expect(stream).toComplete();
+});
+
+test("runs resolvers for deeply nested @client fields", async () => {
+  const query = gql`
+    query Test {
+      user {
+        id
+        bestFriend {
+          firstName
+          lastName
+          fullName @client
+        }
+      }
+    }
+  `;
+
+  const mockLink = new ApolloLink(() => {
+    return of({
+      data: {
+        user: {
+          __typename: "User",
+          id: 1,
+          bestFriend: {
+            __typename: "User",
+            firstName: "Test",
+            lastName: "User",
+          },
+        },
+      },
+    });
+  });
+
+  const localResolversLink = new LocalResolversLink({
+    resolvers: {
+      User: {
+        fullName: (user) => `${user.firstName} ${user.lastName}`,
+      },
+    },
+  });
+
+  const link = ApolloLink.from([localResolversLink, mockLink]);
+  const stream = new ObservableStream(execute(link, { query }));
+
+  await expect(stream).toEmitTypedValue({
+    data: {
+      user: {
+        __typename: "User",
+        id: 1,
+        bestFriend: {
+          __typename: "User",
+          firstName: "Test",
+          lastName: "User",
+          fullName: "Test User",
+        },
+      },
+    },
+  });
+
+  await expect(stream).toComplete();
+});
+
+test("has access to query variables in @client resolvers", async () => {
+  const query = gql`
+    query WithVariables($id: ID!) {
+      foo @client {
+        bar(id: $id)
+      }
+    }
+  `;
+
+  const link = new LocalResolversLink({
+    resolvers: {
+      Query: {
+        foo: () => ({ __typename: "Foo" }),
+      },
+      Foo: {
+        bar: (_: any, { id }: { id: number }) => id,
+      },
+    },
+  });
+
+  const stream = new ObservableStream(
+    execute(link, { query, variables: { id: 1 } })
+  );
+
+  await expect(stream).toEmitTypedValue({
+    data: { foo: { __typename: "Foo", bar: 1 } },
+  });
+
+  await expect(stream).toComplete();
+});
+
+test("passes context to @client resolvers", async () => {
+  const query = gql`
+    query WithContext {
+      foo @client {
+        bar
+      }
+    }
+  `;
+
+  const link = new LocalResolversLink({
+    resolvers: {
+      Query: {
+        foo: () => ({ __typename: "Foo" }),
+      },
+      Foo: {
+        // @ts-expect-error FIXME before this is merged
+        bar: (_data: any, _args: any, { id }: { id: number }) => id,
+      },
+    },
+  });
+
+  const stream = new ObservableStream(
+    execute(link, { query, context: { id: 1 } })
+  );
+
+  await expect(stream).toEmitTypedValue({
+    data: { foo: { __typename: "Foo", bar: 1 } },
+  });
+
+  await expect(stream).toComplete();
+});
+
+test("combines local @client resolver results with server results, for the same field", async () => {
+  const query = gql`
+    query author {
+      author {
+        name
+        stats {
+          totalPosts
+          postsToday @client
+        }
+      }
+    }
+  `;
+
+  const serverQuery = gql`
+    query author {
+      author {
+        name
+        stats {
+          totalPosts
+        }
+      }
+    }
+  `;
+
+  const mockLink = new MockLink([
+    {
+      request: { query: serverQuery },
+      result: {
+        data: {
+          author: {
+            name: "John Smith",
+            stats: {
+              totalPosts: 100,
+              __typename: "Stats",
+            },
+            __typename: "Author",
+          },
+        },
+      },
+    },
+  ]);
+
+  const localResolversLink = new LocalResolversLink({
+    resolvers: {
+      Stats: {
+        postsToday: () => 10,
+      },
+    },
+  });
+
+  const link = ApolloLink.from([localResolversLink, mockLink]);
+  const stream = new ObservableStream(execute(link, { query }));
+
+  await expect(stream).toEmitTypedValue({
+    data: {
+      author: {
+        __typename: "Author",
+        name: "John Smith",
+        stats: {
+          __typename: "Stats",
+          totalPosts: 100,
+          postsToday: 10,
+        },
+      },
+    },
+  });
+
+  await expect(stream).toComplete();
+});
+
+test("handles resolvers that return booleans", async () => {
+  const query = gql`
+    query CartDetails {
+      isInCart @client
+    }
+  `;
+
+  const link = new LocalResolversLink({
+    resolvers: {
+      Query: {
+        isInCart: () => false,
+      },
+    },
+  });
+
+  const stream = new ObservableStream(execute(link, { query }));
+
+  await expect(stream).toEmitTypedValue({ data: { isInCart: false } });
+  await expect(stream).toComplete();
+});
+
+test("does not run resolvers without @client directive", async () => {
+  const query = gql`
+    query Mixed {
+      foo @client {
+        bar
+      }
+      bar {
+        baz
+      }
+    }
+  `;
+
+  const serverQuery = gql`
+    query Mixed {
+      bar {
+        baz
+      }
+    }
+  `;
+
+  const mockLink = new MockLink([
+    {
+      request: { query: serverQuery },
+      result: { data: { bar: { __typename: "Bar", baz: true } } },
+    },
+  ]);
+
+  const barResolver = jest.fn(() => ({ __typename: `Bar`, baz: false }));
+  const localResolversLink = new LocalResolversLink({
+    resolvers: {
+      Query: {
+        foo: () => ({ __typename: `Foo`, bar: true }),
+        bar: barResolver,
+      },
+    },
+  });
+
+  const link = ApolloLink.from([localResolversLink, mockLink]);
+  const stream = new ObservableStream(execute(link, { query }));
+
+  await expect(stream).toEmitTypedValue({
+    data: {
+      foo: { __typename: "Foo", bar: true },
+      bar: { __typename: "Bar", baz: true },
+    },
+  });
+  await expect(stream).toComplete();
+
+  expect(barResolver).not.toHaveBeenCalled();
+});
+
+test("allows child resolvers from a parent resolved field from a local resolver", async () => {
+  const query = gql`
+    query UserData {
+      userData @client {
+        firstName
+        lastName
+        fullName
+      }
+    }
+  `;
+
+  const link = new LocalResolversLink({
+    resolvers: {
+      Query: {
+        userData() {
+          return {
+            __typename: "User",
+            firstName: "Ben",
+            lastName: "Newman",
+          };
+        },
+      },
+      User: {
+        fullName(data) {
+          return data.firstName + " " + data.lastName;
+        },
+      },
+    },
+  });
+
+  const stream = new ObservableStream(execute(link, { query }));
+
+  await expect(stream).toEmitTypedValue({
+    data: {
+      userData: {
+        __typename: "User",
+        firstName: "Ben",
+        lastName: "Newman",
+        fullName: "Ben Newman",
+      },
+    },
+  });
+  await expect(stream).toComplete();
+});
+
+test("can use remote result to resolve @client field", async () => {
+  const query = gql`
+    query Member {
+      member {
+        firstName
+        lastName
+        fullName @client
+      }
+    }
+  `;
+
+  const mockLink = new ApolloLink(() =>
+    of({
+      data: {
+        member: {
+          __typename: "Member",
+          firstName: "John",
+          lastName: "Smithsonian",
+        },
+      },
+    })
+  );
+
+  const localResolversLink = new LocalResolversLink({
+    resolvers: {
+      Member: {
+        fullName(member) {
+          return `${member.firstName} ${member.lastName}`;
+        },
+      },
+    },
+  });
+
+  const link = ApolloLink.from([localResolversLink, mockLink]);
+  const stream = new ObservableStream(execute(link, { query }));
+
+  await expect(stream).toEmitTypedValue({
+    data: {
+      member: {
+        __typename: "Member",
+        firstName: "John",
+        lastName: "Smithsonian",
+        fullName: "John Smithsonian",
+      },
+    },
+  });
+  await expect(stream).toComplete();
+});

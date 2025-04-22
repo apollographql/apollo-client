95537796a90e368ed210ae99f5695beaac0c8d4c
Jerel Miller <jerelmiller@gmail.com>
2025-04-21 21:51:49 -0600

    Move context test to own file

--- src/link/local-resolvers/__tests__/LocalResolversLink/base.test.ts
+++ src/link/local-resolvers/__tests__/LocalResolversLink/base.test.ts
@@ -201,38 +201,6 @@ test("has access to query variables in @client resolvers", async () => {
   await expect(stream).toComplete();
 });
 
-test("passes context to @client resolvers", async () => {
-  const query = gql`
-    query WithContext {
-      foo @client {
-        bar
-      }
-    }
-  `;
-
-  const link = new LocalResolversLink({
-    resolvers: {
-      Query: {
-        foo: () => ({ __typename: "Foo" }),
-      },
-      Foo: {
-        // @ts-expect-error FIXME before this is merged
-        bar: (_data: any, _args: any, { id }: { id: number }) => id,
-      },
-    },
-  });
-
-  const stream = new ObservableStream(
-    execute(link, { query, context: { id: 1 } })
-  );
-
-  await expect(stream).toEmitTypedValue({
-    data: { foo: { __typename: "Foo", bar: 1 } },
-  });
-
-  await expect(stream).toComplete();
-});
-
 test("combines local @client resolver results with server results, for the same field", async () => {
   const query = gql`
     query author {

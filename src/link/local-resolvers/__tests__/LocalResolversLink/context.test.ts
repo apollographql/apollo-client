2b71170bea94067040772f174e84088649cdf2eb
Jerel Miller <jerelmiller@gmail.com>
2025-04-21 21:58:58 -0600

    Add more context tests

--- src/link/local-resolvers/__tests__/LocalResolversLink/context.test.ts
+++ src/link/local-resolvers/__tests__/LocalResolversLink/context.test.ts
@@ -1,3 +1,4 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { LocalResolversLink } from "@apollo/client/link/local-resolvers";
 import {
   executeWithDefaultContext as execute,
@@ -36,4 +37,145 @@ test("passes context to @client resolvers", async () => {
   });
 
   await expect(stream).toComplete();
+});
+
+test("passes apollo context to @client resolvers", async () => {
+  const query = gql`
+    query WithContext {
+      foo @client {
+        bar
+      }
+    }
+  `;
+
+  const barResolver = jest.fn(() => 1);
+  const link = new LocalResolversLink({
+    resolvers: {
+      Query: {
+        foo: () => ({ __typename: "Foo" }),
+      },
+      Foo: { bar: barResolver },
+    },
+  });
+
+  const client = new ApolloClient({ cache: new InMemoryCache() });
+  const stream = new ObservableStream(execute(link, { query }, { client }));
+
+  await expect(stream).toEmitTypedValue({
+    data: { foo: { __typename: "Foo", bar: 1 } },
+  });
+
+  await expect(stream).toComplete();
+
+  expect(barResolver).toHaveBeenCalledWith(
+    { __typename: "Foo" },
+    null,
+    {
+      client,
+      cache: client.cache,
+    },
+    {
+      field: expect.objectContaining({
+        name: { kind: "Name", value: "bar" },
+      }),
+      fragmentMap: expect.any(Object),
+    }
+  );
+});
+
+test("mixes apollo context and passed context to @client resolvers", async () => {
+  const query = gql`
+    query WithContext {
+      foo @client {
+        bar
+      }
+    }
+  `;
+
+  const barResolver = jest.fn(() => 1);
+  const link = new LocalResolversLink({
+    resolvers: {
+      Query: {
+        foo: () => ({ __typename: "Foo" }),
+      },
+      Foo: { bar: barResolver },
+    },
+  });
+
+  const client = new ApolloClient({ cache: new InMemoryCache() });
+  const stream = new ObservableStream(
+    execute(link, { query, context: { id: 1 } }, { client })
+  );
+
+  await expect(stream).toEmitTypedValue({
+    data: { foo: { __typename: "Foo", bar: 1 } },
+  });
+
+  await expect(stream).toComplete();
+
+  expect(barResolver).toHaveBeenCalledWith(
+    { __typename: "Foo" },
+    null,
+    {
+      id: 1,
+      client,
+      cache: client.cache,
+    },
+    {
+      field: expect.objectContaining({
+        name: { kind: "Name", value: "bar" },
+      }),
+      fragmentMap: expect.any(Object),
+    }
+  );
+});
+
+test("overwrites client and cache fields if provided in context", async () => {
+  const query = gql`
+    query WithContext {
+      foo @client {
+        bar
+      }
+    }
+  `;
+
+  const barResolver = jest.fn(() => 1);
+  const link = new LocalResolversLink({
+    resolvers: {
+      Query: {
+        foo: () => ({ __typename: "Foo" }),
+      },
+      Foo: { bar: barResolver },
+    },
+  });
+
+  const client = new ApolloClient({ cache: new InMemoryCache() });
+  const stream = new ObservableStream(
+    execute(
+      link,
+      { query, context: { client: "client", cache: "cache" } },
+      { client }
+    )
+  );
+
+  await expect(stream).toEmitTypedValue({
+    data: { foo: { __typename: "Foo", bar: 1 } },
+  });
+
+  await expect(stream).toComplete();
+
+  expect(barResolver).toHaveBeenCalledWith(
+    { __typename: "Foo" },
+    null,
+    {
+      client,
+      cache: client.cache,
+    },
+    {
+      field: expect.objectContaining({
+        name: { kind: "Name", value: "bar" },
+      }),
+      fragmentMap: expect.any(Object),
+    }
+  );
 });

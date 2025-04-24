224a054900cd2742395cde5e99168f37e0d84ac0
Jerel Miller <jerelmiller@gmail.com>
2025-04-23 18:37:10 -0600

    Add equality tester for local resolvers error

---
+++ src/config/jest/areLocalResolversErrorsEqual.ts
@@ -0,0 +1,22 @@
+import type { Tester } from "@jest/expect-utils";
+
+import { LocalResolversError } from "@apollo/client/errors";
+
+export const areLocalResolversErrorsEqual: Tester = function (
+  a,
+  b,
+  customTesters
+) {
+  const isALocalResolversError = LocalResolversError.is(a);
+  const isBLocalResolversError = LocalResolversError.is(b);
+
+  if (isALocalResolversError && isBLocalResolversError) {
+    return (
+      a.message === b.message && this.equals(a.path, b.path, customTesters)
+    );
+  } else if (isALocalResolversError === isBLocalResolversError) {
+    return undefined;
+  } else {
+    return false;
+  }
+};

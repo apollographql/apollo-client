a30f917951e3d8a1f794f9df04c6cb191dc9c27e
Jerel Miller <jerelmiller@gmail.com>
2025-04-25 15:10:33 -0600

    Add test file with scalar

--- tests.codegen.ts
+++ tests.codegen.ts
@@ -14,6 +14,13 @@ const config: CodegenConfig = {
         ],
         plugins: [{ add: { content: "/* eslint-disable */" } }],
       }),
+    "./src/link/local-resolvers/__tests__/LocalResolversLink/fixtures/local-resolvers-with-scalar.ts":
+      createLocalResolversLinkCodegenConfig({
+        schema: [
+          "./src/link/local-resolvers/__tests__/LocalResolversLink/fixtures/localSchemaWithScalars.graphql",
+        ],
+        plugins: [{ add: { content: "/* eslint-disable */" } }],
+      }),
   },
 };
 

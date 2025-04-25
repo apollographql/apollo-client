4015d4ccd00a6f02cfe19c1906ba8c7a1be84408
Jerel Miller <jerelmiller@gmail.com>
2025-04-25 14:31:23 -0600

    Add additional config options and types

--- src/link/local-resolvers/codegen/types.ts
+++ src/link/local-resolvers/codegen/types.ts
@@ -1,11 +1,16 @@
 import type { LocalResolversLink } from "@apollo/client/link/local-resolvers";
 
-// Note: GraphQL Codegen does not handle namespaces properly for the
-// `customResolverFn` option, so this type needs to exist outside of a
-// namespace.
+// Note: These types are not defined in a namespace because GraphQL Codegen
+// does not handle namespaces correctly for some options. Instead of guessing
+// which work and which don't, we provide all types used in the config helper as
+// separate types.
+
 export type LocalResolversLinkResolverFn<
   TResult,
   TParent = unknown,
   _TContext = never,
   TArgs = Record<string, never>,
 > = LocalResolversLink.Resolver<TResult, TParent, TArgs>;
+
+export type LocalResolversLinkResolveInfo = LocalResolversLink.ResolveInfo;
+export type LocalResolversLinkContextType = LocalResolversLink.ResolverContext;

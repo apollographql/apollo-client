bcde65adcad5aaf4f3192d33a5cc94b1ffe5a2fe
Jerel Miller <jerelmiller@gmail.com>
2025-04-26 02:20:44 -0600

    Warn for scalars

--- src/link/local-resolvers/codegen/visitor.ts
+++ src/link/local-resolvers/codegen/visitor.ts
@@ -349,7 +349,10 @@ export class LocalResolversLinkVisitor extends BaseResolversVisitor<
     return this.clearOptional(baseValue);
   }
 
-  ScalarTypeDefinition(_node: ScalarTypeDefinitionNode): string {
+  ScalarTypeDefinition(node: ScalarTypeDefinitionNode): string {
+    console.warn(
+      `Custom scalars type '${node.name.value}' is ignored and cannot be resolved with \`LocalResolversLink\`. Please map the scalar type to a primitive with the \`scalars\` config.`
+    );
     this._hasScalars = true;
     return "";
   }

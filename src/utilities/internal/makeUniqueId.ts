0cf5746224a042f212ddae110abec2ed37c650a4
Jerel Miller <jerelmiller@gmail.com>
2025-04-30 17:01:06 -0600

    Remove note about memory leak

--- src/utilities/internal/makeUniqueId.ts
+++ src/utilities/internal/makeUniqueId.ts
@@ -1,8 +1,5 @@
 const prefixCounts = new Map<string, number>();
 
-// TODO: This looks like it could be a memory leak since it sets the prefix in
-// the map above, but never gets cleaned up.
-
 /**
  * These IDs won't be globally unique, but they will be unique within this
  * process, thanks to the counter, and unguessable thanks to the random suffix.

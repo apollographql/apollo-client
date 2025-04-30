a1d582e3570aa81153d1778088b31915b9d8fc5c
Jerel Miller <jerelmiller@gmail.com>
2025-04-30 14:29:58 -0600

    Mark internal

--- src/utilities/internal/isExecutionPatchIncrementalResult.ts
+++ src/utilities/internal/isExecutionPatchIncrementalResult.ts
@@ -3,6 +3,7 @@ import type {
   FetchResult,
 } from "@apollo/client/link";
 
+/** @internal */
 export function isExecutionPatchIncrementalResult<T>(
   value: FetchResult<T>
 ): value is ExecutionPatchIncrementalResult {

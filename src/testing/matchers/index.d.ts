0c868d780c9bb6c02e43deda18822194c7f2f86f
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-04-30 11:39:46 +0200

    new matchers: `toRerenderWithSimilarSnapshot` and `toRerenderWithStrictEqualSnapshot`

--- src/testing/matchers/index.d.ts
+++ src/testing/matchers/index.d.ts
@@ -83,6 +83,18 @@ interface ApolloCustomMatchers<R = void, T = {}> {
   toStrictEqualTyped: [T] extends [Promise<infer TResult>] ?
     (expected: FilterUnserializableProperties<TResult>) => R
   : (expected: FilterUnserializableProperties<T>) => R;
+
+  toRerenderWithSimilarSnapshot: T extends RenderStream<infer Snapshot> ?
+    (options: ToRerenderWithSimilarSnapshotOptions<Snapshot>) => Promise<R>
+  : {
+      error: "matcher needs to be called on a `RenderStream` instance";
+    };
+
+  toRerenderWithStrictEqualSnapshot: T extends RenderStream<infer Snapshot> ?
+    (options?: NextRenderOptions) => Promise<R>
+  : {
+      error: "matcher needs to be called on a `RenderStream` instance";
+    };
 }
 
 interface ApolloCustomAsymmetricMatchers {

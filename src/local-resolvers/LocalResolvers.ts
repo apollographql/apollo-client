155f79423e5afb4f07b1dcf2c2bf5ec2763d25ce
Jerel Miller <jerelmiller@gmail.com>
2025-05-08 09:44:41 -0600

    Don't check exported variables from server field in resolve phase

--- src/local-resolvers/LocalResolvers.ts
+++ src/local-resolvers/LocalResolvers.ts
@@ -395,14 +395,16 @@ export class LocalResolvers<
     const result = rootValue?.[field.name.value];
 
     if (result == null) {
-      for (const [name, def] of Object.entries(
-        execContext.exportedVariableDefs
-      )) {
-        if (def.ancestors.has(field) && def.required) {
-          throw new LocalResolversError(
-            `${"Field"} '${field.name.value}' is \`${result}\` which contains exported required variable '${name}'. Ensure this value is in the cache or make the variable optional.`,
-            { path }
-          );
+      if (execContext.phase === "exports") {
+        for (const [name, def] of Object.entries(
+          execContext.exportedVariableDefs
+        )) {
+          if (def.ancestors.has(field) && def.required) {
+            throw new LocalResolversError(
+              `${"Field"} '${field.name.value}' is \`${result}\` which contains exported required variable '${name}'. Ensure this value is in the cache or make the variable optional.`,
+              { path }
+            );
+          }
         }
       }
 

8c0779885c5efbe73c90666e96e535006cce8c12
Jerel Miller <jerelmiller@gmail.com>
2025-05-08 02:14:06 -0600

    Add test to check phase can be accessed

--- src/local-resolvers/__tests__/LocalResolvers/context.test.ts
+++ src/local-resolvers/__tests__/LocalResolvers/context.test.ts
@@ -78,3 +78,46 @@ test("can access request context in resolvers", async () => {
     data: { foo: { __typename: "Foo", bar: 1 } },
   });
 });
+
+test("can access phase in resolver context", async () => {
+  const document = gql`
+    query WithContext($phase: String!) {
+      foo @client {
+        bar @export(as: "phase")
+      }
+    }
+  `;
+
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: ApolloLink.empty(),
+  });
+
+  const localResolvers = new LocalResolvers({
+    resolvers: {
+      Query: {
+        foo: () => ({ __typename: "Foo" }),
+      },
+      Foo: {
+        bar: (_data, _args, { phase }) => phase,
+      },
+    },
+  });
+
+  await expect(
+    localResolvers.getExportedVariables({
+      document,
+      client,
+      context: {},
+      variables: {},
+    })
+  ).resolves.toStrictEqualTyped({
+    phase: "exports",
+  });
+
+  await expect(
+    localResolvers.execute({ document, client, context: {} })
+  ).resolves.toStrictEqualTyped({
+    data: { foo: { __typename: "Foo", bar: "resolve" } },
+  });
+});

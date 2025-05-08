4d2396df9ef2d14df095c6957b70b0be42ebb2ab
Jerel Miller <jerelmiller@gmail.com>
2025-05-08 10:32:01 -0600

    Copy types test from #12579

---
+++ src/local-resolvers/__tests__/LocalResolvers/types.test.ts
@@ -0,0 +1,307 @@
+import { LocalResolvers } from "@apollo/client/local-resolvers";
+
+import type { RootValue } from "./fixtures/rootValue.js";
+
+describe.skip("Type tests", () => {
+  test("allows resolvers of anything with no generic", () => {
+    interface Food {
+      __typename: "Food";
+      name: string;
+    }
+    new LocalResolvers();
+    new LocalResolvers({ resolvers: {} });
+
+    new LocalResolvers({
+      resolvers: {
+        Query: {
+          currentUserId: () => "1",
+        },
+        User: {
+          favoriteFood: () => ({ __typename: "Food" }),
+        },
+        Food: {
+          name: (parent: Food) => parent.name,
+          ingredients: () => {},
+        },
+      },
+    });
+
+    new LocalResolvers({
+      rootValue: {
+        env: "development",
+      },
+      resolvers: {
+        Query: {
+          currentUserId: () => "1",
+        },
+        User: {
+          favoriteFood: () => ({ __typename: "Food" }),
+        },
+        Food: {
+          name: (parent: Food) => parent.name,
+          ingredients: () => {},
+        },
+      },
+    });
+  });
+
+  test("works with codegen resolver types", async () => {
+    type Resolvers = import("./fixtures/local-resolvers.js").Resolvers;
+    const { FoodCategory } = await import("./fixtures/local-resolvers.js");
+
+    // @ts-expect-error missing argument
+    new LocalResolvers<Resolvers>();
+    // @ts-expect-error missing resolvers option
+    new LocalResolvers<Resolvers>({});
+
+    new LocalResolvers<Resolvers>({
+      // @ts-expect-error missing Query resolver
+      resolvers: {},
+    });
+
+    new LocalResolvers<Resolvers>({
+      rootValue: {
+        env: "prod",
+      },
+      resolvers: {
+        Query: {
+          currentUserId: () => "1",
+        },
+        User: {
+          favoriteFood: () => ({
+            __typename: "Food",
+            name: "Pasta",
+            categories: [FoodCategory.Italian],
+          }),
+        },
+        Food: {
+          name: (food) => food.name?.toUpperCase() ?? null,
+          categories: (food, { limit, offset }) => {
+            limit = limit ?? 5;
+            return food.categories?.slice(offset, offset + limit) ?? [];
+          },
+        },
+      },
+    });
+
+    new LocalResolvers<Resolvers>({
+      resolvers: {
+        // @ts-expect-error missing currentUserId resolver
+        Query: {},
+      },
+    });
+
+    new LocalResolvers<Resolvers>({
+      resolvers: {
+        Query: {
+          // @ts-expect-error wrong return type
+          currentUserId: () => {
+            return true;
+          },
+        },
+      },
+    });
+
+    new LocalResolvers<Resolvers>({
+      resolvers: {
+        User: {
+          // @ts-expect-error missing __typename
+          favoriteFood: () => ({ name: "Pizza" }),
+        },
+      },
+    });
+
+    new LocalResolvers<Resolvers>({
+      rootValue: { env: "dev" },
+      resolvers: {
+        Query: {
+          currentUserId: () => "1",
+        },
+        User: {
+          favoriteFood: () => ({ __typename: "Food" }),
+        },
+      },
+    });
+
+    new LocalResolvers<Resolvers>({
+      resolvers: {
+        // @ts-expect-error unknown typename
+        Invalid: {},
+      },
+    });
+
+    new LocalResolvers<Resolvers>({
+      resolvers: {
+        Query: {
+          // @ts-expect-error unknown field
+          invalid: () => 1,
+        },
+      },
+    });
+  });
+
+  test("filters out scalar resolvers", () => {
+    type Resolvers =
+      import("./fixtures/local-resolvers-with-scalar.js").Resolvers;
+
+    new LocalResolvers<Resolvers>({
+      resolvers: {
+        // @ts-expect-error unknown type
+        Date: {
+          field: () => {},
+        },
+      },
+    });
+
+    new LocalResolvers<Resolvers>({
+      resolvers: {
+        User: {
+          // ok: type is unknown
+          lastLoggedInAt: () => {},
+        },
+      },
+    });
+  });
+
+  test("rootValue", () => {
+    type Resolvers = import("./fixtures/local-resolvers.js").Resolvers;
+
+    new LocalResolvers<Resolvers, RootValue>({
+      rootValue: {
+        env: "dev",
+      },
+      resolvers: {
+        Query: {
+          currentUserId: (rootValue) => rootValue.env,
+        },
+      },
+    });
+
+    new LocalResolvers<Resolvers, RootValue>({
+      rootValue: {
+        // @ts-expect-error invalid value
+        env: "staging",
+      },
+      resolvers: {
+        Query: {
+          currentUserId: (rootValue) => rootValue.env,
+        },
+      },
+    });
+
+    new LocalResolvers<Resolvers, RootValue>({
+      rootValue: () => ({
+        env: "prod",
+      }),
+      resolvers: {
+        Query: {
+          currentUserId: (rootValue) => rootValue.env,
+        },
+      },
+    });
+
+    new LocalResolvers<Resolvers, RootValue>({
+      // @ts-expect-error incorrect value
+      rootValue: () => ({
+        env: "staging",
+      }),
+      resolvers: {
+        Query: {
+          currentUserId: (rootValue) => rootValue.env,
+        },
+      },
+    });
+
+    new LocalResolvers<Resolvers, RootValue>({
+      rootValue: {
+        env: "prod",
+      },
+      resolvers: {
+        Query: {
+          currentUserId: (rootValue: RootValue) => rootValue.env,
+        },
+      },
+    });
+
+    new LocalResolvers<Resolvers, RootValue>({
+      rootValue: {
+        env: "prod",
+      },
+      resolvers: {
+        Query: {
+          // @ts-expect-error parent is incorrect type
+          currentUserId: (rootValue: { invalid: boolean }) => "1",
+        },
+      },
+    });
+
+    new LocalResolvers<Resolvers>({
+      rootValue: {
+        // @ts-expect-error invalid value for env
+        env: "staging",
+      },
+      resolvers: {
+        Query: {
+          currentUserId: (rootValue) => rootValue.env,
+        },
+      },
+    });
+
+    new LocalResolvers({
+      rootValue: {
+        // @ts-expect-error invalid value for env
+        env: "staging",
+      },
+      resolvers: {
+        Query: {
+          currentUserId: (_rootValue: RootValue) => 1,
+        },
+      },
+    });
+
+    new LocalResolvers({
+      // @ts-expect-error invalid value for env
+      rootValue: () => ({
+        env: "staging",
+      }),
+      resolvers: {
+        Query: {
+          currentUserId: (_rootValue: RootValue) => 1,
+        },
+      },
+    });
+
+    // @ts-expect-error rootValue is not provided
+    new LocalResolvers({
+      resolvers: {
+        Query: {
+          currentUserId: (_rootValue: RootValue) => 1,
+        },
+      },
+    });
+
+    // @ts-expect-error argument required
+    new LocalResolvers<
+      { User?: { isLoggedIn: LocalResolvers.Resolver } },
+      RootValue
+    >();
+
+    new LocalResolvers<
+      { User?: { isLoggedIn: LocalResolvers.Resolver } },
+      RootValue
+    >(
+      // @ts-expect-error rootValue is required
+      {}
+    );
+
+    new LocalResolvers<
+      { User?: { isLoggedIn: LocalResolvers.Resolver } },
+      RootValue
+    >(
+      // @ts-expect-error rootValue is required
+      {
+        resolvers: {},
+      }
+    );
+  });
+});

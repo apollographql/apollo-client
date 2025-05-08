a40b209e2e4bd90f96115ff3c6574af90a434b0e
Jerel Miller <jerelmiller@gmail.com>
2025-05-08 15:02:43 -0600

    Add writeQuery

--- src/local-resolvers/__tests__/LocalResolvers/cache.test.ts
+++ src/local-resolvers/__tests__/LocalResolvers/cache.test.ts
@@ -506,6 +506,22 @@ test("reads from the cache with a read function on a nested object field by defa
     link: ApolloLink.empty(),
   });
 
+  client.writeQuery({
+    query: gql`
+      query {
+        user {
+          id
+        }
+      }
+    `,
+    data: {
+      user: {
+        __typename: "User",
+        id: 1,
+      },
+    },
+  });
+
   const localResolvers = new LocalResolvers();
 
   await expect(

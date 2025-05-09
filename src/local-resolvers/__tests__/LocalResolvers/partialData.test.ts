204ce86c9a74c363857587d53bc03b5504689775
Jerel Miller <jerelmiller@gmail.com>
2025-05-09 13:22:31 -0600

    Update some partial data tests

--- src/local-resolvers/__tests__/LocalResolvers/partialData.test.ts
+++ src/local-resolvers/__tests__/LocalResolvers/partialData.test.ts
@@ -4,43 +4,6 @@ import { spyOnConsole } from "@apollo/client/testing/internal";
 
 import { gql } from "./testUtils.js";
 
-test("sets field to null and warns if resolver not defined when returnPartialData is false", async () => {
-  using _ = spyOnConsole("warn");
-  const document = gql`
-    query {
-      user {
-        id
-        isLoggedIn @client
-      }
-    }
-  `;
-
-  const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    link: ApolloLink.empty(),
-  });
-
-  const localResolvers = new LocalResolvers();
-
-  await expect(
-    localResolvers.execute({
-      document,
-      client,
-      context: {},
-      remoteResult: { data: { user: { __typename: "User", id: 1 } } },
-      returnPartialData: false,
-    })
-  ).resolves.toStrictEqualTyped({
-    data: { user: { __typename: "User", id: 1, isLoggedIn: null } },
-  });
-
-  expect(console.warn).toHaveBeenCalledTimes(1);
-  expect(console.warn).toHaveBeenCalledWith(
-    "Could not find a resolver for the '%s' field. The field value has been set to `null`.",
-    "User.isLoggedIn"
-  );
-});
-
 test("omits field and does not warn if resolver not defined when returnPartialData is true", async () => {
   using _ = spyOnConsole("warn");
   const document = gql`
@@ -72,4 +35,58 @@ test("omits field and does not warn if resolver not defined when returnPartialDa
   });
 
   expect(console.warn).not.toHaveBeenCalled();
+});
+
+test("omits client fields without cached values when running forced resolvers with returnPartialData: true", async () => {
+  using _ = spyOnConsole("warn");
+  const document = gql`
+    query {
+      user {
+        id
+        name @client
+        isLoggedIn @client(always: true)
+      }
+    }
+  `;
+
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: ApolloLink.empty(),
+  });
+
+  let nameCount = 0;
+  let isLoggedInCount = 0;
+  const localResolvers = new LocalResolvers({
+    resolvers: {
+      User: {
+        name: () => {
+          nameCount++;
+          return "John Smith";
+        },
+        isLoggedIn: () => {
+          isLoggedInCount++;
+          return true;
+        },
+      },
+    },
+  });
+
+  await expect(
+    localResolvers.execute({
+      document,
+      client,
+      context: {},
+      remoteResult: { data: { user: { __typename: "User", id: 1 } } },
+      returnPartialData: true,
+      onlyRunForcedResolvers: true,
+    })
+  ).resolves.toStrictEqualTyped({
+    // Note: name is omitted because we are only running forced resolvers and
+    // have no cached value for name
+    data: { user: { __typename: "User", id: 1, isLoggedIn: true } },
+  });
+
+  expect(console.warn).not.toHaveBeenCalled();
+  expect(nameCount).toBe(0);
+  expect(isLoggedInCount).toBe(1);
 });

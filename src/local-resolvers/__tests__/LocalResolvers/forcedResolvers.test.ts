925ca6e5cba78d34e120e783e3a6795f9e49ce24
Jerel Miller <jerelmiller@gmail.com>
2025-05-08 23:14:04 -0600

    Fix issue where parent resolver ran if there was a child forced resolver

--- src/local-resolvers/__tests__/LocalResolvers/forcedResolvers.test.ts
+++ src/local-resolvers/__tests__/LocalResolvers/forcedResolvers.test.ts
@@ -123,3 +123,68 @@ test("only runs forced resolvers for fields marked with `@client(always: true)`,
   expect(nameCount).toEqual(0);
   expect(isLoggedInCount).toEqual(1);
 });
+
+test("runs nested forced resolvers from non-forced client descendant field", async () => {
+  const document = gql`
+    query UserDetails {
+      user @client {
+        id
+        name
+        isLoggedIn @client(always: true)
+      }
+    }
+  `;
+
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: ApolloLink.empty(),
+  });
+
+  let userCount = 0;
+  let isLoggedInCount = 0;
+
+  const localResolvers = new LocalResolvers({
+    resolvers: {
+      Query: {
+        user() {
+          userCount += 1;
+          return { __typename: "User", id: 1, name: "John Smith" };
+        },
+      },
+      User: {
+        isLoggedIn() {
+          isLoggedInCount += 1;
+          return true;
+        },
+      },
+    },
+  });
+
+  client.writeQuery({
+    query: document,
+    data: {
+      user: {
+        __typename: "User",
+        id: 1,
+        name: "John Smith",
+        isLoggedIn: true,
+      },
+    },
+  });
+
+  await expect(
+    localResolvers.execute({
+      document,
+      client,
+      context: {},
+      onlyRunForcedResolvers: true,
+    })
+  ).resolves.toStrictEqualTyped({
+    data: {
+      user: { __typename: "User", id: 1, name: "John Smith", isLoggedIn: true },
+    },
+  });
+
+  expect(userCount).toEqual(0);
+  expect(isLoggedInCount).toEqual(1);
+});

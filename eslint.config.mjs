d3f1ad517de85caf47d7d3c55f8d4872ce9138b3
Jerel Miller <jerelmiller@gmail.com>
2025-05-12 14:58:21 -0600

    Add test codegen config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -48,6 +48,7 @@ export default [
         rules: localRules,
       },
     },
+    ignores: ["tests.codegen.ts"],
 
     languageOptions: {
       globals: {
---
+++ tests.codegen.ts
@@ -0,0 +1,42 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+// @ts-ignore for some reason this allows codegen to load the plugin, otherwise it errors
+await import("@apollo/client/local-state/codegen");
+
+const config: CodegenConfig = {
+  hooks: {
+    afterAllFileWrite: ["prettier --write"],
+  },
+  generates: {
+    "./src/local-state/__tests__/LocalState/fixtures/local-resolvers.ts": {
+      schema: [
+        "./src/local-state/__tests__/LocalState/fixtures/localSchema.graphql",
+      ],
+      plugins: [
+        { add: { content: "/* eslint-disable */" } },
+        "typescript",
+        "@apollo/client/local-state/codegen",
+      ],
+      config: {
+        nonOptionalTypename: true,
+        rootValueType: "./rootValue.js#RootValue",
+      },
+    },
+    "./src/local-state/__tests__/LocalState/fixtures/local-resolvers-without-context-value.ts":
+      {
+        schema: [
+          "./src/local-state/__tests__/LocalState/fixtures/localSchema.graphql",
+        ],
+        plugins: [
+          { add: { content: "/* eslint-disable */" } },
+          "typescript",
+          "@apollo/client/local-state/codegen",
+        ],
+        config: {
+          nonOptionalTypename: true,
+        },
+      },
+  },
+};
+
+export default config;

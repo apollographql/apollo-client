f446e1af72a3a720ed3607391a55236335e830aa
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-05-26 10:30:06 +0200

    Exclude `testing/internal` entrypoint in build

--- config/jest.config.ts
+++ config/jest.config.ts
@@ -27,6 +27,9 @@ const defaults = {
   },
   resolver: "<rootDir>/config/jest/resolver.ts",
   prettierPath: null,
+  moduleNameMapper: {
+    "^@apollo/client/testing/internal$": "<rootDir>/testing/internal/index.ts",
+  },
 };
 
 const ignoreDTSFiles = ".d.ts$";
@@ -78,6 +81,7 @@ const standardReact18Config = {
     "src/react/ssr/__tests__/prerenderStatic.test.tsx",
   ],
   moduleNameMapper: {
+    ...defaults.moduleNameMapper,
     "^react$": "react-18",
     "^react-dom$": "react-dom-18",
     "^react-dom/(.*)$": "react-dom-18/$1",
@@ -89,6 +93,7 @@ const standardReact17Config = {
   displayName: "ReactDOM 17",
   testPathIgnorePatterns: react17TestFileIgnoreList,
   moduleNameMapper: {
+    ...defaults.moduleNameMapper,
     "^react$": "react-17",
     "^react-dom$": "react-dom-17",
     "^react-dom/client$": "<rootDir>/../config/jest/react-dom-17-client.js",

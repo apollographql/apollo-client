f450fe91d223c35e6a4f9b89cbb57e53b2c18003
Jerel Miller <jerelmiller@gmail.com>
2025-05-27 21:46:48 -0600

    Remove outdated comment

--- src/__tests__/fetchMore.ts
+++ src/__tests__/fetchMore.ts
@@ -1998,12 +1998,6 @@ test("uses updateQuery to update the result of the query with no-cache queries",
 
   const stream = new ObservableStream(observable);
 
-  // TODO: This assertion was added to this test because I just cannot figure out what
-  // condition causes the loading state to be emitted when first subscribing.
-  // Another test expects the loading state with a no-cache query as well, but
-  // this one didn't. This is a temporary fix to get the test to pass.
-  //
-  // This will be normalized with the work to change notifyOnNetworkStatusChange.
   await expect(stream).toEmitTypedValue({
     data: undefined,
     loading: true,

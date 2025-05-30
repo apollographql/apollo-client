958b0166941f09507ba2327251116c083636281c
Jerel Miller <jerelmiller@gmail.com>
2025-05-30 17:23:49 -0600

    Add test for returnPartialData: false and new vars with partial data

--- src/core/__tests__/ObservableQuery.ts
+++ src/core/__tests__/ObservableQuery.ts
@@ -6881,3 +6881,103 @@ describe.skip("type tests", () => {
     });
   });
 });
+
+test("does not return partial cache data when `returnPartialData` is false and new variables are passed in", async () => {
+  const partialQuery = gql`
+    query MyCar($id: ID) {
+      car(id: $id) {
+        id
+        make
+      }
+    }
+  `;
+
+  const query = gql`
+    query MyCar($id: ID) {
+      car(id: $id) {
+        id
+        make
+        model
+      }
+    }
+  `;
+
+  const cache = new InMemoryCache();
+  const client = new ApolloClient({
+    cache,
+    link: new MockLink([
+      {
+        request: { query, variables: { id: 2 } },
+        result: {
+          data: {
+            car: { __typename: "Car", id: 2, make: "Ford", model: "Bronco" },
+          },
+        },
+        delay: 50,
+      },
+    ]),
+  });
+
+  cache.writeQuery({
+    query,
+    variables: { id: 1 },
+    data: {
+      car: {
+        __typename: "Car",
+        id: 1,
+        make: "Ford",
+        model: "Pinto",
+      },
+    },
+  });
+
+  cache.writeQuery({
+    query: partialQuery,
+    variables: { id: 2 },
+    data: {
+      car: {
+        __typename: "Car",
+        id: 2,
+        make: "Ford",
+        model: "Bronco",
+      },
+    },
+  });
+
+  const observable = client.watchQuery({
+    query,
+    variables: { id: 1 },
+    returnPartialData: false,
+  });
+
+  const stream = new ObservableStream(observable);
+
+  await expect(stream).toEmitTypedValue({
+    data: {
+      car: { __typename: "Car", id: 1, make: "Ford", model: "Pinto" },
+    },
+    loading: false,
+    networkStatus: NetworkStatus.ready,
+    partial: false,
+  });
+
+  void observable.reobserve({ variables: { id: 2 } });
+
+  await expect(stream).toEmitTypedValue({
+    data: undefined,
+    loading: true,
+    networkStatus: NetworkStatus.setVariables,
+    partial: true,
+  });
+
+  expect(observable.getCurrentResult()).toBe(stream.getCurrent());
+
+  await expect(stream).toEmitTypedValue({
+    data: { car: { __typename: "Car", id: 2, make: "Ford", model: "Bronco" } },
+    loading: false,
+    networkStatus: NetworkStatus.ready,
+    partial: false,
+  });
+
+  expect(observable.getCurrentResult()).toBe(stream.getCurrent());
+});

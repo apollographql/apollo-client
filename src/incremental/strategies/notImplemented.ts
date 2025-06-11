254bc156e5261aeed5219e2c9964e512c88536c9
Jerel Miller <jerelmiller@gmail.com>
2025-06-11 17:20:25 -0600

    Update not implemented strategy

--- src/incremental/strategies/notImplemented.ts
+++ src/incremental/strategies/notImplemented.ts
@@ -1,11 +1,12 @@
 import type { Incremental } from "../types.js";
 
-export function notImplementedStrategy(): Incremental.Strategy {
+export function notImplementedStrategy(): Incremental.Strategy<never> {
   return {
+    isIncrementalPatchResult: (result): result is never => false,
     prepareRequest: (request) => request,
     startRequest: () => {
       throw new Error(
-        "`@defer` is not supported. Please us a different strategy"
+        "`@defer` is not supported. Please use a different strategy."
       );
     },
   };

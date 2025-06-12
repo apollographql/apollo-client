f13996c969de35a234ac6c9dec84f1b219496528
Jerel Miller <jerelmiller@gmail.com>
2025-06-11 18:37:39 -0600

    Add defer defer20220824 strategy

--- src/incremental/index.ts
+++ src/incremental/index.ts
@@ -1,2 +1,6 @@
 export type { Incremental } from "./types.js";
 export { notImplementedStrategy } from "./strategies/notImplemented.js";
+export {
+  defer20220824,
+  defer20220824 as graphqlAlpha2,
+} from "./strategies/defer20220824.js";

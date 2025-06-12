2ce734e8fdfdb5cb93b1668501ee1f16bf66794d
Jerel Miller <jerelmiller@gmail.com>
2025-06-11 18:37:49 -0600

    Adjust some incremnental types

--- src/incremental/types.ts
+++ src/incremental/types.ts
@@ -2,11 +2,11 @@ import type { GraphQLRequest } from "@apollo/client";
 
 export declare namespace Incremental {
   export interface ExecutionResult {
-    Initial: unknown;
-    Subsequent: unknown;
+    Initial: any;
+    Subsequent: any;
   }
 
-  export type Path = Array<string | number>;
+  export type Path = ReadonlyArray<string | number>;
 
   export interface Pending {
     id: string;
@@ -15,9 +15,12 @@ export declare namespace Incremental {
   }
 
   export interface Strategy<
-    TExecutionResult extends Incremental.ExecutionResult,
+    TExecutionResult extends
+      Incremental.ExecutionResult = Incremental.ExecutionResult,
   > {
-    isIncrementalPatchResult: (result: unknown) => result is TExecutionResult;
+    isIncrementalPatchResult: (
+      result: Record<string, any>
+    ) => result is TExecutionResult;
     prepareRequest: (request: GraphQLRequest) => GraphQLRequest;
     startRequest: () => IncrementalRequest<TExecutionResult>;
   }
@@ -27,7 +30,7 @@ export declare namespace Incremental {
     Chunk = TExecutionResult["Initial"] | TExecutionResult["Subsequent"],
   > {
     hasNext: boolean;
-    append: (chunk: Chunk) => this;
+    append: (chunk: Chunk) => void;
     apply: <TData>(data: TData, chunk: Chunk) => TData;
 
     // reservered for future

2116f9568893e3bedb6f430701cf2300bb790f16
Jerel Miller <jerelmiller@gmail.com>
2025-06-11 18:44:57 -0600

    Throw when calling prepareRequest

--- src/incremental/strategies/notImplemented.ts
+++ src/incremental/strategies/notImplemented.ts
@@ -1,9 +1,19 @@
+import { hasDirectives } from "@apollo/client/utilities/internal";
+
 import type { Incremental } from "../types.js";
 
 export function notImplementedStrategy(): Incremental.Strategy<never> {
   return {
     isIncrementalPatchResult: (result): result is never => false,
-    prepareRequest: (request) => request,
+    prepareRequest: (request) => {
+      if (hasDirectives(["defer"], request.query)) {
+        throw new Error(
+          "`@defer` is not supported. Please use a different strategy."
+        );
+      }
+
+      return request;
+    },
     startRequest: () => {
       throw new Error(
         "`@defer` is not supported. Please use a different strategy."

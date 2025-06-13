18886ed5b194c09dcba4e48831771fc90b11af90
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-06-13 17:57:38 +0200

    remove unused code

--- src/incremental/strategies/defer20220824.ts
+++ src/incremental/strategies/defer20220824.ts
@@ -6,7 +6,6 @@ import {
   DeepMerger,
   hasDirectives,
   isNonEmptyArray,
-  mergeIncrementalData,
 } from "@apollo/client/utilities/internal";
 
 import type { Incremental } from "../types.js";
@@ -89,24 +88,8 @@ class DeferRequest
 }
 
 export function defer20220824(): Incremental.Strategy<defer20220824.ExecutionResult> {
-  function isIncrementalSubsequentResult(
-    result: Record<string, any>
-  ): result is defer20220824.SubsequentResult {
-    return "incremental" in result;
-  }
-
-  function isIncrementalInitialResult(
-    result: Record<string, any>
-  ): result is defer20220824.InitialResult {
-    return "hasNext" in result && "data" in result;
-  }
-
   return {
-    isIncrementalResult: (
-      result: Record<string, any>
-    ): result is defer20220824.SubsequentResult | defer20220824.InitialResult =>
-      isIncrementalInitialResult(result) ||
-      isIncrementalSubsequentResult(result),
+    isIncrementalResult,
     isIncrementalSubsequentResult,
     isIncrementalInitialResult,
     prepareRequest: (request) => {
@@ -124,4 +107,51 @@ export function defer20220824(): Incremental.Strategy<defer20220824.ExecutionRes
     startRequest: ({ query, initialChunk }) =>
       new DeferRequest(query, initialChunk),
   };
+}
+
+function isIncrementalSubsequentResult(
+  result: Record<string, any>
+): result is defer20220824.SubsequentResult {
+  return "incremental" in result;
+}
+
+function isIncrementalInitialResult(
+  result: Record<string, any>
+): result is defer20220824.InitialResult {
+  return "hasNext" in result && "data" in result;
+}
+
+function isIncrementalResult(
+  result: Record<string, any>
+): result is defer20220824.SubsequentResult | defer20220824.InitialResult {
+  return (
+    isIncrementalInitialResult(result) || isIncrementalSubsequentResult(result)
+  );
+}
+
+function mergeIncrementalData<TData extends object>(
+  prevResult: TData,
+  result:
+    | defer20220824.InitialResult<TData>
+    | defer20220824.SubsequentResult<TData>
+) {
+  let mergedData = prevResult;
+  const merger = new DeepMerger();
+  if (
+    isIncrementalSubsequentResult(result) &&
+    isNonEmptyArray(result.incremental)
+  ) {
+    result.incremental.forEach(({ data, path }) => {
+      if (!data || !path) return;
+      for (let i = path.length - 1; i >= 0; --i) {
+        const key = path[i];
+        const isNumericKey = !isNaN(+key);
+        const parent: Record<string | number, any> = isNumericKey ? [] : {};
+        parent[key] = data;
+        data = parent as typeof data;
+      }
+      mergedData = merger.merge(mergedData, data);
+    });
+  }
+  return mergedData as TData;
 }

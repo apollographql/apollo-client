1a29c1b1f3229c2961ba558dc9f3204501ac10fd
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-06-13 12:53:56 +0200

    handle errors thrown in `prepareRequest`

--- src/incremental/strategies/notImplemented.ts
+++ src/incremental/strategies/notImplemented.ts
@@ -9,6 +9,7 @@ export function notImplementedStrategy(): Incremental.Strategy<never> {
     isIncrementalInitialResult: (result): result is never => false,
     prepareRequest: (request) => {
       if (hasDirectives(["defer"], request.query)) {
+        // TODO: throwing behavior needs a test - does this end up in the stream correctly?
         throw new Error(
           "`@defer` is not supported. Please use a different strategy."
         );
@@ -17,6 +18,7 @@ export function notImplementedStrategy(): Incremental.Strategy<never> {
       return request;
     },
     startRequest: () => {
+      // TODO: throwing behavior needs a test - does this end up in the stream correctly?
       throw new Error(
         "`@defer` is not supported. Please use a different strategy."
       );

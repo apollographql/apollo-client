b2103fcb1ad2abebfea1d0a27630e17518664fd8
Jerel Miller <jerelmiller@gmail.com>
2025-06-20 00:10:36 -0600

    Remove utilities if empty

--- scripts/codemods/ac3-to-ac4/imports.ts
+++ scripts/codemods/ac3-to-ac4/imports.ts
@@ -61,6 +61,7 @@ const transform: Transform = function transform(file, api) {
   }
 
   removeImportIfEmpty("@apollo/client");
+  removeImportIfEmpty("@apollo/client/utilities");
 
   return source.toSource();
 

ab13aeee047f580ef870a26c185c78530383b7e1
Jerel Miller <jerelmiller@gmail.com>
2025-06-27 16:39:40 -0600

    Add deprecation for toPromise/fromPromise

--- src/link/utils/fromPromise.ts
+++ src/link/utils/fromPromise.ts
@@ -1,5 +1,21 @@
 import { Observable } from "../../utilities/index.js";
 
+/**
+ * @deprecated `fromPromise` will be removed in Apollo Client 4.0. This is safe
+ * to use in 3.x.
+ *
+ * **Recommended now**
+ *
+ * No action needed
+ *
+ * **When upgrading**
+ *
+ * Use RxJS's [`from`](https://rxjs.dev/api/index/function/from) function.
+ *
+ * ```ts
+ * const observable = from(promise);
+ * ```
+ */
 export function fromPromise<T>(promise: Promise<T>): Observable<T> {
   return new Observable<T>((observer) => {
     promise

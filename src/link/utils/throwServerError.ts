6094e775582dfac326f08f33bd605dd81d1e67ce
Jerel Miller <jerelmiller@gmail.com>
2025-06-27 16:39:29 -0600

    Add deprecation to throwServerError

--- src/link/utils/throwServerError.ts
+++ src/link/utils/throwServerError.ts
@@ -4,6 +4,23 @@ export type ServerError = Error & {
   statusCode: number;
 };
 
+/**
+ * @deprecated `throwServerError` will be removed in Apollo Client 4.0. This is
+ * safe to use in Apollo Client 3.x.
+ *
+ * **Recommended now**
+ *
+ * No action needed
+ *
+ * **When migrating**
+ *
+ * `ServerError` is a subclass of `Error`. To throw a server error, use
+ * `throw new ServerError(...)` instead.
+ *
+ * ```ts
+ * throw new ServerError("error message", { response, result });
+ * ```
+ */
 export const throwServerError = (
   response: Response,
   result: any,

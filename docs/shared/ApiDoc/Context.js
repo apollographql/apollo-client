c2a3323c7a3301e77237d51b3637659a5a46ff8c
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-07-03 14:16:07 +0200

    delete unused documentation files

--- docs/shared/ApiDoc/Context.js
+++
@@ -1,6 +0,0 @@
-import { useMDXComponents } from "@mdx-js/react";
-
-export const useApiDocContext = function () {
-  const MDX = useMDXComponents();
-  return MDX.useApiDocContext(this, arguments);
-};

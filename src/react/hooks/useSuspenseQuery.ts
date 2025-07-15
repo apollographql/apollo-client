b8b86cd50515957d47428274138ee72981c16db3
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-07-15 11:43:43 +0200

    update documentation types for `useFragment`

--- src/react/hooks/useSuspenseQuery.ts
+++ src/react/hooks/useSuspenseQuery.ts
@@ -31,7 +31,6 @@ import type {
   DocumentationTypes as UtilityDocumentationTypes,
   NoInfer,
   VariablesOption,
-  DecoratedPromise,
 } from "@apollo/client/utilities/internal";
 
 import type { SkipToken } from "./constants.js";

ede52a48d0b6071d05dc046fc2ccae0be3984e39
Jerel Miller <jerelmiller@gmail.com>
2025-07-21 16:34:24 -0600

    Add defaults to deprecated WatchFragmentOptions

--- src/cache/deprecated.ts
+++ src/cache/deprecated.ts
@@ -1,8 +1,10 @@
-import type { ApolloCache } from "@apollo/client";
+import type { ApolloCache, OperationVariables } from "@apollo/client";
 
 /** @deprecated Use `ApolloCache.WatchFragmentOptions` instead */
-export type WatchFragmentOptions<TData, TVariables> =
-  ApolloCache.WatchFragmentOptions<TData, TVariables>;
+export type WatchFragmentOptions<
+  TData = unknown,
+  TVariables extends OperationVariables = OperationVariables,
+> = ApolloCache.WatchFragmentOptions<TData, TVariables>;
 
 /** @deprecated Use `ApolloCache.WatchFragmentResult` instead */
 export type WatchFragmentResult<TData> = ApolloCache.WatchFragmentResult<TData>;

d5b007294049924da04dab50e40d160e9de86468
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-07-25 11:15:58 +0200

    format correctly parsed mdx, ignore incorrectly parsed

--- config/prettier/format-mdx3.js
+++ config/prettier/format-mdx3.js
@@ -24,18 +24,23 @@ export const printers = {
     embed(path, options) {
       const node = path.node;
       if (node.type === "jsx") {
-        // We will not format embedded JSX as Prettier gets the spacing wrong
-        return (
-          node.value
-            .split("\n")
-            // But we need to restore the original indentation
-            .map((line, idx) =>
-              idx === 0 ? line : (
-                " ".repeat(node.position.indent[idx - 1] - 1) + line
+        // If the node was parsed incorrectly because it followed the MDX3 format (no spacing around JSX tags),
+        // we will not try to format it as MDX, but instead return the original value.
+
+        // We detect that by looking at `value` - if it's only a starting or closing tag, it was parsed correctly.
+        const correctlyParsedMatch = node.value.match(/^(<\/?[^>]*>)$/);
+        if (!correctlyParsedMatch)
+          return (
+            node.value
+              .split("\n")
+              // But we need to restore the original indentation
+              .map((line, idx) =>
+                idx === 0 ? line : (
+                  " ".repeat(node.position.indent[idx - 1] - 1) + line
+                )
               )
-            )
-            .join("\n")
-        );
+              .join("\n")
+          );
       }
       return markdown.printers.mdast.embed(path, options);
     },

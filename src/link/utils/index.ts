2c063dad7bf8b8ffad8d570586ef46f431ec1942
Jerel Miller <jerelmiller@gmail.com>
2025-07-29 11:02:14 -0600

    Don't export validateOperation from link/utils

--- src/link/utils/index.ts
+++ src/link/utils/index.ts
@@ -1,3 +1,2 @@
-export { validateOperation } from "./validateOperation.js";
 export { createOperation } from "./createOperation.js";
 export { filterOperationVariables } from "./filterOperationVariables.js";

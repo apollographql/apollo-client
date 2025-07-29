3a0d11f205fa7e67ecbd396db80ecff2db6aff6d
Jerel Miller <jerelmiller@gmail.com>
2025-07-29 10:51:16 -0600

    Don't export transfromOperation from link utils

--- src/link/utils/index.ts
+++ src/link/utils/index.ts
@@ -1,4 +1,3 @@
 export { validateOperation } from "./validateOperation.js";
 export { createOperation } from "./createOperation.js";
-export { transformOperation } from "./transformOperation.js";
 export { filterOperationVariables } from "./filterOperationVariables.js";

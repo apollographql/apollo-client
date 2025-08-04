ad78ad052568db4b99204a9e74308f43730c775e
Jerel Miller <jerelmiller@gmail.com>
2025-08-04 11:21:55 -0600

    Formatting

--- src/link/error/index.ts
+++ src/link/error/index.ts
@@ -80,7 +80,8 @@ export function onError(errorHandler: ErrorLink.ErrorHandler) {
  * moving back up your [link chain](https://apollographql.com/docs/react/introduction#handling-a-response). The `errorHandler` function should
  * not return a value unless you want to [retry the operation](https://apollographql.com/docs/react/data/error-handling#retrying-operations).
  *
- * For more information on the types of errors that might be encountered, see the guide on [error handling](https://apollographql.com/docs/react/data/error-handling).
+ * For more information on the types of errors that might be encountered, see
+ * the guide on [error handling](https://apollographql.com/docs/react/data/error-handling).
  *
  * @example
  *

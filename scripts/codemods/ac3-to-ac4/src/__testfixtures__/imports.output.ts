d79b3f315bad77d8ba91d29676b233a9b6fdd9ba
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-08-05 13:47:19 +0200

    use `rename.importType` when creating a new import

--- scripts/codemods/ac3-to-ac4/src/__testfixtures__/imports.output.ts
+++ scripts/codemods/ac3-to-ac4/src/__testfixtures__/imports.output.ts
@@ -1,22 +1,17 @@
 /* eslint-disable import/order */
 
-import type { QueryRef, QueryHookOptions } from "@apollo/client/react";
-const queryRef: QueryRef = {} as any;
-const queryOptions: QueryHookOptions = {} as any;
 
-import type { ApolloProvider } from "@apollo/client/react";
+
+const queryRef: QueryRef = {} as any;
+const queryOptions: useQuery.Options = {} as any;
+
 const providerProps: ApolloProvider.Props = {} as any;
 
 import type { ErrorLink } from "@apollo/client/link/error";
 const response: ErrorLink.ErrorHandlerOptions = {} as any;
 
-import { useQuery, ApolloProvider } from "@apollo/client/react";
-
-import type { LoadQueryFunction } from "@apollo/client/react";
-
-import { getApolloContext } from "@apollo/client/react";
+import { getApolloContext, ApolloProvider, useQuery, QueryRef, useLoadableQuery } from "@apollo/client/react";
 
 import { omitDeep } from "@apollo/client/utilities/internal";
-
 import type { SetContextLink } from "@apollo/client/link/context";
 const contextSetter: SetContextLink.LegacyContextSetter = (() => {}) as any;
--- scripts/codemods/ac3-to-ac4/src/util/handleIdentiferRename.ts
+++ scripts/codemods/ac3-to-ac4/src/util/handleIdentiferRename.ts
@@ -117,7 +117,7 @@ export function handleIdentiferRename({
                 j.importDeclaration(
                   [specifier],
                   j.literal(to.module || from.module),
-                  importType
+                  rename.importType
                 )
               );
             }

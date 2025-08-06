d0ce36fa013afa22c29207475389ef4392c8ce59
Jerel Miller <jerelmiller@gmail.com>
2025-08-06 11:00:28 -0600

    Update getMainDefinition doc block

--- src/utilities/internal/getMainDefinition.ts
+++ src/utilities/internal/getMainDefinition.ts
@@ -9,11 +9,41 @@ import { newInvariantError } from "@apollo/client/utilities/invariant";
 import { checkDocument } from "./checkDocument.js";
 
 /**
- * Returns the first operation definition found in this document.
- * If no operation definition is found, the first fragment definition will be returned.
- * If no definitions are found, an error will be thrown.
+ * Returns the first operation definition from a GraphQL document. The function
+ * prioritizes operation definitions over fragment definitions, which makes it
+ * suitable for documents that may contain both. If no operation definition is
+ * found, the first fragment definition will be returned. If no definitions are
+ * found, an error is thrown.
  *
- * @internal
+ * @remarks
+ *
+ * Use this function when you need to perform more advanced tasks with the main
+ * definition AST node. If you want to determine when a document is a specific
+ * operation type, prefer the `isQueryOperation`, `isMutationOperation`, and
+ * `isSubscriptionOperation` utility functions instead.
+ *
+ * @param queryDoc - The GraphQL document to extract the definition from
+ * @returns The main operation or fragment definition AST node
+ *
+ * @example
+ *
+ * ```ts
+ * import { gql } from "@apollo/client";
+ * import { getMainDefinition } from "@apollo/client/utilities";
+ *
+ * const query = gql`
+ *   query GetUser($id: ID!) {
+ *     user(id: $id) {
+ *       name
+ *       email
+ *     }
+ *   }
+ * `;
+ *
+ * const definition = getMainDefinition(query);
+ * ```
+ *
+ * @throws When the document contains no operation or fragment definitions
  */
 export function getMainDefinition(
   queryDoc: DocumentNode

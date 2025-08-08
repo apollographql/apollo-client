4eac75447f63bd4170496c9aeb6a7f274d2c70c3
Jerel Miller <jerelmiller@gmail.com>
2025-08-08 15:01:48 -0600

    Update doc blocks for utilities (#12831)

--- src/utilities/common/stripTypename.ts
+++ src/utilities/common/stripTypename.ts
@@ -5,6 +5,17 @@ import { omitDeep } from "@apollo/client/utilities/internal";
  *
  * @param value - The object or array that should have `__typename` removed.
  * @returns The object with all `__typename` properties removed.
+ *
+ * @example
+ *
+ * ```ts
+ * stripTypename({
+ *   __typename: "User",
+ *   id: 1,
+ *   profile: { __typename: "Profile", name: "John Doe" },
+ * });
+ * // => { id: 1, profile: { name: "John Doe"}}
+ * ```
  */
 export function stripTypename<T>(value: T) {
   return omitDeep(value, "__typename");

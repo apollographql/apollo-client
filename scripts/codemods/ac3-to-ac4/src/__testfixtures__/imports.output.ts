cd13551be42ea0b986761f520d23ce6c8760330c
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-08-11 16:07:34 +0200

    update tests

--- scripts/codemods/ac3-to-ac4/src/__testfixtures__/imports.output.ts
+++ scripts/codemods/ac3-to-ac4/src/__testfixtures__/imports.output.ts
@@ -5,12 +5,14 @@
 const queryRef: QueryRef = {} as any;
 const queryOptions: useQuery.Options = {} as any;
 
+import type { QueryRef, useLoadableQuery } from "@apollo/client/react";
 const providerProps: ApolloProvider.Props = {} as any;
 
 import type { ErrorLink } from "@apollo/client/link/error";
 const response: ErrorLink.ErrorHandlerOptions = {} as any;
 
-import { getApolloContext, QueryRef, ApolloProvider, useQuery, useLoadableQuery } from "@apollo/client/react";
+import { getApolloContext, ApolloProvider, useQuery } from "@apollo/client/react";
 import { omitDeep } from "@apollo/client/utilities/internal";
+
 import type { SetContextLink } from "@apollo/client/link/context";
 const contextSetter: SetContextLink.LegacyContextSetter = (() => {}) as any;

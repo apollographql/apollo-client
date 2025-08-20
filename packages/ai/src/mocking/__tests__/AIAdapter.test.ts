4e4031a998cfb2445f88e16f0b0ce2ff2e86910b
Greg Wardwell <gwardwell@users.noreply.github.com>
2025-08-20 15:32:56 -0400

    test(ai): add jest config and simple test

---
+++ packages/ai/src/mocking/__tests__/AIAdapter.test.ts
@@ -0,0 +1,23 @@
+import { AIAdapter } from "../AIAdapter.js";
+
+class DerivedAdapter extends AIAdapter {
+  constructor() {
+    super();
+  }
+
+  public generateObject(prompt: string): Promise<any> {
+    return Promise.resolve({
+      data: null,
+    });
+  }
+}
+
+describe("AIAdapter derived class", () => {
+  it("should be able to generate an object", async () => {
+    const adapter = new DerivedAdapter();
+    const result = await adapter.generateObject("Hello, world!");
+    expect(result).toEqual({
+      data: null,
+    });
+  });
+});

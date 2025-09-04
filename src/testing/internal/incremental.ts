ba1f15e1dc082dd45b844143e2445583059d3bb3
Jerel Miller <jerelmiller@gmail.com>
2025-09-04 16:25:48 -0600

    Move mockDeferStream to own file

--- src/testing/internal/incremental.ts
+++ src/testing/internal/incremental.ts
@@ -1,52 +1,10 @@
-import type {
-  FormattedInitialIncrementalExecutionResult,
-  FormattedSubsequentIncrementalExecutionResult,
-  GraphQLFormattedError,
-} from "graphql-17-alpha2";
-
 import type { ApolloPayloadResult } from "@apollo/client";
 
+import { mockDefer20220824 } from "./incremental/defer20220824.js";
 import { mockMultipartStream } from "./incremental/utils.js";
 
-export function mockDeferStream<
-  TData = Record<string, unknown>,
-  TExtensions = Record<string, unknown>,
->() {
-  const { httpLink, enqueue } = mockMultipartStream<
-    | FormattedInitialIncrementalExecutionResult<TData, TExtensions>
-    | FormattedSubsequentIncrementalExecutionResult<TData, TExtensions>
-  >({
-    responseHeaders: new Headers({
-      "Content-Type": 'multipart/mixed; boundary="-"; deferSpec=20220824',
-    }),
-  });
-  return {
-    httpLink,
-    enqueueInitialChunk(
-      chunk: FormattedInitialIncrementalExecutionResult<TData, TExtensions>
-    ) {
-      enqueue(chunk, chunk.hasNext);
-    },
-    enqueueSubsequentChunk(
-      chunk: FormattedSubsequentIncrementalExecutionResult<TData, TExtensions>
-    ) {
-      enqueue(chunk, chunk.hasNext);
-    },
-    enqueueErrorChunk(errors: GraphQLFormattedError[]) {
-      enqueue(
-        {
-          hasNext: true,
-          incremental: [
-            {
-              errors,
-            },
-          ],
-        },
-        true
-      );
-    },
-  };
-}
+// TODO: Update to new name
+export { mockDefer20220824 as mockDeferStream };
 
 export function mockMultipartSubscriptionStream<
   TData = Record<string, unknown>,

7b51170eb9b1d41649c974947ed81a951120573f
Jerel Miller <jerelmiller@gmail.com>
2025-09-04 16:36:33 -0600

    Import directly to avoid another barrel file

--- src/testing/internal/incremental.ts
+++
@@ -1,3 +0,0 @@
-export { mockDefer20220824 } from "./multipart/mockDefer20220824.js";
-export { mockDeferStreamGraphQL17Alpha9 } from "./multipart/mockDeferStreamGraphql17Alpha9.js";
-export { mockMultipartSubscriptionStream } from "./multipart/mockMultipartSubscriptionStream.js";
--- src/testing/internal/index.ts
+++ src/testing/internal/index.ts
@@ -25,11 +25,9 @@ export { createClientWrapper, createMockWrapper } from "./renderHelpers.js";
 export { actAsync } from "./rtl/actAsync.js";
 export { renderAsync } from "./rtl/renderAsync.js";
 export { renderHookAsync } from "./rtl/renderHookAsync.js";
-export {
-  mockDefer20220824,
-  mockDeferStreamGraphQL17Alpha9,
-  mockMultipartSubscriptionStream,
-} from "./incremental.js";
+export { mockDefer20220824 } from "./multipart/mockDefer20220824.js";
+export { mockDeferStreamGraphQL17Alpha9 } from "./multipart/mockDeferStreamGraphql17Alpha9.js";
+export { mockMultipartSubscriptionStream } from "./multipart/mockMultipartSubscriptionStream.js";
 export { resetApolloContext } from "./resetApolloContext.js";
 export {
   createOperationWithDefaultContext,

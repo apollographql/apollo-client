1780c728012b8913f6b31f6543849d096743dcf9
Jerel Miller <jerelmiller@gmail.com>
2025-09-15 15:59:57 -0600

    Add tests for refetches with defer arrays

--- src/core/__tests__/client.watchQuery/defer20220824.test.ts
+++ src/core/__tests__/client.watchQuery/defer20220824.test.ts
@@ -6,6 +6,7 @@ import { InMemoryCache } from "@apollo/client/cache";
 import { Defer20220824Handler } from "@apollo/client/incremental";
 import { ApolloLink } from "@apollo/client/link";
 import {
+  markAsStreaming,
   mockDefer20220824,
   ObservableStream,
 } from "@apollo/client/testing/internal";
@@ -163,3 +164,186 @@ test("deduplicates queries as long as a query still has deferred chunks", async 
   // expect(query5).not.toEmitAnything();
   expect(outgoingRequestSpy).toHaveBeenCalledTimes(2);
 });
+
+it.each([["cache-first"], ["no-cache"]] as const)(
+  "correctly merges deleted rows when receiving a deferred payload",
+  async (fetchPolicy) => {
+    const query = gql`
+      query Characters {
+        characters {
+          id
+          uppercase
+          ... @defer {
+            lowercase
+          }
+        }
+      }
+    `;
+
+    const { httpLink, enqueueInitialChunk, enqueueSubsequentChunk } =
+      mockDefer20220824();
+    const client = new ApolloClient({
+      cache: new InMemoryCache(),
+      link: httpLink,
+      incrementalHandler: new Defer20220824Handler(),
+    });
+
+    const observable = client.watchQuery({ query, fetchPolicy });
+    const stream = new ObservableStream(observable);
+
+    await expect(stream).toEmitTypedValue({
+      data: undefined,
+      dataState: "empty",
+      loading: true,
+      networkStatus: NetworkStatus.loading,
+      partial: true,
+    });
+
+    enqueueInitialChunk({
+      data: {
+        characters: [
+          { __typename: "Character", id: 1, uppercase: "A" },
+          { __typename: "Character", id: 2, uppercase: "B" },
+          { __typename: "Character", id: 3, uppercase: "C" },
+        ],
+      },
+      hasNext: true,
+    });
+
+    await expect(stream).toEmitTypedValue({
+      data: markAsStreaming({
+        characters: [
+          { __typename: "Character", id: 1, uppercase: "A" },
+          { __typename: "Character", id: 2, uppercase: "B" },
+          { __typename: "Character", id: 3, uppercase: "C" },
+        ],
+      }),
+      dataState: "streaming",
+      loading: true,
+      networkStatus: NetworkStatus.streaming,
+      partial: true,
+    });
+
+    enqueueSubsequentChunk({
+      incremental: [{ data: { lowercase: "a" }, path: ["characters", 0] }],
+      hasNext: true,
+    });
+
+    await expect(stream).toEmitTypedValue({
+      data: markAsStreaming({
+        characters: [
+          { __typename: "Character", id: 1, uppercase: "A", lowercase: "a" },
+          { __typename: "Character", id: 2, uppercase: "B" },
+          { __typename: "Character", id: 3, uppercase: "C" },
+        ],
+      }),
+      dataState: "streaming",
+      loading: true,
+      networkStatus: NetworkStatus.streaming,
+      partial: true,
+    });
+
+    enqueueSubsequentChunk({
+      incremental: [
+        { data: { lowercase: "b" }, path: ["characters", 1] },
+        { data: { lowercase: "c" }, path: ["characters", 2] },
+      ],
+      hasNext: false,
+    });
+
+    await expect(stream).toEmitTypedValue({
+      data: {
+        characters: [
+          { __typename: "Character", id: 1, uppercase: "A", lowercase: "a" },
+          { __typename: "Character", id: 2, uppercase: "B", lowercase: "b" },
+          { __typename: "Character", id: 3, uppercase: "C", lowercase: "c" },
+        ],
+      },
+      dataState: "complete",
+      loading: false,
+      networkStatus: NetworkStatus.ready,
+      partial: false,
+    });
+
+    void observable.refetch();
+
+    await expect(stream).toEmitTypedValue({
+      data: {
+        characters: [
+          { __typename: "Character", id: 1, uppercase: "A", lowercase: "a" },
+          { __typename: "Character", id: 2, uppercase: "B", lowercase: "b" },
+          { __typename: "Character", id: 3, uppercase: "C", lowercase: "c" },
+        ],
+      },
+      dataState: "complete",
+      loading: true,
+      networkStatus: NetworkStatus.refetch,
+      partial: false,
+    });
+
+    // on refetch, the list is shorter
+    enqueueInitialChunk({
+      data: {
+        characters: [
+          { __typename: "Character", id: 1, uppercase: "A" },
+          { __typename: "Character", id: 2, uppercase: "B" },
+        ],
+      },
+      hasNext: true,
+    });
+
+    await expect(stream).toEmitTypedValue({
+      data: markAsStreaming({
+        characters:
+          // no-cache fetch policy doesn't merge with existing cache data, so
+          // the lowercase field is not added to each item
+          fetchPolicy === "no-cache" ?
+            [
+              { __typename: "Character", id: 1, uppercase: "A" },
+              { __typename: "Character", id: 2, uppercase: "B" },
+            ]
+          : [
+              {
+                __typename: "Character",
+                id: 1,
+                uppercase: "A",
+                lowercase: "a",
+              },
+              {
+                __typename: "Character",
+                id: 2,
+                uppercase: "B",
+                lowercase: "b",
+              },
+            ],
+      }),
+      dataState: "streaming",
+      loading: true,
+      networkStatus: NetworkStatus.streaming,
+      partial: true,
+    });
+
+    enqueueSubsequentChunk({
+      incremental: [
+        { data: { lowercase: "a" }, path: ["characters", 0] },
+        { data: { lowercase: "b" }, path: ["characters", 1] },
+      ],
+      hasNext: false,
+    });
+
+    await expect(stream).toEmitTypedValue({
+      data: {
+        characters: [
+          { __typename: "Character", id: 1, uppercase: "A", lowercase: "a" },
+          { __typename: "Character", id: 2, uppercase: "B", lowercase: "b" },
+        ],
+      },
+      dataState: "complete",
+      loading: false,
+      networkStatus: NetworkStatus.ready,
+      partial: false,
+    });
+
+    await expect(stream).not.toEmitAnything();
+  }
+);

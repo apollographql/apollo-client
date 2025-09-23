3fbf754325505e39a62c5b90d0ccf8d982d5ba41
Jerel Miller <jerelmiller@gmail.com>
2025-09-23 15:42:20 -0600

    Move warnings to testUtils

--- src/local-state/__tests__/LocalState/base.test.ts
+++ src/local-state/__tests__/LocalState/base.test.ts
@@ -4,14 +4,7 @@ import { LocalState } from "@apollo/client/local-state";
 import { spyOnConsole } from "@apollo/client/testing/internal";
 import { InvariantError } from "@apollo/client/utilities/invariant";
 
-import { gql } from "./testUtils.js";
-
-const WARNINGS = {
-  MISSING_RESOLVER:
-    "Could not find a resolver for the '%s' field nor does the cache resolve the field. The field value has been set to `null`. Either define a resolver for the field or ensure the cache can resolve the value, for example, by adding a 'read' function to a field policy in 'InMemoryCache'.",
-  NO_CACHE:
-    "The '%s' field resolves the value from the cache, for example from a 'read' function, but a 'no-cache' fetch policy was used. The field value has been set to `null`. Either define a local resolver or use a fetch policy that uses the cache to ensure the field is resolved correctly.",
-};
+import { gql, WARNINGS } from "./testUtils.js";
 
 test("runs resolvers for @client queries", async () => {
   const document = gql`

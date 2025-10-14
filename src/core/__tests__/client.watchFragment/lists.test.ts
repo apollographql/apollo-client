0943c50281eb3094db7e6b38910a322322a6fd16
Jerel Miller <jerelmiller@gmail.com>
2025-10-14 16:29:29 -0600

    Add failing tests for reobserve

--- src/core/__tests__/client.watchFragment/lists.test.ts
+++ src/core/__tests__/client.watchFragment/lists.test.ts
@@ -671,3 +671,117 @@ test("works with data masking", async () => {
   await expect(parentStream).not.toEmitAnything();
   await expect(childStream).not.toEmitAnything();
 });
+
+test("can change size of lists with reobserve", async () => {
+  type Item = {
+    __typename: string;
+    id: number;
+    text?: string;
+  };
+
+  const fragment: TypedDocumentNode<Item> = gql`
+    fragment ItemFragment on Item {
+      id
+      text
+    }
+  `;
+
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: ApolloLink.empty(),
+  });
+
+  for (let i = 1; i <= 5; i++) {
+    client.writeFragment({
+      fragment,
+      data: { __typename: "Item", id: i, text: `Item #${i}` },
+    });
+  }
+
+  const observable = client.watchFragment({
+    fragment,
+    from: [
+      { __typename: "Item", id: 1 },
+      { __typename: "Item", id: 2 },
+    ],
+  });
+  const stream = new ObservableStream(observable);
+
+  await expect(stream).toEmitTypedValue([
+    {
+      data: { __typename: "Item", id: 1, text: "Item #1" },
+      dataState: "complete",
+      complete: true,
+    },
+    {
+      data: { __typename: "Item", id: 2, text: "Item #2" },
+      dataState: "complete",
+      complete: true,
+    },
+  ]);
+
+  observable.reobserve({
+    from: [
+      { __typename: "Item", id: 1 },
+      { __typename: "Item", id: 2 },
+      { __typename: "Item", id: 5 },
+    ],
+  });
+
+  await expect(stream).toEmitTypedValue([
+    {
+      data: { __typename: "Item", id: 1, text: "Item #1" },
+      dataState: "complete",
+      complete: true,
+    },
+    {
+      data: { __typename: "Item", id: 2, text: "Item #2" },
+      dataState: "complete",
+      complete: true,
+    },
+    {
+      data: { __typename: "Item", id: 5, text: "Item #5" },
+      dataState: "complete",
+      complete: true,
+    },
+  ]);
+
+  observable.reobserve({
+    from: [
+      { __typename: "Item", id: 1 },
+      { __typename: "Item", id: 5 },
+    ],
+  });
+
+  await expect(stream).toEmitTypedValue([
+    {
+      data: { __typename: "Item", id: 1, text: "Item #1" },
+      dataState: "complete",
+      complete: true,
+    },
+    {
+      data: { __typename: "Item", id: 5, text: "Item #5" },
+      dataState: "complete",
+      complete: true,
+    },
+  ]);
+
+  observable.reobserve({ from: [] });
+
+  await expect(stream).toEmitTypedValue([]);
+
+  observable.reobserve({
+    from: [{ __typename: "Item", id: 6 }],
+  });
+
+  await expect(stream).toEmitTypedValue([
+    {
+      data: {},
+      dataState: "partial",
+      complete: false,
+      missing: "Dangling reference to missing Item:6 object",
+    },
+  ]);
+
+  await expect(stream).not.toEmitAnything();
+});

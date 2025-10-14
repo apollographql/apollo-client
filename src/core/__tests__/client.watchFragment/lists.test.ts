84069910ad0f4043c422615566a6591f59c3d703
Jerel Miller <jerelmiller@gmail.com>
2025-10-14 13:50:20 -0600

    Add tests for lists with data masking

--- src/core/__tests__/client.watchFragment/lists.test.ts
+++ src/core/__tests__/client.watchFragment/lists.test.ts
@@ -439,3 +439,235 @@ test("updates items in the list with cache writes", async () => {
 
   await expect(stream).not.toEmitAnything();
 });
+
+test("works with data masking", async () => {
+  type ItemDetails = {
+    __typename: string;
+    text: string;
+  } & { " $fragmentName"?: "ItemDetailsFragment" };
+
+  type Item = {
+    __typename: string;
+    id: number;
+  } & {
+    " $fragmentRefs"?: { ItemDetailsFragment: ItemDetails };
+  };
+
+  const detailsFragment: TypedDocumentNode<ItemDetails> = gql`
+    fragment ItemDetailsFragment on Item {
+      text
+    }
+  `;
+
+  const fragment: TypedDocumentNode<Item> = gql`
+    fragment ItemFragment on Item {
+      id
+      ...ItemDetailsFragment
+    }
+
+    ${detailsFragment}
+  `;
+
+  const client = new ApolloClient({
+    dataMasking: true,
+    cache: new InMemoryCache(),
+    link: ApolloLink.empty(),
+  });
+  const { cache } = client;
+
+  for (let i = 1; i <= 5; i++) {
+    client.writeFragment({
+      fragment,
+      fragmentName: "ItemFragment",
+      data: { __typename: "Item", id: i, text: `Item #${i}` },
+    });
+  }
+
+  const parentObservable = client.watchFragment({
+    fragment,
+    fragmentName: "ItemFragment",
+    from: [
+      { __typename: "Item", id: 1 },
+      { __typename: "Item", id: 2 },
+      { __typename: "Item", id: 5 },
+    ],
+  });
+  const childObsrevable = client.watchFragment({
+    fragment: detailsFragment,
+    from: [
+      { __typename: "Item", id: 1 },
+      { __typename: "Item", id: 2 },
+      { __typename: "Item", id: 5 },
+    ],
+  });
+  const parentStream = new ObservableStream(parentObservable);
+  const childStream = new ObservableStream(childObsrevable);
+
+  await expect(parentStream).toEmitTypedValue([
+    {
+      data: { __typename: "Item", id: 1 },
+      dataState: "complete",
+      complete: true,
+    },
+    {
+      data: { __typename: "Item", id: 2 },
+      dataState: "complete",
+      complete: true,
+    },
+    {
+      data: { __typename: "Item", id: 5 },
+      dataState: "complete",
+      complete: true,
+    },
+  ]);
+  await expect(childStream).toEmitTypedValue([
+    {
+      data: { __typename: "Item", text: "Item #1" },
+      dataState: "complete",
+      complete: true,
+    },
+    {
+      data: { __typename: "Item", text: "Item #2" },
+      dataState: "complete",
+      complete: true,
+    },
+    {
+      data: { __typename: "Item", text: "Item #5" },
+      dataState: "complete",
+      complete: true,
+    },
+  ]);
+
+  client.writeFragment({
+    fragment,
+    fragmentName: "ItemFragment",
+    data: {
+      __typename: "Item",
+      id: 2,
+      text: "Item #2 updated",
+    },
+  });
+
+  await expect(childStream).toEmitTypedValue([
+    {
+      data: { __typename: "Item", text: "Item #1" },
+      dataState: "complete",
+      complete: true,
+    },
+    {
+      data: { __typename: "Item", text: "Item #2 updated" },
+      dataState: "complete",
+      complete: true,
+    },
+    {
+      data: { __typename: "Item", text: "Item #5" },
+      dataState: "complete",
+      complete: true,
+    },
+  ]);
+  await expect(parentStream).not.toEmitAnything();
+
+  client.cache.batch({
+    update: (cache) => {
+      cache.writeFragment({
+        fragment,
+        fragmentName: "ItemFragment",
+        data: {
+          __typename: "Item",
+          id: 1,
+          text: "Item #1 from batch",
+        },
+      });
+
+      cache.writeFragment({
+        fragment,
+        fragmentName: "ItemFragment",
+        data: {
+          __typename: "Item",
+          id: 5,
+          text: "Item #5 from batch",
+        },
+      });
+    },
+  });
+
+  await expect(childStream).toEmitTypedValue([
+    {
+      data: { __typename: "Item", text: "Item #1 from batch" },
+      dataState: "complete",
+      complete: true,
+    },
+    {
+      data: { __typename: "Item", text: "Item #2 updated" },
+      dataState: "complete",
+      complete: true,
+    },
+    {
+      data: { __typename: "Item", text: "Item #5" },
+      dataState: "complete",
+      complete: true,
+    },
+  ]);
+
+  await expect(childStream).toEmitTypedValue([
+    {
+      data: { __typename: "Item", text: "Item #1 from batch" },
+      dataState: "complete",
+      complete: true,
+    },
+    {
+      data: { __typename: "Item", text: "Item #2 updated" },
+      dataState: "complete",
+      complete: true,
+    },
+    {
+      data: { __typename: "Item", text: "Item #5 from batch" },
+      dataState: "complete",
+      complete: true,
+    },
+  ]);
+  await expect(parentStream).not.toEmitAnything();
+
+  cache.modify({
+    id: cache.identify({ __typename: "Item", id: 1 }),
+    fields: {
+      text: (_, { DELETE }) => DELETE,
+    },
+  });
+
+  await expect(childStream).toEmitTypedValue([
+    {
+      data: { __typename: "Item" },
+      dataState: "partial",
+      complete: false,
+      missing: {
+        text: "Can't find field 'text' on Item:1 object",
+      },
+    },
+    {
+      data: { __typename: "Item", text: "Item #2 updated" },
+      dataState: "complete",
+      complete: true,
+    },
+    {
+      data: { __typename: "Item", text: "Item #5 from batch" },
+      dataState: "complete",
+      complete: true,
+    },
+  ]);
+  await expect(parentStream).not.toEmitAnything();
+
+  // should not cause rerender since its an item not watched
+  client.writeFragment({
+    fragment,
+    fragmentName: "ItemFragment",
+    data: {
+      __typename: "Item",
+      id: 6,
+      text: "Item #6 ignored",
+    },
+  });
+
+  await expect(parentStream).not.toEmitAnything();
+  await expect(childStream).not.toEmitAnything();
+});

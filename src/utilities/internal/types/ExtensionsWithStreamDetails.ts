6cf906aa7bdeede53bb216b87b9f5c11e90548d3
Jerel Miller <jerelmiller@gmail.com>
2026-01-05 18:18:29 -0700

    Consolidate trie type

--- src/utilities/internal/types/ExtensionsWithStreamDetails.ts
+++ src/utilities/internal/types/ExtensionsWithStreamDetails.ts
@@ -1,20 +1,11 @@
-import type { Trie } from "@wry/trie";
-
-import type { Incremental } from "@apollo/client/incremental";
-
 import type { streamInfoSymbol } from "../constants.js";
+
+import type { StreamInfoTrie } from "./StreamInfoTrie.js";
 
 /** @internal */
 export interface ExtensionsWithStreamInfo extends Record<string, unknown> {
   [streamInfoSymbol]?: {
-    current: Trie<{
-      current: Incremental.StreamFieldInfo;
-      previous?: {
-        incoming: unknown;
-        streamFieldInfo: Incremental.StreamFieldInfo;
-        result: unknown;
-      };
-    }>;
+    current: StreamInfoTrie;
   };
 }
 

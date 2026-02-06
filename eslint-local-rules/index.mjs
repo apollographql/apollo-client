640724c7083d8bdeb203d58fdd7d96430a57129d
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2026-02-06 12:54:35 +0100

    migrate eslint from old flatConfig compat to modern flatConfig (#13138)
    Verified locally by comparing the output of
    ```sh
    % yarn eslint --print-config src/core/__tests__/ApolloClient.test.ts >| test.lint                                                                                                                                                 
    % yarn eslint --print-config src/react/hooks/useQuery.ts >| useQuery.lint                                                                                                                                                         
    % yarn eslint --print-config src/core/ApolloClient.ts >| ApolloClient.lint  
    ```
    before and after these changes.

--- eslint-local-rules/index.mjs
+++ eslint-local-rules/index.mjs
@@ -17,6 +17,8 @@ import {
   TDataTVariablesOrder,
 } from "./generics.ts";
 
+/** @type {import("eslint").ESLint.Plugin['rules']} */
+// @ts-ignore - mismatch between different plugin types, but works
 export default {
   "require-using-disposable": requireUsingDisposable,
   "require-disable-act-environment": requireDisableActEnvironment,

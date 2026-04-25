22c2cfaa9278f2141ae9b9a4f7f999549a21caa1
Jerel Miller <jerelmiller@gmail.com>
2026-04-24 20:07:36 -0600

    Remove unneeded 3rd arg for onlineSource

--- src/core/refetchSources/onlineSource.ts
+++ src/core/refetchSources/onlineSource.ts
@@ -3,7 +3,7 @@ import type { RefetchEventManager } from "../RefetchEventManager.js";
 export const onlineSource: RefetchEventManager.EventSource =
   function onlineSource(emit) {
     if (typeof window !== "undefined" && window.addEventListener) {
-      window.addEventListener("online", emit, false);
+      window.addEventListener("online", emit);
 
       return () => {
         window.removeEventListener("online", emit);

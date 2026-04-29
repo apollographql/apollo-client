8d9313f3f4f9840b8797d128f7197591127e9946
Jerel Miller <jerelmiller@gmail.com>
2026-04-29 10:54:05 -0600

    Use ternary in onlineSource

--- src/core/refetchSources/onlineSource.ts
+++ src/core/refetchSources/onlineSource.ts
@@ -5,9 +5,5 @@ import { canUseDOM } from "@apollo/client/utilities/internal";
 import type { RefetchEventManager } from "../RefetchEventManager.js";
 
 export const onlineSource: RefetchEventManager.EventSource<Event> = () => {
-  if (!canUseDOM) {
-    return EMPTY;
-  }
-
-  return fromEvent(window, "online");
+  return canUseDOM ? fromEvent(window, "online") : EMPTY;
 };

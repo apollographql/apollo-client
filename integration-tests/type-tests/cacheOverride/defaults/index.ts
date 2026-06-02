8c891a524c775618fb5845ce259481be5b07ca64
Jerel Miller <jerelmiller@gmail.com>
2026-06-01 22:00:49 -0600

    Add type tests for defaults for cache override

---
+++ integration-tests/type-tests/cacheOverride/defaults/index.ts
@@ -0,0 +1,126 @@
+import {
+  ApolloCache,
+  ApolloClient,
+  ApolloLink,
+  DocumentNode,
+  InMemoryCache,
+  MutationUpdaterFunction,
+  OperationVariables,
+} from "@apollo/client";
+import { useMutation } from "@apollo/client/react";
+import { expectTypeOf } from "expect-type";
+import { test, TestCache } from "../shared/index.js";
+
+declare const mutation: DocumentNode;
+
+test("ApolloClient constructor", () => {
+  {
+    const client = new ApolloClient({
+      cache: new InMemoryCache(),
+      link: ApolloLink.empty(),
+    });
+
+    expectTypeOf(client.cache).toEqualTypeOf<ApolloCache>();
+  }
+
+  {
+    const client = new ApolloClient({
+      cache: new TestCache(),
+      link: ApolloLink.empty(),
+    });
+
+    expectTypeOf(client.cache).toEqualTypeOf<ApolloCache>();
+  }
+});
+
+test("client.mutate", () => {
+  {
+    const client = new ApolloClient({
+      cache: new InMemoryCache(),
+      link: ApolloLink.empty(),
+    });
+
+    client.mutate({
+      mutation,
+      update: (cache) => {
+        expectTypeOf(cache).toEqualTypeOf<ApolloCache>();
+      },
+    });
+  }
+
+  {
+    const client = new ApolloClient({
+      cache: new TestCache(),
+      link: ApolloLink.empty(),
+    });
+
+    client.mutate({
+      mutation,
+      update: (cache) => {
+        expectTypeOf(cache).toEqualTypeOf<ApolloCache>();
+      },
+    });
+  }
+});
+
+test("client.refetchQueries", () => {
+  {
+    const client = new ApolloClient({
+      cache: new InMemoryCache(),
+      link: ApolloLink.empty(),
+    });
+
+    client.refetchQueries({
+      updateCache: (cache) => {
+        expectTypeOf(cache).toEqualTypeOf<ApolloCache>();
+      },
+    });
+  }
+
+  {
+    const client = new ApolloClient({
+      cache: new TestCache(),
+      link: ApolloLink.empty(),
+    });
+
+    client.refetchQueries({
+      updateCache: (cache) => {
+        expectTypeOf(cache).toEqualTypeOf<ApolloCache>();
+      },
+    });
+  }
+});
+
+test("useMutation", () => {
+  const [mutate] = useMutation(mutation, {
+    update: (cache) => {
+      expectTypeOf(cache).toEqualTypeOf<ApolloCache>();
+    },
+    onCompleted: (_, options) => {
+      expectTypeOf(options!.update!).toEqualTypeOf<
+        MutationUpdaterFunction<unknown, OperationVariables, ApolloCache>
+      >;
+    },
+    onError: (_, options) => {
+      expectTypeOf(options!.update!).toEqualTypeOf<
+        MutationUpdaterFunction<unknown, OperationVariables, ApolloCache>
+      >;
+    },
+  });
+
+  mutate({
+    update: (cache) => {
+      expectTypeOf(cache).toEqualTypeOf<ApolloCache>();
+    },
+    onCompleted: (_, options) => {
+      expectTypeOf(options!.update!).toEqualTypeOf<
+        MutationUpdaterFunction<unknown, OperationVariables, ApolloCache>
+      >;
+    },
+    onError: (_, options) => {
+      expectTypeOf(options!.update!).toEqualTypeOf<
+        MutationUpdaterFunction<unknown, OperationVariables, ApolloCache>
+      >;
+    },
+  });
+});

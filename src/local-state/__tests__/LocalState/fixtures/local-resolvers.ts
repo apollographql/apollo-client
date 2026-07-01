d10db14eefc9b3b257fd3802310a71549c1a500b
Jerel Miller <jerelmiller@gmail.com>
2026-07-01 12:53:45 -0600

    Update fixtures based on most recent version of codegen

--- src/local-state/__tests__/LocalState/fixtures/local-resolvers.ts
+++ src/local-state/__tests__/LocalState/fixtures/local-resolvers.ts
@@ -5,24 +5,6 @@ import { DeepPartial } from "@apollo/client/utilities";
 import { ContextValue } from "./context-value.js";
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = {
-  [K in keyof T]: T[K];
-};
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]?: Maybe<T[SubKey]>;
-};
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]: Maybe<T[SubKey]>;
-};
-export type MakeEmpty<
-  T extends { [key: string]: unknown },
-  K extends keyof T,
-> = { [_ in K]?: never };
-export type Incremental<T> =
-  | T
-  | {
-      [P in keyof T]?: P extends " $fragmentName" | "__typename" ? T[P] : never;
-    };
 export type RequireFields<T, K extends keyof T> = Omit<T, K> & {
   [P in K]-?: NonNullable<T[P]>;
 };

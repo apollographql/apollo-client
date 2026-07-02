337f8a3ac6658c15957523566a31fd9119155668
Jerel Miller <jerelmiller@gmail.com>
2026-07-01 20:12:54 -0600

    Add basic test to ensure plumbing works

---
+++ codegen/input-objects/__tests__/plugin.test.ts
@@ -0,0 +1,32 @@
+import { codegen } from "@graphql-codegen/core";
+import { plugin } from "../plugin.js";
+import { parse } from "graphql";
+
+test("works", async () => {
+  const schema = parse(/* GraphQL */ `
+    scalar DateTime
+
+    type Query {
+      posts(at: DateTime): Post
+    }
+
+    type Post {
+      id: ID!
+    }
+  `);
+
+  const output = await codegen({
+    filename: "input-objects.ts",
+    documents: [],
+    plugins: [{ "@apollo/client-graphql-codegen/input-objects": {} }],
+    schema,
+    pluginMap: {
+      "@apollo/client-graphql-codegen/input-objects": {
+        plugin,
+      },
+    },
+    config: {},
+  });
+
+  expect(output).toMatchInlineSnapshot(`""`);
+});

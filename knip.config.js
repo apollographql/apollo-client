ccb2594c6a800a4bc34a30e38914723f562e8925
Jerel Miller <jerelmiller@gmail.com>
2026-07-02 12:45:04 -0600

    Add entrypoint for input-objects to knip

--- knip.config.js
+++ knip.config.js
@@ -33,6 +33,7 @@ const config = {
       "codegen/local-state/config.ts",
       "codegen/local-state/plugin.ts",
       "codegen/local-state/visitor.ts",
+      "codegen/input-objects/index.ts",
       "scripts/codemods/ac3-to-ac4/src/util/getAllExports.ts",
     ]),
   project: [

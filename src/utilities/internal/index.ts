1574ca67f9a4fa83b0f9b0c0e50ee8ffe4d253f7
Jerel Miller <jerelmiller@gmail.com>
2026-07-09 17:26:48 -0600

    Add isTypenameField helper

--- src/utilities/internal/index.ts
+++ src/utilities/internal/index.ts
@@ -55,6 +55,7 @@ export { isField } from "./isField.js";
 export { isNonEmptyArray } from "./isNonEmptyArray.js";
 export { isNonNullObject } from "./isNonNullObject.js";
 export { isPlainObject } from "./isPlainObject.js";
+export { isTypenameField } from "./isTypenameField.js";
 export { makeReference } from "./makeReference.js";
 export { makeUniqueId } from "./makeUniqueId.js";
 export { maybeDeepFreeze } from "./maybeDeepFreeze.js";

191d47df92cd0aa7c7031abf2caf952b650a45d9
Jerel Miller <jerelmiller@gmail.com>
2026-07-17 12:57:54 -0600

    Remove comment

--- src/cache/inmemory/readFromStore.ts
+++ src/cache/inmemory/readFromStore.ts
@@ -301,12 +301,7 @@ export class StoreReader {
     let { result, dataState } = execResult;
 
     if (
-      // If maybeStripPartialDeferredFragments didn't reassign dataState to
-      // streaming, deferPartial is treated the same as partial since it means
-      // one of the defer boundaries is partial
       dataState === "deferPartial" ||
-      // When we don't tolerate incremental responses, streaming becomes the
-      // same as partial since at least one defer boundary has missing data.
       (dataState === "streaming" && !handleIncremental)
     ) {
       dataState = "partial";

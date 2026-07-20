4e65f469bdfdb1bc3f57ccba63eb38847a86b9bc
Jerel Miller <jerelmiller@gmail.com>
2026-07-20 14:14:38 -0600

    Remove outdated export

--- src/utilities/internal/index.ts
+++ src/utilities/internal/index.ts
@@ -1,7 +1,6 @@
 export type { DecoratedPromise } from "./types/DecoratedPromise.js";
 export type { DeepOmit } from "./types/DeepOmit.js";
 export type { ExtensionsWithStreamInfo } from "./types/ExtensionsWithStreamDetails.js";
-export type { FieldMap } from "./collectSiblingFields.js";
 export type { FragmentMap } from "./types/FragmentMap.js";
 export type { FragmentMapFunction } from "./types/FragmentMapFunction.js";
 export type { FulfilledPromise } from "./types/FulfilledPromise.js";

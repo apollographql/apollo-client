f98100f033f017c4691ce75ecc68ee30886f099f
Jerel Miller <jerelmiller@gmail.com>
2026-07-22 22:10:37 -0600

    Mark makeStreamInfoTrie internal

--- src/utilities/internal/makeStreamInfoTrie.ts
+++ src/utilities/internal/makeStreamInfoTrie.ts
@@ -3,6 +3,7 @@ import { Trie } from "@wry/trie";
 import { StreamArrayState } from "./StreamArrayState.js";
 import type { StreamInfoTrie } from "./types/StreamInfoTrie.js";
 
+/** @internal */
 export function makeStreamInfoTrie(): StreamInfoTrie {
   return new Trie(false, (path) => ({
     state: new StreamArrayState(path),

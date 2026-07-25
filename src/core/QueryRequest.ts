4926ed24bc4562ff32e2c7d8af997d062f181aa9
Jerel Miller <jerelmiller@gmail.com>
2026-07-24 20:58:36 -0600

    Create a QueryRequest object

---
+++ src/core/QueryRequest.ts
@@ -0,0 +1,31 @@
+import type { ApolloClient } from "./ApolloClient.js";
+import { NetworkStatus } from "./networkStatus.js";
+import { CacheWriteBehavior } from "./QueryInfo.js";
+import type { OperationVariables } from "./types.js";
+
+export class QueryRequest<TData, TVariables extends OperationVariables> {
+  readonly options: ApolloClient.WatchQueryOptions<TData, TVariables>;
+  readonly networkStatus: NetworkStatus;
+
+  constructor(
+    options: ApolloClient.WatchQueryOptions<TData, TVariables>,
+    networkStatus: NetworkStatus
+  ) {
+    this.options = options;
+    this.networkStatus = networkStatus;
+  }
+
+  get cacheWriteBehavior() {
+    return (
+      this.options.fetchPolicy === "no-cache" ? CacheWriteBehavior.FORBID
+        // Watched queries must opt into overwriting existing data on refetch,
+        // by passing refetchWritePolicy: "overwrite" in their WatchQueryOptions.
+      : (
+        this.networkStatus === NetworkStatus.refetch &&
+        this.options.refetchWritePolicy !== "merge"
+      ) ?
+        CacheWriteBehavior.OVERWRITE
+      : CacheWriteBehavior.MERGE
+    );
+  }
+}

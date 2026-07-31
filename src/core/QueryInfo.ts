d3a7bb98fcefc2b8d24adc5292eca24e859e3157
Jerel Miller <jerelmiller@gmail.com>
2026-07-31 12:16:42 -0600

    Remove unneeded lastDiff application

--- src/core/QueryInfo.ts
+++ src/core/QueryInfo.ts
@@ -323,6 +323,38 @@ export class QueryInfo<
         update: (cache) => {
           const shouldWrite = this.shouldWrite(result, variables);
 
+          // If result is the same as the last result we received from
+          // the network (and the variables match too), avoid writing
+          // result into the cache again. The wisdom of skipping this
+          // cache write is far from obvious, since any cache write
+          // could be the one that puts the cache back into a desired
+          // state, fixing corruption or missing data. However, if we
+          // always write every network result into the cache, we enable
+          // feuds between queries competing to update the same data in
+          // incompatible ways, which can lead to an endless cycle of
+          // cache broadcasts and useless network requests. As with any
+          // feud, eventually one side must step back from the brink,
+          // letting the other side(s) have the last word(s). There may
+          // be other points where we could break this cycle, such as
+          // silencing the broadcast for cache.writeQuery (not a good
+          // idea, since it just delays the feud a bit) or somehow
+          // avoiding the network request that just happened (also bad,
+          // because the server could return useful new data). All
+          // options considered, skipping this cache write seems to be
+          // the least damaging place to break the cycle, because it
+          // reflects the intuition that we recently wrote this exact
+          // result into the cache, so the cache *should* already/still
+          // contain this data. If some other query has clobbered that
+          // data in the meantime, that's too bad, but there will be no
+          // winners if every query blindly reverts to its own version
+          // of the data. This approach also gives the network a chance
+          // to return new data, which will be written into the cache as
+          // usual, notifying only those queries that are directly
+          // affected by the cache updates, as usual. In the future, an
+          // even more sophisticated cache could perhaps prevent or
+          // mitigate the clobbering somehow, but that would make this
+          // particular cache write even less important, and thus
+          // skipping it would be even safer than it is today.
           if (shouldWrite) {
             cache.writeQuery({
               query,
@@ -337,51 +369,6 @@ export class QueryInfo<
               variables,
               dmCount: destructiveMethodCounts.get(this.cache),
             };
-          } else {
-            // If result is the same as the last result we received from
-            // the network (and the variables match too), avoid writing
-            // result into the cache again. The wisdom of skipping this
-            // cache write is far from obvious, since any cache write
-            // could be the one that puts the cache back into a desired
-            // state, fixing corruption or missing data. However, if we
-            // always write every network result into the cache, we enable
-            // feuds between queries competing to update the same data in
-            // incompatible ways, which can lead to an endless cycle of
-            // cache broadcasts and useless network requests. As with any
-            // feud, eventually one side must step back from the brink,
-            // letting the other side(s) have the last word(s). There may
-            // be other points where we could break this cycle, such as
-            // silencing the broadcast for cache.writeQuery (not a good
-            // idea, since it just delays the feud a bit) or somehow
-            // avoiding the network request that just happened (also bad,
-            // because the server could return useful new data). All
-            // options considered, skipping this cache write seems to be
-            // the least damaging place to break the cycle, because it
-            // reflects the intuition that we recently wrote this exact
-            // result into the cache, so the cache *should* already/still
-            // contain this data. If some other query has clobbered that
-            // data in the meantime, that's too bad, but there will be no
-            // winners if every query blindly reverts to its own version
-            // of the data. This approach also gives the network a chance
-            // to return new data, which will be written into the cache as
-            // usual, notifying only those queries that are directly
-            // affected by the cache updates, as usual. In the future, an
-            // even more sophisticated cache could perhaps prevent or
-            // mitigate the clobbering somehow, but that would make this
-            // particular cache write even less important, and thus
-            // skipping it would be even safer than it is today.
-            if (lastDiff && lastDiff.complete) {
-              // Reuse data from the last good (complete) diff that we
-              // received, when possible.
-              result = {
-                ...result,
-                data: lastDiff.result,
-                dataState: "complete",
-              };
-              return;
-            }
-            // If the previous this.diff was incomplete, fall through to
-            // re-reading the latest data with cache.diff, below.
           }
 
           const isNetworkOnly =

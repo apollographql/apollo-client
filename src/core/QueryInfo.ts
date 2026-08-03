f36d8f836bc647b72db5fca875d70763bbe139df
Jerel Miller <jerelmiller@gmail.com>
2026-08-03 16:37:38 -0600

    Remove unused import

--- src/core/QueryInfo.ts
+++ src/core/QueryInfo.ts
@@ -18,7 +18,6 @@ import {
   graphQLResultHasError,
   handleIncrementalSymbol,
   hasDirectives,
-  streamInfoSymbol,
 } from "@apollo/client/utilities/internal";
 import { invariant } from "@apollo/client/utilities/invariant";
 

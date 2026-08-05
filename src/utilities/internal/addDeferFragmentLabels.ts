4d8d0c1b24f35c1769435da95998dfaba087e3e7
Jerel Miller <jerelmiller@gmail.com>
2026-08-04 21:27:02 -0600

    Add document transform that adds labels to all `@defer` boundaries

--- src/utilities/internal/addDeferFragmentLabels.ts
+++ src/utilities/internal/addDeferFragmentLabels.ts
@@ -1,5 +1,33 @@
-import { DocumentTransform } from "@apollo/client";
+import type { ArgumentNode } from "graphql";
+import { Kind, visit } from "graphql";
+
+// eslint-disable-next-line local-rules/import-from-inside-other-export
+import { DocumentTransform } from "../graphql/DocumentTransform.js";
 
 export const addDeferFragmentLabels = new DocumentTransform((document) => {
-  return document;
+  let count = 0;
+
+  return visit(document, {
+    Directive(node) {
+      if (
+        node.name.value !== "defer" ||
+        // Keep labels defined by users
+        node.arguments?.some((arg) => arg.name.value === "label")
+      ) {
+        return;
+      }
+
+      return {
+        ...node,
+        arguments: [
+          ...(node.arguments ?? []),
+          {
+            kind: Kind.ARGUMENT,
+            name: { kind: Kind.NAME, value: "label" },
+            value: { kind: Kind.STRING, value: `ac_${count++}` },
+          } satisfies ArgumentNode,
+        ],
+      };
+    },
+  });
 });

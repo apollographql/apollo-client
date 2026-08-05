6e699e3e9977ee99fb407a86df0a895dc654ca1b
Jerel Miller <jerelmiller@gmail.com>
2026-08-05 00:42:28 -0600

    Use helper in addDeferFragmentLabels

--- src/utilities/internal/addDeferFragmentLabels.ts
+++ src/utilities/internal/addDeferFragmentLabels.ts
@@ -4,6 +4,8 @@ import { Kind, visit } from "graphql";
 // eslint-disable-next-line local-rules/import-from-inside-other-export
 import { DocumentTransform } from "../graphql/DocumentTransform.js";
 
+import { getDirectiveArgValue } from "./getDirectiveArgValue.js";
+
 const RESERVED_PREFIX = "ac_";
 
 export const addDeferFragmentLabels = new DocumentTransform((document) => {
@@ -13,12 +15,12 @@ export const addDeferFragmentLabels = new DocumentTransform((document) => {
     Directive(node) {
       if (node.name.value !== "defer") return;
 
+      // Keep user defined labels
+      const label = getDirectiveArgValue(node, "label", Kind.STRING);
+      if (label && !label.startsWith(RESERVED_PREFIX)) return;
+
       const args = node.arguments ?? [];
       const index = args.findIndex((arg) => arg.name.value === "label");
-      const labelValue = getLabelValue(args[index]);
-
-      // Keep user defined labels
-      if (labelValue && !labelValue.startsWith(RESERVED_PREFIX)) return;
 
       const labelArg: ArgumentNode = {
         kind: Kind.ARGUMENT,
@@ -36,9 +38,3 @@ export const addDeferFragmentLabels = new DocumentTransform((document) => {
     },
   });
 });
-
-function getLabelValue(labelArg: ArgumentNode) {
-  if (labelArg?.value.kind === Kind.STRING) {
-    return labelArg.value.value;
-  }
-}

3169b34670a278c5183a205913cc7edb93df503f
Jerel Miller <jerelmiller@gmail.com>
2026-08-14 09:09:04 -0600

    Remove stale comment

--- src/__tests__/exports.ts
+++ src/__tests__/exports.ts
@@ -1,7 +1,6 @@
 /** @jest-environment node */
-// We run this in a node environment because:
-// A) JSDOM doesn't yet support the TextEncoder/TextDecoder globals added in node 11, meaning certain imports (e.g. reactSSR) will fail (See https://github.com/jsdom/jsdom/issues/2524)
-// B) We're just testing imports/exports, so no reason not to use Node for slightly better performance.
+// We run this in a node environment because we're just testing imports/exports,
+// so no reason not to use Node for slightly better performance.
 
 import { resolve } from "node:path";
 

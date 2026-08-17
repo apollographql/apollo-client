62569390b9f74f08dd354fba63163c15582be64a
Jerel Miller <jerelmiller@gmail.com>
2026-08-17 11:23:52 -0600

    Combine check for Array/ReadonlyArray

--- src/incremental/internal/types.ts
+++ src/incremental/internal/types.ts
@@ -50,9 +50,10 @@ type IsScalarType<TData> =
 export type RemoveNeverishFields<TData> =
   true extends IsAny<TData> ? TData
   : true extends IsScalarType<TData> ? TData
-  : TData extends Array<infer TItem> ? Array<RemoveNeverishFields<TItem>>
   : TData extends ReadonlyArray<infer TItem> ?
-    ReadonlyArray<RemoveNeverishFields<TItem>>
+    TData extends Array<infer TItem> ?
+      Array<RemoveNeverishFields<TItem>>
+    : ReadonlyArray<RemoveNeverishFields<TItem>>
   : // Leave TData alone if it is Record<string, any> and not a specific shape
   string extends keyof TData ? TData
   : // short-circuit on empty object

c347bc92fe2a7c69578bd36dbfedb3e1f8ff0dd1
Jerel Miller <jerelmiller@gmail.com>
2026-09-02 18:43:24 -0600

    Update coerceScalarFieldsToParsed to handle array scalar types

--- src/utilities/internal/coerceScalarFieldsToParsed.ts
+++ src/utilities/internal/coerceScalarFieldsToParsed.ts
@@ -1,6 +1,7 @@
 import type { DocumentNode, FieldNode, SelectionSetNode } from "graphql";
 
-import type { ApolloCache } from "@apollo/client/cache";
+import type { ApolloCache, ScalarType } from "@apollo/client/cache";
+import { __DEV__ } from "@apollo/client/utilities/environment";
 import { invariant } from "@apollo/client/utilities/invariant";
 
 import { createFragmentMap } from "./createFragmentMap.js";
@@ -10,6 +11,7 @@ import { getMainDefinition } from "./getMainDefinition.js";
 import { getOperationDefinition } from "./getOperationDefinition.js";
 import { isField } from "./isField.js";
 import { resultKeyNameFromField } from "./resultKeyNameFromField.js";
+import { unwrapScalarType } from "./unwrapScalarType.js";
 
 /** @internal */
 export function coerceScalarFieldsToParsed(
@@ -25,22 +27,32 @@ export function coerceScalarFieldsToParsed(
     "Document node must be a query, mutation, or subscription"
   );
 
+  function coerceFieldArray(
+    field: FieldNode,
+    fieldValue: unknown[],
+    typename: string | undefined,
+    scalarType: ScalarType | undefined
+  ) {
+    let changed = false;
+
+    const items = fieldValue.map((item) => {
+      const coerced = coerceField(field, item, typename, scalarType);
+
+      changed ||= coerced !== item;
+      return coerced;
+    });
+
+    return changed ? items : fieldValue;
+  }
+
   function coerceField(
     field: FieldNode,
     fieldValue: unknown,
-    typename: string | undefined
+    typename: string | undefined,
+    scalarType: ScalarType | undefined
   ): unknown {
-    let changed = false;
-
-    if (Array.isArray(fieldValue)) {
-      const items = fieldValue.map((item) => {
-        const coerced = coerceField(field, item, typename);
-
-        changed ||= coerced !== item;
-        return coerced;
-      });
-
-      return changed ? items : fieldValue;
+    if (Array.isArray(fieldValue) && !scalarType) {
+      return coerceFieldArray(field, fieldValue, typename, scalarType);
     }
 
     if (field.selectionSet) {
@@ -49,8 +61,32 @@ export function coerceScalarFieldsToParsed(
 
     if (fieldValue === null || !typename) return fieldValue;
 
-    const scalar = cache.getScalarForField(typename, field.name.value);
-    return scalar ? scalar.coerceToParsed(fieldValue) : fieldValue;
+    if (scalarType) {
+      const match = scalarType.match(/^\[(.*)\]$/);
+
+      if (match) {
+        if (Array.isArray(fieldValue)) {
+          return coerceFieldArray(field, fieldValue, typename, match[1]);
+        } else {
+          if (__DEV__) {
+            invariant.warn(
+              "The custom scalar configuration for '%s' uses list type '%s', but the value is not an array. The value was coerced as '%s' anyway.",
+              `${typename}.${field.name.value}`,
+              scalarType,
+              unwrapScalarType(scalarType)
+            );
+          }
+        }
+      }
+
+      const scalar = cache.getScalar(unwrapScalarType(scalarType));
+
+      if (scalar) {
+        return scalar.coerceToParsed(fieldValue);
+      }
+    }
+
+    return fieldValue;
   }
 
   function coerceSelectionSet(
@@ -74,7 +110,14 @@ export function coerceScalarFieldsToParsed(
         if (!Object.hasOwn(data, resultName)) return;
 
         const fieldValue = data[resultName];
-        const coerced = coerceField(selection, fieldValue, typename);
+        const coerced = coerceField(
+          selection,
+          fieldValue,
+          typename,
+          typename ?
+            cache.getScalarTypeForField(typename, selection.name.value)
+          : undefined
+        );
 
         changed ||= coerced !== fieldValue;
         result[resultName] = coerced;
